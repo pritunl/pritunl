2de3b660cd92d6d45699f7ac0fc1dc9837b793ef
Zachary Huff <zach.huff.386@gmail.com>
2013-09-14 15:14:15 -0400

    Changes to orgs list view

--- www/views/orgsList.js
+++ www/views/orgsList.js
@@ -3,15 +3,112 @@ define([
   'underscore',
   'backbone',
   'collections/org',
+  'views/orgsListItem',
   'text!templates/orgsList.html'
-], function($, _, Backbone, OrgCollection,
+], function($, _, Backbone, OrgCollection, OrgsListItemView,
     orgsListTemplate) {
   'use strict';
   var OrgsListView = Backbone.View.extend({
     template: _.template(orgsListTemplate),
+    initialize: function() {
+      this.collection = new OrgCollection();
+      this.listenTo(this.collection, 'reset', this.onReset);
+      this.views = [];
+      this.selected = [];
+    },
     render: function() {
       this.$el.html(this.template());
+      this.collection.reset([
+        {
+          'id': '7ea4e93cb44b4db5ae5322105363677f',
+          'name': 'organizationname1'
+        },
+        {
+          'id': 'f069f8e7566c47f99b72420252d42a4b',
+          'name': 'organizationname2'
+        },
+        {
+          'id': 'e29dd434c59f4b07b19efc1dbdfaff31',
+          'name': 'organizationname3'
+        }
+      ]);
       return this;
+    },
+    removeItem: function(view) {
+      view.$el.slideUp({
+        duration: 250,
+        complete: function() {
+          view.remove();
+        }.bind(this)
+      });
+    },
+    onReset: function(collection) {
+      var i;
+      var modelView;
+      var attr;
+      var modified;
+      var currentModels = [];
+      var newModels = [];
+
+      for (i = 0; i < this.views.length; i++) {
+        currentModels.push(this.views[i].model.get('id'));
+      }
+
+      for (i = 0; i < collection.models.length; i++) {
+        newModels.push(collection.models[i].get('id'));
+      }
+
+      // Remove elements that no longer exists
+      for (i = 0; i < this.views.length; i++) {
+        if (newModels.indexOf(this.views[i].model.get('id')) === -1) {
+          // Remove item from dom and array
+          this.removeItem(this.views[i]);
+          this.views.splice(i, 1);
+          i -= 1;
+        }
+      }
+
+      // Add new elements
+      for (i = 0; i < collection.models.length; i++) {
+        if (currentModels.indexOf(collection.models[i].get('id')) !== -1) {
+          continue;
+        }
+
+        modelView = new OrgsListItemView({model: collection.models[i]});
+        this.views.splice(i, 0, modelView);
+        modelView.render().$el.hide();
+
+        if (i === 0) {
+          this.$('.orgs-list-container').prepend(modelView.el);
+        }
+        else {
+          this.views[i - 1].$el.after(modelView.el);
+        }
+
+        modelView.$el.slideDown(250);
+      }
+
+      // Check for modified data
+      for (i = 0; i < collection.models.length; i++) {
+        modified = false;
+
+        // Check each attr for modified data
+        for (attr in collection.models[i].attributes) {
+          if (collection.models[i].get(attr) !==
+              this.views[i].model.get(attr)) {
+            modified = true;
+            break;
+          }
+        }
+
+        if (!modified) {
+          continue;
+        }
+
+        // If data was modified updated attributes and render
+        this.views[i].model.set(collection.models[i].attributes);
+        this.views[i].render();
+      }
     }
   });
 
