2bcd63be3795eee19cf44969eebeeedca8ee2c1d
Zachary Huff <zach.huff.386@gmail.com>
2013-09-15 12:33:48 -0400

    Add remove org

--- www/views/orgsListItem.js
+++ www/views/orgsListItem.js
@@ -2,26 +2,44 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'views/alert',
   'views/usersList',
   'text!templates/orgsListItem.html'
-], function($, _, Backbone, UsersListView, orgsListItemTemplate) {
+], function($, _, Backbone, AlertView, UsersListView, orgsListItemTemplate) {
   'use strict';
   var OrgsListItemView = Backbone.View.extend({
     template: _.template(orgsListItemTemplate),
+    events: {
+      'click .org-del': 'onDelOrg'
+    },
     initialize: function() {
       this.usersListView = new UsersListView({
         organization: this.model.get('id')
       });
       this.listenTo(this.usersListView, 'select', this.onSelect);
     },
-    onSelect: function(view) {
-      this.trigger('select', view);
-    },
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       this.$('.users-list-container').append(this.usersListView.render().el);
       return this;
-    }
+    },
+    onSelect: function(view) {
+      this.trigger('select', view);
+    },
+    onDelOrg: function() {
+      var alertView = new AlertView({
+        type: 'warning',
+        message: 'Successfully deleted organization.',
+        dismissable: true
+      });
+      $('.alerts-container').append(alertView.render().el);
+      this.$el.slideUp({
+        duration: 250,
+        complete: function() {
+          this.remove();
+        }.bind(this)
+      });
+    },
   });
 
   return OrgsListItemView;
