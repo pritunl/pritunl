679d0277770f28144f24b77452725744681a4d87
Zachary Huff <zach.huff.386@gmail.com>
2013-09-15 19:45:05 -0400

    Add rename org modal view

---
+++ www/views/modalRenameOrg.js
@@ -0,0 +1,28 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/modal',
+  'text!templates/modalRenameOrg.html'
+], function($, _, Backbone, ModalView, modalRenameOrgTemplate) {
+  'use strict';
+  var ModalRenameOrgView = ModalView.extend({
+    template: _.template(modalRenameOrgTemplate),
+    title: 'Rename Organization',
+    okText: 'Rename',
+    initialize: function(options) {
+      this.body = this.template(this.model.toJSON());
+      this.render();
+    },
+    onOk: function() {
+      if (!this.$('input').val()) {
+        this.setAlert('danger', 'Name can not be empty.');
+        return;
+      }
+      this.clearAlert();
+      this.close();
+    }
+  });
+
+  return ModalRenameOrgView;
+});
