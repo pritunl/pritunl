e443dd0cf2e00c9f25ec2efd3ae64d933efbeef0
Zachary Huff <zach.huff.386@gmail.com>
2013-09-15 19:58:11 -0400

    Add org rename functionality

--- www/views/orgsListItem.js
+++ www/views/orgsListItem.js
@@ -4,12 +4,15 @@ define([
   'backbone',
   'views/alert',
   'views/usersList',
+  'views/modalRenameOrg',
   'text!templates/orgsListItem.html'
-], function($, _, Backbone, AlertView, UsersListView, orgsListItemTemplate) {
+], function($, _, Backbone, AlertView, UsersListView, ModalRenameOrgView,
+    orgsListItemTemplate) {
   'use strict';
   var OrgsListItemView = Backbone.View.extend({
     template: _.template(orgsListItemTemplate),
     events: {
+      'click .org-rename': 'onRenameOrg',
       'click .org-del': 'onDelOrg'
     },
     initialize: function() {
@@ -21,10 +24,18 @@ define([
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
       this.$('.users-list-container').append(this.usersListView.render().el);
+      this.$('.org-rename').tooltip({
+        container: this.el
+      });
       return this;
     },
     onSelect: function(view) {
       this.trigger('select', view);
+    },
+    onRenameOrg: function() {
+      var modal = new ModalRenameOrgView({
+        model: this.model
+      });
     },
     onDelOrg: function() {
       var alertView = new AlertView({
