a7cf33f8d893d1de36e434abffc7a103fd99fc28
Zachary Huff <zach.huff.386@gmail.com>
2013-09-16 13:04:47 -0400

    Add rename user modal view

---
+++ www/views/modalRenameUser.js
@@ -0,0 +1,28 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/modal',
+  'text!templates/modalRenameUser.html'
+], function($, _, Backbone, ModalView, modalRenameUserTemplate) {
+  'use strict';
+  var ModalRenameUserView = ModalView.extend({
+    template: _.template(modalRenameUserTemplate),
+    title: 'Rename User',
+    okText: 'Rename',
+    initialize: function(options) {
+      this.body = this.template(this.model.toJSON());
+      this.render();
+    },
+    onOk: function() {
+      if (!this.$('input').val()) {
+        this.setAlert('danger', 'Name can not be empty.');
+        return;
+      }
+      this.clearAlert();
+      this.close();
+    }
+  });
+
+  return ModalRenameUserView;
+});
