9f8f4ee8cb2e55f3cfd24bd575e4c312e17a33a6
Zachary Huff <zach.huff.386@gmail.com>
2013-09-16 13:13:24 -0400

    Add search view to header

--- www/views/header.js
+++ www/views/header.js
@@ -2,14 +2,17 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'views/search',
   'text!templates/header.html'
-], function($, _, Backbone, headerTemplate) {
+], function($, _, Backbone, SearchView, headerTemplate) {
   'use strict';
   var HeaderView = Backbone.View.extend({
     tagName: 'header',
     template: _.template(headerTemplate),
     render: function() {
       this.$el.html(this.template());
+      var searchView = new SearchView();
+      this.$('.navbar-collapse').append(searchView.render().el);
       return this;
     }
   });
