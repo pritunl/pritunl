ce11ab7627c0264cea1f9797bc17836859777635
Zachary Huff <zach.huff.386@gmail.com>
2013-09-16 13:24:35 -0400

    Add search events

--- www/views/search.js
+++ www/views/search.js
@@ -7,9 +7,17 @@ define([
   'use strict';
   var SearchView = Backbone.View.extend({
     template: _.template(searchTemplate),
+    events: {
+      'keydown .search input': 'onSearch',
+      'paste .search input': 'onSearch',
+      'input .search input': 'onSearch',
+      'propertychange .search input': 'onSearch'
+    },
     render: function() {
       this.$el.html(this.template());
       return this;
+    },
+    onSearch: function() {
     }
   });
 
