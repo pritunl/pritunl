89cbd41f5bc71bff03a8258f9c21d5c9462a52fb
Zachary Huff <zach.huff.386@gmail.com>
2013-09-17 20:35:14 -0400

    Add python cert_auth

---
+++ pritunl/cert_auth.py
@@ -0,0 +1,154 @@
+from constants import *
+import threading
+import uuid
+import os
+import subprocess
+
+openssl_lock = threading.Lock()
+
+class CertAuth:
+    def __init__(self, id=None):
+        if id is None:
+            self._initialized = False
+            self.id = uuid.uuid4().hex
+        else:
+            self._initialized = True
+            self.id = id
+        self.path = os.path.join(DATA_DIR, self.id)
+
+        self.index_path = os.path.join(self.path, INDEX_NAME)
+        self.index_attr_path = os.path.join(self.path, INDEX_NAME + '.attr')
+        self.serial_path = os.path.join(self.path, SERIAL_NAME)
+        self.crl_path = os.path.join(self.path, CRL_NAME)
+
+        if not self._initialized:
+            self._initialize()
+
+        self.ca_cert = Cert(self, id=CA_CERT_ID)
+
+    def _initialize(self):
+        self._make_dirs()
+        self.ca_cert = Cert(self, type=CERT_CA)
+
+    def _make_dirs(self):
+        os.makedirs(os.path.join(self.path, REQS_DIR))
+        os.makedirs(os.path.join(self.path, KEYS_DIR), 0700)
+        os.makedirs(os.path.join(self.path, CERTS_DIR))
+        os.makedirs(os.path.join(self.path, INDEXED_CERTS_DIR))
+        os.makedirs(os.path.join(self.path, TEMP_DIR))
+
+        with open(self.index_path, 'a'):
+            os.utime(self.index_path, None)
+
+        with open(self.index_attr_path, 'a'):
+            os.utime(self.index_attr_path, None)
+
+        with open(self.serial_path, 'w') as serial_file:
+            serial_file.write('01\n')
+
+    def get_certs(self):
+        certs = []
+        for cert_id in os.listdir(os.path.join(self.path, CERTS_DIR)):
+            cert_id = cert_id.replace('.crt', '')
+            if cert_id == CA_CERT_ID:
+                continue
+            certs.append(Cert(self, id=cert_id))
+        return certs
+
+    def new_cert(self, type):
+        return Cert(self, type=type)
+
+    def create_crl(self):
+        pass
+
+class Cert:
+    def __init__(self, ca, id=None, type=None):
+        self.ca = ca
+        self.id = id
+
+        if type is not None:
+            self.type = type
+
+        if self.id is None:
+            if type == CERT_CA:
+                self.id = CA_CERT_ID
+            elif type is None:
+                raise AttributeError('Type must be specified')
+            else:
+                self.id = uuid.uuid4().hex
+            self._initialized = False
+        else:
+            self._initialized = True
+
+        self.conf_path = os.path.join(self.ca.path, TEMP_DIR,
+            '%s.conf' % self.id)
+        self.reqs_path = os.path.join(self.ca.path, REQS_DIR,
+            '%s.csr' % self.id)
+        self.key_path = os.path.join(self.ca.path, KEYS_DIR,
+            '%s.key' % self.id)
+        self.cert_path = os.path.join(self.ca.path, CERTS_DIR,
+            '%s.crt' % self.id)
+
+        if not self._initialized:
+            self._initialize()
+
+    def __getattr__(self, name):
+        if name == 'type':
+            self.type = self._load_type()
+            return self.type
+        else:
+            raise AttributeError('Config instance has no attribute %r' % name)
+
+    def _initialize(self):
+        conf_data = CERT_CONF % (self.ca.id, self.ca.path, self.id)
+        with open(self.conf_path, 'w') as conf_file:
+            conf_file.write(conf_data)
+        self._cert_request()
+        self._cert_create()
+        self._delete_conf()
+
+    def _cert_request(self):
+        openssl_lock.acquire()
+        try:
+            args = [
+                'openssl', 'req', '-new', '-batch',
+                '-config', self.conf_path,
+                '-out', self.reqs_path,
+                '-keyout', self.key_path,
+                '-reqexts', '%s_req_ext' % self.type,
+            ]
+            subprocess.check_call(args)
+        finally:
+            openssl_lock.release()
+
+    def _cert_create(self):
+        openssl_lock.acquire()
+        try:
+            args = ['openssl', 'ca', '-batch']
+            if self.type == CERT_CA:
+                args += ['-selfsign']
+            args += [
+                '-config', self.conf_path,
+                '-in', self.reqs_path,
+                '-out', self.cert_path,
+                '-extensions', '%s_ext' % self.type,
+            ]
+            subprocess.check_call(args)
+        finally:
+            openssl_lock.release()
+
+    def _delete_conf(self):
+        os.remove(self.conf_path)
+
+    def _load_type(self):
+        with open(self.cert_path, 'r') as cert_file:
+            cert_data = cert_file.read()
+            if 'CA:TRUE' in cert_data:
+                return CERT_CA
+            elif 'TLS Web Server Authentication' in cert_data:
+                return CERT_SERVER
+            else:
+                return  CERT_CLIENT
+
+    def revoke(self):
+        pass
--- pritunl/constants.py
+++ pritunl/constants.py
@@ -7,3 +7,91 @@ SAVED = 'saved'
 UNSAVED = 'unsaved'
 
 DEFAULT_WWW_PATH = '/usr/share/pritunl/www'
+
+DATA_DIR = './data'
+REQS_DIR = 'reqs'
+KEYS_DIR = 'keys'
+CERTS_DIR = 'certs'
+INDEXED_CERTS_DIR = 'indexed_certs'
+TEMP_DIR = 'temp'
+INDEX_NAME = 'index'
+SERIAL_NAME = 'serial'
+CRL_NAME = 'ca.crl'
+
+CA_CERT_ID = 'ca'
+CERT_CA = 'ca'
+CERT_SERVER = 'server'
+CERT_CLIENT = 'client'
+
+CERT_CONF = """[ default ]
+ca = %s
+dir = %s
+
+[ req ]
+default_bits = 4096
+default_md = sha1
+encrypt_key = no
+utf8 = yes
+string_mask = utf8only
+prompt = no
+distinguished_name = req_dn
+
+[ req_dn ]
+0.domainComponent = pritunl
+1.domainComponent = $ca
+organizationName = $ca
+commonName = %s
+
+[ ca_req_ext ]
+keyUsage = critical,keyCertSign,cRLSign
+basicConstraints = critical,CA:true
+subjectKeyIdentifier = hash
+
+[ server_req_ext ]
+keyUsage = critical,digitalSignature,keyEncipherment
+extendedKeyUsage = serverAuth,clientAuth
+subjectKeyIdentifier = hash
+
+[ client_req_ext ]
+keyUsage = critical,digitalSignature,keyEncipherment
+extendedKeyUsage = clientAuth
+subjectKeyIdentifier = hash
+
+[ ca ]
+default_ca = root_ca
+
+[ root_ca ]
+database = $dir/index
+serial = $dir/serial
+new_certs_dir = $dir/indexed_certs
+certificate = $dir/certs/ca.crt
+private_key = $dir/keys/ca.key
+default_days = 3652
+default_md = sha1
+policy = ca_policy
+
+[ ca_policy ]
+domainComponent = match
+organizationName = match
+commonName = supplied
+
+[ ca_ext ]
+keyUsage = critical,keyCertSign,cRLSign
+basicConstraints = critical,CA:true,pathlen:0
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always
+
+[ server_ext ]
+keyUsage = critical,digitalSignature,keyEncipherment
+basicConstraints = CA:false
+extendedKeyUsage = serverAuth,clientAuth
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always
+
+[ client_ext ]
+keyUsage = critical,digitalSignature,keyEncipherment
+basicConstraints = CA:false
+extendedKeyUsage = clientAuth
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always
+"""
