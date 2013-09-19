57229112a1251719355e07a64109d74967085c1b
Zachary Huff <zach.huff.386@gmail.com>
2013-09-18 21:17:36 -0400

    Changes to delete user modal

--- www/views/modalDeleteUsers.js
+++ www/views/modalDeleteUsers.js
@@ -18,17 +18,36 @@ define([
       this.render();
     },
     onOk: function() {
-      this.triggerEvt = true;
-      this.close();
+      if (this.locked) {
+        return;
+      }
+      this.locked = true;
+      this.setLoading('Deleting users...');
+
+      var i;
+      var users = this.collection.models.slice(0);
+      var count = users.length;
+      for (i = 0; i < users.length; i++) {
+        users[i].destroy({
+          success: function() {
+            count -= 1;
+            if (count === 0) {
+              this.triggerEvt = true;
+              this.close();
+            }
+          }.bind(this),
+          error: function() {
+            this.setAlert('danger',
+              'Failed to delete user, server error occurred.');
+            this.locked = false;
+          }.bind(this)
+        });
+      }
     },
     onRemove: function() {
       var i;
       if (!this.triggerEvt) {
         return;
-      }
-      var users = this.collection.models.slice(0);
-      for (i = 0; i < users.length; i++) {
-        users[i].destroy();
       }
       this.trigger('deleted');
     }
