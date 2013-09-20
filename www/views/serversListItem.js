83049ed0494f2e2a3a67837c62038c2018029afd
Zachary Huff <zach.huff.386@gmail.com>
2013-09-20 00:55:54 -0400

    Changes to server list item view

--- www/views/serversListItem.js
+++ www/views/serversListItem.js
@@ -40,6 +40,7 @@ define([
     },
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
+      this.update();
       this.$el.append(this.serverOrgsListView.render().el);
       return this;
     }
