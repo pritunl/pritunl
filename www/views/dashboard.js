efce6e1bf14dd0dd1af3ad986bf7ddd9a7883980
Zachary Huff <zach.huff.386@gmail.com>
2013-09-22 17:14:20 -0400

    Add alerts container to dashboard view

--- www/views/dashboard.js
+++ www/views/dashboard.js
@@ -3,11 +3,14 @@ define([
   'underscore',
   'backbone',
   'views/dashboardStatus',
-  'views/dashboardLog'
-], function($, _, Backbone, DashboardStatusView, DashboardLogView) {
+  'views/dashboardLog',
+  'text!templates/dashboard.html'
+], function($, _, Backbone, DashboardStatusView, DashboardLogView,
+    dashboardTemplate) {
   'use strict';
   var DashboardView = Backbone.View.extend({
     className: 'dashboard container',
+    template: _.template(dashboardTemplate),
     initialize: function() {
       this.dashboardStatusView = new DashboardStatusView();
       this.addView(this.dashboardStatusView);
@@ -15,7 +18,8 @@ define([
       this.addView(this.dashboardLogView);
     },
     render: function() {
-      this.$el.append(this.dashboardStatusView.render().el);
+      this.$el.html(this.template());
+      this.$el.prepend(this.dashboardStatusView.render().el);
       this.$el.append(this.dashboardLogView.render().el);
       return this;
     }
