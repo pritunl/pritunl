a3b8e95e1acb1ad402e2465443922f04376cb578
Zachary Huff <zach.huff.386@gmail.com>
2013-09-28 23:56:02 -0400

    Bind change password in header to show change password modal

--- www/views/header.js
+++ www/views/header.js
@@ -2,15 +2,34 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'views/alert',
+  'views/modalChangePassword',
   'text!templates/header.html'
-], function($, _, Backbone, headerTemplate) {
+], function($, _, Backbone, AlertView, ModalChangePasswordView,
+    headerTemplate) {
   'use strict';
   var HeaderView = Backbone.View.extend({
     tagName: 'header',
     template: _.template(headerTemplate),
+    events: {
+      'click .change-password a': 'changePassword'
+    },
     render: function() {
       this.$el.html(this.template());
       return this;
+    },
+    changePassword: function() {
+      var modal = new ModalChangePasswordView();
+      this.listenToOnce(modal, 'applied', function() {
+        var alertView = new AlertView({
+          type: 'warning',
+          message: 'Successfully changed password.',
+          dismissable: true
+        });
+        $('.alerts-container').append(alertView.render().el);
+        this.addView(alertView);
+      }.bind(this));
+      this.addView(modal);
     }
   });
 
