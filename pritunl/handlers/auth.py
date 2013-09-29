4340ded669a829a9f2c5557deef221d40ad2a116
Zachary Huff <zach.huff.386@gmail.com>
2013-09-28 22:14:46 -0400

    Delay auth attempts

--- pritunl/handlers/auth.py
+++ pritunl/handlers/auth.py
@@ -1,14 +1,17 @@
 from pritunl.constants import *
 import pritunl.utils as utils
 from pritunl import app_server
+import time
 import flask
 
 @app_server.app.route('/auth', methods=['POST'])
 def auth_post():
     username = flask.request.json['username']
     password = flask.request.json['password']
+    time.sleep(0.5)
 
     if username != 'admin' or password != 'admin':
+        time.sleep(0.5)
         return utils.jsonify({
             'error': AUTH_NOT_VALID,
             'error_msg': AUTH_NOT_VALID_MSG,
