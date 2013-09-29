2d9418c0cd0e849dc0b365ee90514bcdd73ccd9e
Zachary Huff <zach.huff.386@gmail.com>
2013-09-28 21:41:57 -0400

    Add auth model

---
+++ www/models/auth.js
@@ -0,0 +1,19 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var AuthModel = Backbone.Model.extend({
+    defaults: {
+      'authenticated': null,
+      'username': null,
+      'password': null
+    },
+    url: function() {
+      return '/auth';
+    }
+  });
+
+  return AuthModel;
+});
