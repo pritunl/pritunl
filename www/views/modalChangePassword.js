3b3082fcb8fcb469a9388bccccd138e384492325
Zachary Huff <zach.huff.386@gmail.com>
2013-09-28 23:53:37 -0400

    Add change password modal

---
+++ www/views/modalChangePassword.js
@@ -0,0 +1,54 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'models/password',
+  'views/modal',
+  'text!templates/modalChangePassword.html'
+], function($, _, Backbone, PasswordModel, ModalView,
+    modalChangePasswordTemplate) {
+  'use strict';
+  var ModalChangePasswordView = ModalView.extend({
+    className: 'change-password-modal',
+    template: _.template(modalChangePasswordTemplate),
+    title: 'Change Password',
+    okText: 'Change',
+    initialize: function() {
+      this.model = new PasswordModel();
+      ModalChangePasswordView.__super__.initialize.call(this);
+    },
+    body: function() {
+      return this.template();
+    },
+    onOk: function() {
+      if (this.locked) {
+        return;
+      }
+      if (!this.$('.pass').val()) {
+        this.setAlert('danger', 'Password can not be empty.', '.form-group');
+        return;
+      }
+      if (this.$('.pass').val() !== this.$('.verify-pass').val()) {
+        this.setAlert('danger', 'Passwords do not match.', '.form-group');
+        return;
+      }
+      this.locked = true;
+      this.setLoading('Changing password...');
+      this.model.save({
+        password: this.$('.pass').val()
+      }, {
+        success: function() {
+          this.close(true);
+        }.bind(this),
+        error: function() {
+          this.clearLoading();
+          this.setAlert('danger',
+            'Failed to change password, server error occurred.');
+          this.locked = false;
+        }.bind(this)
+      });
+    }
+  });
+
+  return ModalChangePasswordView;
+});
