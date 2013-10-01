d7fd86f8368c08d94379c1663b978e015bd79cb0
Zachary Huff <zach.huff.386@gmail.com>
2013-10-01 16:32:26 -0400

    Changes to setup.py Create drivnal daemon function Add log path to production conf

---
+++ pritunl/__main__.py
@@ -0,0 +1,46 @@
+import optparse
+import sys
+import os
+import pritunl
+
+def drivnal_daemon():
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--daemon', action='store_true',
+        help='Daemonize process')
+    parser.add_option('-p', '--pidfile', type='string',
+        help='Path to create pid file')
+    parser.add_option('-c', '--conf', type='string',
+        help='Path to configuration file')
+    (options, args) = parser.parse_args()
+
+    if options.daemon:
+        pid = os.fork()
+        if pid > 0:
+            if options.pidfile:
+                with open(options.pidfile, 'w') as pid_file:
+                    pid_file.write('%s' % pid)
+            sys.exit(0)
+    else:
+        print '##############################################################'
+        print '#                                                            #'
+        print '#                      /$$   /$$                         /$$ #'
+        print '#                     |__/  | $$                        | $$ #'
+        print '#   /$$$$$$   /$$$$$$  /$$ /$$$$$$   /$$   /$$ /$$$$$$$ | $$ #'
+        print '#  /$$__  $$ /$$__  $$| $$|_  $$_/  | $$  | $$| $$__  $$| $$ #'
+        print '# | $$  \ $$| $$  \__/| $$  | $$    | $$  | $$| $$  \ $$| $$ #'
+        print '# | $$  | $$| $$      | $$  | $$ /$$| $$  | $$| $$  | $$| $$ #'
+        print '# | $$$$$$$/| $$      | $$  |  $$$$/|  $$$$$$/| $$  | $$| $$ #'
+        print '# | $$____/ |__/      |__/   \___/   \______/ |__/  |__/|__/ #'
+        print '# | $$                                                       #'
+        print '# | $$                                                       #'
+        print '# |__/                                                       #'
+        print '#                                                            #'
+        print '##############################################################'
+
+    if options.conf:
+        conf_path = options.conf
+    else:
+        conf_path = '/etc/pritunl.conf'
+
+    pritunl.app_server.conf_path = conf_path
+    pritunl.app_server.run_all()
--- setup.py
+++ setup.py
@@ -1,5 +1,74 @@
 from setuptools import setup
+import os
+import sys
+import copy
+import shlex
+import shutil
+import fileinput
 import pritunl
+
+PATCH_DIR = 'build'
+INSTALL_UPSTART = True
+INSTALL_SYSTEMD = True
+
+prefix = sys.prefix
+for arg in copy.copy(sys.argv):
+    if arg.startswith('--prefix'):
+        prefix = os.path.normpath(shlex.split(arg)[0].split('=')[-1])
+    elif arg == '--no-upstart':
+        sys.argv.remove('--no-upstart')
+        INSTALL_UPSTART = False
+    elif arg == '--no-systemd':
+        sys.argv.remove('--no-systemd')
+        INSTALL_SYSTEMD = False
+
+if not os.path.exists('build'):
+    os.mkdir('build')
+
+data_files = [
+    ('/etc', ['data/etc/pritunl.conf']),
+    ('/var/lib/pritunl', ['data/var/pritunl.db']),
+    ('/var/log', ['data/var/pritunl.log']),
+    ('/usr/share/pritunl/www', ['www/dist/favicon.ico']),
+    ('/usr/share/pritunl/www', ['www/dist/index.html']),
+    ('/usr/share/pritunl/www', ['www/dist/robots.txt']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/fredoka-one.eot']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/fredoka-one.woff']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/glyphicons-halflings-regular.eot']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/glyphicons-halflings-regular.svg']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/glyphicons-halflings-regular.ttf']),
+    ('/usr/share/pritunl/www/fonts',
+        ['www/dist/fonts/glyphicons-halflings-regular.woff']),
+    ('/usr/share/pritunl/www/fonts', ['www/dist/fonts/ubuntu-bold.eot']),
+    ('/usr/share/pritunl/www/fonts', ['www/dist/fonts/ubuntu-bold.woff']),
+    ('/usr/share/pritunl/www/fonts', ['www/dist/fonts/ubuntu.eot']),
+    ('/usr/share/pritunl/www/fonts', ['www/dist/fonts/ubuntu.woff']),
+    ('/usr/share/pritunl/www/css', ['www/dist/css/main.css']),
+    ('/usr/share/pritunl/www/js', ['www/dist/js/main.js']),
+    ('/usr/share/pritunl/www/js', ['www/dist/js/require.min.js']),
+]
+
+patch_files = []
+if INSTALL_UPSTART:
+    patch_files.append('%s/pritunl.conf' % PATCH_DIR)
+    data_files.append(('/etc/init', ['%s/pritunl.conf' % PATCH_DIR]))
+    shutil.copy('data/init/pritunl.conf', '%s/pritunl.conf' % PATCH_DIR)
+if INSTALL_SYSTEMD:
+    patch_files.append('%s/pritunl.service' % PATCH_DIR)
+    data_files.append(('/etc/systemd/system',
+        ['%s/pritunl.service' % PATCH_DIR]))
+    shutil.copy('data/systemd/pritunl.service',
+        '%s/pritunl.service' % PATCH_DIR)
+
+for file_name in patch_files:
+    for line in fileinput.input(file_name, inplace=True):
+        line = line.replace('%PREFIX%', prefix)
+        print line.rstrip('\n')
 
 setup(
     name='pritunl',
@@ -9,12 +78,22 @@ setup(
     author='Zachary Huff',
     author_email='zach.huff.386@gmail.com',
     url='https://github.com/zachhuff386/pritunl',
-    keywords='openvpn',
-    packages=['pritunl'],
+    download_url='https://github.com/zachhuff386/pritunl/archive/%s.tar.gz' % (
+        pritunl.__version__),
+    keywords='openvpn, vpn, management, server, web interface',
+    packages=['pritunl', 'pritunl.handlers'],
     license=open('LICENSE').read(),
     zip_safe=False,
+    install_requires=[
+        'flask',
+        'cherrypy>=3.2.0',
+    ],
+    data_files=data_files,
+    entry_points={
+        'console_scripts': ['pritunl = pritunl.__main__:pritunl_daemon'],
+    },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU Affero General Public License v3',
