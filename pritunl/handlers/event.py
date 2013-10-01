2272c48120939e93f341d6223ba977de60fcf124
Zachary Huff <zach.huff.386@gmail.com>
2013-10-01 13:31:08 -0400

    Fix event handler on server interupt

--- pritunl/handlers/event.py
+++ pritunl/handlers/event.py
@@ -4,11 +4,15 @@ import pritunl.utils as utils
 from pritunl import app_server
 import time
 import uuid
+import flask
 
 @app_server.app.route('/event', methods=['GET'])
 @app_server.app.route('/event/<int:last_event>', methods=['GET'])
 @app_server.auth
 def event_get(last_event=None):
+    if app_server.interrupt:
+        return flask.abort(503)
+
     if not last_event:
         events = [
             {
