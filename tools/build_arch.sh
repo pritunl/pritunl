e224ad89d14ea79190e8034aa54439393d985cb4
Zachary Huff <zach.huff.386@gmail.com>
2013-10-02 14:34:23 -0400

    Add pip and arch aur build scripts

---
+++ tools/build_arch.sh
@@ -0,0 +1,13 @@
+VERSION=`cat ../pritunl/__init__.py | grep __version__ | cut -d\' -f2`
+
+mkdir -p ../build/arch_linux
+cd ../build/arch_linux
+
+wget https://github.com/zachhuff386/pritunl/archive/$VERSION.tar.gz
+
+cp ../../PKGBUILD ./
+
+TAR_SHA256=$(sha256sum $VERSION.tar.gz | cut -d' ' -f1)
+sed -i -e 's/CHANGE_ME/'$TAR_SHA256'/g' PKGBUILD
+
+makepkg --source
---
+++ tools/build_pip.sh
@@ -0,0 +1,16 @@
+VERSION=`cat ../pritunl/__init__.py | grep __version__ | cut -d\' -f2`
+
+mkdir -p ../build/pip
+cd ../build/pip
+
+wget https://github.com/zachhuff386/pritunl/archive/$VERSION.tar.gz
+
+mv $VERSION.tar.gz pritunl-$VERSION.tar.gz
+
+cd ../../
+
+python2 setup.py register
+
+echo 'MD5: '`md5sum ./build/pip/pritunl-$VERSION.tar.gz | cut -d' ' -f1`
+echo 'UPLOAD: ../build/pip/pritunl-'$VERSION'.tar.gz'
+
