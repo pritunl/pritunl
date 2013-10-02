2e90ea9d7272037c4fac1d0e67c24b42c8ee7db7
Zachary Huff <zach.huff.386@gmail.com>
2013-10-02 01:43:08 -0400

    Set version

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -1,6 +1,6 @@
 from constants import *
 __title__ = APP_NAME
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __author__ = 'Zachary Huff'
 __license__ = 'AGPL'
 __copyright__ = 'Copyright 2013 Zachary Huff'
