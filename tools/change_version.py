b75c6d7637f9dfc190c7dd38c16caa2d5a0ee99e
Zachary Huff <zach.huff.386@gmail.com>
2013-10-08 20:48:26 -0400

    Add debian changelog to change version script

--- tools/change_version.py
+++ tools/change_version.py
@@ -13,3 +13,8 @@ for line in fileinput.input('../arch/PKGBUILD', inplace=True):
     if 'pkgver=' in line:
         line = 'pkgver=%s' % VERSION
     print line.rstrip('\n')
+
+for line in fileinput.input('../debian/changelog', inplace=True):
+    if 'pritunl (' in line:
+        line = 'pritunl (%s-0ubuntu1) unstable; urgency=low' % VERSION
+    print line.rstrip('\n')
