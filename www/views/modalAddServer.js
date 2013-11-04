8382afbce0fa55fa8655ece17602620402e5249d
Zachary Huff <zach.huff.386@gmail.com>
2013-11-04 01:54:28 -0500

    Change add server loading message

--- www/views/modalAddServer.js
+++ www/views/modalAddServer.js
@@ -9,7 +9,7 @@ define([
   var ModalAddServerView = ModalServerSettingsView.extend({
     title: 'Add Server',
     okText: 'Add',
-    loadingMsg: 'Adding server, this will take a few minutes...',
+    loadingMsg: 'Adding server, this will take several minutes...',
     errorMsg: 'Failed to add server, server error occurred.',
     initialize: function(options) {
       this.model = new ServerModel({
