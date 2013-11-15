d36de3ea98564a21eb2fc6e90208e6e7b482b674
Zachary Huff <zach.huff.386@gmail.com>
2013-11-14 23:14:39 -0500

    Change version in __init__ and arch pkgbuild

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -1,6 +1,6 @@
 from constants import *
 __title__ = APP_NAME
-__version__ = '0.10.2'
+__version__ = '0.10.3'
 __author__ = 'Zachary Huff'
 __license__ = 'AGPL'
 __copyright__ = 'Copyright 2013 Zachary Huff'
