67a7a471830fbac4900e0c221ec7ce6d3bd2466c
Zachary Huff <zach.huff.386@gmail.com>
2013-12-22 01:18:08 -0500

    Add database test to unittest

--- test_pritunl.py
+++ test_pritunl.py
@@ -1,3 +1,4 @@
+from drivnal import database
 import threading
 import unittest
 import requests
@@ -12,6 +13,7 @@ HEADERS = {
 }
 USERNAME = 'admin'
 PASSWORD = 'admin'
+TEMP_DATABSE_PATH = 'pritunl_test.db'
 
 class Session:
     def __init__(self):
@@ -39,3 +41,53 @@ class Session:
 
     def delete(self, endpoint):
         return self._json_request('delete', endpoint)
+
+
+class Database(unittest.TestCase):
+    def setUp(self):
+        if os.path.isfile(TEMP_DATABSE_PATH):
+            os.remove(TEMP_DATABSE_PATH)
+        self._db = database.Database(TEMP_DATABSE_PATH)
+
+    def tearDown(self):
+        self._db.close()
+        os.remove(TEMP_DATABSE_PATH)
+
+    def _fill_column_family(self, num):
+        for i in xrange(5):
+            for x in xrange(5):
+                self._db.set('column_family_%s' % num, 'row_%s' % i,
+                    'column_%s' % x, 'value_%s' % x)
+
+        for i in xrange(5):
+            for x in xrange(5):
+                value = self._db.get('column_family_%s' % num,
+                    'row_%s' % i, 'column_%s' % x)
+                self.assertEqual(value, 'value_%s' % x)
+
+        for i in xrange(5):
+            for x in xrange(5):
+                self._db.remove('column_family_%s' % num,
+                    'row_%s' % i, 'column_%s' % x)
+
+        for i in xrange(5):
+            for x in xrange(5):
+                value = self._db.get('column_family_%s' % num,
+                    'row_%s' % i, 'column_%s' % x)
+                self.assertEqual(value, None)
+
+    def test_database(self):
+        for i in xrange(2):
+            threads = []
+            for x in xrange(300):
+                thread = threading.Thread(target=self._fill_column_family,
+                    args=(x,))
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+
+if __name__ == '__main__':
+    unittest.main()
