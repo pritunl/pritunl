bdc38897cbb779787a805e7dcabe6b19133b3725
Zachary Huff <zach.huff.386@gmail.com>
2014-01-03 06:56:38 -0500

    Change password http method

--- pritunl/handlers/password.py
+++ pritunl/handlers/password.py
@@ -3,6 +3,16 @@ import pritunl.utils as utils
 from pritunl import app_server
 import flask
 
+@app_server.app.route('/password', methods=['PUT'])
+@app_server.auth
+def password_put():
+    password = flask.request.json['password'][:512]
+    app_server.set_password(password)
+    return utils.jsonify({
+        'password': password,
+    })
+
+# TODO
 @app_server.app.route('/password', methods=['POST'])
 @app_server.auth
 def password_post():
