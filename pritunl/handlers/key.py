42faa5422adae1c7abc7752071502f4d7cfcf3d3
Zachary Huff <zach.huff.386@gmail.com>
2014-01-06 14:28:14 -0500

    Add handler to download indivdual ovpn server confs Change key view page Change headers and mimetype of key archive Fix download links for android browser

--- pritunl/handlers/key.py
+++ pritunl/handlers/key.py
@@ -8,7 +8,8 @@ import uuid
 import time
 
 _key_ids = {}
-_otp_ids = {}
+_view_ids = {}
+_conf_ids = {}
 
 def _get_key_archive(org_id, user_id):
     org = Organization(org_id)
@@ -17,9 +18,9 @@ def _get_key_archive(org_id, user_id):
 
     with open(archive_temp_path, 'r') as archive_file:
         response = flask.Response(response=archive_file.read(),
-            mimetype='application/x-tar')
+            mimetype='application/octet-stream')
         response.headers.add('Content-Disposition',
-            'inline; filename="%s.tar"' % user.name)
+            'attachment; filename="%s.tar"' % user.name)
 
     os.remove(archive_temp_path)
     return response
@@ -32,24 +33,47 @@ def user_key_archive_get(org_id, user_id):
 @app_server.app.route('/key/<org_id>/<user_id>', methods=['GET'])
 @app_server.auth
 def user_key_link_get(org_id, user_id):
+    org = Organization(org_id)
+    servers = org.get_servers()
     key_id = uuid.uuid4().hex
-    otp_id = uuid.uuid4().hex
+    view_id = uuid.uuid4().hex
+
     _key_ids[key_id] = {
         'org_id': org_id,
         'user_id': user_id,
-        'otp_id': otp_id,
+        'view_id': view_id,
         'timestamp': time.time(),
+        'count': 0,
     }
-    _otp_ids[otp_id] = {
+
+    conf_urls = []
+    if app_server.inline_certs:
+        for server in servers:
+            conf_id = uuid.uuid4().hex
+            _conf_ids[conf_id] = {
+                'org_id': org_id,
+                'user_id': user_id,
+                'server_id': server.id,
+                'timestamp': time.time(),
+                'count': 0,
+            }
+            conf_urls.append({
+                'server_name': server.name,
+                'url': '/key/%s.ovpn' % conf_id,
+            })
+
+    _view_ids[view_id] = {
         'org_id': org_id,
         'user_id': user_id,
         'key_id': key_id,
         'timestamp': time.time(),
+        'conf_urls': conf_urls,
     }
+
     return utils.jsonify({
         'id': key_id,
         'key_url': '/key/%s.tar' % key_id,
-        'otp_url': '/key/%s.html' % otp_id,
+        'view_url': '/key/%s' % view_id,
     })
 
 @app_server.app.route('/key/<key_id>.tar', methods=['GET'])
@@ -61,20 +85,26 @@ def user_linked_key_archive_get(key_id):
         return flask.abort(404)
     org_id = _key_ids[key_id]['org_id']
     user_id = _key_ids[key_id]['user_id']
-    del _key_ids[key_id]
+
+    # Fix for android sending two GET requests when downloading
+    _key_ids[key_id]['count'] += 1
+    if _key_ids[key_id]['count'] >= 2:
+        del _key_ids[key_id]
+
     return _get_key_archive(org_id, user_id)
 
-@app_server.app.route('/key/<otp_id>.html', methods=['GET'])
-def user_linked_key_page_get(otp_id):
-    if otp_id not in _otp_ids:
+@app_server.app.route('/key/<view_id>', methods=['GET'])
+def user_linked_key_page_get(view_id):
+    if view_id not in _view_ids:
         return flask.abort(404)
-    elif time.time() - _otp_ids[otp_id]['timestamp'] > KEY_LINK_TIMEOUT:
-        del _otp_ids[otp_id]
+    elif time.time() - _view_ids[view_id]['timestamp'] > KEY_LINK_TIMEOUT:
+        del _view_ids[view_id]
         return flask.abort(404)
-    org_id = _otp_ids[otp_id]['org_id']
-    user_id = _otp_ids[otp_id]['user_id']
-    key_id = _otp_ids[otp_id]['key_id']
-    del _otp_ids[otp_id]
+    org_id = _view_ids[view_id]['org_id']
+    user_id = _view_ids[view_id]['user_id']
+    key_id = _view_ids[view_id]['key_id']
+    conf_urls = _view_ids[view_id]['conf_urls']
+    del _view_ids[view_id]
 
     org = Organization(org_id)
     user = org.get_user(user_id)
@@ -83,11 +113,51 @@ def user_linked_key_page_get(otp_id):
         'key_index.html'), 'r').read()
     key_page = key_page.replace('<%= user_name %>', '%s - %s' % (
         org.name, user.name))
-    key_page = key_page.replace('<%= user_otp_key %>', user.otp_secret)
     key_page = key_page.replace('<%= user_key_url %>', '/key/%s.tar' % (
         key_id))
-    key_page = key_page.replace('<%= user_otp_url %>',
-        'otpauth://totp/%s@%s?secret=%s' % (
-            user.name, org.name, user.otp_secret))
+
+    if org.otp_auth:
+        key_page = key_page.replace('<%= user_otp_key %>', user.otp_secret)
+        key_page = key_page.replace('<%= user_otp_url %>',
+            'otpauth://totp/%s@%s?secret=%s' % (
+                user.name, org.name, user.otp_secret))
+    else:
+        key_page = key_page.replace('<%= user_otp_key %>', '')
+        key_page = key_page.replace('<%= user_otp_url %>', '')
+
+    if app_server.inline_certs:
+        conf_links = ''
+        for conf_url in conf_urls:
+            conf_links += '<a class="sm" title="Download Mobile Key" ' + \
+                'href="%s">Download Mobile Key (%s)</a><br>\n' % (
+                    conf_url['url'], conf_url['server_name'])
+        key_page = key_page.replace('<%= conf_links %>', conf_links)
 
     return key_page
+
+@app_server.app.route('/key/<conf_id>.ovpn', methods=['GET'])
+def user_linked_key_conf_get(conf_id):
+    if conf_id not in _conf_ids:
+        return flask.abort(404)
+    elif time.time() - _conf_ids[conf_id]['timestamp'] > KEY_LINK_TIMEOUT:
+        del _conf_ids[conf_id]
+        return flask.abort(404)
+    org_id = _conf_ids[conf_id]['org_id']
+    user_id = _conf_ids[conf_id]['user_id']
+    server_id = _conf_ids[conf_id]['server_id']
+
+    # Fix for android sending two GET requests when downloading
+    _conf_ids[conf_id]['count'] += 1
+    if _conf_ids[conf_id]['count'] >= 2:
+        del _conf_ids[conf_id]
+
+    org = Organization(org_id)
+    user = org.get_user(user_id)
+    key_conf = user.build_key_conf(server_id)
+
+    response = flask.Response(response=key_conf['conf'],
+        mimetype='application/octet-stream')
+    response.headers.add('Content-Disposition',
+        'attachment; filename="%s"' % key_conf['name'])
+
+    return response
