feacd117fb2ee43eb6a688712460b2f683910789
Zachary Huff <zach.huff.386@gmail.com>
2014-01-10 10:19:25 -0500

    Add default headers

--- pritunl/utils.py
+++ pritunl/utils.py
@@ -6,6 +6,9 @@ def jsonify(data=None, status_code=None):
     if not isinstance(data, basestring):
         data = json.dumps(data)
     response = flask.Response(response=data, mimetype='application/json')
+    response.headers.add('Cache-Control',
+        'no-cache, no-store, must-revalidate')
+    response.headers.add('Pragma', 'no-cache')
     if status_code is not None:
         response.status_code = status_code
     return response
