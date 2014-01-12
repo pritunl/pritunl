783b6cd7916432a396c91c2c5717e149a1a33aa9
Zachary Huff <zach.huff.386@gmail.com>
2014-01-12 17:51:40 -0500

    Log page time to console in testing

--- www/init/testing.js
+++ www/init/testing.js
@@ -6,6 +6,16 @@ define([
 ], function($, _, Backbone) {
   'use strict';
   var initialize = function() {
+    var _ajax = Backbone.ajax;
+    Backbone.ajax = function(options) {
+      var _complete = options.complete;
+      options.complete = function(response) {
+        console.log('time: ' +
+          response.getResponseHeader('X-Execution-Time') + 'ms');
+        _complete(response);
+      };
+      return _ajax.call(Backbone.$, options);
+    };
   };
 
   return initialize;
