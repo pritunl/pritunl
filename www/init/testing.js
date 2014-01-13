f86815006588c8fe25ccc89c453792043c5f21bf
Zachary Huff <zach.huff.386@gmail.com>
2014-01-12 21:50:18 -0500

    Change testing exec time log

--- www/init/testing.js
+++ www/init/testing.js
@@ -10,8 +10,9 @@ define([
     Backbone.ajax = function(options) {
       var _complete = options.complete;
       options.complete = function(response) {
-        console.log('time: ' +
-          response.getResponseHeader('X-Execution-Time') + 'ms');
+        console.log('%c' +
+          response.getResponseHeader('X-Execution-Time') + 'ms ' +
+          this.url, 'color: #0066ff');
         _complete(response);
       };
       return _ajax.call(Backbone.$, options);
