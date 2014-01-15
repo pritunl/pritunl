6396661a70a7b16132f23b4d8f5b50011856ea64
Zachary Huff <zach.huff.386@gmail.com>
2014-01-15 18:59:32 -0500

    Change cache database

--- pritunl/cache.py
+++ pritunl/cache.py
@@ -1,64 +1,49 @@
 from constants import *
 import logging
 import time
+import collections
 
 logger = logging.getLogger(APP_NAME)
 
 class Cache:
     def __init__(self):
-        self._data = {}
+        self._data = collections.defaultdict(
+            lambda: {'ttl': None, 'val': None})
 
-    def get(self, key):
+    def _check_ttl(self, key):
         try:
             ttl = self._data[key]['ttl']
-            if ttl and int(time.time()) > ttl:
-                return
-            return self._data[key]['val']
         except KeyError:
-            pass
+            return
+        if ttl and int(time.time()) > ttl:
+            self.remove(self, key)
+            return True
+        return False
+
+    def get(self, key):
+        if self._check_ttl(key) is False:
+            return self._data[key]['val']
 
     def set(self, key, value):
-        try:
-            self._data[key]['val'] = value
-        except KeyError:
-            self._data[key] = {
-                'ttl': None,
-                'val': value,
-            }
+        self._data[key]['val'] = value
 
     def remove(self, key):
         self._data.pop(key, None)
 
     def exists(self, key):
-        try:
-            ttl = self._data[key]['ttl']
-            if not ttl or int(time.time()) <= ttl:
-                return True
-        except KeyError:
-            pass
+        if self._check_ttl(key) is False:
+            return True
         return False
 
     def expire(self, key, ttl):
         timeout = int(time.time()) + ttl
-        try:
-            self._data[key]['ttl'] = timeout
-        except KeyError:
-            self._data[key] = {
-                'ttl': timeout,
-                'val': None,
-            }
+        self._data[key]['ttl'] = timeout
 
     def set_add(self, key, element):
         try:
-            try:
-                self._data[key]['val'].add(element)
-            except KeyError:
-                self._data[key]['val'] = {element}
-        except KeyError:
-            self._data[key] = {
-                'ttl': None,
-                'val': {element},
-            }
+            self._data[key]['val'].add(element)
+        except AttributeError:
+            self._data[key]['val'] = {element}
 
     def set_remove(self, key, element):
         try:
@@ -67,31 +52,19 @@ class Cache:
             pass
 
     def set_elements(self, key):
-        try:
-            ttl = self._data[key]['ttl']
-            if not ttl or int(time.time()) <= ttl:
-                return self._data[key]['val']
-        except KeyError:
-            pass
+        if self._check_ttl(key) is False:
+            return self._data[key]['val']
         return set()
 
     def dict_get(self, key, field):
-        try:
+        if self._check_ttl(key) is False:
             return self._data[key]['val'][field]
-        except KeyError:
-            pass
 
     def dict_set(self, key, field, value):
         try:
-            try:
-                self._data[key]['val'][field] = value
-            except KeyError:
-                self._data[key]['val'] = {field: value}
-        except KeyError:
-            self._data[key] = {
-                'ttl': None,
-                'val': {field: value},
-            }
+            self._data[key]['val'][field] = value
+        except TypeError:
+            self._data[key]['val'] = {field: value}
 
     def dict_remove(self, key, field, value):
         try:
@@ -99,13 +72,14 @@ class Cache:
         except (KeyError, AttributeError):
             pass
 
+    def dict_get_keys(self, key):
+        if self._check_ttl(key) is False:
+            return set(self._data[key]['val'].keys())
+        return set()
+
     def dict_get_all(self, key):
-        try:
-            ttl = self._data[key]['ttl']
-            if not ttl or int(time.time()) <= ttl:
-                return self._data[key]['val']
-        except KeyError:
-            pass
+        if self._check_ttl(key) is False:
+            return self._data[key]['val']
         return {}
 
 cache_db = Cache()
