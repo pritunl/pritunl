731eca3bffa611ef693a501cf978274615be91bb
Zachary Huff <zach.huff.386@gmail.com>
2014-01-16 15:24:42 -0500

    Remove database object

--- pritunl/database_object.py
+++
@@ -1,9 +0,0 @@
-from constants import *
-from database import Session
-from sqlalchemy import desc
-
-class DatabaseObject:
-    def add(self):
-        session = Session()
-        session.add(self)
-        session.commit()
