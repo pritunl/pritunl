98ddfa0abc8978f13dcd6fbef013497464337bc9
Zachary Huff <zach.huff.386@gmail.com>
2014-01-16 00:41:51 -0500

    Switch database to sqlalchemy

--- pritunl/database.py
+++ pritunl/database.py
@@ -4,184 +4,19 @@ import logging
 import copy
 import urlparse
 import time
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import StaticPool
 
 logger = logging.getLogger(APP_NAME)
 
-class BerkeleyBackend:
-    def __init__(self, db_path):
-        logger.info('Opening berkeley database... %r' % {
-            'path': db_path,
-        })
+Session = sessionmaker()
+Base = declarative_base()
 
-        try:
-            import bsddb3 as bsddb
-        except ImportError:
-            import bsddb
-
-        self._client = bsddb.db.DB()
-        try:
-            self._client.open(db_path, bsddb.db.DB_HASH, bsddb.db.DB_CREATE)
-        except:
-            logger.exception('Failed to open berkeley database. %r' % {
-                'path': db_path,
-            })
-            raise
-        self._sync_lock = threading.Lock()
-        self._sync_interrupt = False
-        self._sync_thread = threading.Thread(target=self._sync_loop)
-        self._sync_thread.start()
-
-    def _sync_loop(self):
-        while not self._sync_interrupt:
-            self.sync()
-            time.sleep(0.5)
-        self.sync()
-
-    def close(self):
-        logger.info('Closing berkeley database...')
-        self._sync_interrupt = True
-        self._sync_thread.join()
-
-    def get(self, key):
-        return self._client.get(key=key)
-
-    def set(self, key, value):
-        self._client.put(key=key.encode(), data=value)
-
-    def remove(self, key):
-        self._client.delete(key=key)
-
-    def keys(self, prefix):
-        return self._client.keys()
-
-    def sync(self):
-        if self._sync_lock.locked():
-            return
-        self._sync_lock.acquire()
-        try:
-            self._client.sync()
-        finally:
-            self._sync_lock.release()
-
-class MemoryBackend:
-    def __init__(self):
-        logger.info('Creating memory database...')
-        self._data = {}
-
-    def close(self):
-        pass
-
-    def get(self, key):
-        try:
-            return self._data[key]
-        except KeyError:
-            pass
-
-    def set(self, key, value):
-        self._data[key] = value
-
-    def remove(self, key):
-        self._data.pop(key, None)
-
-    def keys(self, prefix):
-        return self._data.keys()
-
-class Database:
-    def __init__(self, db_path):
-        self._db_lock = threading.Lock()
-
-        if db_path is None or db_path == 'none':
-            self._db = MemoryBackend()
-        else:
-            self._db = BerkeleyBackend(db_path)
-
-    def _get(self, key):
-        self._db_lock.acquire()
-        try:
-            value = self._db.get(key)
-        finally:
-            self._db_lock.release()
-        return value
-
-    def _prefix_get(self, prefix):
-        prefix_len = len(prefix)
-        keys = {}
-
-        self._db_lock.acquire()
-        try:
-            for key in self._db.keys(prefix):
-                if key.startswith(prefix):
-                    keys[key[prefix_len:]] = self._db.get(key)
-        finally:
-            self._db_lock.release()
-
-        return keys
-
-    def _set(self, key, value):
-        self._db_lock.acquire()
-        try:
-            self._db.set(key, value)
-        finally:
-            self._db_lock.release()
-
-    def _remove(self, key):
-        self._db_lock.acquire()
-        try:
-            self._db.remove(key)
-        except bsddb.db.DBNotFoundError:
-            pass
-        finally:
-            self._db_lock.release()
-
-    def _prefix_remove(self, prefix):
-        prefix_len = len(prefix)
-
-        self._db_lock.acquire()
-        try:
-            for key in self._db.keys(prefix):
-                if key.startswith(prefix):
-                    self._db.remove(key)
-        finally:
-            self._db_lock.release()
-
-    def close(self):
-        self._db.close()
-
-    def get(self, column_family, row=None, column=None):
-        if not row and column:
-            raise TypeError('Must specify a row for column')
-
-        if not row:
-            keys = {}
-            prefix_keys = self._prefix_get('%s-' % column_family)
-
-            for key in prefix_keys:
-                key_split = key.split('-', 1)
-                if key_split[0] not in keys:
-                    keys[key_split[0]] = {}
-                keys[key_split[0]][key_split[1]] = prefix_keys[key]
-
-            return keys
-
-        if not column:
-            return self._prefix_get('%s-%s-' % (column_family, row))
-
-        key_name = '%s-%s-%s' % (column_family, row, column)
-        return self._get(key_name)
-
-    def set(self, column_family, row, column, value):
-        self._set('%s-%s-%s' % (column_family, row, column), value)
-
-    def remove(self, column_family, row=None, column=None):
-        if not row and column:
-            raise TypeError('Must specify a row for column')
-
-        key_name = '%s-' % column_family
-        if row:
-            key_name += '%s-' % row
-
-        if not column:
-            self._prefix_remove(key_name)
-        else:
-            key_name += column
-            self._remove(key_name)
+def connect_database(sql_connection):
+    from log_entry import LogEntry
+    engine = create_engine(sql_connection)
+    Session.configure(bind=engine)
+    engineConn = engine.connect()
+    Base.metadata.create_all(engine)
--- pritunl/database_object.py
+++ pritunl/database_object.py
@@ -1,105 +1,23 @@
 from constants import *
-from pritunl import app_server
-import logging
-
-logger = logging.getLogger(APP_NAME)
+from database import Session
+from sqlalchemy import desc
 
 class DatabaseObject:
-    db = app_server.app_db
-    column_family = 'column_family'
-    bool_columns = set()
-    int_columns = set()
-    float_columns = set()
-    str_columns = set()
-    cached_columns = set()
-    required_columns = set()
+    def add(self):
+        session = Session()
+        session.add(self)
+        session.commit()
 
-    def __init__(self):
-        self.all_columns = self.bool_columns | self.int_columns | \
-            self.float_columns | self.str_columns
-        self.id = None
-
-    def __setattr__(self, name, value):
-        if name != 'all_columns' and name in self.all_columns:
-            if name in self.cached_columns:
-                self.__dict__[name] = value
-
-            if name in self.int_columns or name in self.float_columns:
-                self.db.set(self.column_family,
-                    self.id, name, str(value))
-            elif name in self.bool_columns:
-                self.db.set(self.column_family,
-                    self.id, name, 't' if value else 'f')
+    @classmethod
+    def get_rows(cls, sort_column=None, sort_desc=False):
+        session = Session()
+        if sort_column:
+            if sort_desc:
+                rows = session.query(cls).order_by(
+                    getattr(cls, sort_column).desc())
             else:
-                self.db.set(self.column_family,
-                    self.id, name, value)
+                rows = session.query(cls).order_by(getattr(cls, sort_column))
         else:
-            self.__dict__[name] = value
-
-    def __getattr__(self, name):
-        if name in self.all_columns and self.id:
-            if name in self.cached_columns and name in self.__dict__:
-                return self.__dict__[name]
-
-            value = self.db.get(self.column_family, self.id, name)
-
-            if name in self.int_columns:
-                if value:
-                    value = int(value)
-            elif name in self.float_columns:
-                if value:
-                    value = float(value)
-            elif name in self.bool_columns:
-                if value == 't':
-                    value = True
-                elif value == 'f':
-                    value = False
-
-            if name in self.cached_columns:
-                self.__dict__[name] = value
-
-            return value
-        raise AttributeError('Object instance has no attribute %r' % name)
-
-    @staticmethod
-    def validate(db_obj, row, columns):
-        valid = True
-
-        for column in db_obj.required_columns:
-            if column not in columns:
-                valid = False
-                break
-
-            if column in db_obj.int_columns:
-                try:
-                    int(columns[column])
-                except ValueError:
-                    valid = False
-                    break
-
-        if valid:
-            if 'remove' in columns:
-                logger.debug('Prevented removal of partially complete' + \
-                    ' %s. %r' % (db_obj.column_family, {
-                        'event_id': row,
-                    }))
-                db_obj.db.remove(db_obj.column_family, row, 'remove')
-        else:
-            # Remove broken rows
-            if 'remove' in columns:
-                logger.info(('Removing broken %s from ' + \
-                    'database. %r') % (db_obj.column_family, {
-                        '%s_id' % db_obj.column_family: row,
-                    }))
-                db_obj.db.remove(db_obj.column_family, row)
-            else:
-                logger.debug(('Queueing removal of broken %s from' + \
-                    ' database. %r') % (db_obj.column_family, {
-                        '%s_id' % db_obj.column_family: row,
-                    }))
-                # Its possible row is currently being created and will
-                # be valid once created. Wait for next db clean to
-                # revalidate and remove row.
-                db_obj.db.set(db_obj.column_family, row, 'remove', 'true')
-
-        return valid
+            rows = session.query(cls).all()
+        session.close()
+        return rows
