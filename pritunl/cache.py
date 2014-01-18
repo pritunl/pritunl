5f6f1a8a2db7b0e5cc1b9276197bee1ce77df783
Zachary Huff <zach.huff.386@gmail.com>
2014-01-18 17:43:45 -0500

    Add dict_exists to cache

--- pritunl/cache.py
+++ pritunl/cache.py
@@ -57,7 +57,10 @@ class Cache:
 
     def set_elements(self, key):
         if self._check_ttl(key) is False:
-            return self._data[key]['val'].copy()
+            try:
+                return self._data[key]['val'].copy()
+            except AttributeError:
+                pass
         return set()
 
     def list_append(self, key, value):
@@ -68,20 +71,32 @@ class Cache:
 
     def list_pop(self, key):
         if self._check_ttl(key) is False:
-            return self._data[key]['val'].pop()
+            try:
+                return self._data[key]['val'].pop()
+            except (AttributeError, IndexError):
+                pass
 
     def list_index(self, key, index):
         if self._check_ttl(key) is False:
-            return self._data[key]['val'][index]
+            try:
+                return self._data[key]['val'][index]
+            except (AttributeError, IndexError):
+                pass
 
     def list_elements(self, key):
         if self._check_ttl(key) is False:
-            return list(self._data[key]['val'])
+            try:
+                return list(self._data[key]['val'])
+            except TypeError:
+                pass
         return []
 
     def dict_get(self, key, field):
         if self._check_ttl(key) is False:
-            return self._data[key]['val'][field]
+            try:
+                return self._data[key]['val'][field]
+            except (TypeError, KeyError):
+                pass
 
     def dict_set(self, key, field, value):
         try:
@@ -91,18 +106,32 @@ class Cache:
 
     def dict_remove(self, key, field, value):
         try:
-            self._data[key]['val'].pop(field)
-        except (KeyError, AttributeError):
+            self._data[key]['val'].pop(field, None)
+        except AttributeError:
             pass
+
+    def dict_exists(self, key, field):
+        if self._check_ttl(key) is False:
+            try:
+                return field in self._data[key]['val']
+            except TypeError:
+                pass
+        return False
 
     def dict_get_keys(self, key):
         if self._check_ttl(key) is False:
-            return set(self._data[key]['val'].keys())
+            try:
+                return set(self._data[key]['val'].keys())
+            except AttributeError:
+                pass
         return set()
 
     def dict_get_all(self, key):
         if self._check_ttl(key) is False:
-            return self._data[key]['val'].copy()
+            try:
+                return self._data[key]['val'].copy()
+            except AttributeError:
+                pass
         return {}
 
     def subscribe(self, channel, timeout=None):
