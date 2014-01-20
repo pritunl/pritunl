d7883bb33b99894363a94474a824ab6da8746981
Zachary Huff <zach.huff.386@gmail.com>
2014-01-20 13:21:59 -0500

    Ignore event exec time

--- www/init/testing.js
+++ www/init/testing.js
@@ -10,9 +10,11 @@ define([
     Backbone.ajax = function(options) {
       var _complete = options.complete;
       options.complete = function(response) {
-        console.log('%c' +
-          response.getResponseHeader('X-Execution-Time') + 'ms ' +
-          this.url, 'color: #0066ff');
+        if (this.url.substring(0, 6) !== '/event') {
+          console.log('%c' +
+            response.getResponseHeader('X-Execution-Time') + 'ms ' +
+            this.url, 'color: #0066ff');
+        }
         _complete(response);
       };
       return _ajax.call(Backbone.$, options);
