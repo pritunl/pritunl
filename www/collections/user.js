92b94163ad73cc6cc9bd289c33bff7de28fc4493
Zachary Huff <zach.huff.386@gmail.com>
2014-01-22 17:10:47 -0500

    Add pages to user collection

--- www/collections/user.js
+++ www/collections/user.js
@@ -9,9 +9,32 @@ define([
     model: UserModel,
     initialize: function(options) {
       this.org = options.org;
+      window.user_page = window.user_page || {};
+      this.page = window.user_page[this.org] || 0;
     },
     url: function() {
       return '/user/' + this.org;
+    },
+    parse: function(response) {
+      this.setPage(response.page);
+      this.pageTotal = response.page_total;
+      return response.users;
+    },
+    setPage: function(page) {
+      this.page = page;
+      window.user_page[this.org] = page;
+    },
+    getPage: function() {
+      return this.page;
+    },
+    nextPage: function() {
+      this.page += 1;
+    },
+    prevPage: function() {
+      this.page -= 1;
+    },
+    isLastPage: function() {
+      return this.page === this.pageTotal;
     }
   });
 
