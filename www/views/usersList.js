e6d3dda2dd355dfdca3d256740ffcaf0be280567
Zachary Huff <zach.huff.386@gmail.com>
2014-01-25 15:08:51 -0500

    Fix user list event listner

--- www/views/usersList.js
+++ www/views/usersList.js
@@ -23,7 +23,6 @@ define([
       });
       this.listenTo(window.events, 'users_updated:' +
         this.collection.org, this.update);
-      this.listenTo(window.events, 'organizations_updated', this.update);
       UsersListView.__super__.initialize.call(this);
     },
     removeItem: function(view) {
