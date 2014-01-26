8ddf9d7d6770629270cb81656c4ab62f44694585
Zachary Huff <zach.huff.386@gmail.com>
2014-01-26 01:19:45 -0500

    Fix mistyped constant

--- pritunl/organization.py
+++ pritunl/organization.py
@@ -100,7 +100,7 @@ class Organization(Config):
         users_sort = []
         for user_id in cache_db.set_elements(self.get_cache_key('users')):
             user = User.get_user(self, id=user_id)
-            if not user or user.type == CERT_CLIENT:
+            if not user or user.type == CERT_CA:
                 continue
             name_id = '%s_%s' % (user.name, user_id)
             if user.type == CERT_CLIENT:
