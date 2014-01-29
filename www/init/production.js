752416d2e42dbb715a191fa1bc82d777b2c50841
Zachary Huff <zach.huff.386@gmail.com>
2014-01-29 12:22:40 -0500

    Add exec time logging to production client

--- www/init/production.js
+++ www/init/production.js
@@ -1,4 +1,4 @@
-/* jshint -W098:true */
+/* jshint -W098:true, -W117:true */
 define([
   'jquery',
   'underscore',
@@ -6,6 +6,28 @@ define([
 ], function($, _, Backbone) {
   'use strict';
   var initialize = function() {
+    var _ajax = Backbone.ajax;
+    Backbone.ajax = function(options) {
+      var _complete = options.complete;
+      options.complete = function(response) {
+        if (this.url.substring(0, 6) !== '/event') {
+          var execTime = response.getResponseHeader('Execution-Time');
+          var color;
+          if (execTime > 400) {
+            color = '#ff0000';
+          }
+          else if (execTime > 200) {
+            color = '#ff6b0d';
+          }
+          else {
+            color = '#0066ff';
+          }
+          console.log('%c' + execTime + 'ms ' + this.url, 'color: ' + color);
+        }
+        _complete(response);
+      };
+      return _ajax.call(Backbone.$, options);
+    };
   };
 
   return initialize;
