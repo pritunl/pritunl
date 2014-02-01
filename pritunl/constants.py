82838074e385aca82f118ae76e09a4b66efd0cc4
Zachary Huff <zach.huff.386@gmail.com>
2014-02-01 13:08:35 -0500

    Add client conn and disconn scripts

--- pritunl/constants.py
+++ pritunl/constants.py
@@ -61,6 +61,7 @@ INDEX_ATTR_NAME = 'index.attr'
 SERIAL_NAME = 'serial'
 TLS_VERIFY_NAME = 'tls_verify.py'
 USER_PASS_VERIFY_NAME = 'user_pass_verify.py'
+CLIENT_CONNECT_NAME = 'client_connect.py'
 OVPN_CONF_NAME = 'openvpn.conf'
 OVPN_STATUS_NAME = 'status'
 OVPN_CA_NAME = 'ca.crt'
@@ -222,6 +223,7 @@ ca %s
 cert %s
 key %s
 tls-verify %s
+client-connect %s
 dh %s
 server %s
 ifconfig-pool-persist %s
@@ -240,6 +242,7 @@ OVPN_INLINE_SERVER_CONF = """port %s
 proto %s
 dev %s
 tls-verify %s
+client-connect %s
 server %s
 ifconfig-pool-persist %s
 %s
@@ -288,7 +291,7 @@ VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789='
 auth_log_path = '%s'
 def log_write(line):
     with open(auth_log_path, 'a') as auth_log_file:
-        auth_log_file.write('[TIME=%%s]%%s\\n' %% (
+        auth_log_file.write('[TLS_VERIFY][TIME=%%s]%%s\\n' %% (
             int(time.time()), line.rstrip('\\n')))
 
 try:
@@ -368,7 +371,7 @@ VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789='
 auth_log_path = '%s'
 def log_write(line):
     with open(auth_log_path, 'a') as auth_log_file:
-        auth_log_file.write('[TIME=%%s]%%s\\n' %% (
+        auth_log_file.write('[OTP_VERIFY][TIME=%%s]%%s\\n' %% (
             int(time.time()), line.rstrip('\\n')))
 
 try:
@@ -439,6 +442,167 @@ try:
         exit(1)
     except SocketError:
         log_write('[FAILED] Verification server returned socket error')
+        exit(1)
+except SystemExit:
+    raise
+except:
+    log_write('[EXCEPTION] ' + traceback.format_exc())
+    raise
+
+exit(0)
+"""
+
+# Script will run in python 2 and 3
+CLIENT_CONNECT_SCRIPT = """#!/usr/bin/env python
+import os
+import sys
+import json
+import time
+import traceback
+
+VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789='
+auth_log_path = '%s'
+def log_write(line):
+    with open(auth_log_path, 'a') as auth_log_file:
+        auth_log_file.write('[CLIENT_CONNECT][TIME=%%s]%%s\\n' %% (
+            int(time.time()), line.rstrip('\\n')))
+
+try:
+    try:
+        from urllib2 import urlopen
+    except ImportError:
+        from urllib.request import urlopen
+    try:
+        from urllib2 import Request
+    except ImportError:
+        from urllib.request import Request
+    try:
+        from urllib2 import HTTPError
+    except ImportError:
+        from urllib.error import HTTPError
+    try:
+        from socket import error as SocketError
+    except ImportError:
+        SocketError = ConnectionResetError
+
+    # Get org and common_name from environ
+    tls_env = os.environ.get('tls_id_0')
+    if not tls_env:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+    tls_env = ''.join(x for x in tls_env if x in VALID_CHARS)
+    o_index = tls_env.find('O=')
+    cn_index = tls_env.find('CN=')
+    if o_index < 0 or cn_index < 0:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+    if o_index > cn_index:
+        org = tls_env[o_index + 2:]
+        common_name = tls_env[3:o_index]
+    else:
+        org = tls_env[2:cn_index]
+        common_name = tls_env[cn_index + 3:]
+    if not org or not common_name:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+
+    try:
+        request = Request('%s://localhost:%s' + \\
+            '/server/%s/client_connect')
+        request.add_header('Content-Type', 'application/json')
+        response = urlopen(request, json.dumps({
+            'org_id': org,
+            'user_id': common_name,
+        }).encode('utf-8'))
+        response = json.loads(response.read().decode('utf-8'))
+
+        if response['client_conf']:
+            with open(sys.argv[1], 'w') as client_conf_file:
+                client_conf_file.write(response['client_conf'])
+    except HTTPError as error:
+        log_write('[FAILED] Server returned error: %%s - %%s' %% (
+            error.code, error.reason))
+        exit(1)
+    except SocketError:
+        log_write('[FAILED] Server returned socket error')
+        exit(1)
+except SystemExit:
+    raise
+except:
+    log_write('[EXCEPTION] ' + traceback.format_exc())
+    raise
+
+exit(0)
+"""
+
+# Script will run in python 2 and 3
+CLIENT_DISCONNECT_SCRIPT = """#!/usr/bin/env python
+import os
+import sys
+import json
+import time
+import traceback
+
+VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789='
+auth_log_path = '%s'
+def log_write(line):
+    with open(auth_log_path, 'a') as auth_log_file:
+        auth_log_file.write('[CLIENT_DISCONNECT][TIME=%%s]%%s\\n' %% (
+            int(time.time()), line.rstrip('\\n')))
+
+try:
+    try:
+        from urllib2 import urlopen
+    except ImportError:
+        from urllib.request import urlopen
+    try:
+        from urllib2 import Request
+    except ImportError:
+        from urllib.request import Request
+    try:
+        from urllib2 import HTTPError
+    except ImportError:
+        from urllib.error import HTTPError
+    try:
+        from socket import error as SocketError
+    except ImportError:
+        SocketError = ConnectionResetError
+
+    # Get org and common_name from environ
+    tls_env = os.environ.get('tls_id_0')
+    if not tls_env:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+    tls_env = ''.join(x for x in tls_env if x in VALID_CHARS)
+    o_index = tls_env.find('O=')
+    cn_index = tls_env.find('CN=')
+    if o_index < 0 or cn_index < 0:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+    if o_index > cn_index:
+        org = tls_env[o_index + 2:]
+        common_name = tls_env[3:o_index]
+    else:
+        org = tls_env[2:cn_index]
+        common_name = tls_env[cn_index + 3:]
+    if not org or not common_name:
+        log_write('[FAILED] Missing organization or user id from environ')
+        raise AttributeError('Missing organization or user id from environ')
+
+    try:
+        request = Request('%s://localhost:%s' + \\
+            '/server/%s/client_disconnect')
+        request.add_header('Content-Type', 'application/json')
+        response = urlopen(request, json.dumps({
+            'org_id': org,
+            'user_id': common_name,
+        }).encode('utf-8'))
+    except HTTPError as error:
+        log_write('[FAILED] Server returned error: %%s - %%s' %% (
+            error.code, error.reason))
+        exit(1)
+    except SocketError:
+        log_write('[FAILED] Server returned socket error')
         exit(1)
 except SystemExit:
     raise
