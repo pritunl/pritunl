631de2a6e7b810af2f54701352fcd17dfd09598c
Zachary Huff <zach.huff.386@gmail.com>
2014-02-09 19:39:41 -0500

    Fix filter str

--- pritunl/utils.py
+++ pritunl/utils.py
@@ -134,6 +134,8 @@ def get_cert_block(cert_path):
         return cert_file[start_index:end_index]
 
 def filter_str(in_str):
+    if not in_str:
+        return in_str
     return ''.join(x for x in in_str if x.isalnum() or x in NAME_SAFE_CHARS)
 
 class Response:
