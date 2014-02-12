ac8f48e65768bc69c85b7f41bfabc8aa55f1760d
Zachary Huff <zach.huff.386@gmail.com>
2014-02-12 02:20:56 -0500

    Update dates

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -3,7 +3,7 @@ __title__ = APP_NAME
 __version__ = '0.10.6'
 __author__ = 'Zachary Huff'
 __license__ = 'AGPL'
-__copyright__ = 'Copyright 2013 Zachary Huff'
+__copyright__ = 'Copyright 2013-2014 Zachary Huff'
 
 from app_server import AppServer
 app_server = AppServer()
