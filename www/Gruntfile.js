ce74551d651b2d2b4daf37ada45ccdc702ec5734
Zachary Huff <zach.huff.386@gmail.com>
2014-02-18 14:52:11 -0500

    Add file versioning for static web files

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -1,5 +1,57 @@
 /* jshint strict:false */
+
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
+
 module.exports = function(grunt) {
+  grunt.registerMultiTask('versioning', 'Version static files', function() {
+    var options = this.options({
+      hashLength: 6,
+      encoding: 'utf8',
+      replaceFiles: []
+    });
+    var i;
+    var data;
+    var filePath;
+    var filePathNew;
+    var replacePath;
+    var hash;
+    var staticFiles = this.data.staticFiles;
+    var replaceFiles = this.data.replaceFiles;
+    var searchStr;
+    var replaceStr;
+    var replaces = {};
+
+    for (i = 0; i < staticFiles.length; i++) {
+      filePath = staticFiles[i];
+
+      hash = crypto.createHash('md5').update(grunt.file.read(
+        filePath, options.encoding)).digest('hex');
+      hash = hash.substr(0, options.hashLength);
+
+      filePathNew = path.dirname(filePath) + path.sep + path.basename(
+        filePath, path.extname(filePath)) + '.' + hash + path.extname(
+        filePath);
+
+      fs.rename(filePath, filePathNew);
+      replaces[path.basename(filePath)] = path.basename(filePathNew);
+    }
+
+    for (i = 0; i < replaceFiles.length; i++) {
+      replacePath = replaceFiles[i];
+      data = grunt.file.read(replacePath, options.encoding);
+
+      for (searchStr in replaces) {
+        replaceStr = replaces[searchStr];
+        data = data.replace(new RegExp(searchStr, 'g'), replaceStr);
+      }
+
+      grunt.file.write(replacePath, data, options.encoding);
+    }
+
+  });
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -39,7 +91,7 @@ module.exports = function(grunt) {
     },
 
     clean: {
-      dist: ['dist']
+      dist: ['vendor/dist']
     },
 
     requirejs: {
@@ -177,6 +229,39 @@ module.exports = function(grunt) {
           'vendor/dist/robots.txt': 'root/robots.txt',
           'vendor/dist/index.html': 'root/index.html'
         }
+      },
+      demo: {
+        files: {
+          'vendor/dist/fonts/fredoka-one.eot': 'fonts/fredoka-one.eot',
+          'vendor/dist/fonts/fredoka-one.woff': 'fonts/fredoka-one.woff',
+          'vendor/dist/fonts/glyphicons-halflings-regular.eot':
+            'fonts/glyphicons-halflings-regular.eot',
+          'vendor/dist/fonts/glyphicons-halflings-regular.svg':
+            'fonts/glyphicons-halflings-regular.svg',
+          'vendor/dist/fonts/glyphicons-halflings-regular.ttf':
+            'fonts/glyphicons-halflings-regular.ttf',
+          'vendor/dist/fonts/glyphicons-halflings-regular.woff':
+            'fonts/glyphicons-halflings-regular.woff',
+          'vendor/dist/fonts/ubuntu-bold.eot': 'fonts/ubuntu-bold.eot',
+          'vendor/dist/fonts/ubuntu-bold.woff': 'fonts/ubuntu-bold.woff',
+          'vendor/dist/fonts/ubuntu.eot': 'fonts/ubuntu.eot',
+          'vendor/dist/fonts/ubuntu.woff': 'fonts/ubuntu.woff',
+          'vendor/dist/js/require.min.js': 'vendor/require/require.min.js',
+          'vendor/dist/favicon.ico': 'img/favicon.ico',
+          'vendor/dist/index.html': 'root/index.html'
+        }
+      }
+    },
+
+    versioning: {
+      all: {
+        staticFiles: [
+          'vendor/dist/css/main.css',
+          'vendor/dist/js/main.js',
+        ],
+        replaceFiles: [
+          'vendor/dist/index.html'
+        ],
       }
     }
   });
@@ -187,14 +272,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-  grunt.registerTask('default', ['jshint', 'clean', 'requirejs:production',
-    'less', 'copy']);
+  grunt.registerTask('default', ['jshint', 'clean',
+    'requirejs:production', 'less', 'copy:dist', 'versioning']);
 
-  grunt.registerTask('test', ['jshint', 'clean', 'requirejs:test', 'less',
-    'copy']);
+  grunt.registerTask('test', ['jshint', 'clean', 'requirejs:test',
+    'less', 'copy:dist', 'versioning']);
 
-  grunt.registerTask('demo', ['jshint', 'clean', 'requirejs:demo', 'less',
-    'copy']);
+  grunt.registerTask('demo', ['jshint', 'clean', 'requirejs:demo',
+    'less', 'copy:demo', 'versioning']);
 
   grunt.registerTask('lint', ['jshint']);
 };
