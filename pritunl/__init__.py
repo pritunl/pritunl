c5ad3feea9ab6a59adff1797ec038db3ee4f436a
Zachary Huff <zach.huff.386@gmail.com>
2014-03-23 13:47:36 -0400

    Update packages

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -1,6 +1,6 @@
 from constants import *
 __title__ = APP_NAME
-__version__ = '0.10.7'
+__version__ = '0.10.8'
 __author__ = 'Zachary Huff'
 __license__ = 'AGPL'
 __copyright__ = 'Copyright 2013-2014 Zachary Huff'
