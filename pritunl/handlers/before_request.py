8c8a69490dbee423d068a8cd3182e99b1c6ae2e8
Zachary Huff <zach.huff.386@gmail.com>
2014-03-23 19:38:51 -0400

    Add vpn state to before request check

--- pritunl/handlers/before_request.py
+++ pritunl/handlers/before_request.py
@@ -2,8 +2,20 @@ from pritunl.constants import *
 import pritunl.utils as utils
 from pritunl import app_server
 import flask
+import re
 
 @app_server.app.before_request
 def before_request():
-    if app_server.www_state == DISABLED:
+    path = flask.request.path
+    if app_server.www_state == DISABLED and \
+            not re.match(r'^/server/[a-z0-9]+/tls_verify$', path) and \
+            not re.match(r'^/server/[a-z0-9]+/otp_verify$', path) and \
+            not re.match(r'^/server/[a-z0-9]+/client_connect$', path) and \
+            not re.match(r'^/server/[a-z0-9]+/client_disconnect$', path):
+        raise flask.abort(401, app_server.notification)
+    elif app_server.vpn_state == DISABLED and (
+            re.match(r'^/server/[a-z0-9]+/tls_verify$', path) or
+            re.match(r'^/server/[a-z0-9]+/otp_verify$', path) or
+            re.match(r'^/server/[a-z0-9]+/client_connect$', path) or
+            re.match(r'^/server/[a-z0-9]+/client_disconnect$', path)):
         raise flask.abort(401, app_server.notification)
