30cb8f35e3167e574753a40b30b4d748fc96b704
Zachary Huff <zach.huff.386@gmail.com>
2014-03-31 07:32:54 -0400

    Decrypt token in auth model

--- www/models/auth.js
+++ www/models/auth.js
@@ -1,17 +1,50 @@
 define([
   'jquery',
   'underscore',
-  'backbone'
-], function($, _, Backbone) {
+  'backbone',
+  'sjcl'
+], function($, _, Backbone, SJCL) {
   'use strict';
   var AuthModel = Backbone.Model.extend({
     defaults: {
-      'authenticated': null,
       'username': null,
-      'password': null
+      'password': null,
+      'token': null
     },
     url: function() {
-      return '/auth/session';
+      return '/auth';
+    },
+    parse: function(response) {
+      var i;
+      var tokenData;
+      var hash;
+      var hashDigest;
+      var cipher;
+      var tokenEnc;
+
+      if (response.token) {
+        tokenData = response.token.split('$');
+        hash = new sjcl.hash.sha256();
+        hash.update(this.get('username') + '$' + this.get('password'));
+        hash.update(sjcl.codec.base64.toBits(tokenData[1]));
+        hashDigest = hash.finalize();
+
+        for (i = 0; i < 5; i++) {
+          hashDigest = sjcl.hash.sha256.hash(hashDigest);
+        }
+
+        cipher = new sjcl.cipher.aes(hashDigest)
+        tokenEnc = sjcl.codec.base64.toBits(tokenData[2]);
+        response.token = sjcl.codec.utf8String.fromBits(
+            cipher.decrypt(tokenEnc.slice(0, 4))) +
+          sjcl.codec.utf8String.fromBits(
+            cipher.decrypt(tokenEnc.slice(4, 8)));
+      }
+
+      return response;
+    },
+    isNew: function() {
+      return false;
     }
   });
 
