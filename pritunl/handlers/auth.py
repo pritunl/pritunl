8be05252a62a9e55762d0687c7599d77296da260
Zachary Huff <zach.huff.386@gmail.com>
2014-03-31 21:39:36 -0400

    Remove auth token handlers

--- pritunl/handlers/auth.py
+++ pritunl/handlers/auth.py
@@ -56,26 +56,3 @@ def auth_delete():
     return utils.jsonify({
         'authenticated': False,
     })
-
-@app_server.app.route('/auth/token', methods=['POST'])
-def auth_token_post():
-    username = flask.request.json['username']
-    password = flask.request.json['password']
-    remote_addr = utils.get_remote_addr()
-
-    if not utils.check_auth(username, password, remote_addr):
-        return utils.jsonify({
-            'error': AUTH_INVALID,
-            'error_msg': AUTH_INVALID_MSG,
-        }, 401)
-
-    auth_token = AuthToken()
-    return utils.jsonify({
-        'auth_token': auth_token.id,
-    })
-
-@app_server.app.route('/auth/token/<token>', methods=['DELETE'])
-def auth_token_delete(token):
-    auth_token = AuthToken(token)
-    auth_token.remove()
-    return utils.jsonify({})
