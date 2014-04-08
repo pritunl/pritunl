26049d9a208637452668e91eaad680354224c0c9
Zachary Huff <zach.huff.386@gmail.com>
2014-04-08 02:56:36 -0400

    Show enterprise settings when license key is aviable

--- www/views/header.js
+++ www/views/header.js
@@ -16,15 +16,43 @@ define([
     tagName: 'header',
     template: _.template(headerTemplate),
     events: {
-      'click .enterprise-upgrade a': 'onEnterpriseUpgrade',
-      'click .enterprise-settings a': 'onEnterpriseSettings',
+      'click .enterprise-upgrade a, .enterprise-settings a': 'onEnterprise',
       'click .change-password a': 'changePassword'
     },
     render: function() {
       this.$el.html(this.template());
       return this;
     },
-    onEnterpriseUpgrade: function() {
+    onEnterprise: function() {
+      if (this.onEnterpriseLock) {
+        return;
+      }
+      this.onEnterpriseLock = true;
+      var model = new SubscriptionModel();
+      model.fetch({
+        success: function(model) {
+          if (model.get('license')) {
+            this.enterpriseSettings(model);
+          }
+          else {
+            this.enterpriseUpgrade();
+          }
+          this.onEnterpriseLock = false;
+        }.bind(this),
+        error: function() {
+          var alertView = new AlertView({
+            type: 'danger',
+            message: 'Failed to load subscription information, ' +
+              'server error occurred.',
+            dismissable: true
+          });
+          $('.alerts-container').append(alertView.render().el);
+          this.addView(alertView);
+          this.onEnterpriseLock = false;
+        }.bind(this)
+      });
+    },
+    enterpriseUpgrade: function() {
       var modal = new ModalSubscribeView();
       this.listenToOnce(modal, 'applied', function() {
         var alertView = new AlertView({
@@ -37,41 +65,20 @@ define([
       }.bind(this));
       this.addView(modal);
     },
-    onEnterpriseSettings: function() {
-      if (this.onEnterpriseSettingsLock) {
-        return;
-      }
-      this.onEnterpriseSettingsLock = true;
-      var model = new SubscriptionModel();
-      model.fetch({
-        success: function() {
-          var modal = new ModalEnterpriseView({
-            model: model
-          });
-          this.listenToOnce(modal, 'applied', function() {
-            var alertView = new AlertView({
-              type: 'danger',
-              message: 'Enterprise license removed.',
-              dismissable: true
-            });
-            $('.alerts-container').append(alertView.render().el);
-            this.addView(alertView);
-          }.bind(this));
-          this.addView(modal);
-          this.onEnterpriseSettingsLock = false;
-        }.bind(this),
-        error: function() {
-          var alertView = new AlertView({
-            type: 'danger',
-            message: 'Failed to load subscription information, ' +
-              'server error occurred.',
-            dismissable: true
-          });
-          $('.alerts-container').append(alertView.render().el);
-          this.addView(alertView);
-          this.onEnterpriseSettingsLock = false;
-        }.bind(this)
-      })
+    enterpriseSettings: function(model) {
+      var modal = new ModalEnterpriseView({
+        model: model
+      });
+      this.listenToOnce(modal, 'applied', function() {
+        var alertView = new AlertView({
+          type: 'danger',
+          message: 'Enterprise license removed.',
+          dismissable: true
+        });
+        $('.alerts-container').append(alertView.render().el);
+        this.addView(alertView);
+      }.bind(this));
+      this.addView(modal);
     },
     changePassword: function() {
       var loginView = new LoginView({
