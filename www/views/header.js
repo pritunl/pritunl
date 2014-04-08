01171a96767372e377a1d2b5b3daefc824cc1644
Zachary Huff <zach.huff.386@gmail.com>
2014-04-07 21:00:43 -0400

    Add enterprise modal the header

--- www/views/header.js
+++ www/views/header.js
@@ -3,18 +3,21 @@ define([
   'underscore',
   'backbone',
   'models/auth',
+  'models/subscription',
   'views/alert',
   'views/login',
   'views/modalSubscribe',
+  'views/modalEnterprise',
   'text!templates/header.html'
-], function($, _, Backbone, AuthModel, AlertView, LoginView,
-    ModalSubscribeView, headerTemplate) {
+], function($, _, Backbone, AuthModel, SubscriptionModel, AlertView, LoginView,
+    ModalSubscribeView, ModalEnterpriseView, headerTemplate) {
   'use strict';
   var HeaderView = Backbone.View.extend({
     tagName: 'header',
     template: _.template(headerTemplate),
     events: {
       'click .enterprise-upgrade a': 'onEnterpriseUpgrade',
+      'click .enterprise-settings a': 'onEnterpriseSettings',
       'click .change-password a': 'changePassword'
     },
     render: function() {
@@ -33,6 +36,26 @@ define([
         this.addView(alertView);
       }.bind(this));
       this.addView(modal);
+    },
+    onEnterpriseSettings: function() {
+      var model = new SubscriptionModel();
+      model.fetch({
+        success: function() {
+          var modal = new ModalEnterpriseView({
+            model: model
+          });
+          this.listenToOnce(modal, 'applied', function() {
+            var alertView = new AlertView({
+              type: 'danger',
+              message: 'Enterprise license removed.',
+              dismissable: true
+            });
+            $('.alerts-container').append(alertView.render().el);
+            this.addView(alertView);
+          }.bind(this));
+          this.addView(modal);
+        }.bind(this),
+      })
     },
     changePassword: function() {
       var loginView = new LoginView({
