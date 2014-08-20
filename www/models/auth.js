9fc35582336ebb255166f37329a722083f03eb83
Zachary Huff <zach.huff.386@gmail.com>
2014-08-20 15:30:22 -0400

    Remove encryption from auth model

--- www/models/auth.js
+++ www/models/auth.js
@@ -1,9 +1,8 @@
 define([
   'jquery',
   'underscore',
-  'backbone',
-  'sjcl'
-], function($, _, Backbone, SJCL) {
+  'backbone'
+], function($, _, Backbone) {
   'use strict';
   var AuthModel = Backbone.Model.extend({
     defaults: {
@@ -16,35 +15,6 @@ define([
     },
     url: function() {
       return '/auth';
-    },
-    parse: function(response) {
-      var i;
-      var tokenData;
-      var hash;
-      var hashDigest;
-      var cipher;
-      var tokenEnc;
-
-      if (response.token && !window.demo) {
-        tokenData = response.token.split('$');
-        hash = new SJCL.hash.sha256();
-        hash.update(this.get('username') + '$' + this.get('password'));
-        hash.update(SJCL.codec.base64.toBits(tokenData[1]));
-        hashDigest = hash.finalize();
-
-        for (i = 0; i < 5; i++) {
-          hashDigest = SJCL.hash.sha256.hash(hashDigest);
-        }
-
-        cipher = new SJCL.cipher.aes(hashDigest);
-        tokenEnc = SJCL.codec.base64.toBits(tokenData[2]);
-        response.token = SJCL.codec.utf8String.fromBits(
-            cipher.decrypt(tokenEnc.slice(0, 4))) +
-          SJCL.codec.utf8String.fromBits(
-            cipher.decrypt(tokenEnc.slice(4, 8)));
-      }
-
-      return response;
     },
     isNew: function() {
       return false;
