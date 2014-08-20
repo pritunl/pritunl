4f42e57bb7aae65a54848c27cc479dd4a6909888
Zachary Huff <zach.huff.386@gmail.com>
2014-08-20 15:44:29 -0400

    Update auth handlers

--- pritunl/handlers/auth.py
+++ pritunl/handlers/auth.py
@@ -1,4 +1,5 @@
 from pritunl.constants import *
+from pritunl.administrator import Administrator
 from pritunl.system_conf import SystemConf
 import pritunl.utils as utils
 import pritunl.mongo as mongo
@@ -9,27 +10,46 @@ import flask
 @app_server.app.route('/auth', methods=['GET'])
 @app_server.auth
 def auth_get():
-    return utils.jsonify(utils.get_auth())
+    system_conf = SystemConf()
+    response = flask.request.administrator.dict()
+    response.update({
+        'email_from': system_conf.get('email', 'from_addr'),
+        'email_api_key': system_conf.get('email', 'api_key'),
+    })
+    return utils.jsonify(response)
 
 @app_server.app.route('/auth', methods=['PUT'])
 @app_server.auth
 def auth_put():
-    username = utils.filter_str(flask.request.json.get('username'))
-    password = flask.request.json['password']
-    token = flask.request.json.get('token')
-    email_api_key = flask.request.json.get('email_api_key')
+    administrator = flask.request.administrator
     system_conf = SystemConf()
 
-    utils.set_auth(username, password, token)
+    if 'username' in flask.request.json and flask.request.json['username']:
+        administrator.username = utils.filter_str(
+            flask.request.json['username'])
+    if 'password' in flask.request.json and flask.request.json['password']:
+        administrator.password = flask.request.json['password']
+    if 'token' in flask.request.json and flask.request.json['token']:
+        administrator.generate_token()
+    if 'secret' in flask.request.json and flask.request.json['secret']:
+        administrator.generate_secret()
+
     if 'email_from' in flask.request.json:
         email_from = flask.request.json['email_from']
         system_conf.set('email', 'from_addr', email_from or None)
     if 'email_api_key' in flask.request.json:
         email_api_key = flask.request.json['email_api_key']
         system_conf.set('email', 'api_key', email_api_key or None)
-    system_conf.commit()
 
-    return utils.jsonify(utils.get_auth())
+    system_conf.commit()
+    administrator.commit()
+
+    response = flask.request.administrator.dict()
+    response.update({
+        'email_from': system_conf.get('email', 'from_addr'),
+        'email_api_key': system_conf.get('email', 'api_key'),
+    })
+    return utils.jsonify(response)
 
 @app_server.app.route('/auth/session', methods=['GET'])
 def auth_get():
@@ -42,24 +62,23 @@ def auth_session_post():
     username = flask.request.json['username']
     password = flask.request.json['password']
     remote_addr = utils.get_remote_addr()
+    administrator = utils.check_auth(username, password, remote_addr)
 
-    if not utils.check_auth(username, password, remote_addr):
+    if not administrator:
         return utils.jsonify({
             'error': AUTH_INVALID,
             'error_msg': AUTH_INVALID_MSG,
         }, 401)
 
-    flask.session['auth'] = True
+    flask.session['admin_id'] = administrator.id
     flask.session['timestamp'] = int(time.time())
     if not app_server.ssl:
         flask.session['source'] = remote_addr
 
-    data = {
+    return utils.jsonify({
         'authenticated': True,
-    }
-    if password == DEFAULT_PASSWORD:
-        data['default_password'] = True
-    return utils.jsonify(data)
+        'default': administrator.default,
+    })
 
 @app_server.app.route('/auth/session', methods=['DELETE'])
 def auth_delete():
