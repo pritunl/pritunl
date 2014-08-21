7943b1aa14dac3503693c507cf239288149b748e
Zachary Huff <zach.huff.386@gmail.com>
2014-08-21 02:37:26 -0400

    Move log entries to mongodb

--- pritunl/log_entry.py
+++ pritunl/log_entry.py
@@ -1,53 +1,44 @@
 from constants import *
-from cache import cache_db
 from event import Event
-from cache_object import CacheObject
+from mongo_object import MongoObject
+import mongo
 import time
 import uuid
+import pymongo
 
-class LogEntry(CacheObject):
-    column_family = 'log_entries'
-    str_columns = {'type', 'message'}
-    int_columns = {'time'}
-    cached_columns = {'type', 'message', 'time'}
-    db_instance = cache_db # TODO
+class LogEntry(MongoObject):
+    fields = {
+        'time',
+        'message',
+    }
 
-    def __init__(self, id=None, type=None, message=None):
-        CacheObject.__init__(self)
+    def __init__(self, message=None, **kwargs):
+        MongoObject.__init__(self, **kwargs)
 
-        if id is None:
-            self.transaction_start()
-            self.id = uuid.uuid4().hex
-            self.type = type or INFO
-            self.time = int(time.time())
+        if message is not None:
             self.message = message
-            self.initialize()
-            self.transaction_commit()
-        else:
-            self.id = id
+
+        if not self.exists:
+            self.time = datetime.datetime.now()
+            self.commit()
 
     def dict(self):
         return {
             'id': self.id,
-            'time': self.time,
+            'time': self.time.strftime('%s'),
             'message': self.message,
         }
 
-    def initialize(self):
-        while self.db.list_length(self.column_family) >= LOG_LIMIT:
-            log_entry_id = self.db.list_index(self.column_family, -1)
-            if not log_entry_id:
-                break
-            LogEntry(id=log_entry_id).remove()
-        self.db.list_lpush(self.column_family, self.id)
-        Event(type=LOG_UPDATED)
+    @staticmethod
+    def get_collection():
+        return mongo.get_collection('log_entries')
 
-    def remove(self):
-        self.transaction_start()
-        CacheObject.remove(self)
-        self.transaction_commit()
+    def commit(self, *args, **kwargs):
+        MongoObject.commit(self, *args, **kwargs)
+        Event(type=LOG_UPDATED)
 
     @classmethod
     def iter_log_entries(cls):
-        for log_entry_id in cls.db_instance.list_iter('log_entries'):
-            yield cls(id=log_entry_id)
+        for doc in cls.get_collection().find().sort(
+                'time', pymongo.DESCENDING):
+            yield cls(doc=doc)
