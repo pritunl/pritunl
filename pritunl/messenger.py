af94131af427705f621d64e0b76b1a1a1581ae79
Zachary Huff <zach.huff.386@gmail.com>
2014-08-25 04:50:19 -0400

    Add messenger

---
+++ pritunl/messenger.py
@@ -0,0 +1,44 @@
+from constants import *
+from exceptions import *
+from descriptors import *
+import mongo
+import time
+import datetime
+
+class Messenger:
+    def __init__(self, channel):
+        self.channel = channel
+
+    @static_property
+    def collection(cls):
+        return mongo.get_collection('messages')
+
+    def publish(self, message):
+        self.collection.insert({
+            'time': datetime.datetime.utcnow(),
+            'channel': self.channel,
+            'message': message,
+        })
+
+    def subscribe(self):
+        try:
+            cursor_id = self.collection.find({
+                'channel': self.channel,
+            }).sort('$natural', -1)[0]['_id']
+        except IndexError:
+            cursor_id = None
+        while True:
+            try:
+                spec = {
+                    'channel': self.channel,
+                }
+                if cursor_id:
+                    spec['_id'] = {'$gt': cursor_id}
+                cursor = self.collection.find(spec,
+                    tailable=True, await_data=True)
+                while cursor.alive:
+                    for doc in cursor:
+                        cursor_id = doc['_id']
+                        yield doc
+            except pymongo.errors.AutoReconnect:
+                time.sleep(0.1)
