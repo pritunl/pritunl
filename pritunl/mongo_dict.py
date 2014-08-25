4db1b50e8e7445f68a4adba17d25b3db658686c5
Zachary Huff <zach.huff.386@gmail.com>
2014-08-24 23:09:23 -0400

    Add mongo dict to monitor dict for changes

---
+++ pritunl/mongo_dict.py
@@ -0,0 +1,41 @@
+from constants import *
+from exceptions import *
+import UserDict
+
+class MongoDict(UserDict.UserDict):
+    __class__ = dict
+
+    def __init__(self, initdict=None, changed=True, **kwargs):
+        if isinstance(initdict, dict):
+            self.data = initdict
+        else:
+            UserDict.UserDict.__init__(self, initdict, **kwargs)
+        self.changed = changed
+
+    def __setitem__(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.__setitem__(self, *args, **kwargs)
+
+    def __delitem__(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.__delitem__(self, *args, **kwargs)
+
+    def clear(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.clear(self, *args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.update(self, *args, **kwargs)
+
+    def setdefault(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.setdefault(self, *args, **kwargs)
+
+    def pop(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.pop(self, *args, **kwargs)
+
+    def popitem(self, *args, **kwargs):
+        self.changed = True
+        return UserDict.UserDict.popitem(self, *args, **kwargs)
