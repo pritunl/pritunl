0f33b606188ba4959afd268dda3f9708f7087115
Zachary Huff <zach.huff.386@gmail.com>
2014-08-30 19:56:27 -0400

    Add cached_static_property

--- pritunl/descriptors.py
+++ pritunl/descriptors.py
@@ -8,6 +8,17 @@ class cached_property(object):
         obj.__dict__[self.func.__name__] = value = self.func(obj)
         return value
 
+class cached_static_property(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, obj, objtype):
+        if obj is None:
+            return self.func(objtype)
+        value = self.func(objtype)
+        setattr(obj, self.func.__name__, value)
+        return value
+
 class static_property(object):
     def __init__(self, func):
         self.func = func
