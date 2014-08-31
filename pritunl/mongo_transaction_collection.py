e6a9600182f23d6c9b8171219a4ff49c575b518f
Zachary Huff <zach.huff.386@gmail.com>
2014-08-31 03:52:58 -0400

    Fix mongo_transaction_collection

--- pritunl/mongo_transaction_collection.py
+++ pritunl/mongo_transaction_collection.py
@@ -31,23 +31,24 @@ class MongoTransactionCollection:
 
     def __getattr__(self, name):
         if name in MONGO_ACTION_METHODS:
-            if not self._actions:
+            if self._actions is None:
                 actions = self.append_action_set()[2]
             else:
                 actions = self._actions
             return MongoTransactionAction(actions, name)
-        elif name == 'bulk' and self._action_sets:
+        elif name == 'bulk' and self._action_sets is not None:
             data = self.append_action_set()
             data[1] = True
             return lambda: MongoTransactionCollection(data[2])
-        elif name == 'rollback' and self._action_sets:
+        elif name == 'rollback' and self._action_sets is not None:
             data = self.append_action_set()
             return lambda: MongoTransactionCollection(data[3])
-        elif name == 'post' and self._action_sets:
+        elif name == 'post' and self._action_sets is not None:
             return lambda: MongoTransactionCollection(
                 self.append_action_set()[4])
-        elif name == BULK_EXECUTE and self._action_sets:
+        elif name == BULK_EXECUTE and self._action_sets is not None:
             self.append_action_set()[2] = BULK_EXECUTE
+            return MongoTransactionAction([], name)
         else:
             raise AttributeError('MongoTransactionCollection ' +
                 'instance has no attribute %r' % name)
