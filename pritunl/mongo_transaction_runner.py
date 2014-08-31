78b132ae023a93739085002fbe9a6d1a509a6d1f
Zachary Huff <zach.huff.386@gmail.com>
2014-08-31 04:08:12 -0400

    Add transaction runner

---
+++ pritunl/mongo_transaction_runner.py
@@ -0,0 +1,45 @@
+from constants import *
+from exceptions import *
+from descriptors import *
+from mongo_object import MongoObject
+from mongo_transaction import MongoTransaction
+import mongo
+import pymongo
+import collections
+import datetime
+import bson
+import logging
+import threading
+import time
+
+logger = logging.getLogger(APP_NAME)
+
+class MongoTransactionRunner:
+    def __init__(self):
+        pass
+
+    @static_property
+    def collection(cls):
+        return mongo.get_collection('transaction')
+
+    def check_thread(self):
+        while True:
+            spec = {
+                'timestamp': {'$lt': datetime.datetime.utcnow()},
+            }
+
+            for doc in self.collection.find(spec).sort('priority'):
+                try:
+                    tran = MongoTransaction(doc=doc)
+                    tran.run()
+                except:
+                    logger.exception('Failed to run transaction. %r' % {
+                        'transaction_id': str(doc['_id']),
+                    })
+
+            time.sleep(3)
+
+    def start(self):
+        thread = threading.Thread(target=self.check_thread)
+        thread.daemon = True
+        thread.start()
