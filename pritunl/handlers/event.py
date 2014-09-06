81f6f3a202f37b86ac6861e74269d5659fc73698
Zachary Huff <zach.huff.386@gmail.com>
2014-09-06 18:18:05 -0400

    Use get_events in event handler

--- pritunl/handlers/event.py
+++ pritunl/handlers/event.py
@@ -1,5 +1,5 @@
 from pritunl.constants import *
-from pritunl.messenger import Messenger
+from pritunl.event import Event
 import pritunl.utils as utils
 from pritunl import app_server
 import time
@@ -14,27 +14,4 @@ def event_get(cursor=None):
     if app_server.interrupt:
         return flask.abort(503)
 
-    events = []
-    events_dict = {}
-    messenger = Messenger('events')
-
-    for event in messenger.subscribe(cursor_id=bson.ObjectId(cursor),
-            timeout=10, yield_delay=0.03):
-        event_type, resource_id = event.pop('message')
-        if (event_type, resource_id) in events_dict:
-            old_event = events_dict[(event_type, resource_id)]
-            old_event['id'] = str(event['_id'])
-            old_event['timestamp'] = time.mktime(
-                event['timestamp'].timetuple())
-            continue
-
-        events_dict[(event_type, resource_id)] = event
-        event['id'] = str(event.pop('_id'))
-        event['type'] = event_type
-        event['resource_id'] = resource_id
-        event['timestamp'] = time.mktime(event['timestamp'].timetuple())
-        event.pop('channel')
-
-        events.append(event)
-
-    return utils.jsonify(events)
+    return utils.jsonify(Event.get_events(cursor=cursor))
