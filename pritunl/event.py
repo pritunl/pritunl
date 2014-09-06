d650bd64aa5e7cb297f3b2759a21b6b232af7797
Zachary Huff <zach.huff.386@gmail.com>
2014-09-06 18:17:34 -0400

    Add get_events to event

--- pritunl/event.py
+++ pritunl/event.py
@@ -10,3 +10,30 @@ class Event(object):
     def __init__(self, type, resource_id=None):
         messenger = Messenger('events')
         messenger.publish((type, resource_id))
+
+    @staticmethod
+    def get_events(self, cursor=None):
+        events = []
+        events_dict = {}
+        messenger = Messenger('events')
+
+        for event in messenger.subscribe(cursor_id=bson.ObjectId(cursor),
+                timeout=10, yield_delay=0.03):
+            event_type, resource_id = event.pop('message')
+            if (event_type, resource_id) in events_dict:
+                old_event = events_dict[(event_type, resource_id)]
+                old_event['id'] = str(event['_id'])
+                old_event['timestamp'] = time.mktime(
+                    event['timestamp'].timetuple())
+                continue
+
+            events_dict[(event_type, resource_id)] = event
+            event['id'] = str(event.pop('_id'))
+            event['type'] = event_type
+            event['resource_id'] = resource_id
+            event['timestamp'] = time.mktime(event['timestamp'].timetuple())
+            event.pop('channel')
+
+            events.append(event)
+
+        return events
