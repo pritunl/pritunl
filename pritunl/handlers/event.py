1e4ef054a922f6f4697229073fc5e81f44597ac6
Zachary Huff <zach.huff.386@gmail.com>
2014-09-06 18:14:19 -0400

    Remove duplicate events

--- pritunl/handlers/event.py
+++ pritunl/handlers/event.py
@@ -15,13 +15,26 @@ def event_get(cursor=None):
         return flask.abort(503)
 
     events = []
+    events_dict = {}
     messenger = Messenger('events')
 
     for event in messenger.subscribe(cursor_id=bson.ObjectId(cursor),
             timeout=10):
+        event_type, resource_id = event.pop('message')
+        if (event_type, resource_id) in events_dict:
+            old_event = events_dict[(event_type, resource_id)]
+            old_event['id'] = str(event['_id'])
+            old_event['timestamp'] = time.mktime(
+                event['timestamp'].timetuple())
+            continue
+
+        events_dict[(event_type, resource_id)] = event
         event['id'] = str(event.pop('_id'))
-        event['type'], event['resource_id'] = event.pop('message')
+        event['type'] = event_type
+        event['resource_id'] = resource_id
         event['timestamp'] = time.mktime(event['timestamp'].timetuple())
+        event.pop('channel')
+
         events.append(event)
         break
 
