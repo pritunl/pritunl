f0e90107a5770771cd378d1932900d35aad70113
Zachary Huff <zach.huff.386@gmail.com>
2014-09-07 17:59:49 -0400

    Add sync_ip_pool task

---
+++ pritunl/task_sync_ip_pool.py
@@ -0,0 +1,26 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.task import Task, add_task
+from pritunl.server import Server
+import logging
+import time
+
+logger = logging.getLogger(APP_NAME)
+
+class TaskSyncIpPool(Task):
+    def __init__(self, **kwargs):
+        Task.__init__(self, **kwargs)
+        self.type = 'sync_ip_pool'
+
+    def task(self):
+        for server in Server.iter_servers():
+            try:
+                server.ip_pool.sync_ip_pool()
+            except:
+                logger.exception('Failed to sync server IP pool. %r' % {
+                    'server_id': server.id,
+                    'task_id': self.id,
+                })
+
+add_task('sync_ip_pool', TaskSyncIpPool, minutes=0)
