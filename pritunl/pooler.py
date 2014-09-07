2c333b952d02029b52dd47f3178b6a8d3862255c
Zachary Huff <zach.huff.386@gmail.com>
2014-09-07 17:58:58 -0400

    Add check_org_pool to pooler

--- pritunl/pooler.py
+++ pritunl/pooler.py
@@ -17,6 +17,17 @@ import collections
 logger = logging.getLogger(APP_NAME)
 
 class Pooler(object):
+    def check_org_pool(self):
+        org_pool_count = mongo.get_collection('organizations').find({
+            'type': ORG_POOL,
+        }, {
+            '_id': True,
+        }).count()
+
+        for _ in xrange(ORG_POOL_SIZE - org_pool_count):
+            org = Organization.new_org(type=ORG_POOL)
+            org.commit()
+
     def check_users_pool(self):
         orgs_count = LeastCommonCounter()
 
