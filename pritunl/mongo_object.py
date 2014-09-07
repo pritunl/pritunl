c6d65a8f1a2f58771bc5f68c8122987cadb0b813
Zachary Huff <zach.huff.386@gmail.com>
2014-09-07 15:47:47 -0400

    Remove json patch from mongo_object

--- pritunl/mongo_object.py
+++ pritunl/mongo_object.py
@@ -7,18 +7,6 @@ import bson
 import json
 import os
 
-class JSONEncoderPatched(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (MongoDict, MongoList)):
-            return obj.data
-        raise TypeError(repr(obj) + ' is not JSON serializable')
-dumps_orig = json.dumps
-def dumps_patched(*args, **kwargs):
-    if not kwargs.get('cls'):
-        kwargs['cls'] = JSONEncoderPatched
-    return dumps_orig(*args, **kwargs)
-json.dumps = dumps_patched
-
 class MongoObject(object):
     fields = set()
     fields_default = {}
