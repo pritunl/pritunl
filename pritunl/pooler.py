d0182ec2a605822dbd28d0c731f5e452c5aa7295
Zachary Huff <zach.huff.386@gmail.com>
2014-09-07 23:40:05 -0400

    Remove check_user_pool from pooler

--- pritunl/pooler.py
+++ pritunl/pooler.py
@@ -27,38 +27,3 @@ class Pooler(object):
         for _ in xrange(ORG_POOL_SIZE - org_pool_count):
             org = Organization.new_org(type=ORG_POOL)
             org.commit()
-
-    def check_users_pool(self):
-        orgs_count = LeastCommonCounter()
-
-        org_ids = mongo.get_collection('organizations').find({}, {
-            '_id': True,
-        }).distinct('_id')
-
-        for org_id in org_ids:
-            orgs_count[str(org_id)] = 0
-
-        pools = mongo.get_collection('users').aggregate([
-            {'$match': {
-                'type': CERT_CLIENT_POOL,
-            }},
-            {'$project': {
-                'org_id': 1,
-            }},
-            {'$group': {
-                '_id': '$org_id',
-                'count': {'$sum': 1},
-            }},
-        ])
-
-        for pool in pools['result']:
-            orgs_count[pool['_id']] += pool['count']
-
-        for org_id, count in orgs_count.least_common():
-            if count >= USER_POOL_SIZE:
-                break
-
-            org = Organization.get_org(id=org_id)
-            for _ in xrange(USER_POOL_SIZE - count):
-                user = org.new_user(type=CERT_CLIENT_POOL)
-                user.commit()
