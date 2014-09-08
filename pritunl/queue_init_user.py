b61634f912b5e62d19346541624433afcf5dd68b
Zachary Huff <zach.huff.386@gmail.com>
2014-09-08 02:54:00 -0400

    Add queue_init_user

---
+++ pritunl/queue_init_user.py
@@ -0,0 +1,36 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.queue import Queue, add_queue
+from pritunl.event import Event
+from pritunl import app_server
+import logging
+import time
+
+logger = logging.getLogger(APP_NAME)
+
+class QueueInitUser(Queue):
+    fields = {
+        'org_doc',
+        'user_doc',
+    } | Queue.fields
+
+    def __init__(self, org_doc=None, user_doc=None, **kwargs):
+        Queue.__init__(self, **kwargs)
+        self.type = 'init_user'
+
+        if org_doc is not None:
+            self.org_doc = org_doc
+        if user_doc is not None:
+            self.user_doc = user_doc
+
+    def task(self):
+        from pritunl.user import User
+        from pritunl.organization import Organization
+
+        org = Organization(doc=self.org_doc)
+        user = User(org=org, doc=self.user_doc)
+        user.initialize()
+        user.commit()
+
+add_queue('init_user', QueueInitUser)
