74392bb6c0f78e6cad0a6ecc4db011d45c0134ab
Zachary Huff <zach.huff.386@gmail.com>
2014-09-08 02:49:02 -0400

    Add claim_user to pooler_user

--- pritunl/pooler_user.py
+++ pritunl/pooler_user.py
@@ -17,6 +17,9 @@ import collections
 logger = logging.getLogger(APP_NAME)
 
 class PoolerUser(object):
+    def __init__(self, org=None):
+        self.org_id = org.id
+
     @static_property
     def collection(cls):
         return mongo.get_collection('users')
@@ -24,6 +27,28 @@ class PoolerUser(object):
     @static_property
     def org_collection(cls):
         return mongo.get_collection('organizations')
+
+    def claim_user(self, name=None, email=None, type=None, disabled=None):
+        doc_update = {
+            'type': CERT_CLIENT,
+        }
+
+        if name is not None:
+            doc_update['name'] = name
+        if email is not None:
+            doc_update['email'] = email
+        if type is not None:
+            doc_update['type'] = type
+        if disabled is not None:
+            doc_update['disabled'] = disabled
+
+        doc = self.collection.find_and_modify({
+            'org_id': self.org_id,
+            'type': CERT_CLIENT_POOL,
+        }, doc_update)
+
+        if doc:
+            return doc
 
     @classmethod
     def fill_pool(cls):
