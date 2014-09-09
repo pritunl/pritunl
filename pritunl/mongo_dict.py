a641c4b00d8dabae150ee78c1ae3ab7d23423a96
Zachary Huff <zach.huff.386@gmail.com>
2014-09-08 21:28:54 -0400

    Fix mongo_dict

--- pritunl/mongo_dict.py
+++ pritunl/mongo_dict.py
@@ -2,40 +2,124 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 import UserDict
 
-class MongoDict(UserDict.UserDict):
+class MongoDict(object):
     __class__ = dict
 
-    def __init__(self, initdict=None, changed=True, **kwargs):
-        if isinstance(initdict, dict):
-            self.data = initdict
+    def __init__(self, initdict=None, changed=True):
+        if initdict is None:
+            self.data = None
+        elif hasattr(initdict, 'data'):
+            self.data = initdict.data
         else:
-            UserDict.UserDict.__init__(self, initdict, **kwargs)
+            self.data = initdict
         self.changed = changed
 
-    def __setitem__(self, *args, **kwargs):
-        self.changed = True
-        return UserDict.UserDict.__setitem__(self, *args, **kwargs)
+    def __repr__(self):
+        return repr(self.data)
 
-    def __delitem__(self, *args, **kwargs):
-        self.changed = True
-        return UserDict.UserDict.__delitem__(self, *args, **kwargs)
+    def __cmp__(self, dict):
+        if isinstance(dict, UserDict):
+            return cmp(self.data, dict.data)
+        else:
+            return cmp(self.data, dict)
 
-    def clear(self, *args, **kwargs):
-        self.changed = True
-        return UserDict.UserDict.clear(self, *args, **kwargs)
+    __hash__ = None # Avoid Py3k warning
 
-    def update(self, *args, **kwargs):
-        self.changed = True
-        return UserDict.UserDict.update(self, *args, **kwargs)
+    def __len__(self):
+        return len(self.data)
 
-    def setdefault(self, *args, **kwargs):
-        self.changed = True
-        return UserDict.UserDict.setdefault(self, *args, **kwargs)
+    def __getitem__(self, key):
+        if key in self.data:
+            return self.data[key]
+        if hasattr(self.__class__, "__missing__"):
+            return self.__class__.__missing__(self, key)
+        raise KeyError(key)
 
-    def pop(self, *args, **kwargs):
+    def __setitem__(self, key, item):
         self.changed = True
-        return UserDict.UserDict.pop(self, *args, **kwargs)
+        self.data[key] = item
 
-    def popitem(self, *args, **kwargs):
+    def __delitem__(self, key):
         self.changed = True
-        return UserDict.UserDict.popitem(self, *args, **kwargs)
+        del self.data[key]
+
+    def clear(self):
+        self.changed = True
+        self.data.clear()
+
+    def copy(self):
+        if self.__class__ is UserDict:
+            return UserDict(self.data.copy())
+        import copy
+        data = self.data
+        try:
+            self.data = {}
+            c = copy.copy(self)
+        finally:
+            self.data = data
+        c.update(self)
+        return c
+
+    def keys(self):
+        return self.data.keys()
+
+    def items(self):
+        return self.data.items()
+
+    def iteritems(self):
+        return self.data.iteritems()
+
+    def iterkeys(self):
+        return self.data.iterkeys()
+
+    def itervalues(self):
+        return self.data.itervalues()
+
+    def values(self):
+        return self.data.values()
+
+    def has_key(self, key):
+        return key in self.data
+
+    def update(self, dict=None, **kwargs):
+        self.changed = True
+        if dict is None:
+            pass
+        elif isinstance(dict, UserDict):
+            self.data.update(dict.data)
+        elif isinstance(dict, type({})) or not hasattr(dict, 'items'):
+            self.data.update(dict)
+        else:
+            for k, v in dict.items():
+                self[k] = v
+        if len(kwargs):
+            self.data.update(kwargs)
+
+    def get(self, key, failobj=None):
+        if key not in self:
+            return failobj
+        return self[key]
+
+    def setdefault(self, key, failobj=None):
+        self.changed = True
+        if key not in self:
+            self[key] = failobj
+        return self[key]
+
+    def pop(self, key, *args):
+        self.changed = True
+        return self.data.pop(key, *args)
+
+    def popitem(self):
+        self.changed = True
+        return self.data.popitem()
+
+    def __contains__(self, key):
+        return key in self.data
+
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        d = cls()
+        for key in iterable:
+            d[key] = value
+        return d
