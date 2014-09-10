157a48184bcb452c1bccef315559fbadf575d505
Zachary Huff <zach.huff.386@gmail.com>
2014-09-10 14:52:07 -0400

    Add task_clean_users

---
+++ pritunl/task_clean_users.py
@@ -0,0 +1,33 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.task import Task, add_task
+import logging
+import time
+import mongo
+
+logger = logging.getLogger(APP_NAME)
+
+class TaskCleanUsers(Task):
+    type = 'clean_users'
+
+    @static_property
+    def collection(cls):
+        return mongo.get_collection('users')
+
+    @static_property
+    def org_collection(cls):
+        return mongo.get_collection('organizations')
+
+    def task(self):
+        # Remove users from orgs that dont exists
+        org_ids = self.org_collection.find({}, {
+            '_id',
+        }).distinct('_id')
+        org_ids = [str(x) for x in org_ids]
+
+        self.collection.remove({
+            'org_id': {'$nin': org_ids},
+        })
+
+add_task('clean_users', TaskCleanUsers, hours=5, minutes=17)
