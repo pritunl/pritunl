bf99f3364b83401caf5357515f801ad079d62400
Zachary Huff <zach.huff.386@gmail.com>
2014-09-10 22:23:18 -0400

    Improve fill_pool algorithm

--- pritunl/pooler_user.py
+++ pritunl/pooler_user.py
@@ -40,35 +40,51 @@ class PoolerUser(object):
     @classmethod
     def fill_pool(cls):
         from pritunl.organization import Organization
+
+        orgs = {}
         orgs_count = LeastCommonCounter()
+        type_to_size = {
+            CERT_CLIENT_POOL: USER_POOL_SIZE,
+            CERT_SERVER_POOL: SERVER_USER_POOL_SIZE,
+        }
 
-        org_ids = cls.org_collection.find({}, {
-            '_id': True,
-        }).distinct('_id')
-
-        for org_id in org_ids:
-            orgs_count[str(org_id)] = 0
+        for org in Organization.iter_orgs(type=None):
+            orgs[org.id] = org
+            orgs_count[str(org.id), CERT_CLIENT_POOL] = 0
+            orgs_count[str(org.id), CERT_SERVER_POOL] = 0
 
         pools = cls.collection.aggregate([
             {'$match': {
-                'type': CERT_CLIENT_POOL,
+                'type': {'$in': [CERT_CLIENT_POOL, CERT_SERVER_POOL]},
             }},
             {'$project': {
-                'org_id': 1,
+                'org_id': True,
+                'type': True,
             }},
             {'$group': {
-                '_id': '$org_id',
+                '_id': {
+                    'org_id': '$org_id',
+                    'type': '$type',
+                },
                 'count': {'$sum': 1},
             }},
         ])
 
         for pool in pools['result']:
-            orgs_count[pool['_id']] += pool['count']
+            orgs_count[pool['_id']['org_id'], pool['_id']['type']] += pool[
+                'count']
 
-        for org_id, count in orgs_count.least_common():
-            if count >= USER_POOL_SIZE:
+        new_users = []
+
+        for org_id_user_type, count in orgs_count.least_common():
+            org_id, user_type = org_id_user_type
+            pool_size = type_to_size[user_type]
+
+            if count >= pool_size:
                 break
 
-            org = Organization.get_org(id=org_id)
-            for _ in xrange(USER_POOL_SIZE - count):
-                user = org.new_user(type=CERT_CLIENT_POOL)
+            org = orgs[org_id]
+            new_users.append([(org, user_type)] * (pool_size - count))
+
+        for org, user_type in utils.roundrobin(*new_users):
+            org.new_user(type=user_type, block=False)
