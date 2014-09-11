148017ab1b158e9313b876e000f336eb0c3b4878
Zachary Huff <zach.huff.386@gmail.com>
2014-09-11 04:16:17 -0400

    Add pooler task

---
+++ pritunl/task_pooler.py
@@ -0,0 +1,19 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.task import Task, add_task
+from pritunl.pooler_user import PoolerUser
+from pritunl.pooler_org import PoolerOrg
+import logging
+import time
+
+logger = logging.getLogger(APP_NAME)
+
+class TaskPooler(Task):
+    type = 'pooler'
+
+    def task(self):
+        PoolerOrg.fill_pool()
+        PoolerUser.fill_pool()
+
+add_task(TaskPooler, minutes=(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55))
