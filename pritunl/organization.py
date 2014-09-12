e0e85db6a0d038efbd9e5d24211b9e927c07d975
Zachary Huff <zach.huff.386@gmail.com>
2014-09-12 03:29:14 -0400

    Add priority to queue_init org

--- pritunl/organization.py
+++ pritunl/organization.py
@@ -84,10 +84,10 @@ class Organization(MongoObject):
         self.ca_private_key = ca_user.private_key
         self.ca_certificate = ca_user.certificate
 
-    def queue_initialize(self, block):
+    def queue_initialize(self, block, priority=NORMAL):
         if self.type != ORG_POOL:
             raise TypeError('Only pool orgs can be queued')
-        queue = QueueInitOrgPooled(org_doc=self.export())
+        queue = QueueInitOrgPooled(org_doc=self.export(), priority=priority)
         queue.start(block=block)
 
     def commit(self, *args, **kwargs):
