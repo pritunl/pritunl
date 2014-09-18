5a67b3150ffb93833ddd2802979e9f099b754919
Zachary Huff <zach.huff.386@gmail.com>
2014-09-18 18:16:55 -0400

    Add listener

---
+++ pritunl/listener.py
@@ -0,0 +1,38 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.queue import Queue
+from pritunl.messenger import Messenger
+import pritunl.mongo as mongo
+import pymongo
+import random
+import bson
+import datetime
+import threading
+import time
+import collections
+
+_channels = collections.defaultdict(set)
+
+def add_listener(channel, callback):
+    _channels[channel].add(callback)
+
+def listener_thread():
+    messenger = Messenger()
+
+    while True:
+        try:
+            for msg in messenger.subscribe(_channels.keys()):
+                for listener in _channels[msg['channel']]:
+                    try:
+                        listener(msg)
+                    except:
+                        logger.exception('Error in listener callback')
+        except:
+            logger.exception('Error in listener watch thread.')
+            time.sleep(0.3)
+
+def start():
+    thread = threading.Thread(target=listener_thread)
+    thread.daemon = True
+    thread.start()
