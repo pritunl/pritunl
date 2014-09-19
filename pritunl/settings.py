037278fcdc6a9628bc4c26d006be34e86cd87167
Zachary Huff <zach.huff.386@gmail.com>
2014-09-19 19:06:24 -0400

    Remove settings_group from settings

--- pritunl/settings.py
+++ pritunl/settings.py
@@ -2,40 +2,6 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
 
-class SettingsGroup(object):
-    group = None
-    fields = set()
-
-    def __init__(self):
-        self.changed = set()
-
-    def __setattr__(self, name, value):
-        if name != 'fields' and name in self.fields:
-            self.changed.add(name)
-        object.__setattr__(self, name, value)
-
-    def __getattr__(self, name):
-        if name in self.fields:
-            return self.fields[name]
-        raise AttributeError(
-            'SettingsGroup instance has no attribute %r' % name)
-
-    def get_commit_doc(self, all_fields):
-        doc = {
-            '_id': self.group,
-        }
-
-        for field in self.fields if all_fields else self.changed:
-            doc[field] = getattr(self, field)
-
-        return doc
-
-class SettingsTest(SettingsGroup):
-    group = 'test'
-    fields = {
-        'option': 'default',
-    }
-
 class Settings(object):
     @cached_static_property
     def collection(cls):
