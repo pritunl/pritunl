450e1bacfbcd88ac6108dc2eb9ca826bcd446c1b
Zachary Huff <zach.huff.386@gmail.com>
2014-09-19 23:06:59 -0400

    Use transaction for all commits in settings

--- pritunl/settings.py
+++ pritunl/settings.py
@@ -24,51 +24,34 @@ class Settings(object):
                 setattr(group, field, val)
 
     def commit(self, all_fields=False):
+        docs = []
         has_docs = False
+        messenger = Messenger()
+        transaction = MongoTransaction()
+        collection = transaction.collection(
+            self.collection.collection_name)
 
-        if all_fields:
-            bulk = self.collection.initialize_unordered_bulk_op()
+        for group in dir(self):
+            if group[0] == '_' or group in SETTINGS_RESERVED:
+                continue
+            doc = getattr(self, group).get_commit_doc(all_fields)
 
-            for group in dir(self):
-                if group[0] == '_' or group in SETTINGS_RESERVED:
-                    continue
-                doc = getattr(self, group).get_commit_doc(all_fields)
-
-                bulk.find({
+            if doc:
+                has_docs = True
+                collection.bulk().find({
                     '_id': doc['_id'],
                 }).upsert().update({
                     '$set': doc,
                 })
+                docs.append(doc)
 
-            bulk.execute()
-        else:
-            docs = []
-            messenger = Messenger()
-            transaction = MongoTransaction()
-            collection = transaction.collection(
-                self.collection.collection_name)
+        messenger.publish('setting', docs, transaction=transaction)
 
-            for group in dir(self):
-                if group[0] == '_' or group in SETTINGS_RESERVED:
-                    continue
-                doc = getattr(self, group).get_commit_doc(False)
+        if not has_docs:
+            return
 
-                if doc:
-                    has_docs = True
-                    collection.bulk().find({
-                        '_id': doc['_id'],
-                    }).upsert().update({
-                        '$set': doc,
-                    })
-                    docs.append(doc)
-
-            messenger.publish('setting', docs, transaction=transaction)
-
-            if not has_docs:
-                return
-
-            collection.bulk_execute()
-            transaction.commit()
+        collection.bulk_execute()
+        transaction.commit()
 
     def start(self):
         listener.add_listener('setting', self.on_msg)
