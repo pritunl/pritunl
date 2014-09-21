41a1491c78aaf2e3abe6c40c1680268e612348f9
Zachary Huff <zach.huff.386@gmail.com>
2014-09-20 23:41:40 -0400

    Update test_settings

--- pritunl/settings_test.py
+++ pritunl/settings_test.py
@@ -1,8 +1,9 @@
 from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
-from pritunl.settings_group import SettingsGroup
+from pritunl.settings_group import SettingsGroup, add_to_settings
 
+@add_to_settings
 class SettingsTest(SettingsGroup):
     group = 'test'
     fields = {
