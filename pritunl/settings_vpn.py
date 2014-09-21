e230fb4af031f641b9e68953ec449cb1d7315bdb
Zachary Huff <zach.huff.386@gmail.com>
2014-09-21 04:34:35 -0400

    Add vpn settings

---
+++ pritunl/settings_vpn.py
@@ -0,0 +1,13 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.settings_group import SettingsGroup
+
+class SettingsVpn(SettingsGroup):
+    group = 'vpn'
+    fields = {
+        'default_dh_param_bits': 1536,
+        'log_lines': 1024,
+        'status_update_rate': 5,
+        'http_request_timeout': 10,
+    }
