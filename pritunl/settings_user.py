e20c180aa30e9d1bf4d12e353ae8d94b2a9dbf1f
Zachary Huff <zach.huff.386@gmail.com>
2014-09-21 04:34:23 -0400

    Add user settings

---
+++ pritunl/settings_user.py
@@ -0,0 +1,12 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.settings_group import SettingsGroup
+
+class SettingsUser(SettingsGroup):
+    group = 'user'
+    fields = {
+        'otp_secret_len': 16,
+        'cert_key_bits': 4096,
+        'otp_cache_ttl': 43200,
+    }
