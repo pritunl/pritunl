3c4e49eaeac18fc7eac99b20e1e11888d00d2eed
Zachary Huff <zach.huff.386@gmail.com>
2014-09-25 17:45:09 -0400

    Add host list view

---
+++ www/views/hostsList.js
@@ -0,0 +1,44 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'collections/host',
+  'views/list',
+  'views/alert',
+  'views/hostsListItem',
+  'text!templates/hostsList.html'
+], function($, _, Backbone, HostCollection, ListView, AlertView,
+    HostsListItemView, hostsListTemplate) {
+  'use strict';
+  var HostsListView = ListView.extend({
+    className: 'hosts-list',
+    listContainer: '.hosts-list-container',
+    template: _.template(hostsListTemplate),
+    listErrorMsg: 'Failed to load hosts, server error occurred.',
+    events: {},
+    initialize: function() {
+      this.collection = new HostCollection();
+      this.listenTo(window.events, 'hosts_updated', this.update);
+      HostsListView.__super__.initialize.call(this);
+    },
+    buildItem: function(model) {
+      var modelView = new HostsListItemView({
+        model: model
+      });
+      this.listenTo(modelView, 'select', this.onSelect);
+      return modelView;
+    },
+    resetItems: function(views) {
+      if (!views.length) {
+        this.$('.servers-attach-org').attr('disabled', 'disabled');
+        this.$('.no-servers').slideDown(250);
+      }
+      else {
+        this.$('.servers-attach-org').removeAttr('disabled');
+        this.$('.no-servers').slideUp(250);
+      }
+    }
+  });
+
+  return HostsListView;
+});
