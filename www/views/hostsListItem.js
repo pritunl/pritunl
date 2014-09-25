4918c1d6c376db004a1f0f9b834cdb9f25984488
Zachary Huff <zach.huff.386@gmail.com>
2014-09-25 17:45:27 -0400

    Add host list item view

---
+++ www/views/hostsListItem.js
@@ -0,0 +1,119 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/alert',
+  'views/modalHostSettings',
+  'text!templates/hostsListItem.html'
+], function($, _, Backbone, AlertView, ModalHostSettingsView,
+    hostsListItemTemplate) {
+  'use strict';
+  var HostListItemView = Backbone.View.extend({
+    className: 'host',
+    template: _.template(hostsListItemTemplate),
+    events: {
+      'click .host-title a': 'onSettings',
+    },
+    initialize: function() {
+      setTimeout(function() {
+        this.uptimer = setInterval((this._updateTime).bind(this), 1000);
+      }.bind(this), 1000);
+    },
+    deinitialize: function() {
+      clearInterval(this.uptimer);
+    },
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      this.update();
+      return this;
+    },
+    update: function() {
+      this.$('.host-title a').text(this.model.get('name'));
+      this.$('.host-status .status-text').text(
+        this.model.get('status').charAt(0).toUpperCase() +
+        this.model.get('status').slice(1));
+      if (this.model.get('uptime')) {
+        this.$('.host-uptime .status-text').text(
+          window.formatUptime(this.model.get('uptime')));
+      }
+      else {
+        this.$('.host-uptime .status-text').text('-');
+      }
+      if (this.model.get('user_count') === 0) {
+        this.$('.host-users .status-num').text('-/-');
+      }
+      else {
+        this.$('.host-users .status-num').text(this.model.get(
+          'users_online') + '/' + this.model.get('user_count'));
+      }
+
+      if (this.model.get('status') === 'offline') {
+        this.$('.host-del').removeAttr('disabled');
+      }
+      else {
+        this.$('.host-del').attr('disabled', 'disabled');
+      }
+    },
+    onSettings: function() {
+      this.$('.host-title a').addClass('disabled');
+
+      var modal = new ModalHostSettingsView({
+        model: this.model.clone()
+      });
+      this.listenToOnce(modal, 'applied', function() {
+        var alertView = new AlertView({
+          type: 'warning',
+          message: 'Successfully saved host settings.',
+          dismissable: true
+        });
+        $('.alerts-container').append(alertView.render().el);
+        this.addView(alertView);
+      }.bind(this));
+      this.addView(modal);
+
+      this.$('.host-title a').removeClass('disabled');
+    },
+    onDelete: function() {
+      var modal = new ModalDeleteHostView({
+        model: this.model.clone()
+      });
+      this.listenToOnce(modal, 'applied', function() {
+        var alertView = new AlertView({
+          type: 'warning',
+          message: 'Successfully deleted host.',
+          dismissable: true
+        });
+        $('.alerts-container').append(alertView.render().el);
+        this.addView(alertView);
+      }.bind(this));
+      this.addView(modal);
+    },
+    _updateTime: function() {
+      if (!this.model.get('uptime')) {
+        return;
+      }
+      this.model.set({
+        uptime: this.model.get('uptime') + 1
+      });
+      this.$('.host-uptime .status-text').text(
+        window.formatUptime(this.model.get('uptime')));
+    },
+    onToggleHidden: function(evt) {
+      if (!evt.ctrlKey && !evt.shiftKey) {
+        return;
+      }
+      if (this.$el.hasClass('show-hidden')) {
+        this.$('.toggle-hidden').removeClass('label-success');
+        this.$('.toggle-hidden').addClass('label-primary');
+        this.$el.removeClass('show-hidden');
+      }
+      else {
+        this.$('.toggle-hidden').addClass('label-success');
+        this.$('.toggle-hidden').removeClass('label-primary');
+        this.$el.addClass('show-hidden');
+      }
+    }
+  });
+
+  return HostListItemView;
+});
