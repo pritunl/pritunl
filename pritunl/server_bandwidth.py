3bbe45cf004ae7f1520434390462b9b32ec80f69
Zachary Huff <zach.huff.386@gmail.com>
2014-09-26 00:57:11 -0400

    Add random data to server_bandwidth

--- pritunl/server_bandwidth.py
+++ pritunl/server_bandwidth.py
@@ -1,7 +1,12 @@
 from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
+from pritunl import app_server
 import pritunl.mongo as mongo
+import pymongo
+import os
+import json
+import random
 
 class ServerBandwidth(object):
     def __init__(self, server_id):
@@ -141,87 +146,81 @@ class ServerBandwidth(object):
         return data
 
     def get_period_random(self, period):
-        # Generate random bandwidth data for demo and write to file
-        import json
-        import random
         data = {}
         date = datetime.datetime.utcnow()
         date -= datetime.timedelta(microseconds=date.microsecond,
             seconds=date.second)
-        periods = (period,) if period else ('1m', '5m', '30m', '2h', '1d')
 
-        for period in periods:
-            if period == '1m':
-                date_end = date
-                date_cur = date_end - datetime.timedelta(hours=6)
-                date_step = datetime.timedelta(minutes=1)
-                bytes_recv = 700000
-                bytes_sent = 700000
-                bandwidth_rand = lambda x: random.randint(
-                    max(x - 50000, 0), max(x + 50000, 0))
-            elif period == '5m':
-                date_end = date - datetime.timedelta(minutes=date.minute % 5)
-                date_cur = date_end - datetime.timedelta(days=1)
-                date_step = datetime.timedelta(minutes=5)
-                bytes_recv = 3500000
-                bytes_sent = 3500000
-                bandwidth_rand = lambda x: random.randint(
-                    max(x - 250000, 0), max(x + 250000, 0))
-            elif period == '30m':
-                date_end = date - datetime.timedelta(minutes=date.minute % 30)
-                date_cur = date_end - datetime.timedelta(days=7)
-                date_step = datetime.timedelta(minutes=30)
-                bytes_recv = 21000000
-                bytes_sent = 21000000
-                bandwidth_rand = lambda x: random.randint(
-                    max(x - 2000000, 0), max(x + 2000000, 0))
-            elif period == '2h':
-                date_end = date - datetime.timedelta(minutes=date.minute,
-                    hours=date.hour % 2)
-                date_cur = date_end - datetime.timedelta(days=30)
-                date_step = datetime.timedelta(hours=2)
-                bytes_recv = 84000000
-                bytes_sent = 84000000
-                bandwidth_rand = lambda x: random.randint(
-                    max(x - 2000000, 0), max(x + 2000000, 0))
-            elif period == '1d':
-                date_end = date - datetime.timedelta(minutes=date.minute,
-                    hours=date.hour)
-                date_cur = date_end - datetime.timedelta(days=365)
-                date_step = datetime.timedelta(days=1)
-                bytes_recv = 1008000000
-                bytes_sent = 1008000000
-                bandwidth_rand = lambda x: random.randint(
-                    max(x - 100000000, 0), max(x + 100000000, 0))
+        if period == '1m':
+            date_end = date
+            date_cur = date_end - datetime.timedelta(hours=6)
+            date_step = datetime.timedelta(minutes=1)
+            bytes_recv = 700000
+            bytes_sent = 700000
+            bandwidth_rand = lambda x: random.randint(
+                max(x - 50000, 0), max(x + 50000, 0))
+        elif period == '5m':
+            date_end = date - datetime.timedelta(minutes=date.minute % 5)
+            date_cur = date_end - datetime.timedelta(days=1)
+            date_step = datetime.timedelta(minutes=5)
+            bytes_recv = 3500000
+            bytes_sent = 3500000
+            bandwidth_rand = lambda x: random.randint(
+                max(x - 250000, 0), max(x + 250000, 0))
+        elif period == '30m':
+            date_end = date - datetime.timedelta(minutes=date.minute % 30)
+            date_cur = date_end - datetime.timedelta(days=7)
+            date_step = datetime.timedelta(minutes=30)
+            bytes_recv = 21000000
+            bytes_sent = 21000000
+            bandwidth_rand = lambda x: random.randint(
+                max(x - 2000000, 0), max(x + 2000000, 0))
+        elif period == '2h':
+            date_end = date - datetime.timedelta(minutes=date.minute,
+                hours=date.hour % 2)
+            date_cur = date_end - datetime.timedelta(days=30)
+            date_step = datetime.timedelta(hours=2)
+            bytes_recv = 84000000
+            bytes_sent = 84000000
+            bandwidth_rand = lambda x: random.randint(
+                max(x - 2000000, 0), max(x + 2000000, 0))
+        elif period == '1d':
+            date_end = date - datetime.timedelta(minutes=date.minute,
+                hours=date.hour)
+            date_cur = date_end - datetime.timedelta(days=365)
+            date_step = datetime.timedelta(days=1)
+            bytes_recv = 1008000000
+            bytes_sent = 1008000000
+            bandwidth_rand = lambda x: random.randint(
+                max(x - 100000000, 0), max(x + 100000000, 0))
 
-            data_p = {
-                'received': [],
-                'received_total': 0,
-                'sent': [],
-                'sent_total': 0,
-            }
-            data[period] = data_p
+        data = {
+            'received': [],
+            'received_total': 0,
+            'sent': [],
+            'sent_total': 0,
+        }
 
-            while date_cur < date_end:
-                date_cur += date_step
+        while date_cur < date_end:
+            date_cur += date_step
 
-                timestamp = int(date_cur.strftime('%s'))
-                bytes_recv = bandwidth_rand(bytes_recv)
-                bytes_sent = bandwidth_rand(bytes_sent)
+            timestamp = int(date_cur.strftime('%s'))
+            bytes_recv = bandwidth_rand(bytes_recv)
+            bytes_sent = bandwidth_rand(bytes_sent)
 
-                data_p['received'].append((timestamp, bytes_recv))
-                data_p['received_total'] += bytes_recv
-                data_p['sent'].append((timestamp, bytes_sent))
-                data_p['sent_total'] += bytes_sent
+            data['received'].append((timestamp, bytes_recv))
+            data['received_total'] += bytes_recv
+            data['sent'].append((timestamp, bytes_sent))
+            data['sent_total'] += bytes_sent
 
-        if len(periods) == 1:
-            path = os.path.join(app_server.data_path,
-                'demo_bandwidth_%s' % periods[0])
-            with open(path, 'w') as demo_file:
-                demo_file.write(json.dumps(data[periods[0]]))
-            return data[periods[0]]
-        else:
-            path = os.path.join(app_server.data_path, 'demo_bandwidth')
-            with open(path, 'w') as demo_file:
-                demo_file.write(json.dumps(data))
-            return data
+        return data
+
+    def write_periods_random(self):
+        data = {}
+        for period in ('1m', '5m', '30m', '2h', '1d'):
+            data[period] = self.get_period_random(period)
+
+        path = os.path.join(app_server.temp_path, 'demo_bandwidth')
+        with open(path, 'w') as demo_file:
+            demo_file.write(json.dumps(data))
+        return data
