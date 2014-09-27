d8087f7991b071fd6fb0971641b23cdf78196eae
Zachary Huff <zach.huff.386@gmail.com>
2014-09-26 22:07:29 -0400

    Fix handlers import

--- pritunl/handlers/__init__.py
+++ pritunl/handlers/__init__.py
@@ -4,4 +4,4 @@ import importlib
 for module_name in os.listdir(os.path.dirname(__file__)):
     if module_name == '__init__.py' or module_name[-3:] != '.py':
         continue
-    __import__('pritunl.handlers.' + module_name[:-3], locals(), globals())
+    __import__('pritunl.handlers.' + module_name[:-3])
