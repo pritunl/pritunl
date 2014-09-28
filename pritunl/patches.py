1b8e00d3125f4eaf81653621c04ccb1f2d841365
Zachary Huff <zach.huff.386@gmail.com>
2014-09-28 08:54:53 -0400

    Time db calls

--- pritunl/patches.py
+++ pritunl/patches.py
@@ -7,6 +7,7 @@ import json
 import pymongo
 import random
 import flask
+import time
 
 class JSONEncoderPatched(json.JSONEncoder):
     def default(self, obj):
@@ -34,52 +35,89 @@ insert_orig = pymongo.collection.Collection.insert
 def insert(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.write_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return insert_orig(self, *args, **kwargs)
+    val = insert_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
+
 pymongo.collection.Collection.insert = insert
 
 update_orig = pymongo.collection.Collection.update
 def update(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.write_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return update_orig(self, *args, **kwargs)
+    val = update_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
 pymongo.collection.Collection.update = update
 
 remove_orig = pymongo.collection.Collection.remove
 def remove(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.write_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return remove_orig(self, *args, **kwargs)
+    val = remove_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
 pymongo.collection.Collection.remove = remove
 
 find_orig = pymongo.collection.Collection.find
 def find(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.query_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return find_orig(self, *args, **kwargs)
+    val = find_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
 pymongo.collection.Collection.find = find
 
 find_and_modify_orig = pymongo.collection.Collection.find_and_modify
 def find_and_modify(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.write_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return find_and_modify_orig(self, *args, **kwargs)
+    val = find_and_modify_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
 pymongo.collection.Collection.find_and_modify = find_and_modify
 
 aggregate_orig = pymongo.collection.Collection.aggregate
 def aggregate(self, *args, **kwargs):
     if flask.ctx.has_request_context():
         flask.g.query_count += 1
+        start = time.time()
+    else:
+        start = None
     if RANDOM_ERROR_RATE and random.random() <= RANDOM_ERROR_RATE:
         raise random.choice(_mongo_errors)('Test error')
-    return aggregate_orig(self, *args, **kwargs)
+    val = aggregate_orig(self, *args, **kwargs)
+    if start:
+        flask.g.query_time += (time.time() - start)
+    return val
 pymongo.collection.Collection.aggregate = aggregate
