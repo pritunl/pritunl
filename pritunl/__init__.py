fe735414f3b1495b9bbee0f83d7575a0d53aa336
Zachary Huff <zach.huff.386@gmail.com>
2014-09-28 08:50:23 -0400

    Fix init

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -1,10 +1,7 @@
 from pritunl.constants import *
-import pritunl.patches
+
 __title__ = APP_NAME
 __version__ = '0.10.12'
 __author__ = 'Pritunl'
 __license__ = 'Custom'
 __copyright__ = 'Copyright 2013-2014 Pritunl'
-
-from pritunl.app_server import AppServer
-app_server = AppServer()
