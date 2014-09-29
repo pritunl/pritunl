79138c666d46fe35a8ce323374f2b27d24b52e31
Zachary Huff <zach.huff.386@gmail.com>
2014-09-29 06:57:43 -0400

    Fix bulk find in server

--- pritunl/server/bandwidth.py
+++ pritunl/server/bandwidth.py
@@ -82,7 +82,7 @@ class ServerBandwidth(object):
 
             if bulk:
                 bulk.find(spec).upsert().update(doc)
-                bulk.fint(rem_spec).remove()
+                bulk.find(rem_spec).remove()
             else:
                 self.collection.update(spec, doc, upsert=True)
                 self.collection.remove(rem_spec)
