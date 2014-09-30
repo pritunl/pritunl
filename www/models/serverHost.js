d5e16e0e154c7fd598fc64dc289b60c0e35d7b81
Zachary Huff <zach.huff.386@gmail.com>
2014-09-29 23:30:47 -0400

    Add server host model

---
+++ www/models/serverHost.js
@@ -0,0 +1,25 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var ServerHostModel = Backbone.Model.extend({
+    defaults: {
+      'id': null,
+      'server': null,
+      'name': null
+    },
+    url: function() {
+      var url = '/server/' + this.get('server') + '/host';
+
+      if (this.get('id')) {
+        url += '/' + this.get('id');
+      }
+
+      return url;
+    }
+  });
+
+  return ServerHostModel;
+});
