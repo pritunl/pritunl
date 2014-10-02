19e9aa0a1dc5c9572882c7304eeaf0c416d8e566
Zachary Huff <zach.huff.386@gmail.com>
2014-10-01 22:58:44 -0400

    Add settings group_file

---
+++ pritunl/settings/group_file.py
@@ -0,0 +1,32 @@
+from pritunl.settings.group_base import SettingsGroupBase
+
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+
+import json
+import os
+
+class SettingsGroupFile(SettingsGroupBase):
+    type = 'file'
+    path = None
+
+    def load(self):
+        if not os.path.isfile(self.path):
+            return
+
+        with open(self.path, 'r') as settings_file:
+            doc = json.loads(settings_file.read())
+
+        for field, value in doc.items():
+            setattr(self, field, value)
+
+    def commit(self):
+        doc = {}
+
+        for field in self.fields:
+            if hasattr(self, field):
+                doc[field] = getattr(self, field)
+
+        with open(self.path, 'w') as settings_file:
+            settings_file.write(json.dumps(doc, indent=4))
