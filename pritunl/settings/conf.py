9b3bad5bb3322be13de32d37127fb4f0053ae632
Zachary Huff <zach.huff.386@gmail.com>
2014-10-02 01:25:45 -0400

    Add conf settings

---
+++ pritunl/settings/conf.py
@@ -0,0 +1,22 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl.settings.group_file import SettingsGroupFile
+
+class SettingsConf(SettingsGroupFile):
+    group = 'conf'
+    path = 'test.conf'
+    fields = {
+        'debug': False,
+        'ssl': True,
+        'static_cache': True,
+        'port': 9700,
+        'pooler': True,
+        'log_path': '/var/log/pritunl.log',
+        'www_path': '/usr/share/pritunl/www',
+        'server_cert_path': '/etc/pritunl.crt',
+        'server_key_path': '/etc/pritunl.key',
+        'bind_addr': '0.0.0.0',
+        'mongodb_url': 'mongodb://localhost:27017/pritunl',
+        'mongodb_collection_prefix': None,
+    }
