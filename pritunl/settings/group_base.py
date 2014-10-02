b12914c3cc4acaf9b765abdb9d9e130492aa7eb9
Zachary Huff <zach.huff.386@gmail.com>
2014-10-01 22:56:48 -0400

    Add group_base

---
+++ pritunl/settings/group_base.py
@@ -0,0 +1,13 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+
+class SettingsGroupBase(object):
+    group = None
+    fields = {}
+
+    def __getattr__(self, name):
+        if name in self.fields:
+            return self.fields[name]
+        raise AttributeError('%s instance has no attribute %r' % (
+            self.__class__.__name__, name))
