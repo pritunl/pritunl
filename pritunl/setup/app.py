b0fe759765a92065f066236f35b7670257fc0339
Zachary Huff <zach.huff.386@gmail.com>
2014-10-02 22:08:31 -0400

    Update setup logger

--- pritunl/setup/app.py
+++ pritunl/setup/app.py
@@ -2,9 +2,6 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
 from pritunl.settings import settings
-from pritunl.app import app
-
-import flask
 
 def setup_app():
     if settings.conf.debug and settings.conf.ssl:
--- pritunl/setup/logger.py
+++ pritunl/setup/logger.py
@@ -2,11 +2,12 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
 from pritunl.settings import settings
-from pritunl import logger
 
 import logging
 
 def setup_logger():
+    from pritunl.app import app
+    from pritunl import logger
     if settings.conf.log_path:
         logger.log_handler = logging.handlers.RotatingFileHandler(
             settings.conf.log_path, maxBytes=1000000, backupCount=1)
@@ -24,3 +25,7 @@ def setup_logger():
         '%(message)s'))
 
     logger.logger.addHandler(logger.log_handler)
+
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.addFilter(logger.log_filter)
+    app.logger.addHandler(logger.log_handler)
