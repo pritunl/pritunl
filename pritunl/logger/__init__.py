7c9cadd228e4266c298859ce6bfce5ddf37d5de0
Zachary Huff <zach.huff.386@gmail.com>
2014-10-02 21:19:16 -0400

    Update logger setup

--- pritunl/logger/__init__.py
+++ pritunl/logger/__init__.py
@@ -9,18 +9,17 @@ from pritunl.settings import settings
 
 import logging
 
-_logger_setup = False
-_logger = logging.getLogger(APP_NAME)
+logger = logging.getLogger(APP_NAME)
 log_filter = None
 log_handler = None
 
 def _log(log_level, log_msg, log_type, **kwargs):
-    if not _logger_setup:
+    if not log_filter or not log_handler:
         raise TypeError('Logger not setup')
     if log_level == 'exception':
-        getattr(_logger, log_level)(log_msg)
+        getattr(logger, log_level)(log_msg)
     else:
-        getattr(_logger, log_level)(log_msg, extra={
+        getattr(logger, log_level)(log_msg, extra={
                 'type': log_type,
                 'data': kwargs,
             })
@@ -42,28 +41,3 @@ def critical(log_msg, log_type=None, **kwargs):
 
 def exception(log_msg, log_type=None, **kwargs):
     _log('exception', log_msg, log_type, **kwargs)
-
-def setup_logger():
-    # TODO move to setup
-    global _logger_setup
-    global log_filter
-    global log_handler
-
-    if settings.conf.log_path:
-        log_handler = logging.handlers.RotatingFileHandler(
-            settings.conf.log_path, maxBytes=1000000, backupCount=1)
-    else:
-        log_handler = logging.StreamHandler()
-
-    log_filter = LogFilter()
-    _logger.addFilter(log_filter)
-
-    _logger.setLevel(logging.DEBUG)
-    log_handler.setLevel(logging.DEBUG)
-
-    log_handler.setFormatter(LogFormatter(
-        '[%(asctime)s][%(levelname)s][%(module)s][%(lineno)d] ' +
-        '%(message)s'))
-
-    _logger.addHandler(log_handler)
-    _logger_setup = True
