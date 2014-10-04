39fe3199eb7e393a5bc3933b8424fc4c0515d7c7
Zachary Huff <zach.huff.386@gmail.com>
2014-10-03 22:52:27 -0400

    Add server host list view

---
+++ www/views/serverHostsList.js
@@ -0,0 +1,46 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'collections/serverHost',
+  'views/list',
+  'views/serverHostsListItem',
+  'text!templates/serverHostsList.html'
+], function($, _, Backbone, ServerHostCollection, ListView,
+    ServerHostsListItemView, serverHostsListTemplate) {
+  'use strict';
+  var ServerHostsListView = ListView.extend({
+    className: 'server-hosts-list',
+    template: _.template(serverHostsListTemplate),
+    listErrorMsg: 'Failed to load server hosts, ' +
+      'server error occurred.',
+    initialize: function(options) {
+      this.collection = new ServerHostCollection({
+        server: options.server.get('id')
+      });
+      this.listenTo(window.events, 'server_hosts_updated:' +
+        options.server.get('id'), this.update);
+      this.server = options.server;
+      this.serverView = options.serverView;
+      ServerHostsListView.__super__.initialize.call(this);
+    },
+    buildItem: function(model) {
+      var modelView = new ServerHostsListItemView({
+        model: model,
+        server: this.server
+      });
+      return modelView;
+    },
+    resetItems: function(views) {
+      if (!views.length) {
+        this.$('.no-hosts').slideDown(250);
+      }
+      else {
+        this.$('.no-hosts').slideUp(250);
+      }
+      this.serverView.updateButtons();
+    }
+  });
+
+  return ServerHostsListView;
+});
