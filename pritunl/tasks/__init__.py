3c4c017078aa9e1097ef8f738c9e5acc78b54a7b
Zachary Huff <zach.huff.386@gmail.com>
2014-10-04 01:09:54 -0400

    Add task init

---
+++ pritunl/tasks/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+for module_name in os.listdir(os.path.dirname(__file__)):
+    if module_name == '__init__.py' or module_name[-3:] != '.py':
+        continue
+    __import__('pritunl.tasks.' + module_name[:-3])
