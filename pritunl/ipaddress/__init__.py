ca6ff0994caaa99373bef98d748dc4fb456458d6
Zachary Huff <zach.huff.386@gmail.com>
2014-10-05 00:44:04 -0400

    Add ipaddress

---
+++ pritunl/ipaddress/__init__.py
@@ -0,0 +1,2 @@
+from pritunl.ipaddress.ipaddress import *
+from pritunl.ipaddress.vpn_ipv4_network import VpnIPv4Network
---
+++ pritunl/ipaddress/vpn_ipv4_network.py
@@ -0,0 +1,27 @@
+from pritunl.ipaddress import ipaddress
+
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+
+class VpnIPv4Network(ipaddress.IPv4Network):
+    def iterhost_sets(self):
+        cur = int(self.network) + 1
+        bcast = int(self.broadcast) - 1
+        found = False
+
+        for _ in xrange(255):
+            ip_addr_endpoint = str(ipaddress.IPAddress(
+                cur, version=self._version)).split('.')[-1]
+            if ip_addr_endpoint in VALID_IP_ENDPOINTS:
+                found = True
+                break
+            cur += 1
+
+        if not found:
+            raise TypeError('Invalid IPv4Network')
+
+        while cur <= bcast:
+            yield ipaddress.IPAddress(cur, version=self._version), \
+                ipaddress.IPAddress(cur + 1, version=self._version)
+            cur += 4
