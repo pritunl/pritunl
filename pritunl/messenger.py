6e1a676a72e946592af5e3fc79ed6d4a8fd55998
Zachary Huff <zach.huff.386@gmail.com>
2014-10-04 20:54:31 -0400

    Restructure messenger

--- pritunl/messenger.py
+++ pritunl/messenger.py
@@ -2,108 +2,106 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.descriptors import *
 from pritunl import mongo
+
 import pymongo
 import time
 import datetime
 import bson
 
-class Messenger(object):
-    @cached_static_property
-    def collection(cls):
-        return mongo.get_collection('messages')
+collection = mongo.get_collection('messages')
 
-    def publish(self, channels, message, extra=None, transaction=None):
-        doc = {
-            'timestamp': datetime.datetime.utcnow(),
-            'message': message,
-        }
+def publish(channels, message, extra=None, transaction=None):
+    doc = {
+        'timestamp': datetime.datetime.utcnow(),
+        'message': message,
+    }
 
-        if extra:
-            for key, val in extra.items():
-                doc[key] = val
+    if extra:
+        for key, val in extra.items():
+            doc[key] = val
 
-        if transaction:
-            tran_collection = transaction.collection(
-                self.collection.name_str)
-
-            if isinstance(channels, str):
-                doc['channel'] = channels
-                tran_collection.update({
-                    '_id': bson.ObjectId(),
-                }, doc, upsert=True)
-            else:
-                for channel in channels:
-                    doc_copy = doc.copy()
-                    doc_copy['channel'] = channel
-
-                    tran_collection.bulk().find({
-                        '_id': bson.ObjectId(),
-                    }).upsert().update(doc_copy)
-                tran_collection.bulk_execute()
-        else:
-            if isinstance(channels, str):
-                doc['channel'] = channels
-                docs = doc
-            else:
-                docs = []
-                for channel in channels:
-                    doc_copy = doc.copy()
-                    doc_copy['channel'] = channel
-                    docs.append(doc_copy)
-            self.collection.insert(docs)
-
-    def get_cursor_id(self, channels):
-        spec = {}
+    if transaction:
+        tran_collection = transaction.collection(
+            collection.name_str)
 
         if isinstance(channels, str):
-            spec['channel'] = channels
+            doc['channel'] = channels
+            tran_collection.update({
+                '_id': bson.ObjectId(),
+            }, doc, upsert=True)
         else:
-            spec['channel'] = {'$in': channels}
+            for channel in channels:
+                doc_copy = doc.copy()
+                doc_copy['channel'] = channel
 
+                tran_collection.bulk().find({
+                    '_id': bson.ObjectId(),
+                }).upsert().update(doc_copy)
+            tran_collection.bulk_execute()
+    else:
+        if isinstance(channels, str):
+            doc['channel'] = channels
+            docs = doc
+        else:
+            docs = []
+            for channel in channels:
+                doc_copy = doc.copy()
+                doc_copy['channel'] = channel
+                docs.append(doc_copy)
+        collection.insert(docs)
+
+def get_cursor_id(channels):
+    spec = {}
+
+    if isinstance(channels, str):
+        spec['channel'] = channels
+    else:
+        spec['channel'] = {'$in': channels}
+
+    try:
+        return collection.find(spec).sort(
+            '$natural', pymongo.DESCENDING)[0]['_id']
+    except IndexError:
+        pass
+
+def subscribe(channels, cursor_id=None, timeout=None,
+        yield_delay=None):
+    start_time = time.time()
+    cursor_id = cursor_id or get_cursor_id(channels)
+    while True:
         try:
-            return self.collection.find(spec).sort(
-                '$natural', pymongo.DESCENDING)[0]['_id']
-        except IndexError:
-            pass
+            spec = {}
 
-    def subscribe(self, channels, cursor_id=None, timeout=None,
-            yield_delay=None):
-        start_time = time.time()
-        cursor_id = cursor_id or self.get_cursor_id(channels)
-        while True:
-            try:
-                spec = {}
+            if isinstance(channels, str):
+                spec['channel'] = channels
+            else:
+                spec['channel'] = {'$in': channels}
 
-                if isinstance(channels, str):
-                    spec['channel'] = channels
-                else:
-                    spec['channel'] = {'$in': channels}
+            if cursor_id:
+                spec['_id'] = {'$gt': cursor_id}
+            cursor = collection.find(spec, tailable=True,
+                await_data=True).sort('$natural', pymongo.ASCENDING)
 
-                if cursor_id:
-                    spec['_id'] = {'$gt': cursor_id}
-                cursor = self.collection.find(spec, tailable=True,
-                    await_data=True).sort('$natural', pymongo.ASCENDING)
+            while cursor.alive:
+                for doc in cursor:
+                    cursor_id = doc['_id']
+                    yield doc
 
-                while cursor.alive:
-                    for doc in cursor:
-                        cursor_id = doc['_id']
-                        yield doc
+                    if yield_delay:
+                        time.sleep(yield_delay)
 
-                        if yield_delay:
-                            time.sleep(yield_delay)
+                        spec = {
+                            '_id': {'$gt': cursor_id},
+                            'channel': channels,
+                        }
+                        cursor = collection.find(spec).sort(
+                            '$natural', pymongo.ASCENDING)
 
-                            spec = {
-                                '_id': {'$gt': cursor_id},
-                                'channel': channels,
-                            }
-                            cursor = self.collection.find(spec).sort(
-                                '$natural', pymongo.ASCENDING)
+                        for doc in cursor:
+                            yield doc
 
-                            for doc in cursor:
-                                yield doc
-
-                            return
-                    if timeout and time.time() - start_time >= timeout:
                         return
-            except pymongo.errors.AutoReconnect:
-                time.sleep(0.2)
+                if timeout and time.time() - start_time >= timeout:
+                    return
+        except pymongo.errors.AutoReconnect:
+            time.sleep(0.2)
