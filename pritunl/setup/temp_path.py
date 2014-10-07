d3e40526e6c20bcff0c6a36f2474b151b94571dc
Zachary Huff <zach.huff.386@gmail.com>
2014-10-06 23:36:45 -0400

    Add support for random temp dir

--- pritunl/setup/temp_path.py
+++ pritunl/setup/temp_path.py
@@ -4,9 +4,10 @@ from pritunl.descriptors import *
 from pritunl import settings
 
 import os
+import uuid
 
 def setup_temp_path():
-    # TODO
-    settings.conf.temp_path = 'tmp/pritunl'
+    settings.conf.temp_path = settings.conf.temp_path.replace(
+        '%r', uuid.uuid4().hex)
     if not os.path.isdir(settings.conf.temp_path):
         os.makedirs(settings.conf.temp_path)
