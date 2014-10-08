0f1b69ef32090176dbf803e767f4b0330bab9f20
Zachary Huff <zach.huff.386@gmail.com>
2014-10-07 20:18:02 -0400

    Add server output

---
+++ pritunl/server/output.py
@@ -0,0 +1,83 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.descriptors import *
+from pritunl import settings
+from pritunl import mongo
+from pritunl import event
+
+import pymongo
+import os
+import json
+import random
+
+class ServerOutput(object):
+    def __init__(self, server_id):
+        self.server_id = server_id
+
+    @cached_static_property
+    def collection(cls):
+        return mongo.get_collection('servers_output')
+
+    def clear_output(self):
+        self.collection.remove({
+            'server_id': self.server_id,
+        })
+        event.Event(type=SERVER_OUTPUT_UPDATED, resource_id=self.server_id)
+
+    def push_output(self, output):
+        self.collection.insert({
+            'server_id': self.server_id,
+            'timestamp': datetime.datetime.utcnow(),
+            'output': output.rstrip('\n'),
+        })
+
+        doc_ids = self.collection.aggregate([
+            {'$match': {
+                'server_id': self.server_id,
+            }},
+            {'$project': {
+                '_id': True,
+                'timestamp': True,
+            }},
+            {'$sort': {
+                'timestamp': pymongo.DESCENDING,
+            }},
+            {'$skip': 10000},
+            {'$group': {
+                '_id': None,
+                'doc_ids': {'$push': '$_id'},
+            }},
+        ])['result']
+
+        if doc_ids:
+            doc_ids = doc_ids[0]['doc_ids']
+
+            self.collection.remove({
+                '_id': {'$in': doc_ids},
+            })
+
+        event.Event(type=SERVER_OUTPUT_UPDATED, resource_id=self.server_id)
+
+    def get_output(self):
+        output = self.collection.aggregate([
+            {'$match': {
+                'server_id': self.server_id,
+            }},
+            {'$project': {
+                '_id': False,
+                'timestamp': True,
+                'output': True,
+            }},
+            {'$sort': {
+                'timestamp': pymongo.ASCENDING,
+            }},
+            {'$group': {
+                '_id': None,
+                'output': {'$push': '$output'},
+            }},
+        ])['result']
+
+        if output:
+            output = output[0]['output']
+
+        return '\n'.join(output)
