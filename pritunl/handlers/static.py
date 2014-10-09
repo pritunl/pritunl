95f83c4425099fe5f176302688df8811b96e898e
Zachary Huff <zach.huff.386@gmail.com>
2014-10-09 04:52:10 -0400

    Update static handler imports

--- pritunl/handlers/static.py
+++ pritunl/handlers/static.py
@@ -1,5 +1,6 @@
 from pritunl.constants import *
 from pritunl.exceptions import *
+from pritunl.descriptors import *
 from pritunl import app
 from pritunl import settings
 from pritunl import static
