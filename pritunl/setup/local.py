5d50eb92861725e4c1810c485afce3f82d8f5dc5
Zachary Huff <zach.huff.386@gmail.com>
2014-10-09 23:49:57 -0400

    Store host_id in file

--- pritunl/setup/local.py
+++ pritunl/setup/local.py
@@ -8,10 +8,20 @@ from pritunl import utils
 
 import hashlib
 import uuid
+import os
 
 def setup_local():
     settings.local.openssl_heartbleed = not utils.check_openssl()
-    settings.local.host_id = hashlib.sha1(str(uuid.getnode())).hexdigest()
+
+    if os.path.isfile(settings.conf.uuid_path):
+        with open(settings.conf.uuid_path, 'r') as uuid_file:
+            settings.local.host_id = uuid_file.read().strip()
+    else:
+        settings.local.host_id = uuid.uuid4().hex
+
+        with open(settings.conf.uuid_path, 'w') as uuid_file:
+            uuid_file.write(settings.local.host_id)
+
     settings.local.version = __version__
     settings.local.version_int = int(
         ''.join([x.zfill(2) for x in settings.local.version.split('.')]))
