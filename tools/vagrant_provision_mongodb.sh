0eb37d4da77974243dd9185f37c72cd6cb193290
Zachary Huff <zach.huff.386@gmail.com>
2014-10-11 15:19:01 -0400

    Add mongodb provision script

---
+++ tools/vagrant_provision_mongodb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee /etc/apt/sources.list.d/mongodb.list
+
+apt-get update -qq
+
+apt-get install -qq -y mongodb-org
+
+sed -i "s/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g" /etc/mongod.conf
+service mongod restart
