45418ab288ec100adb34dd26b8542414a40d5850
Zachary Huff <zach.huff.386@gmail.com>
2014-10-12 02:23:45 -0400

    Add ntp to vagrant provision scripts

--- tools/vagrant_provision.sh
+++ tools/vagrant_provision.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+ntpdate ntp.ubuntu.com
+
 apt-get install -qq -y python-software-properties
 
 add-apt-repository -y ppa:pritunl/pritunl-testing
@@ -6,7 +8,7 @@ add-apt-repository -y ppa:pritunl/pritunl-testing
 apt-get update -qq
 
 # Dev requirements
-apt-get install -qq -y python-flask python-cherrypy3 python-crypto python-objgraph python-pymongo openvpn htop
+apt-get install -qq -y python-flask python-cherrypy3 python-crypto python-objgraph python-pymongo openvpn htop ntp
 
 # Build requirements
 apt-get install -qq -y devscripts debhelper python-all python-setuptools 1> /dev/null
