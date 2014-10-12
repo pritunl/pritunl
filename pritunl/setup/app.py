de7dea4e7fbea947a8ce12184f4a2c7c00b04292
Zachary Huff <zach.huff.386@gmail.com>
2014-10-11 23:53:35 -0400

    Add has_c warnings to app setup

--- pritunl/setup/app.py
+++ pritunl/setup/app.py
@@ -3,6 +3,15 @@ from pritunl.exceptions import *
 from pritunl.descriptors import *
 from pritunl import settings
 
+import pymongo
+import bson
+
 def setup_app():
+    if not pymongo.has_c():
+        logger.warning('Failed to load pymongo c bindings')
+
+    if not bson.has_c():
+        logger.warning('Failed to load bson c bindings')
+
     if settings.conf.debug and settings.conf.ssl:
         settings.conf.ssl = False
