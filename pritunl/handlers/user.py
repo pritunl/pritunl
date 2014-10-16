c7f00decf61515b57ecc188197c671c111fa44a1
Zachary Huff <zach.huff.386@gmail.com>
2014-10-16 02:35:15 -0400

    Fix user handler

--- pritunl/handlers/user.py
+++ pritunl/handlers/user.py
@@ -45,7 +45,7 @@ def user_get(org_id, user_id=None, page=None):
         if svr.otp_auth:
             otp_auth = True
         for instance in svr.instances:
-            for client, client_id in instance['clients'].iteritems():
+            for client_id, client in instance['clients'].iteritems():
                 if client_id not in clients:
                     clients[client_id] = {}
                 clients[client_id][svr.id] = client
