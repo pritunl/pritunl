130439e9e7767ffb7fbf3e8281f909acf126c7a5
Zachary Huff <zach.huff.386@gmail.com>
2014-10-19 19:01:21 -0400

    Add server output methods

--- pritunl/server/utils.py
+++ pritunl/server/utils.py
@@ -1,4 +1,9 @@
-from pritunl.server.server import Server
+from pritunl.server.output import ServerOutput
+from pritunl.server.output_link import ServerOutputLink
+from pritunl.server.bandwidth import ServerBandwidth
+from pritunl.server.ip_pool import ServerIpPool
+from pritunl.server.instance import ServerInstance
+from pritunl.server.server import Server, dict_fields
 
 from pritunl.constants import *
 from pritunl.exceptions import *
@@ -62,3 +67,24 @@ def iter_servers(fields=None):
         fields = {key: True for key in fields}
     for doc in Server.collection.find({}, fields).sort('name'):
         yield Server(doc=doc)
+
+def iter_servers_dict():
+    fields = {key: True for key in dict_fields}
+    for doc in Server.collection.find({}, fields).sort('name'):
+        yield Server(doc=doc).dict()
+
+def output_get(server_id):
+    return ServerOutput(server_id).get_output()
+
+def output_clear(server_id):
+    ServerOutput(server_id).clear_output()
+
+def output_link_get(server_id):
+    return ServerOutputLink(server_id).get_output()
+
+def output_link_clear(server_id):
+    svr = get_server(id=server_id, fields=['_id', 'links'])
+    ServerOutputLink(server_id).clear_output(svr.links.keys())
+
+def bandwidth_get(server_id, period):
+    return ServerBandwidth(server_id).get_period(period)
