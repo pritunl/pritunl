f15226e77e2693e021a27bdb31c5c34c1f60c8c5
Zachary Huff <zach.huff.386@gmail.com>
2014-10-20 04:29:52 -0400

    Update host model

--- www/models/host.js
+++ www/models/host.js
@@ -4,29 +4,25 @@ define([
   'backbone'
 ], function($, _, Backbone) {
   'use strict';
-  var ServerModel = Backbone.Model.extend({
+  var HostModel = Backbone.Model.extend({
     defaults: {
       'name': null,
       'status': null,
       'uptime': null,
       'user_count': null,
       'users_online': null,
-      'public_address': null,
+      'public_address': null
     },
     url: function() {
       var url = '/host';
 
       if (this.get('id')) {
         url += '/' + this.get('id');
-
-        if (this.get('operation')) {
-          url += '/' + this.get('operation');
-        }
       }
 
       return url;
     }
   });
 
-  return ServerModel;
+  return HostModel;
 });
