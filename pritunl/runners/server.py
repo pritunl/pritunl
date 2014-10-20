faec871fb5d8fd3e3fc4aaf04701ccd3a24c089a
Zachary Huff <zach.huff.386@gmail.com>
2014-10-19 22:52:51 -0400

    Update server runner

--- pritunl/runners/server.py
+++ pritunl/runners/server.py
@@ -33,89 +33,14 @@ def _on_msg(msg):
             if instance['host_id'] == settings.local.host_id:
                 return
 
-        prefered_host = msg.get('prefered_host')
+        prefered_hosts = msg.get('prefered_hosts')
 
-        if prefered_host and settings.local.host_id != prefered_host:
+        if prefered_hosts and settings.local.host_id not in prefered_hosts:
             time.sleep(0.1)
 
         svr.run(send_events=msg.get('send_events'))
     except:
         logger.exception('Failed to run server.')
 
-def _server_check_thread():
-    collection = mongo.get_collection('servers')
-
-    while True:
-        try:
-            timestamp_spec = utils.now() - datetime.timedelta(
-                seconds=settings.vpn.server_ping_ttl)
-
-            docs = collection.find({
-                'instances.ping_timestamp': {'$lt': timestamp_spec},
-            }, {
-                '_id': True,
-                'instances': True,
-            })
-
-            for doc in docs:
-                for instance in doc['instances']:
-                    if instance['ping_timestamp'] < timestamp_spec:
-                        collection.update({
-                            '_id': doc['_id'],
-                            'instances.instance_id': instance['instance_id'],
-                        }, {
-                            '$pull': {
-                                'instances': {
-                                    'instance_id': instance['instance_id'],
-                                },
-                            },
-                            '$inc': {
-                                'instances_count': -1,
-                            },
-                        })
-
-            yield
-
-            response = collection.aggregate([
-                {'$match': {
-                    'status': True,
-                    'start_timestamp': {'$lt': timestamp_spec},
-                }},
-                {'$project': {
-                    '_id': True,
-                    'hosts': True,
-                    'instances': True,
-                    'offline_instances_count': {
-                        '$subtract': [
-                            '$replica_count',
-                            '$instances_count',
-                        ],
-                    }
-                }},
-                {'$match': {
-                    'offline_instances_count': {'$gt': 0},
-                }},
-            ])['result']
-
-            yield
-
-            for doc in response:
-                active_hosts = set([x['host_id'] for x in doc['instances']])
-                hosts = list(set(doc['hosts']) - active_hosts)
-                if not hosts:
-                    continue
-                prefered_host = random.choice(hosts)
-
-                messenger.publish('servers', 'start', extra={
-                    'server_id': str(doc['_id']),
-                    'send_events': True,
-                    'prefered_host': prefered_host,
-                })
-        except:
-            logger.exception('Error checking server states.')
-
-        yield interrupter_sleep(settings.vpn.server_ping)
-
 def start_server():
-    threading.Thread(target=_server_check_thread).start()
     listener.add_listener('servers', _on_msg)
