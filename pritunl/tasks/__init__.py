fcef4c088eda026736a3397970a00aa36d44a8e9
Zachary Huff <zach.huff.386@gmail.com>
2014-10-20 22:13:13 -0400

    Add missing task import

--- pritunl/tasks/__init__.py
+++ pritunl/tasks/__init__.py
@@ -2,4 +2,5 @@ import pritunl.tasks.clean_ip_pool
 import pritunl.tasks.clean_users
 import pritunl.tasks.pooler
 import pritunl.tasks.sync_ip_pool
+import pritunl.tasks.server
 import pritunl.tasks.clean_servers
