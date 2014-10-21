651ad5d6704d4c8fe1f57cc3ff074297b2148e96
Zachary Huff <zach.huff.386@gmail.com>
2014-10-21 13:48:17 -0400

    Fix event get

--- pritunl/handlers/event.py
+++ pritunl/handlers/event.py
@@ -9,9 +9,13 @@ from pritunl import auth
 import time
 import uuid
 import flask
+import bson
 
 @app.app.route('/event', methods=['GET'])
 @app.app.route('/event/<cursor>', methods=['GET'])
 @auth.session_auth
 def event_get(cursor=None):
+    if cursor is not None:
+        cursor = bson.ObjectId(cursor)
+
     return utils.jsonify(event.get_events(cursor=cursor))
