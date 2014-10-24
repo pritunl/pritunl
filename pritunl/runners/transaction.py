9e13bba83c44cda6d8a8674d7739a13506a2b818
Zachary Huff <zach.huff.386@gmail.com>
2014-10-24 14:49:29 -0400

    Add exception logger to transaction runner

--- pritunl/runners/transaction.py
+++ pritunl/runners/transaction.py
@@ -19,20 +19,26 @@ def _check_thread():
     collection = mongo.get_collection('transaction')
 
     while True:
-        spec = {
-            'ttl_timestamp': {'$lt': utils.now()},
-        }
+        try:
+            spec = {
+                'ttl_timestamp': {'$lt': utils.now()},
+            }
 
-        for doc in collection.find(spec).sort('priority'):
-            try:
-                tran = transaction.Transaction(doc=doc)
-                tran.run()
-            except:
-                logger.exception('Failed to run transaction. %r' % {
-                    'transaction_id': doc['_id'],
-                })
+            for doc in collection.find(spec).sort('priority'):
+                try:
+                    tran = transaction.Transaction(doc=doc)
+                    tran.run()
+                except:
+                    logger.exception('Failed to run transaction. %r' % {
+                        'transaction_id': doc['_id'],
+                    })
 
-        yield interrupter_sleep(settings.mongo.tran_ttl)
+            yield interrupter_sleep(settings.mongo.tran_ttl)
+        except GeneratorExit:
+            raise
+        except:
+            logger.exception('Error in transaction runner thread.')
+            time.sleep(0.5)
 
 def start_transaction():
     threading.Thread(target=_check_thread).start()
