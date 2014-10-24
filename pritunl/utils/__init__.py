2da8619f51d513c85a91e7c8e29a18a99e05f07d
Zachary Huff <zach.huff.386@gmail.com>
2014-10-24 15:03:14 -0400

    Add none_queue to utils init

--- pritunl/utils/__init__.py
+++ pritunl/utils/__init__.py
@@ -2,4 +2,5 @@ from pritunl.utils.json_helpers import *
 from pritunl.utils.least_common_counter import *
 from pritunl.utils.misc import *
 from pritunl.utils.network import *
+from pritunl.utils.none_queue import NoneQueue
 from pritunl.utils import request
