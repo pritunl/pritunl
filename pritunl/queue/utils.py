68de4a7e1912be61f49b875f190fecfa36552f57
Zachary Huff <zach.huff.386@gmail.com>
2014-10-26 14:05:02 -0400

    Add queue and utils

---
+++ pritunl/queue/utils.py
@@ -0,0 +1,68 @@
+from pritunl.queue.com import QueueCom
+from pritunl.queue.queue import Queue
+
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.helpers import *
+from pritunl import settings
+from pritunl.queue import com
+from pritunl import logger
+from pritunl import mongo
+from pritunl import messenger
+from pritunl import utils
+
+import bson
+import datetime
+import logging
+import threading
+import time
+import importlib
+import os
+
+queue_types = {}
+reserve_types = {}
+
+def get(doc):
+    return queue_types[doc['type']](doc=doc)
+
+def start(queue_type, transaction=None, block=False, block_timeout=30,
+        *args, **kwargs):
+    que = queue_types[queue_type](*args, **kwargs)
+    que.start(transaction=transaction, block=block,
+        block_timeout=block_timeout)
+    return que
+
+def stop(queue_id=None, spec=None, transaction=None):
+    if queue_id is not None:
+        pass
+    elif spec is not None:
+        doc = Queue.collection.find_one(spec, {
+            '_id': True,
+        })
+        if not doc:
+            return
+        queue_id = doc['_id']
+    else:
+        raise ValueError('Must provide queue_id or spec')
+
+    messenger.publish('queue', [STOP, queue_id], transaction=transaction)
+
+def iter_queues(spec=None):
+    for doc in Queue.collection.find(spec or {}).sort('priority'):
+        yield queue_types[doc['type']](doc=doc)
+
+def add_queue(cls):
+    queue_types[cls.type] = cls
+    return cls
+
+def add_reserve(reserve_type):
+    def add_reserve_wrap(func):
+        reserve_types[reserve_type] = func
+        return func
+    return add_reserve_wrap
+
+def reserve(reserve_type, *args, **kwargs):
+    return reserve_types[reserve_type](*args, **kwargs)
+
+def find(spec):
+    return Queue.collection.find_one(spec)
