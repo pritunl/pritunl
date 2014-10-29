c5685a4f7d8446052c9c65d3405761efd7778110
Zachary Huff <zach.huff.386@gmail.com>
2014-10-29 12:46:32 -0400

    Add log limit to settings

--- pritunl/settings/app.py
+++ pritunl/settings/app.py
@@ -14,6 +14,7 @@ class SettingsApp(SettingsGroupMongo):
         'notification_server': 'http://ip.pritunl.com/notification',
         'update_check_rate': 3600,
         'session_timeout': 86400,
+        'log_limit': 10240,
         'log_entry_limit': 50,
         'rate_limit_sleep': 0.5,
         'short_url_length': 5,
