0f663a7b313493bb66ca87c4efffbe1b49029d5b
Zachary Huff <zach.huff.386@gmail.com>
2014-10-31 13:48:36 -0400

    Add logs command to main

--- pritunl/__main__.py
+++ pritunl/__main__.py
@@ -10,7 +10,7 @@ Commands:
   start                 Start server
   version               Print the version and exit
   reset-password        Reset administrator password
-"""
+  logs                  View server logs"""
 
 def pritunl_daemon(default_conf=None):
     parser = optparse.OptionParser(usage=USAGE)
@@ -20,6 +20,10 @@ def pritunl_daemon(default_conf=None):
         help='Path to create pid file')
     parser.add_option('-c', '--conf', type='string',
         help='Path to configuration file')
+    parser.add_option('--archive', action='store_true',
+        help='Archive log file')
+    parser.add_option('--tail', action='store_true',
+        help='Tail log file')
     (options, args) = parser.parse_args()
 
     if args:
@@ -43,6 +47,25 @@ def pritunl_daemon(default_conf=None):
         print 'Administrator password successfully reset:\n' + \
             '  username: "%s"\n  password: "%s"' % (
                 DEFAULT_USERNAME, DEFAULT_PASSWORD)
+
+    if cmd == 'logs':
+        from pritunl.constants import DEFAULT_USERNAME, DEFAULT_PASSWORD
+        from pritunl import setup
+        from pritunl import logger
+        setup.setup_db()
+
+        if options.archive:
+            if len(args) > 1:
+                archive_path = args[1]
+            else:
+                archive_path = './'
+            print 'Log archived to: ' + logger.archive_log(archive_path)
+        elif options.tail:
+            for msg in logger.tail_log_lines():
+                print msg
+        else:
+            print logger.get_log_lines()
+
         sys.exit(0)
 
     if cmd != 'start':
