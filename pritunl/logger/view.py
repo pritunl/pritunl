5db65e0a91fb2693e5dc4d92aacaf8766e75ce73
Zachary Huff <zach.huff.386@gmail.com>
2014-10-31 16:01:52 -0400

    Fix syntax error in log view

--- pritunl/logger/view.py
+++ pritunl/logger/view.py
@@ -42,7 +42,7 @@ class LogView(object):
                 log_level = line_split[2][1:]
                 log_msg = line_split[3]
 
-                return '\033[1;%sm[%s]\033[0m\033[1m[%s]\033' +
+                return '\033[1;%sm[%s]\033[0m\033[1m[%s]\033' + \
                         '[0m\033[0;%sm[%s]\033[0m%s' % (
                     self.host_colors[log_host], log_host,
                     log_time,
