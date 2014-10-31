2c3d013f1ed36c75a1b01d3b46673ce1109a164b
Zachary Huff <zach.huff.386@gmail.com>
2014-10-31 10:03:19 -0400

    Add utopic to build script

--- tools/vagrant_build_ubuntu.sh
+++ tools/vagrant_build_ubuntu.sh
@@ -19,9 +19,13 @@ debuild -S
 sed -i -e 's/0ubuntu1/0ubuntu1~precise/g' debian/changelog
 sed -i -e 's/trusty;/precise;/g' debian/changelog
 debuild -S
+sed -i -e 's/0ubuntu1~precise/0ubuntu1~utopic/g' debian/changelog
+sed -i -e 's/precise;/utopic;/g' debian/changelog
+debuild -S
 
 cd ..
 
 echo '\n\nRUN COMMANDS BELOW TO UPLOAD:'
 echo 'sudo dput ppa:pritunl/ppa ../build/debian/pritunl_'$VERSION'-0ubuntu1_source.changes'
 echo 'sudo dput ppa:pritunl/ppa ../build/debian/pritunl_'$VERSION'-0ubuntu1~precise_source.changes'
+echo 'sudo dput ppa:pritunl/ppa ../build/debian/pritunl_'$VERSION'-0ubuntu1~utopic_source.changes'
