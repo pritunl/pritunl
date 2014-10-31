67470bfba2e475bad449782282d5f825c8c3b9fb
Zachary Huff <zach.huff.386@gmail.com>
2014-10-31 14:01:53 -0400

    Add log view to logger

--- pritunl/logger/view.py
+++ pritunl/logger/view.py
@@ -8,6 +8,45 @@ from pritunl import utils
 import pymongo
 import tarfile
 import os
+import collections
+
+class LogView(object):
+    def __init__(self):
+        self.colors = (x for x in BASH_COLORS)
+        self.host_colors = collections.defaultdict(lambda: self.get_color())
+        self.log_colors = {
+            'DEBUG': '90',
+            'INFO': '94',
+            'WARNING': '93',
+            'ERROR': '91',
+            'CRITICAL': '95',
+        }
+
+    def get_color(self):
+        try:
+            return self.colors.next()
+        except StopIteration:
+            self.colors = (x for x in BASH_COLORS)
+            return get_color()
+
+    def format_line(self, line):
+        if line[0] == '[':
+            try:
+                line_split = line.split(']', 3)
+                log_host = line_split[0][1:]
+                log_time = line_split[1][1:]
+                log_level = line_split[2][1:]
+                log_msg = line_split[3]
+
+                return '\033[1;%sm[%s]\033[0m[%s]\033[0;%sm[%s]\033[0m%s' % (
+                    self.host_colors[log_host], log_host,
+                    log_time,
+                    self.log_colors.get(log_level), log_level,
+                    log_msg,
+                )
+            except:
+               pass
+        return line
 
 def get_log_lines():
     collection = mongo.get_collection('log')
