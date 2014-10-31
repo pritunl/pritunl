51c9e8e142ca01d44369485654ba7f750b3b0b5d
Zachary Huff <zach.huff.386@gmail.com>
2014-10-31 13:46:29 -0400

    Add archive_log to logger view

--- pritunl/logger/view.py
+++ pritunl/logger/view.py
@@ -6,6 +6,8 @@ from pritunl import mongo
 from pritunl import utils
 
 import pymongo
+import tarfile
+import os
 
 def get_log_lines():
     collection = mongo.get_collection('log')
@@ -24,3 +26,23 @@ def get_log_lines():
         output = output[0]['output']
 
     return '\n'.join(output)
+
+def archive_log(archive_path):
+    temp_path = utils.get_temp_path()
+    if os.path.isdir(archive_path):
+        archive_path = os.path.join(archive_path, LOG_ARCHIVE_NAME + '.tar')
+    output_path = os.path.join(temp_path, LOG_ARCHIVE_NAME)
+
+    try:
+        os.makedirs(temp_path)
+        tar_file = tarfile.open(archive_path, 'w')
+        try:
+            with open(output_path, 'w') as log_file:
+                log_file.write(get_log_lines())
+            tar_file.add(output_path, arcname=LOG_ARCHIVE_NAME)
+        finally:
+            tar_file.close()
+    finally:
+        utils.rmtree(temp_path)
+
+    return archive_path
