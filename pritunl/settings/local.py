5b695411708f1e54d6489fd9fecafde44c1c72f9
Zachary Huff <zach.huff.386@gmail.com>
2014-11-03 03:50:01 -0500

    Add server events to local settings

--- pritunl/settings/local.py
+++ pritunl/settings/local.py
@@ -4,10 +4,17 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.helpers import *
 
+import threading
+
+server_start = threading.Event()
+server_start.set()
+
 class SettingsLocal(SettingsGroupLocal):
     group = 'local'
     fields = {
         'quiet': False,
         'public_ip': None,
+        'server_ready': threading.Event(),
+        'server_start': server_start,
         'host_ping_timestamp': None,
     }
