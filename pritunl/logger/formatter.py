d1eecac068909fb608155bd33478aef43ce40c81
Zachary Huff <zach.huff.386@gmail.com>
2014-11-04 17:33:35 -0500

    Format traceback in log messages

--- pritunl/logger/formatter.py
+++ pritunl/logger/formatter.py
@@ -16,8 +16,12 @@ class LogFormatter(logging.Formatter):
             logging.Formatter.format(self, record)
 
         if hasattr(record, 'data') and record.data:
+            traceback = record.data.pop('traceback', None)
             width = len(max(record.data, key=len))
             for key, val in record.data.items():
                 formatted_record += '\n    %s = %r' % (key.ljust(width), val)
+            if traceback:
+                formatted_record += '\nTraceback (most recent call last):\n'
+                formatted_record += ''.join(traceback).rstrip('\n')
 
         return formatted_record
