7c0517fcb4e041e873b3275a0ad57fb26c700102
Zachary Huff <zach.huff.386@gmail.com>
2014-11-03 21:41:47 -0500

    Formate printed log messages

--- pritunl/logger/handler.py
+++ pritunl/logger/handler.py
@@ -1,3 +1,5 @@
+from pritunl.logger.view import LogView
+
 from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.helpers import *
@@ -12,8 +14,12 @@ class LogHandler(logging.Handler):
     def __init__(self):
         logging.Handler.__init__(self)
 
+    @cached_property
+    def log_view(self):
+        return LogView()
+
     def emit(self, record):
         msg = self.format(record)
         log_queue.append(msg)
         if not settings.local.quiet:
-            print msg
+            print self.log_view.format_line(msg)
