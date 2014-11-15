b2aea2377f6d429490e9bd5d7500a17f237942e1
Zachary Huff <zach.huff.386@gmail.com>
2014-11-15 06:04:53 -0500

    Fix formatting in wsgiserver

--- pritunl/wsgiserver/__init__.py
+++ pritunl/wsgiserver/__init__.py
@@ -2,8 +2,7 @@
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
-#   modification,
-# are permitted provided that the following conditions are met:
+# modification, are permitted provided that the following conditions are met:
 
 #     * Redistributions of source code must retain the above copyright notice,
 #       this list of conditions and the following disclaimer.
