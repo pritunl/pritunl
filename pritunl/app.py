06394cb921fce19302971237222cee9c591f46af
Zachary Huff <zach.huff.386@gmail.com>
2014-11-15 18:41:58 -0500

    Add validate_peer to app

--- pritunl/app.py
+++ pritunl/app.py
@@ -42,7 +42,27 @@ def after_request(response):
 def _run_wsgi():
     logger.info('Starting server', 'app')
 
-    server = cherrypy.wsgiserver.CherryPyWSGIServer(
+    get_time = time.time
+    peers_expire_count = {}
+
+    class CherryPyWSGIServerPatched(wsgiserver.CherryPyWSGIServer):
+        def validate_peer(self, peer):
+            cur_time = get_time()
+            peer = peer[0]
+            expire, count = peers_expire_count.get(peer, (None, None))
+            if expire and cur_time <= expire:
+                if count > settings.app.peer_limit:
+                    return False
+                peers_expire_count[peer] = (expire, count + 1)
+            else:
+                peers_expire_count[peer] = (
+                    cur_time + settings.app.peer_limit_timeout, 1)
+            return True
+
+        def validate_request(self, peer, request):
+            return self.validate_peer(peer)
+
+    server = CherryPyWSGIServerPatched(
         (settings.conf.bind_addr, settings.conf.port), app,
         request_queue_size=settings.app.request_queue_size,
         server_name=wsgiserver.CherryPyWSGIServer.version)
