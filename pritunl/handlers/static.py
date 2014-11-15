02dd4eaf1c5d4a37b1f7bbda4eade810ad24c1a4
Zachary Huff <zach.huff.386@gmail.com>
2014-11-15 17:57:35 -0500

    Improve static file handlers

--- pritunl/handlers/static.py
+++ pritunl/handlers/static.py
@@ -2,31 +2,45 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.helpers import *
 from pritunl import app
+from pritunl import auth
 from pritunl import settings
 from pritunl import static
 from pritunl import auth
 import os
 import flask
 
-@app.app.route('/', methods=['GET'])
-@app.app.route('/favicon.ico', methods=['GET'])
-@app.app.route('/robots.txt', methods=['GET'])
-def root_static_get():
-    file_name = flask.request.path.lstrip('/')
-    if not file_name or file_name == 'app':
-        file_name = 'index.html'
-        cache = False
-    else:
-        cache = True
-    static_file = static.StaticFile(settings.conf.www_path,
-        file_name, cache=cache)
-    return static_file.get_response()
-
 @app.app.route('/s/<path:file_path>', methods=['GET'])
+@auth.session_auth
 def static_get(file_path):
     try:
         static_file = static.StaticFile(settings.conf.www_path,
             file_path, cache=True)
     except InvalidStaticFile:
         return flask.abort(404)
+    return static_file.get_response()
+
+@app.app.route('/favicon.ico', methods=['GET'])
+def favicon_static_get():
+    static_file = static.StaticFile(settings.conf.www_path,
+        'favicon.ico', cache=True)
+    return static_file.get_response()
+
+@app.app.route('/robots.txt', methods=['GET'])
+def robots_static_get():
+    static_file = static.StaticFile(settings.conf.www_path,
+        'robots.txt', cache=True)
+    return static_file.get_response()
+
+@app.app.route('/', methods=['GET'])
+def index_static_get():
+    if not auth.check_session():
+        return flask.redirect('login')
+    static_file = static.StaticFile(settings.conf.www_path,
+        'index.html', cache=False)
+    return static_file.get_response()
+
+@app.app.route('/login', methods=['GET'])
+def login_static_get():
+    static_file = static.StaticFile(settings.conf.www_path,
+        'login.html', cache=False)
     return static_file.get_response()
