49bc6d2e77753889e9443f2dfb3069e17b714129
Zachary Huff <zach.huff.386@gmail.com>
2014-11-16 03:34:25 -0500

    Add limiter runner

---
+++ pritunl/runners/limiter.py
@@ -0,0 +1,29 @@
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl.helpers import *
+from pritunl import logger
+from pritunl import settings
+from pritunl import limiter
+
+import time
+import threading
+
+@interrupter
+def _limiter_runner_thread():
+    while True:
+        try:
+            cur_time = time.time()
+            for peer, (expire, count) in limiter.peers_expire_count.items():
+                if cur_time > expire:
+                    limiter.peers_expire_count.pop(peer, None)
+
+            yield interrupter_sleep(settings.app.peer_limit_timeout * 2)
+
+        except GeneratorExit:
+            raise
+        except:
+            logger.exception('Error in limiter runner thread', 'runners')
+            time.sleep(0.5)
+
+def start_limiter():
+    threading.Thread(target=_limiter_runner_thread).start()
