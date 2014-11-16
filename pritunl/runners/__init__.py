4708f6e4edf34f12e42d8c81e01cde6aba6c8802
Zachary Huff <zach.huff.386@gmail.com>
2014-11-16 03:36:13 -0500

    Start limiter in runners

--- pritunl/runners/__init__.py
+++ pritunl/runners/__init__.py
@@ -9,6 +9,7 @@ from pritunl.runners.host import start_host
 from pritunl.runners.subscription import start_subscription
 from pritunl.runners.server import start_server
 from pritunl.runners.time_sync import start_time_sync
+from pritunl.runners.limiter import start_limiter
 from pritunl.runners.listener import start_listener
 
 def start_all():
@@ -23,5 +24,6 @@ def start_all():
     start_subscription()
     start_server()
     start_time_sync()
+    start_limiter()
 
     start_listener()
