af38b096135b63a5a1c5b3b87bf8c53a6d0ac1a7
Zachary Huff <zach.huff.386@gmail.com>
2014-11-19 04:06:01 -0500

    Add support for 0.10.x conf files

--- pritunl/settings/group_file.py
+++ pritunl/settings/group_file.py
@@ -15,8 +15,33 @@ class SettingsGroupFile(SettingsGroupBase):
         if not os.path.isfile(self.path):
             return
 
-        with open(self.path, 'r') as settings_file:
-            doc = json.loads(settings_file.read())
+        try:
+            with open(self.path, 'r') as settings_file:
+                doc = json.loads(settings_file.read())
+        except ValueError:
+            doc = {}
+
+            with open(self.path, 'r') as settings_file:
+                for line in settings_file.readlines():
+                    line = line.rstrip('\n')
+                    name, value = line.split('=', 1)
+
+                    if name in ('debug', 'ssl'):
+                        if value in ('true', 't', 'yes', 'y'):
+                            value = True
+                        elif value in ('false', 'f', 'no', 'n'):
+                            value = False
+                        else:
+                            value = None
+                        setattr(self, name, value)
+                    elif name == 'port':
+                        setattr(self, name, int(value))
+                    elif name in ('log_path', 'www_path', 'data_path',
+                            'db_path'):
+                        setattr(self, name, os.path.normpath(value))
+                    elif name in ('bind_addr'):
+                        setattr(self, name, value)
+                self.commit()
 
         for field, value in doc.items():
             setattr(self, field, value)
