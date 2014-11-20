b33dc33656d9a2e23d09ef2c18061f2ddc3e58dd
Zachary Huff <zach.huff.386@gmail.com>
2014-11-20 16:53:46 -0500

    Use file logger when mongo not setup

--- pritunl/logger/handler.py
+++ pritunl/logger/handler.py
@@ -30,6 +30,11 @@ class LogHandler(logging.Handler):
 
     def emit(self, record):
         msg = self.format(record)
-        log_queue.append(msg)
+
+        if not settings.local.logger_runner:
+            self.file_handler.emit(record)
+        else:
+            log_queue.append(msg)
+
         if not settings.local.quiet:
             print self.log_view.format_line(msg)
