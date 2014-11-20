d83b79334304ddd8d5ac1ae948703fecbd2490fb
Zachary Huff <zach.huff.386@gmail.com>
2014-11-20 17:05:56 -0500

    Remove mongo warnings from app setup

--- pritunl/setup/app.py
+++ pritunl/setup/app.py
@@ -3,15 +3,6 @@ from pritunl.exceptions import *
 from pritunl.helpers import *
 from pritunl import settings
 
-import pymongo
-import bson
-
 def setup_app():
-    if not pymongo.has_c():
-        logger.warning('Failed to load pymongo c bindings')
-
-    if not bson.has_c():
-        logger.warning('Failed to load bson c bindings')
-
     if settings.conf.debug and settings.conf.ssl:
         settings.conf.ssl = False
