83f235ca882d7fd2bf380a36681bf62a1585bc77
Zachary Huff <zach.huff.386@gmail.com>
2014-11-21 15:15:03 -0500

    Remove settings handlers from auth

--- pritunl/handlers/auth.py
+++ pritunl/handlers/auth.py
@@ -13,65 +13,6 @@ import time
 import flask
 import bson
 
-@app.app.route('/settings', methods=['GET'])
-@auth.session_auth
-def settings_get():
-    response = flask.g.administrator.dict()
-    response.update({
-        'theme': settings.app.theme,
-        'email_from': settings.app.email_from_addr,
-        'email_api_key': settings.app.email_api_key,
-    })
-    return utils.jsonify(response)
-
-@app.app.route('/settings', methods=['PUT'])
-@auth.session_auth
-def settings_put():
-    admin = flask.g.administrator
-
-    if 'username' in flask.request.json and flask.request.json['username']:
-        admin.username = utils.filter_str(
-            flask.request.json['username']).lower()
-    if 'password' in flask.request.json and flask.request.json['password']:
-        admin.password = flask.request.json['password']
-    if 'token' in flask.request.json and flask.request.json['token']:
-        admin.generate_token()
-    if 'secret' in flask.request.json and flask.request.json['secret']:
-        admin.generate_secret()
-
-    settings_commit = False
-    if 'email_from' in flask.request.json:
-        settings_commit = True
-        email_from = flask.request.json['email_from']
-        settings.app.email_from_addr = email_from or None
-    if 'email_api_key' in flask.request.json:
-        settings_commit = True
-        email_api_key = flask.request.json['email_api_key']
-        settings.app.email_api_key = email_api_key or None
-    if 'theme' in flask.request.json:
-        settings_commit = True
-        theme = 'dark' if flask.request.json['theme'] == 'dark' else 'light'
-
-        if theme != settings.app.theme:
-            if theme == 'dark':
-                event.Event(type=THEME_DARK)
-            else:
-                event.Event(type=THEME_LIGHT)
-
-        settings.app.theme = theme
-
-    if settings_commit:
-        settings.commit()
-
-    admin.commit(admin.changed)
-
-    response = flask.g.administrator.dict()
-    response.update({
-        'email_from': settings.app.email_from_addr,
-        'email_api_key': settings.app.email_api_key,
-    })
-    return utils.jsonify(response)
-
 @app.app.route('/auth/session', methods=['POST'])
 def auth_session_post():
     username = flask.request.json['username']
