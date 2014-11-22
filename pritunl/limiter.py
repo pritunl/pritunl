28b7994baec76d916f38dafa2bd65c4337a5ac09
Zachary Huff <zach.huff.386@gmail.com>
2014-11-22 15:26:58 -0500

    Create limiter class

--- pritunl/limiter.py
+++ pritunl/limiter.py
@@ -8,21 +8,35 @@ from pritunl import wsgiserver
 import time
 
 _get_time = time.time
-peers_expire_count = {}
+
+class Limiter(object):
+    def __init__(self, group_name, limit_name, limit_timeout_name):
+        self.peers_expire_count = {}
+        self.group_name = group_name
+        self.limit_name = limit_name
+        self.limit_timeout_name = limit_timeout_name
+
+    def validate(self, peer):
+        settings_group = getattr(settings, self.group_name)
+        limit = getattr(settings_group, self.limit_name)
+        limit_timeout = getattr(settings_group, self.limit_timeout_name)
+
+        cur_time = _get_time()
+        peer = peer[0]
+        expire, count = peers_expire_count.get(peer, (None, None))
+        if expire and cur_time <= expire:
+            if count > limit:
+                return False
+            peers_expire_count[peer] = (expire, count + 1)
+        else:
+            peers_expire_count[peer] = (cur_time + limit_timeout, 1)
+        return True
+
+_wsgi_limiter = Limiter('app', 'peer_limit', 'peer_limit_timeout')
 
 class CherryPyWSGIServerLimited(wsgiserver.CherryPyWSGIServer):
-	def validate_peer(self, peer):
-		cur_time = _get_time()
-		peer = peer[0]
-		expire, count = peers_expire_count.get(peer, (None, None))
-		if expire and cur_time <= expire:
-			if count > settings.app.peer_limit:
-				return False
-			peers_expire_count[peer] = (expire, count + 1)
-		else:
-			peers_expire_count[peer] = (
-				cur_time + settings.app.peer_limit_timeout, 1)
-		return True
+    def validate_peer(self, peer):
+        return _wsgi_limiter.validate(peer)
 
-	def validate_request(self, peer, request):
-		return self.validate_peer(peer)
+    def validate_request(self, peer, request):
+        return _wsgi_limiter.validate(peer)
