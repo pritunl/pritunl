431e14d996b4d6ed35aaf2a74b886199bb407516
Zachary Huff <zach.huff.386@gmail.com>
2014-11-22 15:28:13 -0500

    Add peer_limit to vpn settings

--- pritunl/settings/vpn.py
+++ pritunl/settings/vpn.py
@@ -7,6 +7,8 @@ from pritunl.helpers import *
 class SettingsVpn(SettingsGroupMongo):
     group = 'vpn'
     fields = {
+        'peer_limit': 300,
+        'peer_limit_timeout': 10,
         'default_dh_param_bits': 1536,
         'log_lines': 5000,
         'server_ping': 3,
