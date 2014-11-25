c28136fdc631cce2c0508b46ecff10889bdbc300
Zachary Huff <zach.huff.386@gmail.com>
2014-11-25 14:13:54 -0500

    Add mail to imports in utils

--- pritunl/utils/__init__.py
+++ pritunl/utils/__init__.py
@@ -1,5 +1,6 @@
 from pritunl.utils.json_helpers import *
 from pritunl.utils.least_common_counter import *
+from pritunl.utils.mail import *
 from pritunl.utils.misc import *
 from pritunl.utils.network import *
 from pritunl.utils.none_queue import NoneQueue
