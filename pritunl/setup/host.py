0a9780f7999791e92b752891fd05dbbb11b627dc
Zachary Huff <zach.huff.386@gmail.com>
2014-11-26 16:48:19 -0500

    Setup subscription in host

--- pritunl/setup/host.py
+++ pritunl/setup/host.py
@@ -1,7 +1,17 @@
 from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.helpers import *
+from pritunl import mongo
 
 def setup_host():
+    collection = mongo.get_collection('settings')
+
+    collection.update({
+        '_id': 'subscription',
+    }, {'$setOnInsert': {
+        'active': None,
+        'plan': None,
+    }}, upsert=True)
+
     from pritunl import host
     host.init()
