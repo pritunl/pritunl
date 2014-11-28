8fb8c64611616bcb066de72c0dfd1421847b1159
Zachary Huff <zach.huff.386@gmail.com>
2014-11-27 20:51:38 -0500

    Add modal feeback view

---
+++ www/views/modalFeedback.js
@@ -0,0 +1,42 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/modal',
+  'text!templates/modalFeedback.html'
+], function($, _, Backbone, ModalView, modalFeedbackTemplate) {
+  'use strict';
+  var lastOrg;
+  var ModalFeedbackView = ModalView.extend({
+    className: 'feedback-modal',
+    template: _.template(modalFeedbackTemplate),
+    title: 'Anonymous Feedback/Bug Report',
+    okText: 'Submit',
+    body: function() {
+      return this.template();
+    },
+    onOk: function() {
+      var message = this.$('textarea').val();
+
+      this.setLoading('Submitting...');
+      $.ajax({
+          type: 'POST',
+          url: 'https://app.pritunl.com/feedback',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            'message': message,
+          }),
+          success: function() {
+            this.close(true);
+          }.bind(this),
+          error: function() {
+            this.clearLoading();
+            this.setAlert('danger',
+              'Failed to submit feedback, unknown server error.');
+          }.bind(this)
+      });
+    }
+  });
+
+  return ModalFeedbackView;
+});
