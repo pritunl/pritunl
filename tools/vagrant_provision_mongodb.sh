62e9d268c4b90d0efe874d91b177c5296931d15d
Zachary Huff <zach.huff.386@gmail.com>
2014-11-30 17:06:52 -0500

    Update mongodb provision

--- tools/vagrant_provision_mongodb.sh
+++ tools/vagrant_provision_mongodb.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 ntpdate ntp.ubuntu.com
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee /etc/apt/sources.list.d/mongodb.list
+add-apt-repository -y ppa:pritunl/pritunl-dev
 apt-get update -qq
-apt-get install -qq -y mongodb-org ntp
-sed -i "s/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g" /etc/mongod.conf
-service mongod restart
+apt-get install -qq -y mongodb-server ntp
+sed -i "s/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g" /etc/mongodb.conf
+service mongodb restart
