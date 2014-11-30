e6cd476e45b07b7dde25c8011dda19b12d632c30
Zachary Huff <zach.huff.386@gmail.com>
2014-11-30 17:07:10 -0500

    Update node provision script

--- tools/vagrant_provision_node.sh
+++ tools/vagrant_provision_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ntpdate ntp.ubuntu.com
-add-apt-repository -y ppa:pritunl/pritunl-testing
+add-apt-repository -y ppa:pritunl/pritunl-dev
 apt-get update -qq
 apt-get install -qq -y python-flask python-cherrypy3 python-objgraph python-pymongo openvpn htop rng-tools
 echo "HRNGDEVICE=/dev/urandom" | tee /etc/default/rng-tools
