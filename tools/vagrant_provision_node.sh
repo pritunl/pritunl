0f0bebfa68564852c44f6fc2f419c7e075356963
Zachary Huff <zach.huff.386@gmail.com>
2014-12-01 02:52:18 -0500

    Update vagrant provision script

--- tools/vagrant_provision_node.sh
+++ tools/vagrant_provision_node.sh
@@ -2,6 +2,6 @@
 ntpdate ntp.ubuntu.com
 add-apt-repository -y ppa:pritunl/pritunl-dev
 apt-get update -qq
-apt-get install -qq -y python-flask python-cherrypy3 python-objgraph python-pymongo openvpn htop rng-tools
+apt-get install -qq -y python-flask python-objgraph python-pymongo openvpn htop rng-tools
 echo "HRNGDEVICE=/dev/urandom" | tee /etc/default/rng-tools
 /etc/init.d/rng-tools start
