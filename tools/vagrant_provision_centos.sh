3aa9856b25a72be8f089b76f3e4c5ac28e7a2463
Zachary Huff <zach.huff.386@gmail.com>
2014-12-02 21:51:24 -0500

    Remove pymonngo from centos provision script

--- tools/vagrant_provision_centos.sh
+++ tools/vagrant_provision_centos.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
 wget http://mirrors.rit.edu/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 rpm -i epel-release-7-2.noarch.rpm
 
-yum install -y gcc rpm-build redhat-rpm-config python python2-devel python-flask python-pymongo pyOpenSSL
+yum install -y gcc rpm-build redhat-rpm-config python python2-devel python-flask pyOpenSSL
