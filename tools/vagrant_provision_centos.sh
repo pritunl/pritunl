cd1785f5464997efb00a270a54eb5288dad1b9b4
Zachary Huff <zach.huff.386@gmail.com>
2014-12-02 19:40:25 -0500

    Add packages to centos provision script

--- tools/vagrant_provision_centos.sh
+++ tools/vagrant_provision_centos.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+wget http://mirrors.rit.edu/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+rpm -i epel-release-7-2.noarch.rpm
+
+yum install -y rpm-build redhat-rpm-config python python2-devel python-flask python-pymongo pyOpenSSL
