41e1d0832ca7aad3d6f72a435eaacdcec09788a7
Zachary Huff <zach.huff.386@gmail.com>
2014-12-02 21:46:50 -0500

    Add missing packaing in centos provision script

--- tools/vagrant_provision_centos.sh
+++ tools/vagrant_provision_centos.sh
@@ -3,4 +3,4 @@
 wget http://mirrors.rit.edu/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 rpm -i epel-release-7-2.noarch.rpm
 
-yum install -y rpm-build redhat-rpm-config python python2-devel python-flask python-pymongo pyOpenSSL
+yum install -y gcc rpm-build redhat-rpm-config python python2-devel python-flask python-pymongo pyOpenSSL
