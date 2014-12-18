030c486e92ef93883b04fd823ff5078b65ffa069
Zachary Huff <zach.huff.386@gmail.com>
2014-12-17 21:57:51 -0500

    Create new release

--- centos/pritunl.spec
+++ centos/pritunl.spec
@@ -1,5 +1,5 @@
 %define pkgname pritunl
-%define pkgver 1.0.460.81
+%define pkgver 1.0.462.12
 %define pymongo_pkgver 2.7.2
 %define pkgrelease 1
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shlex
 import shutil
 import fileinput
 
-VERSION = '1.0.460.81'
+VERSION = '1.0.462.12'
 PATCH_DIR = 'build'
 install_upstart = True
 install_systemd = True
