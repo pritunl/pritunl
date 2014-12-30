366851036f89992051bf3909b3401b15fb02a6ed
Zachary Huff <zach.huff.386@gmail.com>
2014-12-29 21:42:13 -0500

    Fix spacing in static handler

--- pritunl/handlers/static.py
+++ pritunl/handlers/static.py
@@ -2,12 +2,10 @@ from pritunl.constants import *
 from pritunl.exceptions import *
 from pritunl.helpers import *
 from pritunl import app
-from pritunl import auth
 from pritunl import settings
 from pritunl import static
 from pritunl import auth
 
-import os
 import flask
 
 @app.app.route('/s/<path:file_path>', methods=['GET'])
@@ -18,18 +16,21 @@ def static_get(file_path):
             file_path, cache=True)
     except InvalidStaticFile:
         return flask.abort(404)
+
     return static_file.get_response()
 
 @app.app.route('/favicon.ico', methods=['GET'])
 def favicon_static_get():
     static_file = static.StaticFile(settings.conf.www_path,
         'favicon.ico', cache=True)
+
     return static_file.get_response()
 
 @app.app.route('/robots.txt', methods=['GET'])
 def robots_static_get():
     static_file = static.StaticFile(settings.conf.www_path,
         'robots.txt', cache=True)
+
     return static_file.get_response()
 
 @app.app.route('/', methods=['GET'])
@@ -38,6 +39,7 @@ def index_static_get():
         return flask.redirect('login')
     static_file = static.StaticFile(settings.conf.www_path,
         'index.html', cache=False)
+
     return static_file.get_response()
 
 @app.app.route('/login', methods=['GET'])
