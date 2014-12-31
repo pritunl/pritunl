35f3e2f86fd6af2bf81fedb059a2c32ddf99fc0e
Zachary Huff <zach.huff.386@gmail.com>
2014-12-31 04:12:32 -0500

    Add navbar to components

---
+++ dart/lib/components/navbar/navbar.dart
@@ -0,0 +1,12 @@
+library navbar;
+
+import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+
+@Component(
+    selector: 'navbar',
+    templateUrl: 'packages/pritunl/components/navbar/navbar.html'
+)
+class NavbarComp {
+  NavbarComp() {
+  }
+}
