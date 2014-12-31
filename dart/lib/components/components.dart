2006c060b4df5822e7569bc1f7fbdf09c95b9bd4
Zachary Huff <zach.huff.386@gmail.com>
2014-12-31 04:54:16 -0500

    Add navbar to components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -1,5 +1,6 @@
 library components;
 
+import 'package:pritunl/components/navbar/navbar.dart' as navbar;
 import 'package:pritunl/components/status/status.dart' as status;
 import 'package:pritunl/components/rating/rating.dart' as rating;
 
@@ -7,6 +8,7 @@ import 'package:angular/angular.dart' as ng;
 
 class ComponentsMod extends ng.Module {
   ComponentsMod() {
+    this.bind(navbar.NavbarComp);
     this.bind(rating.RatingComp);
     this.bind(status.StatusComp);
   }
