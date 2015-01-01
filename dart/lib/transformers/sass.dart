cf98b7ae26fe81f0c6ac7554db0fc87397747536
Zachary Huff <zach.huff.386@gmail.com>
2015-01-01 01:59:15 -0500

    Change sass layout

--- dart/lib/transformers/sass.dart
+++ dart/lib/transformers/sass.dart
@@ -13,6 +13,42 @@ class SassError implements Exception {
   toString() => output;
 }
 
+convert(content) {
+  return io.Process.start('scss', ['-I', 'lib']).then((process) {
+    var stdout = new StringBuffer();
+    var stderr = new StringBuffer();
+
+    process.stdin.write(content);
+    process.stdin.writeln();
+    process.stdin.writeln();
+    process.stdin.close();
+
+    process.stdout.transform(conv.UTF8.decoder).listen(
+        (x) => stdout.write(x));
+    process.stderr.transform(conv.UTF8.decoder).listen(
+        (x) => stderr.write(x));
+
+    return process.exitCode.then((exitCode) {
+      if (exitCode == 0) {
+        return stdout.toString();
+      } else {
+        var output;
+
+        if (stderr.length != 0) {
+          output = stderr.toString();
+        }
+        else {
+          output = stdout.toString();
+        }
+
+        throw new SassError(output);
+      }
+    });
+  }).catchError((io.ProcessException error) {
+    throw new SassError(error.toString());
+  }, test: (e) => e is io.ProcessException);
+}
+
 class SassTran extends barback.Transformer {
   SassTran.asPlugin();
 
@@ -20,47 +56,13 @@ class SassTran extends barback.Transformer {
     return new async.Future.value(id.extension == '.scss');
   }
 
-  convert(content) {
-    return io.Process.start('scss', ['-I', 'lib']).then((process) {
-      var stdout = new StringBuffer();
-      var stderr = new StringBuffer();
-
-      process.stdin.write(content);
-      process.stdin.writeln();
-      process.stdin.writeln();
-      process.stdin.close();
-
-      process.stdout.transform(conv.UTF8.decoder).listen(
-          (x) => stdout.write(x));
-      process.stderr.transform(conv.UTF8.decoder).listen(
-          (x) => stderr.write(x));
-
-      return process.exitCode.then((exitCode) {
-        if (exitCode == 0) {
-          return stdout.toString();
-        } else {
-          var output;
-
-          if (stderr.length != 0) {
-            output = stderr.toString();
-          }
-          else {
-            output = stdout.toString();
-          }
-
-          throw new SassError(output);
-        }
-      });
-    }).catchError((io.ProcessException error) {
-      throw new SassError(error.toString());
-    }, test: (e) => e is io.ProcessException);
-  }
-
   apply(transform) {
     return transform.primaryInput.readAsString().then((content) {
       var newId = transform.primaryInput.id.changeExtension('.css');
 
-      return this.convert(content).then((newContent) {
+      print(newId);
+
+      return convert(content).then((newContent) {
         transform.addOutput(new barback.Asset.fromString(newId, newContent));
       });
     });
