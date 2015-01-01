54a402ae7de76a608435e6a093f1597ab452ec29
Zachary Huff <zach.huff.386@gmail.com>
2014-12-31 21:08:29 -0500

    Add convert to sass

--- dart/lib/transformers/sass.dart
+++ dart/lib/transformers/sass.dart
@@ -2,6 +2,8 @@ library sass;
 
 import 'package:barback/barback.dart' as barback;
 import 'dart:async' as async;
+import 'dart:io' as io;
+import 'dart:convert' as conv;
 
 class SassError implements Exception {
   var output;
@@ -15,7 +17,43 @@ class SassTran extends barback.Transformer {
   SassTran.asPlugin();
 
   isPrimary(id) {
-    return new async.Future.value(id.extension == '.sass');
+    return new async.Future.value(id.extension == '.scss');
+  }
+
+  convert(content) {
+    return io.Process.start('scss', []).then((pro*cess) {
+      var stdout = new StringBuffer();
+      var stderr = new StringBuffer();
+
+      process.stdin.write(content);
+      process.stdin.writeln();
+      process.stdin.writeln();
+      process.stdin.close();
+
+      process.stdout.transform(conv.UTF8.decoder).listen(
+          (x) => stdout.write(x));
+      process.stderr.transform(conv.UTF8.decoder).listen(
+          (x) => stderr.write(x));
+
+      return process.exitCode.then((exitCode) {
+        if (exitCode == 0) {
+          return stdout.toString();
+        } else {
+          var output;
+
+          if (stderr.length != 0) {
+            output = stderr.toString();
+          }
+          else {
+            output = stdout.toString();
+          }
+
+          throw new SassError(output);
+        }
+      });
+    }).catchError((io.ProcessException error) {
+      throw new SassError(error.toString());
+    }, test: (e) => e is io.ProcessException);
   }
 
   apply(transform) {
