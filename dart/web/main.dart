e1627529a2e7e70e51aca32f7b391fb446d551b1
Zachary Huff <zach.huff.386@gmail.com>
2015-01-02 14:34:54 -0500

    Remove mod from main

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -4,26 +4,12 @@ import 'package:pritunl/routers/routers.dart' as routers;
 import 'package:pritunl/components/components.dart' as components;
 import 'package:pritunl/decorators/decorators.dart' as decorators;
 
-import 'package:angular/angular.dart' as ng;
 import 'package:angular/application_factory.dart' as appfactory;
-
-class Pritunl extends ng.Module {
-  Pritunl() {
-    this.bind(
-      ng.RouteInitializerFn,
-      toValue: routers.Main
-    );
-    this.bind(
-      ng.NgRoutingUsePushState,
-      toValue: new ng.NgRoutingUsePushState.value(false)
-    );
-  }
-}
 
 void main() {
   appfactory.applicationFactory()
+      .addModule(new routers.RoutersMod())
       .addModule(new components.ComponentsMod())
       .addModule(new decorators.DecoratorsMod())
-      .addModule(new Pritunl())
       .run();
 }
