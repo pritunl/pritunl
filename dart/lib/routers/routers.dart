6a43ae6ad4a43e20caf1c147bb9d1a51ab987a04
Zachary Huff <zach.huff.386@gmail.com>
2015-01-02 14:34:37 -0500

    Add module to routers

--- dart/lib/routers/routers.dart
+++ dart/lib/routers/routers.dart
@@ -1,5 +1,18 @@
 library routers;
 
+import 'package:pritunl/routers/main.dart' as main;
+
 import 'package:angular/angular.dart' as ng;
 
-part 'main.dart';
+class RoutersMod extends ng.Module {
+  RoutersMod() {
+    this.bind(
+      ng.RouteInitializerFn,
+      toValue: main.MainRout
+    );
+    this.bind(
+      ng.NgRoutingUsePushState,
+      toValue: new ng.NgRoutingUsePushState.value(false)
+    );
+  }
+}
