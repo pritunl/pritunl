9c8022c9dee504401f4c79badb0266362c7f71d1
Zachary Huff <zach.huff.386@gmail.com>
2015-01-03 13:38:59 -0500

    Fix path in router

--- dart/lib/routers/main.dart
+++ dart/lib/routers/main.dart
@@ -5,7 +5,7 @@ import 'package:angular/angular.dart' as ng;
 route({path, view}) {
   return ng.ngRoute(
     path: path,
-    view: '/packages/pritunl/views/$view'
+    view: 'packages/pritunl/views/$view'
   );
 }
 
