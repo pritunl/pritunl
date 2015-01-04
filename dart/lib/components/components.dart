687edf591c262cf9d404729f2eb8d57344bcda86
Zachary Huff <zach.huff.386@gmail.com>
2015-01-04 16:35:47 -0500

    Add fraction to components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -4,6 +4,7 @@ import 'package:pritunl/components/box_label/box_label.dart' as box_label;
 import 'package:pritunl/components/navbar/navbar.dart' as navbar;
 import 'package:pritunl/components/status/status.dart' as status;
 import 'package:pritunl/components/rating/rating.dart' as rating;
+import 'package:pritunl/components/fraction/fraction.dart' as fraction;
 import 'package:pritunl/components/glyphicon/glyphicon.dart' as glyphicon;
 
 import 'package:angular/angular.dart' as ng;
@@ -14,6 +15,7 @@ class ComponentsMod extends ng.Module {
     this.bind(navbar.NavbarComp);
     this.bind(rating.RatingComp);
     this.bind(status.StatusComp);
+    this.bind(fraction.FractionComp);
     this.bind(glyphicon.GlyphiconComp);
   }
 }
