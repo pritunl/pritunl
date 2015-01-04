bb94fc2e570db3d7121227d1059a40fc1820e671
Zachary Huff <zach.huff.386@gmail.com>
2015-01-04 17:27:16 -0500

    Fix formatting in components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -1,11 +1,19 @@
 library components;
 
-import 'package:pritunl/components/box_label/box_label.dart' as box_label;
-import 'package:pritunl/components/fraction/fraction.dart' as fraction;
-import 'package:pritunl/components/glyphicon/glyphicon.dart' as glyphicon;
-import 'package:pritunl/components/navbar/navbar.dart' as navbar;
-import 'package:pritunl/components/rating/rating.dart' as rating;
-import 'package:pritunl/components/status/status.dart' as status;
+import 'package:pritunl/components/box_label/box_label.dart'
+  as box_label;
+import 'package:pritunl/components/fraction/fraction.dart'
+  as fraction;
+import 'package:pritunl/components/glyphicon/glyphicon.dart'
+  as glyphicon;
+import 'package:pritunl/components/log_entries/log_entries.dart'
+  as log_entries;
+import 'package:pritunl/components/navbar/navbar.dart'
+  as navbar;
+import 'package:pritunl/components/rating/rating.dart'
+  as rating;
+import 'package:pritunl/components/status/status.dart'
+  as status;
 
 import 'package:angular/angular.dart' as ng;
 
