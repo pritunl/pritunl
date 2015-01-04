2c952f4014d151a4c2ba3d5314a1ace1771002bf
Zachary Huff <zach.huff.386@gmail.com>
2015-01-04 16:43:25 -0500

    Use ng-class for glypicon

--- dart/lib/components/glyphicon/glyphicon.dart
+++ dart/lib/components/glyphicon/glyphicon.dart
@@ -4,7 +4,7 @@ import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
 
 @Component(
   selector: 'glyphicon',
-  template: '<span class="glyphicon glyphicon-{{iconType}}"></span>',
+  template: '<span ng-class="iconClass"></span>',
   cssUrl: 'packages/pritunl/components/glyphicon/glyphicon.css'
 )
 class GlyphiconComp {
