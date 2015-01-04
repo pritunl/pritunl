c242ae7f3060ea9cbbc7a7e831098297bdccb394
Zachary Huff <zach.huff.386@gmail.com>
2015-01-04 17:24:20 -0500

    Fix order in components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -1,21 +1,21 @@
 library components;
 
 import 'package:pritunl/components/box_label/box_label.dart' as box_label;
-import 'package:pritunl/components/navbar/navbar.dart' as navbar;
-import 'package:pritunl/components/status/status.dart' as status;
-import 'package:pritunl/components/rating/rating.dart' as rating;
 import 'package:pritunl/components/fraction/fraction.dart' as fraction;
 import 'package:pritunl/components/glyphicon/glyphicon.dart' as glyphicon;
+import 'package:pritunl/components/navbar/navbar.dart' as navbar;
+import 'package:pritunl/components/rating/rating.dart' as rating;
+import 'package:pritunl/components/status/status.dart' as status;
 
 import 'package:angular/angular.dart' as ng;
 
 class ComponentsMod extends ng.Module {
   ComponentsMod() {
     this.bind(box_label.BoxLabelComp);
+    this.bind(fraction.FractionComp);
+    this.bind(glyphicon.GlyphiconComp);
     this.bind(navbar.NavbarComp);
     this.bind(rating.RatingComp);
     this.bind(status.StatusComp);
-    this.bind(fraction.FractionComp);
-    this.bind(glyphicon.GlyphiconComp);
   }
 }
