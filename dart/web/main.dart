ced0096e0ca8dc69c7991491ffd5702d6c3e5774
Zachary Huff <zach.huff.386@gmail.com>
2015-01-04 20:52:36 -0500

    Add formatters to main module

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -3,6 +3,7 @@ library pritunl;
 import 'package:pritunl/routers/routers.dart' as routers;
 import 'package:pritunl/components/components.dart' as components;
 import 'package:pritunl/decorators/decorators.dart' as decorators;
+import 'package:pritunl/formatters/formatters.dart' as formatters;
 
 import 'package:angular/application_factory.dart' as appfactory;
 
@@ -11,5 +12,6 @@ void main() {
       .addModule(new routers.RoutersMod())
       .addModule(new components.ComponentsMod())
       .addModule(new decorators.DecoratorsMod())
+      .addModule(new formatters.FormattersMod())
       .run();
 }
