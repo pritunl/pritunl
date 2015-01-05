b6ab640a3220b8f964fcea8903ad6a38a10ac821
Zachary Huff <zach.huff.386@gmail.com>
2015-01-05 04:28:08 -0500

    Add active to navbar

--- dart/lib/components/navbar/navbar.dart
+++ dart/lib/components/navbar/navbar.dart
@@ -1,5 +1,7 @@
 library navbar;
 
+import 'package:pritunl/settings/settings.dart' as settings;
+
 import 'package:angular/angular.dart' show Component;
 
 @Component(
@@ -8,6 +10,12 @@ import 'package:angular/angular.dart' show Component;
     cssUrl: 'packages/pritunl/components/navbar/navbar.css'
 )
 class NavbarComp {
+  var active;
+
   NavbarComp() {
+    settings.set('active_page', this);
+    settings.observe('active_page', (_, value) {
+      this.active = {value: 'active'};
+    });
   }
 }
