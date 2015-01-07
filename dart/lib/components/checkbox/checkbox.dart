93232e1d219fb1a9335ee923a322677697f3271e
Zachary Huff <zach.huff.386@gmail.com>
2015-01-07 14:58:08 -0500

    Add checkbox component

---
+++ dart/lib/components/checkbox/checkbox.dart
@@ -0,0 +1,11 @@
+library checkbox;
+
+import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+
+@Component(
+  selector: 'checkbox',
+  template: '<div><glyphicon type="ok"></glyphicon></div>',
+  cssUrl: 'packages/pritunl/components/checkbox/checkbox.css'
+)
+class CheckboxComp {
+}
