7c6b1a14ec4f2cb42adbbf27486d1617ea848c8b
Zachary Huff <zach.huff.386@gmail.com>
2015-01-07 17:03:48 -0500

    Add state to checkbox

--- dart/lib/components/checkbox/checkbox.dart
+++ dart/lib/components/checkbox/checkbox.dart
@@ -4,8 +4,15 @@ import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
 
 @Component(
   selector: 'checkbox',
-  template: '<div><glyphicon type="ok"></glyphicon></div>',
+  template: '<div ng-click="onClick()">'
+    '<glyphicon type="ok" ng-if="state"></glyphicon></div>',
   cssUrl: 'packages/pritunl/components/checkbox/checkbox.css'
 )
 class CheckboxComp {
+  @NgTwoWay('state')
+  var state;
+
+  onClick() {
+    this.state = this.state != true;
+  }
 }
