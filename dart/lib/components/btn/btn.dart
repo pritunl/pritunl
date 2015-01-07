e56f6506e8138c39eb7a44efb8200e91f04f3965
Zachary Huff <zach.huff.386@gmail.com>
2015-01-07 03:18:56 -0500

    Add style attr to btn

--- dart/lib/components/btn/btn.dart
+++ dart/lib/components/btn/btn.dart
@@ -1,6 +1,7 @@
 library btn;
 
 import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'btn',
@@ -8,12 +9,24 @@ import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
     'ng-disabled="disabled"><content></content></button>',
   cssUrl: 'packages/pritunl/components/btn/btn.css'
 )
-class BtnComp {
+class BtnComp implements ng.ShadowRootAware {
   @NgAttr('type')
   var type;
 
   @NgAttr('size')
   var size;
+
+  @NgAttr('width')
+  var width;
+
+  @NgAttr('min-width')
+  var minWidth;
+
+  @NgAttr('height')
+  var height;
+
+  @NgAttr('min-height')
+  var minHeight;
 
   var _disabled;
   @NgAttr('disabled')
@@ -26,6 +39,23 @@ class BtnComp {
     }
     else {
       this._disabled = false;
+    }
+  }
+
+  onShadowRoot(root) {
+    var elem = root.querySelector('button');
+
+    if (this.width != null) {
+      elem.style.width = this.width;
+    }
+    if (this.minWidth != null) {
+      elem.style.minWidth = this.minWidth;
+    }
+    if (this.height != null) {
+      elem.style.height = this.height;
+    }
+    if (this.minHeight != null) {
+      elem.style.minHeight = this.minHeight;
     }
   }
 }
