af5442bb644123ed29c02a0a0866396ebdf233d5
Zachary Huff <zach.huff.386@gmail.com>
2015-01-06 21:58:17 -0500

    Add padding attr to box_label

--- dart/lib/components/box_label/box_label.dart
+++ dart/lib/components/box_label/box_label.dart
@@ -1,13 +1,22 @@
 library box_label;
 
 import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'box-label',
   template: '<span ng-class="type"><content></content></span>',
   cssUrl: 'packages/pritunl/components/box_label/box_label.css'
 )
-class BoxLabelComp {
+class BoxLabelComp implements ng.ShadowRootAware {
   @NgAttr('type')
   var type;
+
+  @NgAttr('padding')
+  var padding;
+
+  onShadowRoot(root) {
+    root.querySelector('span')
+      ..style.padding = this.padding;
+  }
 }
