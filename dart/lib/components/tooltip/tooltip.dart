e8f5c807b7e98781bd71ef670af65e45b1236a37
Zachary Huff <zach.huff.386@gmail.com>
2015-01-08 14:56:15 -0500

    Add shadow root to tooltip

--- dart/lib/components/tooltip/tooltip.dart
+++ dart/lib/components/tooltip/tooltip.dart
@@ -1,13 +1,18 @@
 library tooltip;
 
 import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
 
 @Component(
   selector: 'tooltip',
   templateUrl: 'packages/pritunl/components/tooltip/tooltip.html',
   cssUrl: 'packages/pritunl/components/tooltip/tooltip.css'
 )
-class TooltipComp {
+class TooltipComp implements ng.ShadowRootAware {
+  var element;
+  var root;
+
   @NgTwoWay('state')
   var state;
 
@@ -17,5 +22,9 @@ class TooltipComp {
 
   hide() {
     this.state = false;
+  }
+
+  onShadowRoot(root) {
+    this.root = root;
   }
 }
