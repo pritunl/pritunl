7597a4264d3ca3aed961b35ebfadcc493df75fad
Zachary Huff <zach.huff.386@gmail.com>
2015-01-08 14:55:49 -0500

    Add tooltip to components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -24,6 +24,8 @@ import 'package:pritunl/components/organizations/organizations.dart'
   as organizations;
 import 'package:pritunl/components/status/status.dart'
   as status;
+import 'package:pritunl/components/tooltip/tooltip.dart'
+  as tooltip;
 import 'package:pritunl/components/user/user.dart'
   as user;
 import 'package:pritunl/components/users/users.dart'
@@ -45,6 +47,7 @@ class ComponentsMod extends ng.Module {
     this.bind(organization.OrganizationComp);
     this.bind(organizations.OrganizationsComp);
     this.bind(status.StatusComp);
+    this.bind(tooltip.TooltipComp);
     this.bind(user.UserComp);
     this.bind(users.UsersComp);
   }
