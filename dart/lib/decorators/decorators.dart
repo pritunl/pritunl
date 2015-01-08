3c071e79db3b6bbf2ce151ea2e177d84fe4975c6
Zachary Huff <zach.huff.386@gmail.com>
2015-01-08 14:57:23 -0500

    Add tooltip attach decorator

--- dart/lib/decorators/decorators.dart
+++ dart/lib/decorators/decorators.dart
@@ -1,11 +1,13 @@
 library decorators;
 
 import 'package:pritunl/decorators/noselect.dart' as noselect;
+import 'package:pritunl/decorators/tooltip_attach.dart' as tooltip_attach;
 
 import 'package:angular/angular.dart' as ng;
 
 class DecoratorsMod extends ng.Module {
   DecoratorsMod() {
     this.bind(noselect.NoSelectDec);
+    this.bind(tooltip_attach.TooltipAttachDec);
   }
 }
