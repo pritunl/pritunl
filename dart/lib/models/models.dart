5e429de30133277171a72c83a40faf47694d3390
Zachary Huff <zach.huff.386@gmail.com>
2015-01-09 02:17:28 -0500

    Add models

---
+++ dart/lib/models/models.dart
@@ -0,0 +1,8 @@
+library model;
+
+import 'package:angular/angular.dart' as ng;
+
+class ModelsMod extends ng.Module {
+  ModelsMod() {
+  }
+}
--- dart/web/main.dart
+++ dart/web/main.dart
@@ -4,6 +4,7 @@ import 'package:pritunl/routers/routers.dart' as routers;
 import 'package:pritunl/components/components.dart' as components;
 import 'package:pritunl/decorators/decorators.dart' as decorators;
 import 'package:pritunl/formatters/formatters.dart' as formatters;
+import 'package:pritunl/models/models.dart' as models;
 
 import 'package:angular/application_factory.dart' as appfactory;
 import 'package:angular/animate/module.dart' as animate;
@@ -14,6 +15,7 @@ void main() {
     .addModule(new components.ComponentsMod())
     .addModule(new decorators.DecoratorsMod())
     .addModule(new formatters.FormattersMod())
+    .addModule(new models.ModelsMod())
     .addModule(new animate.AnimationModule())
     .run();
 }
