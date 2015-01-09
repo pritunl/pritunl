f0321676b87d7757e33bb142fb2ae6b117172a6b
Zachary Huff <zach.huff.386@gmail.com>
2015-01-08 19:51:54 -0500

    Fix imports in autofocus

--- dart/lib/decorators/autofocus.dart
+++ dart/lib/decorators/autofocus.dart
@@ -1,8 +1,8 @@
 library autofocus;
 
-import 'dart:html' as dom;
 import 'package:angular/angular.dart' show Decorator;
 import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
 
 @Decorator(
   selector: '[autofocus]'
