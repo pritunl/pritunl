a2e290598ee6c5e929037623a5cd48ac03e0d79f
Zachary Huff <zach.huff.386@gmail.com>
2015-01-09 13:24:58 -0500

    Update models

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -1,11 +1,13 @@
-library model;
+library models;
 
+import 'package:pritunl/models/log_entry.dart' as log_entry;
 import 'package:pritunl/models/status.dart' as status;
 
 import 'package:angular/angular.dart' as ng;
 
 class ModelsMod extends ng.Module {
   ModelsMod() {
+    this.bind(log_entry.LogEntry);
     this.bind(status.Status);
   }
 }
