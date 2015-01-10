f7c61aa875d3a05a373fb02636314e9cba52815b
Zachary Huff <zach.huff.386@gmail.com>
2015-01-10 00:13:45 -0500

    Improve tooltip attach

--- dart/lib/decorators/tooltip_attach.dart
+++ dart/lib/decorators/tooltip_attach.dart
@@ -9,6 +9,7 @@ import 'dart:html' as dom;
 )
 class TooltipAttachDec {
   var element;
+  var state;
 
   @NgAttr('tooltip-attach')
   var tooltipAttach;
@@ -27,17 +28,38 @@ class TooltipAttachDec {
     return ng.ngDirectives(tooltip)[0];
   }
 
-  TooltipAttachDec(dom.Element this.element) {
-    this.element.onMouseEnter.listen((_) {
-      var x = this.element.offsetLeft;
-      var y = this.element.offsetTop;
-      var width = this.element.offsetWidth;
-      var height = this.element.offsetHeight;
+  show() {
+    if (this.state == true) {
+      return;
+    }
+    this.state = true;
 
-      this.tooltip.show(x + (width / 2), y - height  - 10);
-    });
-    this.element.onMouseLeave.listen((_) {
-      this.tooltip.hide();
-    });
+    var x = this.element.offsetLeft;
+    var y = this.element.offsetTop;
+    var width = this.element.offsetWidth;
+    var height = this.element.offsetHeight;
+
+    this.tooltip.show(x + (width / 2), y - height  - 10);
+  }
+
+  hide() {
+    if (this.state != true) {
+      return;
+    }
+    this.state = false;
+    this.tooltip.hide();
+  }
+
+  TooltipAttachDec(dom.Element this.element) {
+    this.element
+      ..onMouseEnter.listen((_) {
+        this.show();
+      })
+      ..onMouseOver.listen((_) {
+        this.show();
+      })
+      ..onMouseLeave.listen((_) {
+        this.hide();
+      });
   }
 }
