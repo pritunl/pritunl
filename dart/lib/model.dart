6bd831b689369a9f5a136eea56c937f572ab8f23
Zachary Huff <zach.huff.386@gmail.com>
2015-01-11 14:53:12 -0500

    Add loading to model

--- dart/lib/model.dart
+++ dart/lib/model.dart
@@ -2,20 +2,38 @@ library model;
 
 import 'package:angular/angular.dart' as ng;
 import 'dart:mirrors' as mirrors;
+import 'dart:async' as async;
+import 'dart:math' as math;
 
 class Model {
+  var _loadCheckId;
   var http;
   var url;
   var errorStatus;
   var errorData;
+  var loading;
 
   Model(ng.Http this.http);
 
   fetch() {
+    var loadCheckId = new math.Random().nextInt(32000);
+    this._loadCheckId = loadCheckId;
+    new async.Future.delayed(
+      new Duration(milliseconds: 200), () {
+        if (this._loadCheckId == loadCheckId) {
+          this.loading = true;
+        }
+      });
+
     return this.http.get(this.url).then((response) {
+      this._loadCheckId = null;
+      this.loading = false;
+
       this.import(response.data);
       return response.data;
     }).catchError((err) {
+      this._loadCheckId = null;
+      this.loading = false;
       this.errorStatus = err.status;
       this.errorData = err.data;
       throw err;
