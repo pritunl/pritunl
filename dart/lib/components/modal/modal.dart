d625018c14b3b23011f32f7e08bf4a4c1d99b99f
Zachary Huff <zach.huff.386@gmail.com>
2015-01-11 21:21:53 -0500

    Add modal component

---
+++ dart/lib/components/modal/modal.dart
@@ -0,0 +1,13 @@
+library modal;
+
+import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+
+@Component(
+  selector: 'modal',
+  templateUrl: 'packages/pritunl/components/modal/modal.html',
+  cssUrl: 'packages/pritunl/components/modal/modal.css'
+)
+class ModalComp {
+  ModalComp() {
+  }
+}
