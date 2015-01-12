e2862b958c155106250b89add9f8da66f625be5e
Zachary Huff <zach.huff.386@gmail.com>
2015-01-12 18:09:42 -0500

    Add form group

---
+++ dart/lib/components/form_group/form_group.dart
@@ -0,0 +1,11 @@
+library form_group;
+
+import 'package:angular/angular.dart' show Component, NgTwoWay, NgAttr;
+
+@Component(
+  selector: 'form-group',
+  template: '<content></content>',
+  cssUrl: 'packages/pritunl/components/form_group/form_group.css'
+)
+class FormGroupComp {
+}
