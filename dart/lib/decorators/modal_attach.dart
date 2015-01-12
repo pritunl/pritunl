19d21cc2e1ee334fad1eae508ea1d2bd0c3e40f2
Zachary Huff <zach.huff.386@gmail.com>
2015-01-12 04:08:44 -0500

    Add modal attach

---
+++ dart/lib/decorators/modal_attach.dart
@@ -0,0 +1,44 @@
+library modal_attach;
+
+import 'package:angular/angular.dart' show Decorator, NgAttr;
+import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
+
+@Decorator(
+  selector: '[modal-attach]'
+)
+class ModalAttachDec {
+  var element;
+  var state;
+
+  @NgAttr('modal-attach')
+  var modalAttach;
+
+  get modal {
+    var modal;
+    var selector = this.modalAttach;
+
+    if (selector != '' && selector != null) {
+      modal = this.element.parent.querySelector(selector);
+    }
+    else {
+      modal = this.element.previousElementSibling;
+    }
+
+    return ng.ngDirectives(modal)[0];
+  }
+
+  show() {
+    this.modal.state = true;
+  }
+
+  hide() {
+    this.modal.state = false;
+  }
+
+  ModalAttachDec(dom.Element this.element) {
+    this.element.onClick.listen((_) {
+      this.show();
+    });
+  }
+}
