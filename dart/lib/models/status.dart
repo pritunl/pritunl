92a543a1ae9ced2afa68b48c5ff0c63f0c2ac8cb
Zachary Huff <zach.huff.386@gmail.com>
2015-01-13 03:26:56 -0500

    Update status model

--- dart/lib/models/status.dart
+++ dart/lib/models/status.dart
@@ -7,17 +7,40 @@ import 'package:angular/angular.dart' as ng;
 
 @Injectable()
 class Status extends model.Model {
-  var org_count;
-  var users_online;
-  var user_count;
-  var servers_online;
-  var server_count;
-  var hosts_online;
-  var host_count;
-  var server_version;
-  var current_host;
-  var public_ip;
-  var local_networks;
+  @model.Attr('org_count')
+  var orgCount;
+
+  @model.Attr('users_online')
+  var usersOnline;
+
+  @model.Attr('user_count')
+  var userCount;
+
+  @model.Attr('servers_online')
+  var serversOnline;
+
+  @model.Attr('server_count')
+  var serverCount;
+
+  @model.Attr('hosts_online')
+  var hostsOnline;
+
+  @model.Attr('host_count')
+  var hostCount;
+
+  @model.Attr('server_version')
+  var serverVersion;
+
+  @model.Attr('current_host')
+  var currentHost;
+
+  @model.Attr('public_ip')
+  var publicIp;
+
+  @model.Attr('local_networks')
+  var localNetworks;
+
+  @model.Attr('notification')
   var notification;
 
   get url {
