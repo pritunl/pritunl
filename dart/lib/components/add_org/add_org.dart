630e4382c92f92a51b16d8ab1ba207a244d28e97
Zachary Huff <zach.huff.386@gmail.com>
2015-01-13 18:47:08 -0500

    Show error border in add org

--- dart/lib/components/add_org/add_org.dart
+++ dart/lib/components/add_org/add_org.dart
@@ -3,19 +3,33 @@ library add_org;
 import 'package:pritunl/models/organization.dart' as organization;
 
 import 'package:angular/angular.dart' show Component;
+import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'add-org',
   templateUrl: 'packages/pritunl/components/add_org/add_org.html',
   cssUrl: 'packages/pritunl/components/add_org/add_org.css'
 )
-class AddOrgComp {
+class AddOrgComp implements ng.ShadowRootAware {
   var org;
+  var root;
 
   AddOrgComp(organization.Organization this.org);
 
+  onShadowRoot(root) {
+    this.root = root;
+  }
+
   add() {
-    this.org.create(['name']);
+    if (this.org.name == null) {
+      var form = this.root.querySelector('form-control');
+      form.classes.add('danger');
+      return false;
+    }
+
+    this.org.create(['name']).then(() {
+      this.org.clear();
+    });
   }
 
   cancel() {
