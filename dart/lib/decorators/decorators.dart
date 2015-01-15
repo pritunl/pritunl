95aa23cc3f0945d45c9d9843ee9c4eedf440ca05
Zachary Huff <zach.huff.386@gmail.com>
2015-01-14 22:21:43 -0500

    Update decorators

--- dart/lib/decorators/decorators.dart
+++ dart/lib/decorators/decorators.dart
@@ -4,7 +4,7 @@ import 'package:pritunl/decorators/autofocus.dart' as autofocus;
 import 'package:pritunl/decorators/modal_attach.dart' as modal_attach;
 import 'package:pritunl/decorators/noselect.dart' as noselect;
 import 'package:pritunl/decorators/shift_click.dart' as shift_click;
-import 'package:pritunl/decorators/tooltip_attach.dart' as tooltip_attach;
+import 'package:pritunl/decorators/tooltip.dart' as tooltip;
 
 import 'package:angular/angular.dart' as ng;
 
@@ -14,6 +14,6 @@ class DecoratorsMod extends ng.Module {
     this.bind(modal_attach.ModalAttachDec);
     this.bind(noselect.NoSelectDec);
     this.bind(shift_click.ShiftClickDec);
-    this.bind(tooltip_attach.TooltipAttachDec);
+    this.bind(tooltip.TooltipDec);
   }
 }
