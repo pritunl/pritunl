1225cea1f88fe4831847bf0f21361ddf57a63b72
Zachary Huff <zach.huff.386@gmail.com>
2015-01-15 00:47:47 -0500

    Change tooltip element name

--- dart/lib/decorators/tooltip.dart
+++ dart/lib/decorators/tooltip.dart
@@ -9,28 +9,28 @@ import 'dart:async' as async;
 )
 class TooltipDec {
   var element;
-  var tooltipElement;
+  var tooltipElem;
 
   @NgAttr('tooltip')
   var tooltip;
 
   show() {
-    if (this.tooltipElement != null) {
+    if (this.tooltipElem != null) {
       return;
     }
     if (this.tooltip == null || this.tooltip == '') {
       return;
     }
 
-    this.tooltipElement = new dom.DivElement();
+    this.tooltipElem = new dom.DivElement();
     var inner = new dom.DivElement();
     var arrow = new dom.DivElement();
 
-    this.tooltipElement.append(inner);
-    this.tooltipElement.append(arrow);
+    this.tooltipElem.append(inner);
+    this.tooltipElem.append(arrow);
     inner.appendText(this.tooltip);
 
-    this.tooltipElement.style
+    this.tooltipElem.style
       ..position = 'absolute'
       ..display = 'inline-block'
       ..zIndex = '1070'
@@ -56,27 +56,27 @@ class TooltipDec {
       ..marginTop = '2px'
       ..marginLeft = '-5px';
 
-    this.element.insertAdjacentElement('beforeBegin', this.tooltipElement);
+    this.element.insertAdjacentElement('beforeBegin', this.tooltipElem);
 
     var bounds = this.element.getBoundingClientRect();
-    var tooltipBounds = this.tooltipElement.getBoundingClientRect();
+    var tooltipBounds = this.tooltipElem.getBoundingClientRect();
     var x = bounds.left + bounds.width / 2 - tooltipBounds.width / 2;
     var y = bounds.top - tooltipBounds.height - 5;
 
-    this.tooltipElement.style
+    this.tooltipElem.style
       ..left = '${x}px'
       ..top = '${y}px'
       ..opacity = '1';
   }
 
   hide() {
-    if (this.tooltipElement == null) {
+    if (this.tooltipElem == null) {
       return;
     }
-    this.tooltipElement.style.opacity = '0';
+    this.tooltipElem.style.opacity = '0';
     new async.Timer(new Duration(milliseconds: 150), () {
-      this.tooltipElement.remove();
-      this.tooltipElement = null;
+      this.tooltipElem.remove();
+      this.tooltipElem = null;
     });
   }
 
