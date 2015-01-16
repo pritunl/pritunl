7e52b6046a7fb72dd788d73d0b2a10144dd4b2ed
Zachary Huff <zach.huff.386@gmail.com>
2015-01-15 20:27:32 -0500

    Add modal base

---
+++ dart/lib/bases/modal.dart
@@ -0,0 +1,39 @@
+library modal;
+
+import 'package:pritunl/utils/utils.dart' as utils;
+
+import 'package:angular/angular.dart' as ng;
+
+class ModalBase implements ng.ShadowRootAware {
+  var root;
+
+  var _alertElem;
+  get alertElem {
+    if (this._alertElem == null) {
+      this._alertElem = this.root.querySelector('alert');
+    }
+    return this._alertElem;
+  }
+
+  var _alert;
+  get alert {
+    if (this._alert == null) {
+      this._alert = utils.getDirective(this.alertElem);
+    }
+    return this._alert;
+  }
+
+  onShadowRoot(root) {
+    this.root = root;
+  }
+
+  reset() {
+    var form = this.root.querySelector('form-control');
+    form.classes.remove('danger');
+    this.alert.text = null;
+  }
+
+  cancel() {
+    this.reset();
+  }
+}
