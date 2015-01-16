a809659967701ef554becf0d60cbe727d4959a02
Zachary Huff <zach.huff.386@gmail.com>
2015-01-15 20:27:50 -0500

    Use modal base in add org

--- dart/lib/components/add_org/add_org.dart
+++ dart/lib/components/add_org/add_org.dart
@@ -1,47 +1,23 @@
 library add_org;
 
+import 'package:pritunl/bases/modal.dart' as modal_base;
 import 'package:pritunl/models/organization.dart' as organization;
-import 'package:pritunl/utils/utils.dart' as utils;
 
 import 'package:angular/angular.dart' show Component;
-import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'add-org',
   templateUrl: 'packages/pritunl/components/add_org/add_org.html',
   cssUrl: 'packages/pritunl/components/add_org/add_org.css'
 )
-class AddOrgComp implements ng.ShadowRootAware {
+class AddOrgComp extends modal_base.ModalBase {
   var org;
-  var root;
 
   AddOrgComp(organization.Organization this.org);
 
-  var _alertElem;
-  get alertElem {
-    if (this._alertElem == null) {
-      this._alertElem = this.root.querySelector('alert');
-    }
-    return this._alertElem;
-  }
-
-  var _alert;
-  get alert {
-    if (this._alert == null) {
-      this._alert = utils.getDirective(this.alertElem);
-    }
-    return this._alert;
-  }
-
-  onShadowRoot(root) {
-    this.root = root;
-  }
-
   reset() {
-    var form = this.root.querySelector('form-control');
-    form.classes.remove('danger');
+    super.reset();
     this.org.clear();
-    this.alert.text = null;
   }
 
   add() {
@@ -62,9 +38,5 @@ class AddOrgComp implements ng.ShadowRootAware {
     this.org.create(['name']).then((_) {
       this.reset();
     });
-  }
-
-  cancel() {
-    this.reset();
   }
 }
