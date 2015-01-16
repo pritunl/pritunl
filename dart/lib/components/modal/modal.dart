0b07032b317f026bc30adb79bee2d34604ee11df
Zachary Huff <zach.huff.386@gmail.com>
2015-01-16 14:33:20 -0500

    Add ok and cancel text to modal

--- dart/lib/components/modal/modal.dart
+++ dart/lib/components/modal/modal.dart
@@ -1,6 +1,6 @@
 library modal;
 
-import 'package:angular/angular.dart' show Component, NgCallback;
+import 'package:angular/angular.dart' show Component, NgAttr, NgCallback;
 
 @Component(
   selector: 'modal',
@@ -16,7 +16,28 @@ class ModalComp {
   @NgCallback('on-cancel')
   var onCancel;
 
-  ModalComp() {
+  var _okText;
+  @NgAttr('ok-text')
+  get okText {
+    if (this._okText != null && this._okText != '') {
+      return this._okText;
+    }
+    return 'Ok';
+  }
+  set okText(val) {
+    this._okText = val;
+  }
+
+  var _cancelText;
+  @NgAttr('cancel-text')
+  get cancelText {
+    if (this._cancelText != null && this._cancelText != '') {
+      return this._cancelText;
+    }
+    return 'Cancel';
+  }
+  set cancelText(val) {
+    this._cancelText = val;
   }
 
   open() {
