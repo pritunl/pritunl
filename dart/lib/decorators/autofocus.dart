9737e2f7fd1550e4bbe6fb46b27b850f734b2cc8
Zachary Huff <zach.huff.386@gmail.com>
2015-01-16 20:12:25 -0500

    Remove comment in autofocus

--- dart/lib/decorators/autofocus.dart
+++ dart/lib/decorators/autofocus.dart
@@ -4,8 +4,6 @@ import 'package:angular/angular.dart' show Decorator;
 import 'package:angular/angular.dart' as ng;
 import 'dart:html' as dom;
 
-// TODO Not working
-
 @Decorator(
   selector: '[autofocus]'
 )
