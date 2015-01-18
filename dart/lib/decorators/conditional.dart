c03de838b697cfd841a47b820b57ec6e99ef2bd0
Zachary Huff <zach.huff.386@gmail.com>
2015-01-17 20:25:11 -0500

    Add conditional

---
+++ dart/lib/decorators/conditional.dart
@@ -0,0 +1,36 @@
+library conditional;
+
+import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
+
+abstract class Conditional {
+  final ng.ViewFactory _viewFactory;
+  final ng.ViewPort _viewPort;
+  final ng.Scope _scope;
+  dom.Element element;
+  ng.View _view;
+
+  Conditional(this._viewFactory, this._viewPort, this._scope);
+
+  void set condition(value) {
+    if (value == true) {
+      this._ensureViewExists();
+    } else {
+      this._ensureViewDestroyed();
+    }
+  }
+
+  void _ensureViewExists() {
+    if (this._view == null) {
+      this._view = this._viewPort.insertNew(this._viewFactory);
+      this.element = this._view.nodes[0];
+    }
+  }
+
+  void _ensureViewDestroyed() {
+    if (this._view != null) {
+      this._viewPort.remove(this._view);
+      this._view = null;
+    }
+  }
+}
