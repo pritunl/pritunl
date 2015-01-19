1a2905797b1ab79dc35ef2b04f2925a9e2a453b7
Zachary Huff <zach.huff.386@gmail.com>
2015-01-18 19:52:43 -0500

    Use control base for form select

--- dart/lib/components/form_select/form_select.dart
+++ dart/lib/components/form_select/form_select.dart
@@ -1,5 +1,8 @@
 library form_select_comp;
 
+import 'package:pritunl/bases/form_control/form_control.dart' as
+  form_control_base;
+
 import 'package:angular/angular.dart' show Component, NgCallback, NgTwoWay,
   NgOneWay, NgAttr;
 import 'package:angular/angular.dart' as ng;
@@ -13,44 +16,8 @@ import 'dart:html' as dom;
     '{{optText({\'\$model\': colModel})}}</option></select>',
   cssUrl: 'packages/pritunl/components/form_select/form_select.css'
 )
-class FormSelectComp implements ng.ShadowRootAware {
-  dom.Element element;
-
-  var _width;
-  @NgAttr('width')
-  String get width {
-    return this._width;
-  }
-  set width(String width) {
-    if (this.element != null) {
-      this.element.style.width = width;
-    }
-    this._width = width;
-  }
-
-  var _height;
-  @NgAttr('height')
-  String get height {
-    return this._height;
-  }
-  set height(String height) {
-    if (this.element != null) {
-      this.element.style.height = height;
-    }
-    this._height = height;
-  }
-
-  var _padding;
-  @NgAttr('padding')
-  String get padding {
-    return this._padding;
-  }
-  set padding(String padding) {
-    if (this.element != null) {
-      this.element.style.padding = padding;
-    }
-    this._padding = padding;
-  }
+class FormSelectComp extends form_control_base.FormControlBase {
+  String controlSelector = 'select';
 
   @NgAttr('form-tooltip')
   String formTooltip;
