58463899fd9a012c06bd000672be3dda7f93242a
Zachary Huff <zach.huff.386@gmail.com>
2015-01-20 04:36:23 -0500

    Fix import in orgs

--- dart/lib/collections/organizations.dart
+++ dart/lib/collections/organizations.dart
@@ -1,14 +1,14 @@
 library organization_col;
 
 import 'package:pritunl/collection.dart' as collection;
-import 'package:pritunl/models/organization.dart' as organization;
+import 'package:pritunl/models/organization.dart' as org;
 
 import 'package:angular/angular.dart' show Injectable;
 import 'package:angular/angular.dart' as ng;
 
 @Injectable()
 class Organizations extends collection.Collection {
-  Type model = organization.Organization;
+  Type model = org.Organization;
 
   String get url {
     return '/organization';
