5dcac1d16ead72cc5b5ffdd3f5d0c89421f42263
Zachary Huff <zach.huff.386@gmail.com>
2015-01-20 18:15:38 -0500

    Update add user bulk

--- dart/lib/components/add_user_bulk/add_user_bulk.dart
+++ dart/lib/components/add_user_bulk/add_user_bulk.dart
@@ -1,10 +1,11 @@
 library add_user_bulk_comp;
 
+import 'package:pritunl/collections/organizations.dart' as organizations;
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
-import 'package:pritunl/models/user_bulk.dart' as user_bulk;
+import 'package:pritunl/collections/users.dart' as usrs;
 
-import 'package:angular/angular.dart' show Component;
+import 'package:angular/angular.dart' show Component, NgOneWay;
 import 'dart:async' as async;
 
 @Component(
@@ -13,16 +14,53 @@ import 'dart:async' as async;
 )
 class AddUserBulkComp extends modal_content.ModalContent {
   String users;
+  usrs.Users model;
 
-  user_bulk.UserBulk model;
+  @NgOneWay('orgs')
+  organizations.Organizations orgs;
 
   AddUserBulkComp(this.model);
 
   void submit(async.Future closeHandler()) {
-    print(this.users);
+    var usersLines;
 
-    return;
-    this.model.create(['name']).then((_) {
+    if (this.users != null) {
+      usersLines = this.users.split('\n');
+    }
+
+    if (usersLines == null || usersLines.length == 0) {
+      this.setFormError('.users', 'User list cannot be empty');
+      return;
+    }
+
+    for(var line in usersLines) {
+      var data = {};
+      line = line.split(',');
+
+      if (line[0] == '') {
+        continue;
+      }
+
+      data['name'] = line[0];
+
+      if (line.length > 1 && line[1] != '') {
+        data['email'] = line[1];
+      }
+
+      this.model.add(data);
+    }
+
+    var valid = this.validateForms({
+      'name': '.users',
+      'email': '.users',
+    });
+
+    if (valid != true) {
+      this.model.clear();
+      return;
+    }
+
+    this.model.create(['name', 'email']).then((_) {
       super.submit(closeHandler);
     });
   }
