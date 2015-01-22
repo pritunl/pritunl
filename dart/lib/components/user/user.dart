f03ababa10d7f11e9c9a7d04b01b77a59101770e
Zachary Huff <zach.huff.386@gmail.com>
2015-01-22 13:10:02 -0500

    Update user component

--- dart/lib/components/user/user.dart
+++ dart/lib/components/user/user.dart
@@ -1,9 +1,9 @@
 library user_comp;
 
-import 'package:pritunl/collections/users.dart' as users;
 import 'package:pritunl/models/user.dart' as user;
 
-import 'package:angular/angular.dart' show Component, NgAttr, NgTwoWay;
+import 'package:angular/angular.dart' show Component, NgOneWay, NgTwoWay,
+  NgCallback;
 
 @Component(
   selector: 'user',
@@ -11,14 +11,15 @@ import 'package:angular/angular.dart' show Component, NgAttr, NgTwoWay;
   cssUrl: 'packages/pritunl/components/user/user.css'
 )
 class UserComp {
-  bool showServers;
-
-  @NgTwoWay('model')
+  @NgOneWay('model')
   user.User model;
 
-  @NgTwoWay('collection')
-  users.Users collection;
-
-  @NgAttr('selected')
+  @NgTwoWay('selected')
   bool selected;
+
+  @NgTwoWay('show-servers')
+  bool showServers;
+
+  @NgOneWay('show-hidden')
+  bool showHidden;
 }
