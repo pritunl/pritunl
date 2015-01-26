0c48cbe633ac4f8b3c42c80438d7dbddce59b3a0
Zachary Huff <zach.huff.386@gmail.com>
2015-01-26 01:25:39 -0500

    Add key to models

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -1,5 +1,6 @@
 library models;
 
+import 'package:pritunl/models/key.dart' as key;
 import 'package:pritunl/models/log_entry.dart' as log_entry;
 import 'package:pritunl/models/organization.dart' as organization;
 import 'package:pritunl/models/status.dart' as status;
@@ -9,6 +10,7 @@ import 'package:angular/angular.dart' as ng;
 
 class ModelsMod extends ng.Module {
   ModelsMod() {
+    this.bind(key.Key);
     this.bind(log_entry.LogEntry);
     this.bind(organization.Organization);
     this.bind(status.Status);
