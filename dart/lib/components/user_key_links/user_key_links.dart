c82ccb297e78b0f17c1f41050e99d28acc37a5ea
Zachary Huff <zach.huff.386@gmail.com>
2015-01-26 01:24:37 -0500

    Get user key on open

--- dart/lib/components/user_key_links/user_key_links.dart
+++ dart/lib/components/user_key_links/user_key_links.dart
@@ -3,18 +3,27 @@ library user_key_links_comp;
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
 import 'package:pritunl/models/user.dart' as user;
+import 'package:pritunl/models/key.dart' as ky;
 
 import 'package:angular/angular.dart' show Component, NgOneWayOneTime;
 import 'dart:async' as async;
 
 @Component(
   selector: 'user-key-links',
-  templateUrl: 'packages/pritunl/components/user_key_links/user_key_links.html'
+  templateUrl: 'packages/pritunl/components/user_key_links/user_key_links.html',
+  cssUrl: 'packages/pritunl/components/user_key_links/user_key_links.css'
 )
 class UserKeyLinksComp extends modal_content.ModalContent {
+  ky.Key model;
+
   @NgOneWayOneTime('model')
-  user.User model;
+  user.User user;
+
+  UserKeyLinksComp(this.model);
 
   void show() {
+    this.model.user = this.user.id;
+    this.model.org = this.user.organization;
+    this.model.fetch();
   }
 }
