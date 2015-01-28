d8e3f8f810297057d80de572fcccea36da83c15b
Zachary Huff <zach.huff.386@gmail.com>
2015-01-28 03:54:50 -0500

    Add qrcode text to user otp

--- dart/lib/components/user_otp/user_otp.dart
+++ dart/lib/components/user_otp/user_otp.dart
@@ -5,20 +5,21 @@ import 'package:pritunl/bases/modal_content/modal_content.dart' as
 import 'package:pritunl/models/user.dart' as user;
 
 import 'package:angular/angular.dart' show Component, NgOneWayOneTime;
-import 'package:angular/angular.dart' as ng;
-import 'dart:html' as dom;
 
 @Component(
   selector: 'user-otp',
   templateUrl: 'packages/pritunl/components/user_otp/user_otp.html',
   cssUrl: 'packages/pritunl/components/user_otp/user_otp.css'
 )
-class UserOtpComp extends modal_content.ModalContent implements
-    ng.ShadowRootAware {
+class UserOtpComp extends modal_content.ModalContent {
   @NgOneWayOneTime('model')
   user.User model;
 
-  void onShadowRoot(dom.ShadowRoot root) {
+  String get qrcodeText {
+    return 'otpauth://totp/${this.model.name}@'
+      '${this.model.organizationName}?secret=${this.model.otpSecret}';
+  }
 
+  void newKey() {
   }
 }
