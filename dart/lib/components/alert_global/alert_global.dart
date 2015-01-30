d4be899a382d07853de6a4bf33eb5e3929012f2a
Zachary Huff <zach.huff.386@gmail.com>
2015-01-30 17:58:11 -0500

    Add dismissable to alert global

--- dart/lib/components/alert_global/alert_global.dart
+++ dart/lib/components/alert_global/alert_global.dart
@@ -8,9 +8,13 @@ import 'dart:collection' as collection;
 @Component(
   selector: 'alert-global',
   template: '<alert ng-repeat="alert in alerts" type="alert.type" '
-    'text="alert.text"></alert>',
+    'text="alert.text" on-dismiss="onDismiss(alert)" dismissible></alert>',
   cssUrl: 'packages/pritunl/components/alert_global/alert_global.css'
 )
 class AlertGlobalComp {
   collection.Queue<alrt.Alert> alerts = alrt.alerts;
+
+  void onDismiss(alrt.Alert alert) {
+    this.alerts.remove(alert);
+  }
 }
