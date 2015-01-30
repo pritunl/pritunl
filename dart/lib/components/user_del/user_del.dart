02dbc18b181aed7d2fbf0d3e0de32eb32f79cf83
Zachary Huff <zach.huff.386@gmail.com>
2015-01-29 20:21:42 -0500

    Add destroy users to user del

--- dart/lib/components/user_del/user_del.dart
+++ dart/lib/components/user_del/user_del.dart
@@ -2,7 +2,7 @@ library user_del;
 
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
-import 'package:pritunl/models/user.dart' as user;
+import 'package:pritunl/models/user.dart' as usr;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 import 'dart:async' as async;
@@ -14,9 +14,22 @@ import 'dart:async' as async;
 )
 class UserDelComp extends modal_content.ModalContent {
   @NgOneWay('users')
-  Set<user.User> users;
+  Set<usr.User> users;
+
+  async.Future _destroyUsers(Iterator<usr.User> users) {
+    users.moveNext();
+    if (users.current == null) {
+      return null;
+    }
+
+    return users.current.destroy().then((_) {
+      return this._destroyUsers(users);
+    });
+  }
 
   void submit(async.Future closeHandler()) {
-    super.submit(closeHandler);
+    this._destroyUsers(this.users.iterator).then((_) {
+      super.submit(closeHandler);
+    });
   }
 }
