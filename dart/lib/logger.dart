9bfb117863e41d950768c559ab56ae76749c5633
Zachary Huff <zach.huff.386@gmail.com>
2015-01-30 19:52:27 -0500

    Add warning to logger

--- dart/lib/logger.dart
+++ dart/lib/logger.dart
@@ -11,30 +11,42 @@ void setup() {
   });
 }
 
-void config(message, [Object error, StackTrace stackTrace]) {
-  logging.Logger.root.config(message, error, stackTrace);
-}
-
-void fine(message, [Object error, StackTrace stackTrace]) {
-  logging.Logger.root.fine(message, error, stackTrace);
-}
-
-void finer(message, [Object error, StackTrace stackTrace]) {
-  logging.Logger.root.finer(message, error, stackTrace);
-}
-
+// Debug
 void finest(message, [Object error, StackTrace stackTrace]) {
   logging.Logger.root.finest(message, error, stackTrace);
 }
 
+// Debug
+void finer(message, [Object error, StackTrace stackTrace]) {
+  logging.Logger.root.finer(message, error, stackTrace);
+}
+
+// Debug
+void fine(message, [Object error, StackTrace stackTrace]) {
+  logging.Logger.root.fine(message, error, stackTrace);
+}
+
+// Debug
+void config(message, [Object error, StackTrace stackTrace]) {
+  logging.Logger.root.config(message, error, stackTrace);
+}
+
+// Info
 void info(message, [Object error, StackTrace stackTrace]) {
   logging.Logger.root.config(message, error, stackTrace);
 }
 
+// Warning
+void warning(message, [Object error, StackTrace stackTrace]) {
+  logging.Logger.root.warning(message, error, stackTrace);
+}
+
+// Error
 void severe(message, [Object error, StackTrace stackTrace]) {
   logging.Logger.root.severe(message, error, stackTrace);
 }
 
+// Critical
 void shout(message, [Object error, StackTrace stackTrace]) {
   logging.Logger.root.shout(message, error, stackTrace);
 }
