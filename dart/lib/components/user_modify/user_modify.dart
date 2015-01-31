9d44cfa85f17ce2637cef9b6e15bab9c7aa97892
Zachary Huff <zach.huff.386@gmail.com>
2015-01-31 01:49:56 -0500

    Add alerts to user modify

--- dart/lib/components/user_modify/user_modify.dart
+++ dart/lib/components/user_modify/user_modify.dart
@@ -3,6 +3,8 @@ library user_modify_comp;
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
 import 'package:pritunl/models/user.dart' as user;
+import 'package:pritunl/alert.dart' as alrt;
+import 'package:pritunl/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWayOneTime;
 import 'dart:async' as async;
@@ -21,18 +23,24 @@ class UserModifyComp extends modal_content.ModalContent {
     this.model = this.origModel.clone();
   }
 
-  void submit(async.Future closeHandler()) {
+  async.Future submit(async.Future closeHandler()) {
     var valid = this.validateForms({
       'name': '.name',
       'email': '.email',
     });
 
     if (valid != true) {
-      return;
+      return null;
     }
 
-    this.model.save(['name', 'email']).then((_) {
-      super.submit(closeHandler);
+    return this.model.save(['name', 'email']).then((_) {
+      return super.submit(closeHandler);
+    }).then((_) {
+      new alrt.Alert('Successfully modified user.', 'success');
+    }).catchError((err) {
+      logger.severe('Failed to modified user', err);
+      this.setAlert('Failed to modified user, server error occurred.',
+        'danger');
     });
   }
 }
