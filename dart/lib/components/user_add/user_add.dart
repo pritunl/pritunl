8819db58c2697565fadc1fcad572d3976d9c4dfc
Zachary Huff <zach.huff.386@gmail.com>
2015-01-31 04:34:29 -0500

    Add missing import in user add

--- dart/lib/components/user_add/user_add.dart
+++ dart/lib/components/user_add/user_add.dart
@@ -5,6 +5,7 @@ import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
 import 'package:pritunl/models/user.dart' as user;
 import 'package:pritunl/alert.dart' as alrt;
+import 'package:pritunl/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 import 'dart:async' as async;
