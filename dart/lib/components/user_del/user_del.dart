d033997e114c058604e061d2a238e7c6f570dd00
Zachary Huff <zach.huff.386@gmail.com>
2015-01-31 01:49:08 -0500

    Add alerts to user del

--- dart/lib/components/user_del/user_del.dart
+++ dart/lib/components/user_del/user_del.dart
@@ -3,6 +3,8 @@ library user_del;
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
 import 'package:pritunl/models/user.dart' as usr;
+import 'package:pritunl/alert.dart' as alrt;
+import 'package:pritunl/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 import 'dart:async' as async;
@@ -27,9 +29,15 @@ class UserDelComp extends modal_content.ModalContent {
     });
   }
 
-  void submit(async.Future closeHandler()) {
-    this._destroyUsers(this.users.iterator).then((_) {
-      super.submit(closeHandler);
+  async.Future submit(async.Future closeHandler()) {
+    return this._destroyUsers(this.users.iterator).then((_) {
+      return super.submit(closeHandler);
+    }).then((_) {
+      new alrt.Alert('Successfully added user.', 'success');
+    }).catchError((err) {
+      logger.severe('Failed to delete users', err);
+      this.setAlert('Failed to delete users, server error occurred.',
+        'danger');
     });
   }
 }
