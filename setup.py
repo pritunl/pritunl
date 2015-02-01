2651feecbbbc0f32ad6a5c57e58884ed5ecf80a5
Zachary Huff <zach.huff.386@gmail.com>
2015-02-01 04:46:32 -0500

    Remove pymongo package ver

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'flask>=0.8',
-        'pymongo>=2.7.1',
+        'pymongo', # TODO utopic package missing ver files - pymongo>=2.7.1
     ],
     data_files=data_files,
     entry_points={
