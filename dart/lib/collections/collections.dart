980f48ba46982af2824959017c75dfb196893412
Zachary Huff <zach.huff.386@gmail.com>
2015-02-02 14:49:32 -0500

    Add events to collections

--- dart/lib/collections/collections.dart
+++ dart/lib/collections/collections.dart
@@ -1,5 +1,6 @@
 library collections;
 
+import 'package:pritunl/collections/events.dart' as events;
 import 'package:pritunl/collections/log_entries.dart' as log_entries;
 import 'package:pritunl/collections/organizations.dart' as organizations;
 import 'package:pritunl/collections/users.dart' as users;
@@ -8,6 +9,7 @@ import 'package:angular/angular.dart' as ng;
 
 class CollectionsMod extends ng.Module {
   CollectionsMod() {
+    this.bind(events.Events);
     this.bind(log_entries.LogEntries);
     this.bind(organizations.Organizations);
     this.bind(users.Users);
