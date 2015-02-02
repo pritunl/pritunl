14efe4bc730fe1410778a37d97c3a9eaa86d7d7e
Zachary Huff <zach.huff.386@gmail.com>
2015-02-02 14:10:46 -0500

    Remove unused import in user_email

--- dart/lib/components/user_email/user_email.dart
+++ dart/lib/components/user_email/user_email.dart
@@ -3,7 +3,6 @@ library user_email_comp;
 import 'package:pritunl/bases/modal_content/modal_content.dart' as
   modal_content;
 import 'package:pritunl/models/user.dart' as usr;
-import 'package:pritunl/alert.dart' as alrt;
 import 'package:pritunl/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
