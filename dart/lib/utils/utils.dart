b75f588d4da2b19e80680fc5d4f7b5c5a2d9c91c
Zachary Huff <zach.huff.386@gmail.com>
2015-02-02 00:35:18 -0500

    Add http error to utils

--- dart/lib/utils/utils.dart
+++ dart/lib/utils/utils.dart
@@ -1,7 +1,9 @@
 library utils;
 
+import 'package:angular/angular.dart' as ng;
 import 'package:angular/introspection.dart' as introspection;
 import 'dart:html' as dom;
+import 'dart:convert' as convert;
 
  dynamic getDirective(dom.Node node, [var type]) {
   while (node != null) {
@@ -36,4 +38,30 @@ import 'dart:html' as dom;
 
 String getDomain() {
   return '${dom.window.location.protocol}//${dom.window.location.host}';
+}
+
+class HttpError {
+  static var _JSON_START = new RegExp(r'^\s*(\[|\{[^\{])');
+  static var _JSON_END = new RegExp(r'[\}\]]\s*$');
+  static var _PROTECTION_PREFIX = new RegExp('^\\)\\]\\}\',?\\n');
+
+  ng.HttpResponse resp;
+  String error;
+  String errorMsg;
+
+  HttpError(ng.HttpResponse err) {
+    if (err.data is String) {
+      var data = err.data.replaceFirst(_PROTECTION_PREFIX, '');
+      if (data.contains(_JSON_START) && data.contains(_JSON_END)) {
+        data = convert.JSON.decode(data);
+      }
+
+      this.resp = new ng.HttpResponse.copy(err, data: data);
+      this.error = data['error'];
+      this.errorMsg = data['error_msg'];
+    }
+    else {
+      this.resp = err;
+    }
+  }
 }
