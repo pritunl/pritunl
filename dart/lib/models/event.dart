61e789062909150ecadfaf280f33bca37a8f34d8
Zachary Huff <zach.huff.386@gmail.com>
2015-02-02 14:35:39 -0500

    Add event model

---
+++ dart/lib/models/event.dart
@@ -0,0 +1,23 @@
+library event_mod;
+
+import 'package:pritunl/model.dart' as model;
+
+import 'package:angular/angular.dart' show Injectable;
+import 'package:angular/angular.dart' as ng;
+
+@Injectable()
+class Event extends model.Model {
+  @model.Attribute('id')
+  String id;
+
+  @model.Attribute('type')
+  String type;
+
+  @model.Attribute('resource_id')
+  String resourceId;
+
+  @model.Attribute('timestamp')
+  String timestamp;
+
+  Event(ng.Http http) : super(http);
+}
