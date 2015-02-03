9a11b711ee7b66c3918b9d9cfd94db17e535db1e
Zachary Huff <zach.huff.386@gmail.com>
2015-02-02 20:57:23 -0500

    Call events in collection

--- dart/lib/collections/events.dart
+++ dart/lib/collections/events.dart
@@ -3,6 +3,7 @@ library events_col;
 import 'package:pritunl/collection.dart' as collection;
 import 'package:pritunl/models/event.dart' as event;
 import 'package:pritunl/logger.dart' as logger;
+import 'package:pritunl/event.dart' as evnt;
 
 import 'package:angular/angular.dart' show Injectable;
 import 'package:angular/angular.dart' as ng;
@@ -28,6 +29,22 @@ class Events extends collection.Collection {
   void imported() {
     if (this.length > 0) {
       this.cursor = this[this.length - 1].id;
+    }
+
+    for (var event in this) {
+      var listenerSets = [event.type];
+
+      if (event.resourceId != null) {
+        listenerSets.add('${event.type}:${event.resourceId}');
+      }
+
+      listenerSets.forEach((listeners) {
+        if (listeners != null) {
+          listeners.forEach((listener) {
+            listener(event);
+          });
+        }
+      });
     }
   }
 
