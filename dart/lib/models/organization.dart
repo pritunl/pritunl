2fa1a269bd3a0a4d753807633d1194a2f66846fa
Zachary Huff <zach.huff.386@gmail.com>
2015-02-04 18:47:04 -0500

    Change import names in org

--- dart/lib/models/organization.dart
+++ dart/lib/models/organization.dart
@@ -1,35 +1,32 @@
 library organization_mod;
 
-import 'package:pritunl/model.dart' as model;
+import 'package:pritunl/model.dart' as mdl;
 import 'package:pritunl/collections/users.dart' as usrs;
 
 import 'package:angular/angular.dart' show Injectable;
 import 'package:angular/angular.dart' as ng;
 
 @Injectable()
-class Organization extends model.Model {
-  @model.Linked()
+class Organization extends mdl.Model {
   usrs.Users users;
 
-  @model.Attribute('name')
+  @mdl.Attribute('name')
   String name;
 
-  @model.Validator('name')
+  @mdl.Validator('name')
   void nameValidator(val) {
     if (val == null || val == '') {
-      throw new model.Invalid('empty', 'Organization name cannot be empty');
+      throw new mdl.Invalid('empty', 'Organization name cannot be empty');
     }
   }
 
-  @model.Attribute('user_count')
+  @mdl.Attribute('user_count')
   int userCount;
 
-  @model.Attribute('id')
+  @mdl.Attribute('id')
   String id;
 
-  Organization(ng.Http http) :
-    users = new usrs.Users(http),
-    super(http);
+  Organization(ng.Http http) : super(http);
 
   String get url {
     var url = '/organization';
