e781ec5288c97d672770a99349c1957aec673350
Zachary Huff <zach.huff.386@gmail.com>
2015-02-08 21:00:24 -0500

    Add attrs to server model

--- dart/lib/models/server.dart
+++ dart/lib/models/server.dart
@@ -26,6 +26,63 @@ class Server extends mdl.Model {
   @mdl.Attribute('status')
   String status;
 
+  @mdl.Attribute('uptime')
+  int uptime;
+
+  @mdl.Attribute('user_count')
+  int user_count;
+
+  @mdl.Attribute('users_online')
+  int users_online;
+
+  @mdl.Attribute('devices_online')
+  int devices_online;
+
+  @mdl.Attribute('network')
+  String network;
+
+  @mdl.Attribute('bind_address')
+  String bind_address;
+
+  @mdl.Attribute('port')
+  int port;
+
+  @mdl.Attribute('protocol')
+  String protocol;
+
+  @mdl.Attribute('dh_param_bits')
+  int dh_param_bits;
+
+  @mdl.Attribute('mode')
+  String mode;
+
+  @mdl.Attribute('multi_device')
+  bool multi_device;
+
+  @mdl.Attribute('local_networks')
+  List<String> local_networks;
+
+  @mdl.Attribute('dns_servers')
+  List<String> dns_servers;
+
+  @mdl.Attribute('search_domain')
+  String search_domain;
+
+  @mdl.Attribute('otp_auth')
+  bool otp_auth;
+
+  @mdl.Attribute('cipher')
+  String cipher;
+
+  @mdl.Attribute('jumbo_frames')
+  bool jumbo_frames;
+
+  @mdl.Attribute('lzo_compression')
+  bool lzo_compression;
+
+  @mdl.Attribute('debug')
+  bool debug;
+
   Server(ng.Http http) : super(http);
 
   String get url {
