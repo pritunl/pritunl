9644a9c351a669fc8099035b5ac2acf629d2002f
Zachary Huff <zach.huff.386@gmail.com>
2015-02-09 18:43:31 -0500

    Update formatters

--- dart/lib/formatters/formatters.dart
+++ dart/lib/formatters/formatters.dart
@@ -1,11 +1,13 @@
 library formatters;
 
+import 'package:pritunl/formatters/capitalize.dart' as capitalize;
 import 'package:pritunl/formatters/datetime.dart' as datetime;
 
 import 'package:angular/angular.dart' as ng;
 
 class FormattersMod extends ng.Module {
   FormattersMod() {
+    this.bind(capitalize.CapitalizeForm);
     this.bind(datetime.DatetimeForm);
   }
 }
