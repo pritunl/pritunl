9671e915043f255bccf87e09337c5a3fffb88bc1
Zachary Huff <zach.huff.386@gmail.com>
2015-02-12 19:10:57 -0500

    Fetch output on attach

--- dart/lib/components/server/server.dart
+++ dart/lib/components/server/server.dart
@@ -1,20 +1,34 @@
 library server_comp;
 
 import 'package:pritunl/models/server.dart' as svr;
+import 'package:pritunl/alert.dart' as alrt;
+import 'package:pritunl/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWayOneTime;
+import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'server',
   templateUrl: 'packages/pritunl/components/server/server.html',
   cssUrl: 'packages/pritunl/components/server/server.css'
 )
-class ServerComp {
+class ServerComp implements ng.AttachAware {
+  bool showHidden;
+
   @NgOneWayOneTime('model')
   svr.Server model;
-  bool showHidden;
 
   void toggleHidden() {
     this.showHidden = this.showHidden != true;
+  }
+
+  void attach() {
+    this.model.output.fetch().then((_) {
+      print(this.model.output.output);
+    }).catchError((err) {
+      logger.severe('Failed to load server output', err);
+      new alrt.Alert('Failed to load server output, server error occurred.',
+      'danger');
+    });
   }
 }
