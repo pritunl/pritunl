cab6194b46eec49311348a13f2994ad37d635919
Zachary Huff <zach.huff.386@gmail.com>
2015-02-12 19:12:25 -0500

    Add server output to models

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -5,6 +5,7 @@ import 'package:pritunl/models/key.dart' as ky;
 import 'package:pritunl/models/log_entry.dart' as log_ent;
 import 'package:pritunl/models/organization.dart' as organization;
 import 'package:pritunl/models/server.dart' as server;
+import 'package:pritunl/models/server_output.dart' as svr_output;
 import 'package:pritunl/models/status.dart' as stus;
 import 'package:pritunl/models/user.dart' as usr;
 
@@ -17,6 +18,7 @@ class ModelsMod extends ng.Module {
     this.bind(log_ent.LogEntry);
     this.bind(organization.Organization);
     this.bind(server.Server);
+    this.bind(svr_output.ServerOutput);
     this.bind(stus.Status);
     this.bind(usr.User);
   }
