0860e2d816959cd101a8cf3388d8536034e62c19
Zachary Huff <zach.huff.386@gmail.com>
2015-02-13 17:23:05 -0500

    Add server id to server orgs collection

--- dart/lib/collections/server_orgs.dart
+++ dart/lib/collections/server_orgs.dart
@@ -1,18 +1,19 @@
 library organizations_col;
 
 import 'package:pritunl/collection.dart' as collec;
-import 'package:pritunl/models/organization.dart' as organization;
+import 'package:pritunl/models/server_org.dart' as svr_org;
 
 import 'package:angular/angular.dart' show Injectable;
 import 'package:angular/angular.dart' as ng;
 
 @Injectable()
 class ServerOrgs extends collec.Collection {
-  Type model = organization.Organization;
+  Type model = svr_org.ServerOrg;
+  String server;
 
-  Organizations(ng.Http http) : super(http);
+  ServerOrgs(ng.Http http, this.server) : super(http);
 
   String get url {
-    return '/organization';
+    return '/server/${this.server}/organization';
   }
 }
