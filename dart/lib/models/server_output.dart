454a27c1bb2910d3919273e883b2d93c04701d6e
Zachary Huff <zach.huff.386@gmail.com>
2015-02-12 19:13:20 -0500

    Use server for id in server output

--- dart/lib/models/server_output.dart
+++ dart/lib/models/server_output.dart
@@ -1,21 +1,21 @@
 library server_output_mod;
 
 import 'package:pritunl/model.dart' as mdl;
+import 'package:pritunl/models/server.dart' as svr;
 
 import 'package:angular/angular.dart' show Injectable;
 import 'package:angular/angular.dart' as ng;
 
 @Injectable()
 class ServerOutput extends mdl.Model {
-  @mdl.Attribute('id')
-  String id;
+  svr.Server server;
 
   @mdl.Attribute('output')
   List<String> output;
 
-  ServerOutput(ng.Http http) : super(http);
+  ServerOutput(ng.Http http, this.server) : super(http);
 
   String get url {
-    return '/server/${this.id}/output';
+    return '/server/${this.server.id}/output';
   }
 }
