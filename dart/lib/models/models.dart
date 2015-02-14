dfd31493acc9d887ebe7806826865cb150eaa323
Zachary Huff <zach.huff.386@gmail.com>
2015-02-13 21:16:02 -0500

    Add server org to components

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -5,6 +5,7 @@ import 'package:pritunl/models/key.dart' as ky;
 import 'package:pritunl/models/log_entry.dart' as log_ent;
 import 'package:pritunl/models/organization.dart' as organization;
 import 'package:pritunl/models/server.dart' as svr;
+import 'package:pritunl/models/server_org.dart' as svr_org;
 import 'package:pritunl/models/server_output.dart' as svr_output;
 import 'package:pritunl/models/status.dart' as stus;
 import 'package:pritunl/models/user.dart' as usr;
@@ -18,6 +19,7 @@ class ModelsMod extends ng.Module {
     this.bind(log_ent.LogEntry);
     this.bind(organization.Organization);
     this.bind(svr.Server);
+    this.bind(svr_org.ServerOrg);
     this.bind(svr_output.ServerOutput);
     this.bind(stus.Status);
     this.bind(usr.User);
