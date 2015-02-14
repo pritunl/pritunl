5d7c0ec2e4a5316a4c29720a39e09feff53f4478
Zachary Huff <zach.huff.386@gmail.com>
2015-02-13 19:53:00 -0500

    User server instance in server orgs

--- dart/lib/collections/server_orgs.dart
+++ dart/lib/collections/server_orgs.dart
@@ -1,6 +1,7 @@
 library organizations_col;
 
 import 'package:pritunl/collection.dart' as collec;
+import 'package:pritunl/models/server.dart' as svr;
 import 'package:pritunl/models/server_org.dart' as svr_org;
 
 import 'package:angular/angular.dart' show Injectable;
@@ -9,11 +10,11 @@ import 'package:angular/angular.dart' as ng;
 @Injectable()
 class ServerOrgs extends collec.Collection {
   Type model = svr_org.ServerOrg;
-  String server;
+  svr.Server server;
 
   ServerOrgs(ng.Http http, this.server) : super(http);
 
   String get url {
-    return '/server/${this.server}/organization';
+    return '/server/${this.server.id}/organization';
   }
 }
