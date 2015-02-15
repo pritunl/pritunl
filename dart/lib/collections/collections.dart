a530f23d9f271a4c67dc3a58adf4159bb93195c1
Zachary Huff <zach.huff.386@gmail.com>
2015-02-14 20:23:07 -0500

    Fix collection import names

--- dart/lib/collections/collections.dart
+++ dart/lib/collections/collections.dart
@@ -4,8 +4,8 @@ import 'package:pritunl/collections/events.dart' as evnts;
 import 'package:pritunl/collections/log_entries.dart' as log_ents;
 import 'package:pritunl/collections/organizations.dart' as organizations;
 import 'package:pritunl/collections/server_hosts.dart' as svr_hsts;
-import 'package:pritunl/collections/server_orgs.dart' as svg_orgs;
-import 'package:pritunl/collections/servers.dart' as servers;
+import 'package:pritunl/collections/server_orgs.dart' as svr_orgs;
+import 'package:pritunl/collections/servers.dart' as svrs;
 import 'package:pritunl/collections/users.dart' as usrs;
 
 import 'package:angular/angular.dart' as ng;
@@ -16,8 +16,8 @@ class CollectionsMod extends ng.Module {
     this.bind(log_ents.LogEntries);
     this.bind(organizations.Organizations);
     this.bind(svr_hsts.ServerHosts);
-    this.bind(svg_orgs.ServerOrgs);
-    this.bind(servers.Servers);
+    this.bind(svr_orgs.ServerOrgs);
+    this.bind(svrs.Servers);
     this.bind(usrs.Users);
   }
 }
