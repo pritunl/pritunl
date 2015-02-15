accbecd6545e6e93cfc6608366fde0c9efcae2c0
Zachary Huff <zach.huff.386@gmail.com>
2015-02-15 01:37:14 -0500

    Change import names in models

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -7,8 +7,8 @@ import 'package:pritunl/models/organization.dart' as organization;
 import 'package:pritunl/models/server.dart' as svr;
 import 'package:pritunl/models/server_host.dart' as svr_hst;
 import 'package:pritunl/models/server_org.dart' as svr_org;
-import 'package:pritunl/models/server_output.dart' as svr_output;
-import 'package:pritunl/models/server_link_output.dart' as svr_link_output;
+import 'package:pritunl/models/server_output.dart' as svr_otpt;
+import 'package:pritunl/models/server_link_output.dart' as svr_lnk_otpt;
 import 'package:pritunl/models/status.dart' as stus;
 import 'package:pritunl/models/user.dart' as usr;
 
@@ -23,8 +23,8 @@ class ModelsMod extends ng.Module {
     this.bind(svr.Server);
     this.bind(svr_hst.ServerHost);
     this.bind(svr_org.ServerOrg);
-    this.bind(svr_output.ServerOutput);
-    this.bind(svr_link_output.ServerLinkOutput);
+    this.bind(svr_otpt.ServerOutput);
+    this.bind(svr_lnk_otpt.ServerLinkOutput);
     this.bind(stus.Status);
     this.bind(usr.User);
   }
