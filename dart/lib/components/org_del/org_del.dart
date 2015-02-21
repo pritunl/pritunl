50b6f9dba60754909ee787b8def61ec54dc3deea
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 13:52:02 -0500

    Remove comment in org del

--- dart/lib/components/org_del/org_del.dart
+++ dart/lib/components/org_del/org_del.dart
@@ -46,8 +46,7 @@ class OrgDelComp extends modal_content_base.ModalContent {
     this.okDisabled = true;
 
     return this.model.destroy().then((_) {
-      // TODO The org component will already be removed from dom
-      //return super.submit(closeHandler);
+      return super.submit(closeHandler);
     }).then((_) {
       new alrt.Alert('Successfully deleted organization.', 'success');
     }).catchError((err) {
