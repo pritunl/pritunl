c6036a28a51e1d7329013b3e357877b16d833a74
Zachary Huff <zach.huff.386@gmail.com>
2015-04-14 11:24:10 -0400

    Add pymongo to constants

--- pritunl/constants.py
+++ pritunl/constants.py
@@ -1,5 +1,12 @@
 import string
 import datetime
+import pymongo
+
+PYMONGO3 = False
+try:
+    PYMONGO3 = int(pymongo.version.split('.')[0]) >= 3
+except:
+    pass
 
 APP_NAME = 'pritunl'
 APP_NAME_FORMATED = 'Pritunl'
