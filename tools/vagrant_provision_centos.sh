305e9db055fb4a5190073690c3f55a701a523d7d
Zachary Huff <zach.huff.386@gmail.com>
2015-05-18 14:04:24 -0400

    Update centos provision

--- tools/vagrant_provision_centos.sh
+++ tools/vagrant_provision_centos.sh
@@ -2,4 +2,7 @@
 wget http://mirrors.rit.edu/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
 rpm -i epel-release-7-2.noarch.rpm
 
-yum install -y gcc rpm-build redhat-rpm-config python python2-devel python-flask pyOpenSSL
+yum install -y gcc rpm-build redhat-rpm-config python python-virtualenv python-setuptools python2-devel
+
+mkdir /usr/lib/pritunl
+chown vagrant:vagrant /usr/lib/pritunl
