9bc7e0c979185e5b2e7cd2a2cea59489298fc0cb
Zachary Huff <zach.huff.386@gmail.com>
2015-07-06 17:02:16 -0400

    Update setup_cert

--- pritunl/setup/server_cert.py
+++ pritunl/setup/server_cert.py
@@ -9,18 +9,34 @@ import subprocess
 import os
 
 def setup_server_cert():
-    if not os.path.isfile(settings.conf.server_cert_path) or \
-            not os.path.isfile(settings.conf.server_key_path):
+    server_cert_path = os.path.join(settings.conf.temp_path, 'server.crt')
+    server_key_path = os.path.join(settings.conf.temp_path, 'server.key')
+
+    if not settings.app.server_cert or not settings.app.server_key:
         logger.info('Generating server ssl cert', 'setup')
         try:
             utils.check_output_logged([
                 'openssl', 'req', '-batch', '-x509', '-nodes', '-sha256',
                 '-newkey', 'rsa:4096',
                 '-days', '3652',
-                '-keyout', settings.conf.server_key_path,
-                '-out', settings.conf.server_cert_path,
+                '-keyout', server_key_path,
+                '-out', server_cert_path,
             ])
         except subprocess.CalledProcessError:
             logger.exception('Failed to generate server ssl cert', 'setup')
             raise
+
         os.chmod(settings.conf.server_key_path, 0600)
+
+        with open(server_cert_path, 'r') as server_cert_file:
+            settings.app.server_cert = server_cert_file.read().strip()
+        with open(server_key_path, 'r') as server_key_file:
+            settings.app.server_key = server_key_file.read().strip()
+
+        settings.commit()
+    else:
+        with open(server_cert_path, 'w') as server_cert_file:
+            server_cert_file.write(settings.app.server_cert)
+        with open(server_key_path, 'w') as server_key_file:
+            os.chmod(settings.conf.server_key_path, 0600)
+            server_key_file.write(settings.app.server_key)
