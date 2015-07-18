837ab070b9b221398c3239dee222e4ca61f03453
Zachary Huff <zach.huff.386@gmail.com>
2015-07-17 23:21:02 -0400

    Remove test machine provision scripts

--- tools/vagrant_provision_mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-ntpdate ntp.ubuntu.com
-add-apt-repository -y ppa:pritunl/pritunl-dev
-apt-get update -qq
-apt-get install -qq -y mongodb-server ntp
-sed -i "s/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g" /etc/mongodb.conf
-service mongodb restart
--- tools/vagrant_provision_node.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-ntpdate ntp.ubuntu.com
-add-apt-repository -y ppa:pritunl/pritunl-dev
-apt-get update -qq
-apt-get install -qq -y python-flask python-objgraph python-pymongo openvpn htop rng-tools
-echo "HRNGDEVICE=/dev/urandom" | tee /etc/default/rng-tools
-/etc/init.d/rng-tools start
