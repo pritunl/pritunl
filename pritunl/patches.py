1cced721154439a3bf69f73363f693837bcb4e3e
Zachary Huff <zach.huff.386@gmail.com>
2015-07-20 23:26:27 -0400

    Remove comment in patches

--- pritunl/patches.py
+++ pritunl/patches.py
@@ -8,19 +8,6 @@ import flask
 import time
 import bson
 
-# For MongoDict MongoList
-# class JSONEncoderPatched(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, (MongoDict, MongoList)):
-#             return obj.data
-#         raise TypeError(repr(obj) + ' is not JSON serializable')
-# dumps_orig = json.dumps
-# def dumps_patched(*args, **kwargs):
-#     if not kwargs.get('cls'):
-#         kwargs['cls'] = JSONEncoderPatched
-#     return dumps_orig(*args, **kwargs)
-# json.dumps = dumps_patched
-
 _mongo_errors = []
 for error_attr in (
             'AutoReconnect',
