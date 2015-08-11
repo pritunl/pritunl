518a5498b1b9b0de1f1df7e4a24a0a8ac8b66d32
Zachary Huff <zach.huff.386@gmail.com>
2015-08-11 05:35:46 -0400

    Update centos url

--- tools/vagrant_provision_centos.sh
+++ tools/vagrant_provision_centos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget http://mirrors.rit.edu/fedora/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -i epel-release-7-2.noarch.rpm
 
 yum install -y gcc rpm-build redhat-rpm-config python python-virtualenv python-setuptools python2-devel libffi-devel openssl-devel
