53c656285bd19ba549afe14fab9d2aa7c1be11df
Zachary Huff <zach.huff.386@gmail.com>
2015-08-21 22:22:59 -0400

    Remove provision script

--- tools/vagrant_provision_centos.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -i epel-release-7-2.noarch.rpm
-
-yum install -y gcc rpm-build redhat-rpm-config python python-virtualenv python-setuptools python2-devel libffi-devel openssl-devel
-
-mkdir /usr/lib/pritunl
-chown vagrant:vagrant /usr/lib/pritunl
