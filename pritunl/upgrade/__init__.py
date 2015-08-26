ca78642e684a714ad904952137d54cb520bf33c5
Zachary Huff <zach.huff.386@gmail.com>
2015-08-26 10:40:51 -0400

    Update upgrade

--- pritunl/upgrade/__init__.py
+++ pritunl/upgrade/__init__.py
@@ -1,10 +1,12 @@
+from pritunl.upgrade.upgrade_1_4 import upgrade_1_4
+
 from pritunl import logger
+from pritunl import utils
 
 def upgrade_server():
-    # if utils.get_db_ver_int() < 1000306624015:
-    #     logger.info('Running 0.10.x database upgrade', 'upgrade')
-    #     upgrade_0_10_x()
-    #     utils.set_db_ver('1.0.0')
-    # else:
-    #     logger.info('DB version changed, no upgrade needed', 'upgrade')
-    logger.info('No upgrade needed', 'upgrade')
+    if utils.get_db_ver_int() < utils.get_int_ver('1.4.0.0'):
+        logger.info('Running 1.4 database upgrade', 'upgrade')
+        upgrade_1_4()
+        utils.set_db_ver('1.4.0.0')
+    else:
+        logger.info('No upgrade needed', 'upgrade')
