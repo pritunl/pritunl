5b6f178c00a07cc0e72febb53bb7fb53176157cb
Zachary Huff <zach.huff.386@gmail.com>
2015-09-04 01:09:16 -0400

    Fix server list item

--- www/views/userServersListItem.js
+++ www/views/userServersListItem.js
@@ -28,6 +28,7 @@ define([
           platform = 'apple';
         }
         else if (platform === 'linux' || platform === 'chrome') {
+          platform = platform;
         }
         else {
           platform = 'desktop';
