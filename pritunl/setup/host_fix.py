9c5c5eb8f7d152939ac9403723e05b74973beb5f
Zachary Huff <zach.huff.386@gmail.com>
2015-09-18 10:46:31 -0400

    Fix check in host fix

--- pritunl/setup/host_fix.py
+++ pritunl/setup/host_fix.py
@@ -1,7 +1,7 @@
 from pritunl import settings
 
 def setup_host_fix():
-    if settings.app.license:
+    if settings.app.license and settings.app.license_plan != "premium":
         return
 
     from pritunl import server
