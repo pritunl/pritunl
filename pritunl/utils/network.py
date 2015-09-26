967bf340fa09f67b11f20f0a861c0cbb22795f35
Zachary Huff <zach.huff.386@gmail.com>
2015-09-26 12:39:12 -0400

    Add ip4to6

--- pritunl/utils/network.py
+++ pritunl/utils/network.py
@@ -217,3 +217,22 @@ def find_interface(network):
 
         if address in network and data['netmask'] == str(network.netmask):
             return data
+
+def ip4to6(prefix, net, addr):
+    addrs = addr.split('.')
+    net, cidr = net.split('/')
+    nets = net.split('.')
+    cidr = int(cidr)
+
+    net_num = int(nets[0]) * 256**3 + int(nets[1]) * 256**2 + \
+        int(nets[2]) * 256**1 + int(nets[3]) * 256**0
+    net_hex = hex(net_num + cidr * 256**4)
+
+    addr6 = prefix + ':' + net_hex[2:6].lstrip('0')
+    x = net_hex[6:10].lstrip('0')
+    if x:
+        addr6 += ':' + x
+    addr6 += ':' + net_hex[10:14].lstrip('0') +  ':' + addrs[0] + ':' + \
+        addrs[1] + ':' + addrs[2] + ':' + addrs[3] + '/64'
+
+    return addr6
