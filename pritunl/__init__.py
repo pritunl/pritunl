6ffe6c62a9428c0e9f176476e0a1d2a0fb38ce13
Zachary Huff <zach.huff.386@gmail.com>
2015-10-25 23:54:43 -0400

    Fix versions

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -2,7 +2,7 @@ from pritunl import patches
 from pritunl.constants import *
 
 __title__ = 'pritunl'
-__version__ = '1.7.773.19'
+__version__ = '1.6.757.9'
 __author__ = 'Pritunl'
 __email__ = 'contact@pritunl.com'
 __license__ = 'Custom'
