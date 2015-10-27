e8501b8e415b10270bc2c9725d2456e0731db294
Zachary Huff <zach.huff.386@gmail.com>
2015-10-27 04:41:07 -0400

    Add dns setup

---
+++ pritunl/setup/dns.py
@@ -0,0 +1,29 @@
+from pritunl.helpers import *
+from pritunl import settings
+from pritunl import logger
+from pritunl import utils
+
+import os
+import threading
+
+@interrupter
+def _dns_thread():
+    while True:
+        try:
+            utils.check_output_logged(
+                ['pritunl-dns'],
+                env=dict(os.environ, **{
+                    'DB': settings.conf.mongodb_uri,
+                    'DB_PREFIX': settings.conf.mongodb_collection_prefix or '',
+                }),
+            )
+        except GeneratorExit:
+            raise
+        except:
+            logger.exception('Error in dns server', 'setup')
+
+        time.sleep(1)
+        yield
+
+def setup_dns():
+    threading.Thread(target=_dns_thread).start()
