98500adfaf743395bc21d9b03a2b8b4ae590afd6
Zachary Huff <zach.huff.386@gmail.com>
2015-11-09 12:32:15 -0500

    Add saml to sso

--- pritunl/sso/__init__.py
+++ pritunl/sso/__init__.py
@@ -1,2 +1,3 @@
 from pritunl.sso.duo import auth_duo
 from pritunl.sso.google import verify_google
+from pritunl.sso.saml import verify_saml
---
+++ pritunl/sso/saml.py
@@ -0,0 +1,2 @@
+def verify_saml(username, email, org_id):
+    return True, org_id
