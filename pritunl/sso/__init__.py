62c8eab8365be6eeebd1369538b406d25e05cb03
Zachary Huff <zach.huff.386@gmail.com>
2015-11-21 13:47:29 -0500

    Update sso init

--- pritunl/sso/__init__.py
+++ pritunl/sso/__init__.py
@@ -2,3 +2,4 @@ from pritunl.sso.duo import auth_duo
 from pritunl.sso.google import verify_google
 from pritunl.sso.saml import verify_saml
 from pritunl.sso.okta import auth_okta
+from pritunl.sso.onelogin import auth_onelogin
