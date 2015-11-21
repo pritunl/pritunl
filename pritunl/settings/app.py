3794d9b10fe3d4b3f2ef253cf567cc32aee206a8
Zachary Huff <zach.huff.386@gmail.com>
2015-11-21 07:36:38 -0500

    Change okta push default

--- pritunl/settings/app.py
+++ pritunl/settings/app.py
@@ -51,7 +51,7 @@ class SettingsApp(SettingsGroupMongo):
         'sso_saml_duo_skip_unavailable': False,
         'sso_okta_token': None,
         'sso_okta_poll_rate': 0.25,
-        'sso_okta_skip_unavailable': False,
+        'sso_okta_skip_unavailable': True,
         'queue_low_thread_limit': 4,
         'queue_med_thread_limit': 2,
         'queue_high_thread_limit': 1,
