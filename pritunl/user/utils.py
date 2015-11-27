c2ad620c04a6e7a198180316ce9a4354822b626b
Zachary Huff <zach.huff.386@gmail.com>
2015-11-27 16:45:15 -0500

    Fix user reserve

--- pritunl/user/utils.py
+++ pritunl/user/utils.py
@@ -19,7 +19,7 @@ def new_pooled_user(org, type):
 
 def reserve_pooled_user(org, name=None, email=None, type=CERT_CLIENT,
         auth_type=None, disabled=None, resource_id=None,
-        bypass_secondary=None):
+        dns_servers=None, dns_suffix=None, bypass_secondary=None):
     doc = {}
 
     if name is not None:
@@ -34,6 +34,10 @@ def reserve_pooled_user(org, name=None, email=None, type=CERT_CLIENT,
         doc['disabled'] = disabled
     if resource_id is not None:
         doc['resource_id'] = resource_id
+    if dns_servers is not None:
+        doc['dns_servers'] = dns_servers
+    if dns_suffix is not None:
+        doc['dns_suffix'] = dns_suffix
     if bypass_secondary is not None:
         doc['bypass_secondary'] = bypass_secondary
 
