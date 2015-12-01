b2161898930546f5e11a7acf1500daeb1821d2db
Zachary Huff <zach.huff.386@gmail.com>
2015-11-30 23:36:48 -0500

    Add gravatar option

--- pritunl/settings/user.py
+++ pritunl/settings/user.py
@@ -3,6 +3,7 @@ from pritunl.settings.group_mongo import SettingsGroupMongo
 class SettingsUser(SettingsGroupMongo):
     group = 'user'
     fields = {
+        'gravatar': True,
         'otp_secret_len': 16,
         'cert_key_bits': 4096,
         'cert_message_digest': 'sha256',
