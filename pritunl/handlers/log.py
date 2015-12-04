5cc28218ec592e2f66963f7e4f02a6f0cccaabce
Zachary Huff <zach.huff.386@gmail.com>
2015-12-03 22:40:49 -0500

    Update log entries handler

--- pritunl/handlers/log.py
+++ pritunl/handlers/log.py
@@ -1,11 +1,16 @@
+from pritunl.constants import *
 from pritunl import utils
 from pritunl import app
 from pritunl import auth
 from pritunl import logger
+from pritunl import settings
 
 @app.app.route('/log', methods=['GET'])
 @auth.session_auth
 def log_get():
+    if settings.app.demo_mode:
+        return utils.jsonify(DEMO_LOG_ENTRIES)
+
     log_entries = []
 
     for log_entry in logger.iter_log_entries():
