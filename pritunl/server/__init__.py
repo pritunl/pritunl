74c4dc7af6526b287d8a17e3cff0a9a3785a0a6b
Zachary Huff <zach.huff.386@gmail.com>
2015-12-08 17:41:20 -0500

    Update imports in server

--- pritunl/server/__init__.py
+++ pritunl/server/__init__.py
@@ -1,4 +1,4 @@
-from pritunl.server.server import Server
+from pritunl.server.server import Server, dict_fields, operation_fields
 from pritunl.server.bandwidth import ServerBandwidth
 from pritunl.server.listener import on_msg
 from pritunl.server.ip_pool import *
