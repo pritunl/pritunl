ea1613b24c8d8383075a490d4f4dd6b6ef1d30ef
Zachary Huff <zach.huff.386@gmail.com>
2015-12-08 19:36:32 -0500

    Add ios platform to server list item view

--- www/views/userServersListItem.js
+++ www/views/userServersListItem.js
@@ -29,7 +29,7 @@ define([
       if (platform === 'win') {
         platform = 'windows';
       }
-      else if (platform === 'mac') {
+      else if (platform === 'mac' || platform === 'ios') {
         platform = 'apple';
       }
       else if (platform === 'linux' || platform === 'chrome') {
