9004250222f95809a52478289980736e9e01cdd3
Zachary Huff <zach.huff.386@gmail.com>
2015-12-15 21:14:39 -0500

    Change time_sync interval

--- pritunl/runners/time_sync.py
+++ pritunl/runners/time_sync.py
@@ -11,7 +11,7 @@ def _time_sync_thread():
             utils.sync_time()
         except:
             logger.exception('Failed to sync time', 'runners')
-        yield interrupter_sleep(15)
+        yield interrupter_sleep(1800)
 
 def start_time_sync():
     threading.Thread(target=_time_sync_thread).start()
