b046edf0e401570379a82dce29080c5d55db2743
Zachary Huff <zach.huff.386@gmail.com>
2015-12-17 12:17:30 -0500

    Add threading to callqueue

--- pritunl/callqueue.py
+++ pritunl/callqueue.py
@@ -1,9 +1,11 @@
 from pritunl import logger
 
+import threading
 import Queue
 
 class CallQueue(object):
-    def __init__(self, maxsize=0):
+    def __init__(self, checker, maxsize=0):
+        self._check = checker
         self._queue = Queue.Queue(maxsize)
 
     def put(self, func, *args, **kwargs):
@@ -17,3 +19,16 @@ class CallQueue(object):
             pass
         except:
             logger.exception('Error in queued called', 'callqueue')
+
+    def _thread(self):
+        while True:
+            self.call(timeout=0.5)
+
+            if self._check():
+                return
+
+    def start(self, threads=1):
+        for _ in xrange(threads):
+            thread = threading.Thread(target=self._thread)
+            thread.daemon = True
+            thread.start()
