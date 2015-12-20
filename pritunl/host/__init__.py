cfc10cd659650d357ab3b7728480b44b73f8a908
Zachary Huff <zach.huff.386@gmail.com>
2015-12-19 22:30:49 -0500

    Add global clients to host

--- pritunl/host/__init__.py
+++ pritunl/host/__init__.py
@@ -1,3 +1,10 @@
 from pritunl.host.host import Host
 from pritunl.host.usage import HostUsage
 from pritunl.host.utils import *
+
+from pritunl import docdb
+
+global_clients = docdb.DocDb(
+    'instance_id',
+    'client_id',
+)
