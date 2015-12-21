f914f7085d50ab414fd1cef9c4940f5900f60647
Zachary Huff <zach.huff.386@gmail.com>
2015-12-21 00:22:38 -0500

    Add server_count to host doc

--- pritunl/runners/host.py
+++ pritunl/runners/host.py
@@ -88,6 +88,7 @@ def _keep_alive_thread():
             settings.local.host.collection.update({
                 '_id': settings.local.host.id,
             }, {'$set': {
+                'server_count': len(host.global_servers),
                 'device_count': host.global_clients.count({}),
                 'cpu_usage': cpu_usage,
                 'mem_usage': mem_usage,
