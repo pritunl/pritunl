2ff62206451765253128c7f7d888f5e4cd9fd3e6
Zachary Huff <zach.huff.386@gmail.com>
2015-12-28 12:31:43 -0500

    Handle onselect in admins view

--- www/views/admins.js
+++ www/views/admins.js
@@ -15,7 +15,30 @@ define([
     },
     initialize: function() {
       this.adminsList = new AdminsListView();
+      this.listenTo(this.adminsList, 'select', this.onSelect);
       this.addView(this.adminsList);
+      this.selected = [];
+    },
+    onSelect: function(view) {
+      var i;
+
+      if (view.getSelect()) {
+        this.selected.push(view);
+      }
+      else {
+        for (i = 0; i < this.selected.length; i++) {
+          if (this.selected[i] === view) {
+            this.selected.splice(i, 1);
+          }
+        }
+      }
+
+      if (this.selected.length) {
+        this.$('.admins-del-selected').removeAttr('disabled');
+      }
+      else {
+        this.$('.admins-del-selected').attr('disabled', 'disabled');
+      }
     },
     render: function() {
       this.$el.html(this.template());
