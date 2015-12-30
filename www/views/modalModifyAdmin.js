00f99b2902ec1166a4f7cc1f5bef2403bd2c416b
Zachary Huff <zach.huff.386@gmail.com>
2015-12-30 16:20:05 -0500

    Add super user to modify admin view

--- www/views/modalModifyAdmin.js
+++ www/views/modalModifyAdmin.js
@@ -13,6 +13,7 @@ define([
     okText: 'Modify',
     events: function() {
       return _.extend({
+        'click .super-user-toggle': 'onSuperSelect',
         'click .otp-auth-toggle': 'onOtpAuthSelect',
         'click .auth-api-toggle': 'onAuthSelect',
         'click .auth-token input, .auth-secret input': 'onClickInput',
@@ -25,6 +26,21 @@ define([
     update: function() {
       this.$('.auth-token input').val(this.model.get('auth_token'));
       this.$('.auth-secret input').val(this.model.get('auth_secret'));
+    },
+    getSuperSelect: function() {
+      return this.$('.super-user-toggle .selector').hasClass('selected');
+    },
+    setSuperSelect: function(state) {
+      if (state) {
+        this.$('.super-user-toggle .selector').addClass('selected');
+        this.$('.super-user-toggle .selector-inner').show();
+      } else {
+        this.$('.super-user-toggle .selector').removeClass('selected');
+        this.$('.super-user-toggle .selector-inner').hide();
+      }
+    },
+    onSuperSelect: function() {
+      this.setSuperSelect(!this.getSuperSelect());
     },
     getOtpAuthSelect: function() {
       return this.$('.otp-auth-toggle .selector').hasClass('selected');
@@ -87,6 +103,7 @@ define([
     onOk: function() {
       var username = this.$('.username input').val();
       var password = this.$('.password input').val();
+      var superUser = this.getSuperSelect();
       var otpAuth = this.getOtpAuthSelect();
       var authApi = this.getAuthSelect();
 
@@ -97,6 +114,7 @@ define([
 
       var data = {
         username: username,
+        super_user: superUser,
         otp_auth: otpAuth,
         auth_api: authApi,
         token: null,
