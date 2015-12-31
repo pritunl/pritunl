d7716fd27793a1b50b7fb7abfa8f28d9df70c428
Zachary Huff <zach.huff.386@gmail.com>
2015-12-31 01:17:01 -0500

    Generate otp secret when enabled

--- pritunl/handlers/admin.py
+++ pritunl/handlers/admin.py
@@ -121,6 +121,8 @@ def admin_put(admin_id):
     otp_auth = flask.request.json.get('otp_auth')
     if otp_auth is not None:
         if otp_auth != admin.otp_auth:
+            admin.generate_otp_secret()
+
             admin.audit_event('admin_updated',
                 'Administrator two-step authentication %s' % (
                     'disabled' if otp_auth else 'enabled'),
