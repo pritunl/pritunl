c6521ddab0988f597653ad9b03afedbbaa331ea9
Zachary Huff <zach.huff.386@gmail.com>
2015-12-31 04:03:20 -0500

    Remove admin audit from user handler

--- pritunl/handlers/user.py
+++ pritunl/handlers/user.py
@@ -460,9 +460,6 @@ def user_otp_secret_put(org_id, user_id):
 @app.app.route('/user/<org_id>/<user_id>/audit', methods=['GET'])
 @auth.session_auth
 def user_audit_get(org_id, user_id):
-    if org_id == 'admin' and user_id == 'admin':
-        return utils.jsonify(auth.get_audit_events())
-
     org = organization.get_by_id(org_id)
     user = org.get_user(user_id)
 
