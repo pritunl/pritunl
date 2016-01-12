7cd5754144753345d3cedbea8e3ccc43bcd253b9
Zachary Huff <zach.huff.386@gmail.com>
2016-01-12 00:40:04 -0500

    Block logs in demo

--- pritunl/handlers/logs.py
+++ pritunl/handlers/logs.py
@@ -2,10 +2,14 @@ from pritunl import app
 from pritunl import logger
 from pritunl import utils
 from pritunl import auth
+from pritunl import settings
 
 @app.app.route('/logs', methods=['GET'])
 @auth.session_auth
 def logs_get():
+    if settings.app.demo_mode:
+        return utils.demo_blocked()
+
     log_view = logger.LogView()
     return utils.jsonify({
         'output': log_view.get_log_lines(
