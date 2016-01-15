69b05ee384aad353a2f37973a8cdbccb382c64f4
Zachary Huff <zach.huff.386@gmail.com>
2016-01-15 11:22:52 -0500

    Sudo pacur in builder

--- tools/builder.py
+++ tools/builder.py
@@ -407,8 +407,10 @@ elif cmd == 'build':
 
     # Run pacur project build
     for build_target in BUILD_TARGETS:
-        subprocess.check_call(['pacur', 'project', 'build', build_target],
-            cwd=pacur_path)
+        subprocess.check_call(
+            ['sudo', 'pacur', 'project', 'build', build_target],
+            cwd=pacur_path,
+        )
 
 
 elif cmd == 'upload':
@@ -437,7 +439,10 @@ elif cmd == 'upload':
 
 
     # Run pacur project build
-    subprocess.check_call(['pacur', 'project', 'repo'], cwd=pacur_path)
+    subprocess.check_call(
+        ['sudo', 'pacur', 'project', 'repo'],
+        cwd=pacur_path,
+    )
 
 
     # Sync mirror
