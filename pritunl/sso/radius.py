f3f06663dd3d34b8e87ddc571724bc9643f63d0f
Zachary Huff <zach.huff.386@gmail.com>
2016-01-26 02:59:24 -0500

    Add radius to sso

---
+++ pritunl/sso/radius.py
@@ -0,0 +1,20 @@
+from pritunl import radius
+from pritunl import settings
+
+def verify_radius(username, password):
+    host = settings.app.sso_host.split(':')
+    if len(host) > 1:
+        port = int(host[1])
+    else:
+        port = 1645
+    host = host[0]
+
+    resp = radius.authenticate(
+        username.encode(),
+        password.encode(),
+        settings.app.sso_secret,
+        host=host,
+        port=port,
+    )
+
+    return resp == 1, None
