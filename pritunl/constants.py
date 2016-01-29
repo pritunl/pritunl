aca1eab3b92f7d698b75067f307b9ac5070d9bd2
Zachary Huff <zach.huff.386@gmail.com>
2016-01-28 20:18:58 -0500

    Add pin disabled

--- pritunl/constants.py
+++ pritunl/constants.py
@@ -520,8 +520,11 @@ PIN_NOT_DIGITS_MSG = 'Pin must contain only digits.'
 PIN_TOO_SHORT = 'pin_too_short'
 PIN_TOO_SHORT_MSG = 'Pin is not long enough.'
 
-PIN_REQUIRED = 'pin_required'
-PIN_REQUIRED_MSG = 'Pin is required.'
+PIN_IS_REQUIRED = 'pin_required'
+PIN_IS_REQUIRED_MSG = 'Pin is required.'
+
+PIN_IS_DISABLED = 'pin_disabled'
+PIN_IS_DISABLED_MSG = 'Pin is disabled.'
 
 PIN_RADIUS = 'pin_radius'
 PIN_RADIUS_MSG = 'Pin cannot be used with Radius users.'
