c828b81d2a64e2afc0cdd47332a6913033233b0f
Zachary Huff <zach.huff.386@gmail.com>
2016-02-18 01:28:11 -0500

    Add on_client listener

--- pritunl/setup/port_forwarding.py
+++ pritunl/setup/port_forwarding.py
@@ -3,3 +3,4 @@ from pritunl import listener
 def setup_port_forwarding():
     from pritunl import clients
     listener.add_listener('port_forwarding', clients.on_port_forwarding)
+    listener.add_listener('client', clients.on_client)
