343a8bc05fdcafa2c9f4c134363f7b3e2c362298
Zachary Huff <zach.huff.386@gmail.com>
2016-02-21 23:32:05 -0500

    Disable routing with bridged mode

--- pritunl/clients/clients.py
+++ pritunl/clients/clients.py
@@ -61,7 +61,8 @@ class Clients(object):
     @property
     def route_clients(self):
         return self.server.replica_count and self.server.replica_count > 1 \
-            and self.server.inter_client
+            and self.server.inter_client and \
+            self.server.network_mode != BRIDGE
 
     def get_org(self, org_id):
         org = self.obj_cache.get(org_id)
