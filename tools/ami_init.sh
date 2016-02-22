446bede231ba2df111c778b7872f5c1f38b04f2d
Zachary Huff <zach.huff.386@gmail.com>
2016-02-21 23:32:34 -0500

    Update ami_init

--- tools/ami_init.sh
+++ tools/ami_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "[pritunl]\nname=Pritunl Repository\nbaseurl=http://repo.pritunl.com/stable/yum/centos/7/\ngpgcheck=1\nenabled=1" > /etc/yum.repos.d/pritunl.repo
 echo -e "description \"Pritunl Override\"\n\nstart on starting pritunl\nstop on runlevel [!2345]\n\npre-start script\n  stop pritunl\n  sleep 1\nend script" > /etc/init/pritunl-override.conf
-echo -e "\nyum -y clean all\nyum -y upgrade\nsleep 1\nstart pritunl\niptables -t nat -A POSTROUTING -m comment --comment init || true\niptables -t nat -D POSTROUTING -m comment --comment init || true" >> /etc/rc.local
+echo -e "\nyum -y clean all\nyum -y upgrade\nsleep 1\nstart pritunl" >> /etc/rc.local
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys CF8E292A
 gpg --armor --export CF8E292A > key.tmp
 rpm --import key.tmp
