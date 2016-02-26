8edddcd4a22afed584831309ce694a990372b390
Zachary Huff <zach.huff.386@gmail.com>
2016-02-26 15:01:28 -0500

    Add server route model

---
+++ www/models/serverRoute.js
@@ -0,0 +1,27 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var ServerRouteModel = Backbone.Model.extend({
+    defaults: {
+      'id': null,
+      'network': null,
+      'default_route': null,
+      'network_link': null,
+      'nat': null
+    },
+    url: function() {
+      var url = '/server/' + this.get('server') + '/route';
+
+      if (this.get('id')) {
+        url += '/' + this.get('id');
+      }
+
+      return url;
+    }
+  });
+
+  return ServerRouteModel;
+});
