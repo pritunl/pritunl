694723ff322fae43efb3130a81845c166606ae18
Zachary Huff <zach.huff.386@gmail.com>
2016-02-28 13:44:41 -0500

    Fix formatting in clientsy

--- pritunl/clients/clients.py
+++ pritunl/clients/clients.py
@@ -120,7 +120,7 @@ class Clients(object):
                         client_conf += 'iroute-ipv6 %s\n' % network_link
                     else:
                         client_conf += 'iroute %s %s\n' % \
-                                       utils.parse_network(network_link)
+                            utils.parse_network(network_link)
 
             if network_links and not reauth:
                 thread = threading.Thread(target=self.iroute_ping_thread,
