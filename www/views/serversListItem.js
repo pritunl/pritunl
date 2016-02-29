52d657d4023b6d904d451c6c2fa7b93866f43841
Zachary Huff <zach.huff.386@gmail.com>
2016-02-29 15:07:34 -0500

    Remove server mode from server item view

--- www/views/serversListItem.js
+++ www/views/serversListItem.js
@@ -151,13 +151,7 @@ define([
       this.$('.server-public-address .status-text').text(
         this.model.get('public_address'));
 
-      var modes = {
-        'all_traffic': 'All Traffic',
-        'local_traffic': 'Local Traffic Only',
-        'vpn_traffic': 'VPN Traffic Only',
-      };
-      this.$('.server-mode .status-text').text(
-        modes[this.model.get('mode')]);
+      this.$('.server-mode .status-text').text();
 
       this.updateButtons();
     },
