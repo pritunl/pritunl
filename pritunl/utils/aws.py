13e08d414ef27a7b3e649f6802fb5269b1c5ddfe
Zachary Huff <zach.huff.386@gmail.com>
2016-03-09 05:26:22 -0500

    Add add_vpc_route to aws utils

--- pritunl/utils/aws.py
+++ pritunl/utils/aws.py
@@ -1,4 +1,9 @@
 from pritunl.utils.request import get
+from pritunl import settings
+
+import boto
+import boto.ec2
+import boto.vpc
 
 def get_instance_id():
     try:
@@ -13,3 +18,41 @@ def get_instance_id():
         return resp.content
     except:
         pass
+
+def add_vpc_route(region, vpc_id, network, resource_id):
+    region_key = region.replace('-', '_')
+    aws_key = getattr(settings.app, region_key + '_access_key')
+    aws_secret = getattr(settings.app, region_key + '_secret_key')
+
+    vpc_conn = boto.connect_vpc(
+        aws_access_key_id=aws_key,
+        aws_secret_access_key=aws_secret,
+        region=boto.ec2.get_region(region),
+    )
+
+    tables = vpc_conn.get_all_route_tables(filters={'vpc-id': vpc_id})
+    if not tables:
+        raise ValueError('Failed to find routing table')
+    table = tables[0]
+
+    instance_id = None
+    interface_id = None
+    if resource_id.startswith('eni-'):
+        interface_id = resource_id
+    else:
+        instance_id = resource_id
+
+    try:
+        vpc_conn.create_route(
+            table.id,
+            network,
+            instance_id=instance_id,
+            interface_id=interface_id,
+        )
+    except:
+        vpc_conn.replace_route(
+            table.id,
+            network,
+            instance_id=instance_id,
+            interface_id=interface_id,
+        )
