543897cd7140b1fdb6b99491460ae5700812bf3f
Zachary Huff <zach.huff.386@gmail.com>
2016-03-11 05:27:32 -0500

    Add write_server_cert to utils

--- pritunl/utils/cert.py
+++ pritunl/utils/cert.py
@@ -1,6 +1,18 @@
-from pritunl.utils.misc import check_output_logged
+from pritunl.constants import *
+from pritunl.utils.misc import check_output_logged, get_temp_path
+from pritunl import settings
 
 import os
+
+def write_server_cert():
+    server_cert_path = os.path.join(settings.conf.temp_path, SERVER_CERT_NAME)
+    server_key_path = os.path.join(settings.conf.temp_path, SERVER_KEY_NAME)
+
+    with open(server_cert_path, 'w') as server_cert_file:
+        server_cert_file.write(settings.app.server_cert)
+    with open(server_key_path, 'w') as server_key_file:
+        os.chmod(server_key_path, 0600)
+        server_key_file.write(settings.app.server_key)
 
 def generate_server_cert(server_cert_path, server_key_path):
     check_output_logged([
