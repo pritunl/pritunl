3a1f5f893867baaf19b05445079a316c90011752
Zachary Huff <zach.huff.386@gmail.com>
2016-03-11 02:52:54 -0500

    Update upgrade_server

--- pritunl/upgrade/__init__.py
+++ pritunl/upgrade/__init__.py
@@ -2,6 +2,7 @@ from pritunl.upgrade.upgrade_1_4 import upgrade_1_4
 from pritunl.upgrade.upgrade_1_5 import upgrade_1_5
 from pritunl.upgrade.upgrade_1_17 import upgrade_1_17
 from pritunl.upgrade.upgrade_1_18 import upgrade_1_18
+from pritunl.upgrade.upgrade_1_19 import upgrade_1_19
 from pritunl.upgrade.utils import *
 
 from pritunl import logger
@@ -33,6 +34,12 @@ def upgrade_server():
         logger.info('Running 1.18 database upgrade', 'upgrade')
         upgrade_1_18()
         utils.set_db_ver('1.18.0.0')
+
+    if utils.get_db_ver_int() < utils.get_int_ver('1.19.0.0'):
+        upgraded = True
+        logger.info('Running 1.19 database upgrade', 'upgrade')
+        upgrade_1_19()
+        utils.set_db_ver('1.19.0.0')
 
     if not upgraded:
         logger.info('No upgrade needed', 'upgrade')
