585aba777e6f227b5f0e3d40605733e7450a884b
Zachary Huff <zach.huff.386@gmail.com>
2016-03-11 05:28:57 -0500

    Dont write cert in setup server_cert

--- pritunl/setup/server_cert.py
+++ pritunl/setup/server_cert.py
@@ -20,9 +20,3 @@ def setup_server_cert():
             settings.app.server_key = server_key_file.read().strip()
 
         settings.commit()
-    else:
-        with open(server_cert_path, 'w') as server_cert_file:
-            server_cert_file.write(settings.app.server_cert)
-        with open(server_key_path, 'w') as server_key_file:
-            os.chmod(server_key_path, 0600)
-            server_key_file.write(settings.app.server_key)
