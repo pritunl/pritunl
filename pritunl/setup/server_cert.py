fa9789e28fa048b1ed0028f1e92b2ca9e9605400
Zachary Huff <zach.huff.386@gmail.com>
2016-03-12 00:43:43 -0500

    Update server_cert setup

--- pritunl/setup/server_cert.py
+++ pritunl/setup/server_cert.py
@@ -1,24 +1,7 @@
-from pritunl.constants import *
 from pritunl import settings
-from pritunl import logger
 from pritunl import utils
-
-import os
 
 def setup_server_cert():
     if not settings.app.server_cert or not settings.app.server_key:
-        server_cert_path = os.path.join(
-            settings.conf.temp_path, SERVER_CERT_NAME)
-        server_key_path = os.path.join(
-            settings.conf.temp_path, SERVER_KEY_NAME)
-
-        logger.info('Generating server ssl cert', 'setup')
-
-        utils.generate_server_cert(server_cert_path, server_key_path)
-
-        with open(server_cert_path, 'r') as server_cert_file:
-            settings.app.server_cert = server_cert_file.read().strip()
-        with open(server_key_path, 'r') as server_key_file:
-            settings.app.server_key = server_key_file.read().strip()
-
+        utils.create_server_cert()
         settings.commit()
