9e76ae45e732f7deec8ee6dc66508df71c8ac964
Zachary Huff <zach.huff.386@gmail.com>
2016-03-14 04:05:58 -0400

    Update tasks

--- pritunl/tasks/__init__.py
+++ pritunl/tasks/__init__.py
@@ -3,6 +3,7 @@ import pritunl.tasks.clean_ip_pool
 import pritunl.tasks.clean_users
 import pritunl.tasks.clean_network_links
 import pritunl.tasks.pooler
+import pritunl.tasks.route
 import pritunl.tasks.sync_ip_pool
 import pritunl.tasks.server
 import pritunl.tasks.clean_servers
