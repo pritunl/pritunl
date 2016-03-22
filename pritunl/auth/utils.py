9f5b7b5129a0659f6d29e55b99a8ba5dd992031e
Zachary Huff <zach.huff.386@gmail.com>
2016-03-21 23:19:55 -0400

    Add hash_pin_v2

--- pritunl/auth/utils.py
+++ pritunl/auth/utils.py
@@ -58,6 +58,18 @@ def hash_pin_v1(salt, pin):
         hash_digest = pass_hash.digest()
     return hash_digest
 
+def hash_pin_v2(salt, pin):
+    pass_hash = hashlib.sha512()
+    pass_hash.update(pin[:settings.app.password_len_limit])
+    pass_hash.update(base64.b64decode(salt))
+    hash_digest = pass_hash.digest()
+
+    for _ in xrange(100000):
+        pass_hash = hashlib.sha512()
+        pass_hash.update(hash_digest)
+        hash_digest = pass_hash.digest()
+    return hash_digest
+
 def generate_hash_pin_v1(pin):
     salt = base64.b64encode(os.urandom(8))
     pin_hash = base64.b64encode(hash_pin_v1(salt, pin))
