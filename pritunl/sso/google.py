5a0cef27fff923f4a9d047ff988ce2295b383582
Zachary Huff <zach.huff.386@gmail.com>
2016-03-23 21:28:20 -0400

    Fix formatting in sso

--- pritunl/sso/google.py
+++ pritunl/sso/google.py
@@ -2,4 +2,6 @@ from pritunl import settings
 
 def verify_google(user_email):
     user_domain = user_email.split('@')[-1]
-    return user_domain in settings.app.sso_match, None
+    valid = user_domain in settings.app.sso_match
+    org_name = None
+    return valid, org_name
--- pritunl/sso/slack.py
+++ pritunl/sso/slack.py
@@ -1,4 +1,6 @@
 from pritunl import settings
 
 def verify_slack(username, user_team, user_groups):
-    return user_team == settings.app.sso_match[0], None
+    valid = user_team == settings.app.sso_match[0]
+    org_name = None
+    return valid, org_name
