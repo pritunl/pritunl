26ba619d646cc55606f4a29d07aaa90a24748b37
Zachary Huff <zach.huff.386@gmail.com>
2016-03-26 02:44:49 -0400

    Fix routes check task

--- pritunl/tasks/route.py
+++ pritunl/tasks/route.py
@@ -5,6 +5,7 @@ from pritunl import logger
 from pritunl import messenger
 from pritunl import utils
 from pritunl import task
+from pritunl import server
 
 import datetime
 
@@ -32,6 +33,30 @@ class TaskRoute(task.Task):
                 vpc_region = doc['vpc_region']
                 vpc_id = doc['vpc_id']
                 network = doc['network']
+
+                svr = server.get_by_id(server_id)
+                if not svr:
+                    self.routes_collection.remove({
+                        '_id': doc['_id'],
+                    })
+                    continue
+
+                match = False
+                for route in svr.get_routes(include_server_links=True):
+                    route_vpc_region = route['vpc_region']
+                    route_vpc_id = route['vpc_id']
+                    route_network = route['network']
+
+                    if vpc_region == route_vpc_region or \
+                            route_vpc_id == route_vpc_id or \
+                            route_network == route_network:
+                        match = true
+
+                if not match:
+                    self.routes_collection.remove({
+                        '_id': doc['_id'],
+                    })
+                    continue
 
                 messenger.publish('instance', ['route_advertisement',
                     server_id, vpc_region, vpc_id, network])
