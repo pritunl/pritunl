3648b25a18c2e767c2195270a2585f01efa8d688
Zachary Huff <zach.huff.386@gmail.com>
2016-04-11 23:47:51 -0400

    Add debug handlers

---
+++ pritunl/handlers/debug.py
@@ -0,0 +1,8 @@
+from pritunl import auth
+from pritunl import app
+
+@app.app.route('/debug/restart_web', methods=['GET'])
+@auth.session_auth
+def restart_web_get():
+    app.restart_server(1)
+    return 'restarting'
