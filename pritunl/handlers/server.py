45efae26f60442cecfcc4a621851913020c1e66f
Zachary Huff <zach.huff.386@gmail.com>
2016-04-14 22:55:42 -0400

    Update server handler

--- pritunl/handlers/server.py
+++ pritunl/handlers/server.py
@@ -725,11 +725,13 @@ def server_route_post(server_id):
         'ipv6'))
     route_network = flask.request.json['network']
     nat_route = True if flask.request.json.get('nat') else False
+    nat_interface = flask.request.json.get('nat_interface') or None
     vpc_region = utils.filter_str(flask.request.json.get('vpc_region')) or None
     vpc_id = utils.filter_str(flask.request.json.get('vpc_id')) or None
 
     try:
-        route = svr.upsert_route(route_network, nat_route, vpc_region, vpc_id)
+        route = svr.upsert_route(route_network, nat_route, nat_interface,
+            vpc_region, vpc_id)
     except ServerOnlineError:
         return utils.jsonify({
             'error': SERVER_ROUTE_ONLINE,
@@ -771,11 +773,13 @@ def server_route_put(server_id, route_network):
         'ipv6'))
     route_network = route_network.decode('hex')
     nat_route = True if flask.request.json.get('nat') else False
+    nat_interface = flask.request.json.get('nat_interface') or None
     vpc_region = utils.filter_str(flask.request.json.get('vpc_region')) or None
     vpc_id = utils.filter_str(flask.request.json.get('vpc_id')) or None
 
     try:
-        route = svr.upsert_route(route_network, nat_route, vpc_region, vpc_id)
+        route = svr.upsert_route(route_network, nat_route, nat_interface,
+            vpc_region, vpc_id)
     except ServerOnlineError:
         return utils.jsonify({
             'error': SERVER_ROUTE_ONLINE,
