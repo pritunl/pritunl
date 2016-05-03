3643a47e3f12ba37ed7bccd332d6657307ea4a82
Zachary Huff <zach.huff.386@gmail.com>
2016-05-03 15:30:50 -0400

    Remove debug handler

--- pritunl/handlers/debug.py
+++
@@ -1,8 +0,0 @@
-from pritunl import auth
-from pritunl import app
-
-@app.app.route('/debug/restart_web', methods=['GET'])
-@auth.session_auth
-def restart_web_get():
-    app.restart_server(1)
-    return 'restarting web server...'
