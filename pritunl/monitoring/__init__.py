abbf4d6081ab163921c90ddd3be26a2e49eb6d39
Zachary Huff <zach.huff.386@gmail.com>
2016-05-12 15:42:49 -0400

    Add monitoring

---
+++ pritunl/monitoring/__init__.py
@@ -0,0 +1,98 @@
+from pritunl import influxdb
+from pritunl import utils
+from pritunl import settings
+
+import threading
+import urlparse
+
+_queue = []
+_queue_lock = threading.Lock()
+_client = None
+
+def _get_servers(uri):
+    uri = urlparse.urlparse(uri)
+
+    netloc = uri.netloc.split('@', 1)
+    if len(netloc) == 2:
+        username, password = netloc[0].split(':', 1)
+        netloc = netloc[1]
+    else:
+        username = None
+        password = None
+        netloc = netloc[0]
+
+    hosts = []
+    netloc = netloc.split(',')
+    for host in netloc:
+        host, port = host.split(':', 1)
+        try:
+            port = int(port)
+        except:
+            port = 0
+
+        hosts.append((host, port))
+
+    if uri.path:
+        database = uri.path.replace('/', '', 1)
+    else:
+        database = None
+
+    return hosts, username, password, database
+
+def insert_point(measurement, tags, fields):
+    _queue_lock.acquire()
+    try:
+        if not _client:
+            return
+
+        _queue.append({
+            'measurement': measurement,
+            'tags': tags,
+            'time': utils.now(),
+            'time_precision': 'ms',
+            'fields': fields,
+        })
+    finally:
+        _queue_lock.release()
+
+def write_queue():
+    global _queue
+
+    _queue_lock.acquire()
+    try:
+        queue = _queue
+        _queue = []
+    finally:
+        _queue_lock.release()
+
+    _client.write_points(queue)
+
+def init():
+    global _client
+
+    influxdb_uri = settings.app.influxdb_uri
+    if not influxdb_uri:
+        _queue_lock.acquire()
+        try:
+            _client = None
+        finally:
+            _queue_lock.release()
+        return
+
+    hosts, username, password, database = _get_servers(influxdb_uri)
+
+    if len(hosts) == 1:
+        _client = influxdb.InfluxDBClient(
+            hosts[0][0],
+            hosts[0][1],
+            username=username,
+            password=password,
+            database=database,
+        )
+    else:
+        _client = influxdb.InfluxDBClusterClient(
+            hosts,
+            username=username,
+            password=password,
+            database=database,
+        )
