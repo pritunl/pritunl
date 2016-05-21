ede2b8e35e3f660f23149aaa2ebd1e4471129f0c
Zachary Huff <zach.huff.386@gmail.com>
2016-05-20 22:16:22 -0400

    Change mongodb collection size

--- pritunl/setup/mongo.py
+++ pritunl/setup/mongo.py
@@ -65,7 +65,7 @@ def setup_mongo():
 
     if prefix + 'messages' not in cur_collections:
         database.create_collection(prefix + 'messages', capped=True,
-            size=200000, max=2000)
+            size=200192, max=1500)
 
     mongo.collections.update({
         'time_sync': getattr(database, prefix + 'time_sync'),
