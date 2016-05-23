abb003dd7641f69e477a6f2434261d85a7a059ea
Zachary Huff <zach.huff.386@gmail.com>
2016-05-23 00:47:34 -0400

    Update events in server output link

--- pritunl/server/output_link.py
+++ pritunl/server/output_link.py
@@ -11,19 +11,30 @@ class ServerOutputLink(ServerOutput):
     def collection(cls):
         return mongo.get_collection('servers_output_link')
 
-    def send_event(self, link_server_ids):
-        event.Event(type=SERVER_LINK_OUTPUT_UPDATED,
-            resource_id=self.server_id, delay=OUTPUT_DELAY)
+    def send_event(self, link_server_ids, delay=True):
+        if delay:
+            delay = SERVER_OUTPUT_DELAY
+        else:
+            delay = None
+
+        event.Event(
+            type=SERVER_LINK_OUTPUT_UPDATED,
+            resource_id=self.server_id,
+            delay=delay,
+        )
         for link_server_id in link_server_ids:
             if self.server_id != link_server_id:
-                event.Event(type=SERVER_LINK_OUTPUT_UPDATED,
-                    resource_id=link_server_id, delay=OUTPUT_DELAY)
+                event.Event(
+                    type=SERVER_LINK_OUTPUT_UPDATED,
+                    resource_id=link_server_id,
+                    delay=delay,
+                )
 
     def clear_output(self, link_server_ids):
         self.collection.remove({
             'server_id': self.server_id,
         })
-        self.send_event(link_server_ids)
+        self.send_event(link_server_ids, delay=False)
 
     def push_output(self, output, label, link_server_id):
         if self.server_id != link_server_id:
