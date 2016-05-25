b828b04e36af1a613f884464bd636a835a7c290c
Zachary Huff <zach.huff.386@gmail.com>
2016-05-25 19:03:05 -0400

    Fix spacing in plugins

--- pritunl/plugins.py
+++ pritunl/plugins.py
@@ -11,11 +11,9 @@ _handlers = {}
 
 def _get_functions(module):
     funcs = {}
-
     for x in inspect.getmembers(module):
         if inspect.isfunction(x[1]):
             funcs[x[0]] = x[1]
-
     return funcs
 
 def init():
