4154f684cf294107ea947437f70b9848c67f714f
Zachary Huff <zach.huff.386@gmail.com>
2016-05-26 18:58:29 -0400

    Add duo auth to plugin

--- pritunl/example_plugin.py
+++ pritunl/example_plugin.py
@@ -25,3 +25,24 @@ def user_authenticate(host_id, server_id, org_id, user_id, host_name,
         return True, None
     else:
         return False, 'Reason for denial'
+
+# [SYNCHRONOUS] Called after a user has authenticated with Duo when loging
+# into the web console. Must return True or False to accept auth request and
+# an organization name or None. If an organization name is included the user
+# will be added to that organization. If Duo is used as a secondary
+# authentication method the organization name from Duo if set will have
+# priority over the organization name from the primary SSO provider.
+def authenticate_duo_login(host_id, host_name, user_name, remote_ip, **kwargs):
+    if 'auth_ok':
+        return True, 'organization_name'
+    else:
+        return False, None
+
+# [SYNCHRONOUS] Called after a user has authenticated with Duo when connecting
+# to a VPN server. Must return True or False to accept auth request.
+def authenticate_duo_connection(host_id, server_id, org_id, user_id,
+        host_name, server_name, org_name, user_name, remote_ip, **kwargs):
+    if 'auth_ok':
+        return True
+    else:
+        return False
