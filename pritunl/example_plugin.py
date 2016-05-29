576c71292bc5a7055e4ffbc5f0ce4a8982ae451a
Zachary Huff <zach.huff.386@gmail.com>
2016-05-28 23:44:42 -0400

    Change comment in example plugin

--- pritunl/example_plugin.py
+++ pritunl/example_plugin.py
@@ -26,12 +26,12 @@ def user_authenticate(host_id, server_id, org_id, user_id, host_name,
     else:
         return False, 'Reason for denial'
 
-# [SYNCHRONOUS] Called after a user has authenticated with Duo when loging
-# into the web console. Must return True or False to accept auth request and
-# an organization name or None. If an organization name is included the user
-# will be added to that organization. If Duo is used as a secondary
-# authentication method the organization name from Duo if set will have
-# priority over the organization name from the primary SSO provider.
+# [SYNCHRONOUS] Called after a user has authenticated with Duo when
+# loging into the web console. Must return True or False to accept auth
+# request and an organization name or None. If an organization name is
+# included the user will be added to that organization. If Duo is used as a
+# secondary authentication method the organization name from Duo if set will
+# have priority over the organization name from the primary SSO provider.
 def authenticate_duo_login(host_id, host_name, user_name, remote_ip, **kwargs):
     if 'auth_ok':
         return True, 'organization_name'
