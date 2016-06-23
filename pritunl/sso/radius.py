95b8fd518b169e9885a8effc4826258fb39bafd9
Zachary Huff <zach.huff.386@gmail.com>
2016-06-23 05:10:50 -0400

    Parse vendor attributes in radius sso

--- pritunl/sso/radius.py
+++ pritunl/sso/radius.py
@@ -1,5 +1,10 @@
-from pritunl import radius
+from pritunl.constants import *
 from pritunl import settings
+from pritunl.pyrad import client
+from pritunl.pyrad import packet
+from pritunl.pyrad import dictionary
+
+import StringIO
 
 def verify_radius(username, password):
     host = settings.app.sso_host.split(':')
@@ -9,12 +14,27 @@ def verify_radius(username, password):
         port = 1645
     host = host[0]
 
-    resp = radius.authenticate(
-        (settings.app.sso_radius_prefix or '') + username.encode(),
-        password.encode(),
-        settings.app.sso_secret.encode(),
-        host=host,
-        port=port,
+    conn = client.Client(
+        server=host,
+        authport=port,
+        secret=settings.app.sso_secret.encode(),
+        dict=dictionary.Dictionary(StringIO.StringIO(RADIUS_DICTONARY)),
     )
 
-    return resp == 1, None
+    req = conn.CreateAuthPacket(
+        code=packet.AccessRequest,
+        User_Name=(settings.app.sso_radius_prefix or '') + username.encode(),
+    )
+    req['User-Password'] = req.PwCrypt(password.encode())
+
+    reply = conn.SendPacket(req)
+
+    if reply.code != packet.AccessAccept:
+        return False, None, None
+
+    org_names = reply.get((97, 0)) or []
+    groups = set()
+    for group in reply.get((97, 1)) or []:
+        groups.add(group)
+
+    return True, org_names, groups
