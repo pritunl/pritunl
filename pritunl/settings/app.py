aa90bf480806d4d15881c1a2a3f1082e1a518af8
Zachary Huff <zach.huff.386@gmail.com>
2016-06-24 22:29:28 -0400

    Add sso_cache to app settings

--- pritunl/settings/app.py
+++ pritunl/settings/app.py
@@ -64,6 +64,8 @@ class SettingsApp(SettingsGroupMongo):
         'email_from': None,
         'id': None,
         'sso': False,
+        'sso_cache': True,
+        'sso_cache_timeout': 21600,
         'sso_match': None,
         'sso_host': None, # Deprecated
         'sso_token': None, # Deprecated
