f8caf222a825283f3cc24061e74e21950c318832
Zachary Huff <zach.huff.386@gmail.com>
2016-06-27 22:12:59 -0400

    Fix formatting in upgrade

--- pritunl/upgrade/upgrade_1_24.py
+++ pritunl/upgrade/upgrade_1_24.py
@@ -5,11 +5,6 @@ from pritunl.constants import *
 def upgrade_1_24():
     settings_collection = get_collection('settings')
 
-    sso = None
-    sso_host = None
-    sso_token = None
-    sso_secret = None
-
     doc = settings_collection.find_one({'_id': 'app'})
     if doc:
         sso = doc.get('sso')
@@ -17,19 +12,19 @@ def upgrade_1_24():
         sso_token = doc.get('sso_token')
         sso_secret = doc.get('sso_secret')
 
-    if sso:
-        if DUO_AUTH in sso:
-            settings_collection.update({
-                '_id': 'app',
-            }, {'$set': {
-                'sso_duo_host': sso_host,
-                'sso_duo_token': sso_token,
-                'sso_duo_secret': sso_secret,
-            }})
-        elif sso == RADIUS_AUTH:
-            settings_collection.update({
-                '_id': 'app',
-            }, {'$set': {
-                'sso_radius_host': sso_host,
-                'sso_radius_secret': sso_secret,
-            }})
+        if sso:
+            if DUO_AUTH in sso:
+                settings_collection.update({
+                    '_id': 'app',
+                }, {'$set': {
+                    'sso_duo_host': sso_host,
+                    'sso_duo_token': sso_token,
+                    'sso_duo_secret': sso_secret,
+                }})
+            elif sso == RADIUS_AUTH:
+                settings_collection.update({
+                    '_id': 'app',
+                }, {'$set': {
+                    'sso_radius_host': sso_host,
+                    'sso_radius_secret': sso_secret,
+                }})
