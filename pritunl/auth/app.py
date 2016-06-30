6178862fa62e9108a18a682fea2e900a5d4ec3bb
Zachary Huff <zach.huff.386@gmail.com>
2016-06-30 03:39:37 -0400

    Add open_auth

--- pritunl/auth/app.py
+++ pritunl/auth/app.py
@@ -6,6 +6,14 @@ def session_auth(call):
     def _wrapped(*args, **kwargs):
         if not check_session():
             raise flask.abort(401)
+        flask.g.authed = True
         return call(*args, **kwargs)
     _wrapped.__name__ = '%s_session_auth' % call.__name__
+    return _wrapped
+
+def open_auth(call):
+    def _wrapped(*args, **kwargs):
+        flask.g.authed = True
+        return call(*args, **kwargs)
+    _wrapped.__name__ = '%s_open_auth' % call.__name__
     return _wrapped
