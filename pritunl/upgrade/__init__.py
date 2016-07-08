53be375981818826e22ac3cb24178cb658150c36
Zachary Huff <zach.huff.386@gmail.com>
2016-07-08 02:17:37 -0400

    Add min version to upgrade

--- pritunl/upgrade/__init__.py
+++ pritunl/upgrade/__init__.py
@@ -16,37 +16,37 @@ def upgrade_server():
         upgraded = True
         logger.info('Running 1.4 database upgrade', 'upgrade')
         upgrade_1_4()
-        utils.set_db_ver('1.4.0.0')
+        utils.set_db_ver('1.4.0.0', '1.4.0.0')
 
     if utils.get_db_ver_int() < utils.get_int_ver('1.5.0.0'):
         upgraded = True
         logger.info('Running 1.5 database upgrade', 'upgrade')
         upgrade_1_5()
-        utils.set_db_ver('1.5.0.0')
+        utils.set_db_ver('1.5.0.0', '1.5.0.0')
 
     if utils.get_db_ver_int() < utils.get_int_ver('1.17.0.0'):
         upgraded = True
         logger.info('Running 1.17 database upgrade', 'upgrade')
         upgrade_1_17()
-        utils.set_db_ver('1.17.0.0')
+        utils.set_db_ver('1.17.0.0', '1.17.0.0')
 
     if utils.get_db_ver_int() < utils.get_int_ver('1.18.0.0'):
         upgraded = True
         logger.info('Running 1.18 database upgrade', 'upgrade')
         upgrade_1_18()
-        utils.set_db_ver('1.18.0.0')
+        utils.set_db_ver('1.18.0.0', '1.18.0.0')
 
     if utils.get_db_ver_int() < utils.get_int_ver('1.19.0.0'):
         upgraded = True
         logger.info('Running 1.19 database upgrade', 'upgrade')
         upgrade_1_19()
-        utils.set_db_ver('1.19.0.0')
+        utils.set_db_ver('1.19.0.0', '1.19.0.0')
 
     if utils.get_db_ver_int() < utils.get_int_ver('1.24.0.0'):
         upgraded = True
         logger.info('Running 1.24 database upgrade', 'upgrade')
         upgrade_1_24()
-        utils.set_db_ver('1.24.0.0')
+        utils.set_db_ver('1.24.0.0', '1.24.0.0')
 
     if not upgraded:
         logger.info('No upgrade needed', 'upgrade')
