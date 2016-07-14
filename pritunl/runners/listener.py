cb8f51a4ac70abffd5b53a4f57df7693e6502090
Zachary Huff <zach.huff.386@gmail.com>
2016-07-14 02:04:22 -0400

    Use callqueue for listener

--- pritunl/runners/listener.py
+++ pritunl/runners/listener.py
@@ -2,18 +2,22 @@ from pritunl.helpers import *
 from pritunl import listener
 from pritunl import logger
 from pritunl import messenger
+from pritunl import callqueue
 
 import threading
 import time
 
 @interrupter
 def listener_thread():
+    queue = callqueue.CallQueue()
+    queue.start(10)
+
     while True:
         try:
             for msg in messenger.subscribe(listener.channels.keys()):
                 for lstnr in listener.channels[msg['channel']]:
                     try:
-                        lstnr(msg)
+                        queue.put(lstnr, msg)
                     except:
                         logger.exception('Error in listener callback',
                             'runners')
