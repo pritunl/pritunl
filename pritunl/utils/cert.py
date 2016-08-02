74ecbd9cb8954424f19a058419629106b0c86fe5
Zachary Huff <zach.huff.386@gmail.com>
2016-08-02 05:50:59 -0400

    Remove unused cert utils

--- pritunl/utils/cert.py
+++ pritunl/utils/cert.py
@@ -22,17 +22,6 @@ def create_server_cert():
     with open(server_key_path, 'r') as server_key_file:
         settings.app.server_key = server_key_file.read().strip()
 
-def create_server_dh_params():
-    from pritunl import logger
-
-    logger.info('Generating server dh params...', 'utils')
-
-    server_dh_path = generate_server_dh_params(
-        settings.app.server_dh_size)
-
-    with open(server_dh_path, 'r') as server_dh_file:
-        settings.app.server_dh_params = server_dh_file.read().strip()
-
 def write_server_cert(server_cert, server_key, acme_domain):
     server_cert_path = os.path.join(settings.conf.temp_path, SERVER_CERT_NAME)
     server_key_path = os.path.join(settings.conf.temp_path, SERVER_KEY_NAME)
@@ -49,73 +38,6 @@ def write_server_cert(server_cert, server_key, acme_domain):
         server_key_file.write(server_key)
 
     return server_cert_path, server_key_path
-
-def write_server_cert_chain(server_cert, server_key,
-        server_dh_params, acme_domain):
-    server_cert_path = os.path.join(settings.conf.temp_path, SERVER_CERT_NAME)
-    server_chain_path = os.path.join(settings.conf.temp_path,
-        SERVER_CHAIN_NAME)
-    server_key_path = os.path.join(settings.conf.temp_path, SERVER_KEY_NAME)
-    server_dh_path = os.path.join(settings.conf.temp_path, SERVER_DH_NAME)
-
-    server_cert_full = server_cert
-
-    if acme_domain:
-        server_cert_full += LETS_ENCRYPT_INTER
-
-    server_cert_full = server_cert_full.strip()
-    server_cert_full = server_cert_full.split('-----BEGIN CERTIFICATE-----')
-
-    server_certs = collections.deque()
-
-    for cert in server_cert_full:
-        cert = cert.strip()
-        if not cert:
-            continue
-
-        server_certs.append('-----BEGIN CERTIFICATE-----\n' + cert)
-
-    server_cert = server_certs.popleft()
-    server_chain = '\n'.join(server_certs)
-
-    with open(server_cert_path, 'w') as server_cert_file:
-        server_cert_file.write(server_cert)
-    with open(server_key_path, 'w') as server_key_file:
-        os.chmod(server_key_path, 0600)
-        server_key_file.write(server_key)
-
-    if server_certs:
-        with open(server_chain_path, 'w') as server_chain_file:
-            server_chain_file.write(server_chain)
-    else:
-        server_chain_path = None
-
-    if server_dh_params:
-        with open(server_dh_path, 'w') as server_dh_file:
-            os.chmod(server_dh_path, 0600)
-            server_dh_file.write(server_dh_params)
-    else:
-        server_dh_path = None
-
-    return server_cert_path, server_chain_path, server_key_path, server_dh_path
-
-def generate_server_dh_params(dh_size):
-    server_dh_path = os.path.join(settings.conf.temp_path, SERVER_DH_NAME)
-
-    check_output_logged([
-        'openssl',
-        'dhparam', str(dh_size),
-        '-out', server_dh_path,
-    ])
-    os.chmod(server_dh_path, 0600)
-
-    return server_dh_path
-
-def generate_server_dh_params_inline(dh_size):
-    return check_output_logged([
-        'openssl',
-        'dhparam', str(dh_size),
-    ])
 
 def generate_server_cert():
     server_cert_path = os.path.join(settings.conf.temp_path, SERVER_CERT_NAME)
