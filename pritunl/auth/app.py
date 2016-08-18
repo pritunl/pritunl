49526c2a2a8e7b7a3703627a250c0bb2e76337a5
Zachary Huff <zach.huff.386@gmail.com>
2016-08-18 19:41:50 -0400

    Add session auth light

--- pritunl/auth/app.py
+++ pritunl/auth/app.py
@@ -11,6 +11,15 @@ def session_auth(call):
     _wrapped.__name__ = '%s_session_auth' % call.__name__
     return _wrapped
 
+def session_light_auth(call):
+    def _wrapped(*args, **kwargs):
+        if not check_session():
+            return flask.abort(401)
+        flask.g.authed = True
+        return call(*args, **kwargs)
+    _wrapped.__name__ = '%s_session_auth' % call.__name__
+    return _wrapped
+
 def open_auth(call):
     def _wrapped(*args, **kwargs):
         flask.g.authed = True
