960e6179eeeba4e681017748895590ca6addbb9a
Zachary Huff <zach.huff.386@gmail.com>
2016-09-18 22:36:04 -0400

    Fix formatting in server

--- pritunl/server/server.py
+++ pritunl/server/server.py
@@ -760,7 +760,8 @@ class Server(mongo.MongoObject):
         for doc in self.host_collection.find(spec, project):
             address = doc.get('sync_address') or \
                 doc.get('auto_public_host') or \
-                doc['public_address'] or doc['auto_public_address']
+                doc['public_address'] or \
+                doc['auto_public_address']
             if settings.conf.port == 443:
                 remotes.add('https://%s' % address)
             else:
