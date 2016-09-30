d50c2702b93edcca455cb3604cbffeaba4c41eb1
Zachary Huff <zach.huff.386@gmail.com>
2016-09-30 18:56:21 -0400

    Add lib_iptables to settings

--- pritunl/settings/vpn.py
+++ pritunl/settings/vpn.py
@@ -5,6 +5,7 @@ class SettingsVpn(SettingsGroupMongo):
     fields = {
         'ipv6': True,
         'ipv6_route_all': True,
+        'lib_iptables': True,
         'call_queue_threads': 32,
         'client_ttl': 300,
         'peer_limit': 300,
