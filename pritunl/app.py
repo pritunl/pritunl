13a9e18c5c3d41710b187f839770825eeeec0494
Zachary Huff <zach.huff.386@gmail.com>
2016-10-10 17:14:20 -0400

    Fix setup server cert

--- pritunl/app.py
+++ pritunl/app.py
@@ -263,14 +263,9 @@ def _run_wsgi_debug():
         raise
 
 def setup_server_cert():
-    commit = False
-
     if not settings.app.server_cert or not settings.app.server_key:
-        commit = True
         logger.info('Generating server certificate...', 'app')
         utils.create_server_cert()
-
-    if commit:
         settings.commit()
 
 def run_server():
