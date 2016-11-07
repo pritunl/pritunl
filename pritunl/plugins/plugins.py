1cc3c8d353a7258b39182efa7b28eae639d37a5e
Zachary Huff <zach.huff.386@gmail.com>
2016-11-07 16:47:39 -0500

    Check for required plugins

--- pritunl/plugins/plugins.py
+++ pritunl/plugins/plugins.py
@@ -1,6 +1,8 @@
 from pritunl.plugins.utils import *
 from pritunl.plugins import example
 
+from pritunl.exceptions import *
+from pritunl.helpers import *
 from pritunl import callqueue
 from pritunl import settings
 from pritunl import logger
@@ -24,6 +26,9 @@ def init():
 
     modules = []
     plugin_dir = settings.app.plugin_directory
+    plugins_required = set(
+        x.strip() for x in (settings.app.plugin_requred or '').split(',') if x)
+    plugins_loaded = set()
 
     if not os.path.exists(plugin_dir):
         return
@@ -33,7 +38,14 @@ def init():
         file_name, file_ext = os.path.splitext(file_name)
         if file_ext != '.py':
             continue
+        plugins_loaded.add(file_name)
         modules.append(imp.load_source('plugin_' + file_name, file_path))
+
+    missing_plugins = plugins_required - plugins_loaded
+    if missing_plugins:
+        set_global_interrupt()
+        raise PluginMissing(
+            'Missing required plugins %s' % list(missing_plugins))
 
     for module in modules:
         for call_type, handler in get_functions(module).iteritems():
