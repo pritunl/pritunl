6e82f7129103a552062beab9418d544dabfe4931
Zachary Huff <zach.huff.386@gmail.com>
2016-12-10 22:35:37 -0500

    Add support for duo passcode

--- pritunl/sso/duo.py
+++ pritunl/sso/duo.py
@@ -10,8 +10,6 @@ import hashlib
 import urllib
 import httplib
 import requests
-import time
-import threading
 
 def _sign(method, path, params):
     now = email.Utils.formatdate()
@@ -34,210 +32,84 @@ def _sign(method, path, params):
         'Authorization': 'Basic %s' % base64.b64encode(auth),
     }
 
-def auth_duo(username, strong=False, ipaddr=None, type=None, info=None,
-        factor='push', thread=True):
-    if factor == 'push' and thread:
-        state = {
-            'interrupt': False,
-            'valid': None,
-            'org_id': None,
-            'exception': None,
+class Duo(object):
+    def __init__(self, username, remote_ip=None, auth_type=None,
+            info=None, passcode=None):
+        self.username = username
+        self.remote_ip = remote_ip
+        self.auth_type = auth_type
+        self.info = info
+        self.passcode = passcode
+        self._interrupt = False
+        self._valid = False
+
+    def authenticate(self):
+        if settings.app.sso_duo_mode == 'phone':
+            factor = 'phone'
+        elif settings.app.sso_duo_mode == 'passcode':
+            factor = 'passcode'
+        else:
+            factor = 'push'
+
+        self._auth(factor)
+        return self._valid
+
+    def _auth(self, factor):
+        params = {
+            'username': self.username,
+            'factor': factor,
         }
-        state_lock = threading.Lock()
-        state_event = threading.Event()
 
-        def phone_thread():
-            start = time.time()
-            backup_delay = settings.app.sso_duo_backup_delay
+        if self.remote_ip:
+            params['ipaddr'] = self.remote_ip
 
-            while True:
-                if state['interrupt']:
-                    return
-                if time.time() - start >= backup_delay:
-                    break
-                time.sleep(0.1)
+        if factor in ('push', 'phone'):
+            params['device'] = 'auto'
 
-            try:
-                valid, org_id = auth_duo(
-                    username,
-                    strong=strong,
-                    ipaddr=ipaddr,
-                    type=type,
-                    info=info,
-                    factor='phone',
-                    thread=False,
-                )
-            except Exception as error:
-                state_lock.acquire()
-                try:
-                    if state['interrupt']:
-                        return
-                    state['interrupt'] = True
-                    state['exception'] = error
-                    state_event.set()
-                finally:
-                    state_lock.release()
-                return
-
-            if not valid:
-                return
-
-            state_lock.acquire()
-            try:
-                if state['interrupt']:
-                    return
-
-                state['interrupt'] = True
-                state['valid'] = valid
-                state['org_id'] = org_id
-
-                state_event.set()
-            finally:
-                state_lock.release()
-
-        def push_thread():
-            try:
-                valid, org_id = auth_duo(
-                    username,
-                    strong=strong,
-                    ipaddr=ipaddr,
-                    type=type,
-                    info=info,
-                    factor='push',
-                    thread=False,
-                )
-            except UserDuoPushUnavailable:
-                state_lock.acquire()
-                try:
-                    if state['interrupt']:
-                        return
-
-                    state['interrupt'] = True
-                finally:
-                    state_lock.release()
-
-                try:
-                    valid, org_id = auth_duo(
-                        username,
-                        strong=strong,
-                        ipaddr=ipaddr,
-                        type=type,
-                        info=info,
-                        factor='phone',
-                        thread=False,
-                    )
-                except Exception as error:
-                    state['interrupt'] = True
-                    state['exception'] = error
-                    state_event.set()
-                    return
-
-                state['valid'] = valid
-                state['org_id'] = org_id
-                state_event.set()
-                return
-            except Exception as error:
-                state_lock.acquire()
-                try:
-                    if state['interrupt']:
-                        return
-                    state['interrupt'] = True
-                    state['exception'] = error
-                    state_event.set()
-                finally:
-                    state_lock.release()
-                return
-
-            state_lock.acquire()
-            try:
-                if state['interrupt']:
-                    return
-
-                state['interrupt'] = True
-                state['valid'] = valid
-                state['org_id'] = org_id
-                state_event.set()
-            finally:
-                state_lock.release()
-
-        thread = threading.Thread(target=phone_thread)
-        thread.daemon = True
-        thread.start()
-
-        thread = threading.Thread(target=push_thread)
-        thread.daemon = True
-        thread.start()
-
-        state_event.wait()
-
-        if state['exception']:
-            raise state['exception']
-
-        return state['valid'], state['org_id']
-
-    params = {
-        'username': username,
-        'factor': factor,
-        'device': 'auto',
-    }
-
-    if ipaddr:
-        params['ipaddr'] = ipaddr
-
-    if factor == 'push':
-        if type:
-            params['type'] = type
-
-        if info:
-            params['pushinfo'] = urllib.urlencode(info)
-
-    headers = _sign('POST', '/auth/v2/auth', params)
-    url = 'https://%s/auth/v2/auth' % settings.app.sso_duo_host
-
-    try:
-        response = requests.post(url,
-            headers=headers,
-            params=params,
-            timeout=settings.app.sso_timeout,
-        )
-    except httplib.HTTPException:
-        return False, None
-
-    data = response.json()
-    resp_data = data.get('response')
-    if resp_data and resp_data.get('result') == 'allow':
-        if strong and resp_data.get('status') == 'bypass':
-            if SAML_DUO_AUTH in settings.app.sso and \
-                    settings.app.sso_saml_duo_skip_unavailable:
-                logger.warning('Skipping duo auth with bypass',
-                    'sso',
-                    username=username,
-                )
-                allow = True
-            else:
-                allow = False
-                logger.error('Cannot use Duo bypass with profile login',
-                    'sso',
-                    data=resp_data,
-                )
-        else:
-            allow = True
-    elif data.get('code') == 40002:
         if factor == 'push':
-            raise UserDuoPushUnavailable('Duo push is unavailable')
+            if self.auth_type:
+                params['type'] = self.auth_type
 
-        if SAML_DUO_AUTH in settings.app.sso and \
-                settings.app.sso_saml_duo_skip_unavailable:
-            logger.warning('Skipping duo auth for unavailable user', 'sso',
-                username=username,
+            if self.info:
+                params['pushinfo'] = urllib.urlencode(self.info)
+
+        if factor == 'passcode':
+            params['passcode'] = self.passcode
+
+        headers = _sign('POST', '/auth/v2/auth', params)
+        url = 'https://%s/auth/v2/auth' % settings.app.sso_duo_host
+
+        try:
+            response = requests.post(url,
+                headers=headers,
+                params=params,
+                timeout=settings.app.sso_timeout,
             )
-            allow = True
-        else:
-            raise InvalidUser('Invalid username')
-    else:
-        allow = False
-        logger.error('Duo authentication failure', 'sso',
-            data=data,
-        )
+        except httplib.HTTPException:
+            return
 
-    return allow, None
+        data = response.json()
+        resp_data = data.get('response')
+        if resp_data and resp_data.get('result') == 'allow':
+            if resp_data.get('status') == 'bypass':
+                if settings.app.sso == DUO_AUTH:
+                    logger.error('Cannot use Duo bypass with Duo sso',
+                        'sso',
+                        data=resp_data,
+                    )
+                    return
+                else:
+                    logger.info('Skipping Duo auth with bypass',
+                        'sso',
+                        username=self.username,
+                    )
+            self._valid = True
+        elif data.get('code') == 40002:
+            if factor == 'push' and settings.app.sso_duo_mode == 'push_call':
+                self._auth('phone')
+            else:
+                raise InvalidUser('Invalid username')
+        else:
+            logger.error('Duo authentication failure', 'sso',
+                data=data,
+            )
