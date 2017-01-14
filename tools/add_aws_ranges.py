81f498de5306043ed5c1bbaaebb076e9bc68a9d6
Zachary Huff <zach.huff.386@gmail.com>
2017-01-14 06:32:07 -0500

    Update add aws ranges

--- tools/add_aws_ranges.py
+++ tools/add_aws_ranges.py
@@ -69,7 +69,7 @@ def auth_request(method, path, headers=None, data=None):
     auth_timestamp = str(int(time.time()))
     auth_nonce = uuid.uuid4().hex
     auth_string = '&'.join([API_TOKEN, auth_timestamp, auth_nonce,
-        method.upper(), path] + ([data] if data else []))
+        method.upper(), path])
     auth_signature = base64.b64encode(hmac.new(
         API_SECRET, auth_string, hashlib.sha256).digest())
     auth_headers = {
