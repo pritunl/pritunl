16d13ac2c28f4da7cc99b1ab369853d0e88ff347
Zachary Huff <zach.huff.386@gmail.com>
2017-02-09 17:20:46 -0500

    Remove logger filter

--- pritunl/logger/filter.py
+++ pritunl/logger/filter.py
@@ -1,13 +1,5 @@
-from pritunl.constants import *
-
 import logging
 
 class LogFilter(logging.Filter):
     def filter(self, record):
-        if record.levelno == logging.DEBUG:
-            log_type = None
-            if hasattr(record, 'type'):
-                log_type = getattr(record, 'type')
-            if log_type not in LOG_DEBUG_TYPES:
-                return 0
         return 1
