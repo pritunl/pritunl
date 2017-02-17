a8b45b44bf64a8bbbaf6e420ea848ecfefe40646
Zachary Huff <zach.huff.386@gmail.com>
2017-02-17 04:39:49 -0500

    Prevent yubikey replay

--- pritunl/sso/yubico.py
+++ pritunl/sso/yubico.py
@@ -1,14 +1,20 @@
 from pritunl import settings
 from pritunl import logger
+from pritunl import mongo
 
+import hashlib
+import base64
+import pymongo
 import yubico_client
 import certifi
 
-def auth_yubico(key):
-    if len(key) != 44:
+def auth_yubico(yubikey):
+    yubikey_collection = mongo.get_collection('yubikey')
+
+    if len(yubikey) != 44:
         return False, None
 
-    public_id = key[:12]
+    public_id = yubikey[:12]
 
     client = yubico_client.Yubico(
         client_id=settings.app.sso_yubico_client,
@@ -18,9 +24,22 @@ def auth_yubico(key):
     )
 
     try:
-        if client.verify(key) is True:
-            return True, public_id
+        if client.verify(yubikey) is not True:
+            return False, None
     except:
         logger.exception('Yubico authentication error', 'sso')
+        return False, None
 
-    return False, None
+    yubikey_hash = hashlib.sha512()
+    yubikey_hash.update(yubikey)
+    yubikey_hash = base64.b64encode(yubikey_hash.digest())
+
+    try:
+        yubikey_collection.insert({
+            '_id': yubikey_hash,
+        })
+    except pymongo.errors.DuplicateKeyError:
+        logger.error('Yubico replay error', 'sso')
+        return False, None
+
+    return True, public_id
