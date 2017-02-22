1b3c2328717b1dffc6ddb0434d615597a4d3733e
Zachary Huff <zach.huff.386@gmail.com>
2017-02-21 20:18:30 -0500

    Create new release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shlex
 import shutil
 import fileinput
 
-VERSION = '1.27.1258.40snapshot'
+VERSION = '1.27.1259.5snapshot'
 PATCH_DIR = 'build'
 install_systemd = True
 install_upstart = False
