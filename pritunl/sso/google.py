a0bc861238d64ae80e8e66267621ccf7f1b2a796
Zachary Huff <zach.huff.386@gmail.com>
2017-04-06 17:47:41 -0400

    Fix formatting in sso

--- pritunl/sso/google.py
+++ pritunl/sso/google.py
@@ -2,5 +2,4 @@ from pritunl import settings
 
 def verify_google(user_email):
     user_domain = user_email.split('@')[-1]
-    valid = user_domain in settings.app.sso_match
-    return valid
+    return user_domain in settings.app.sso_match
