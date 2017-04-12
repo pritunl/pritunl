be9bf87958dd1f3df9c722e2cc44ef901bfaf01a
Zachary Huff <zach.huff.386@gmail.com>
2017-04-12 18:00:38 -0400

    Add us east 2 to models

--- www/models/vpcs.js
+++ www/models/vpcs.js
@@ -7,6 +7,7 @@ define([
   var VpcsModel = Backbone.Model.extend({
     defaults: {
       'us-east-1': [],
+      'us-east-2': [],
       'us-west-1': [],
       'us-west-2': [],
       'eu-west-1': [],
