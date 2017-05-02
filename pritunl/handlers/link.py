65e4cde6e59b89ba30629cc73567133c27635533
Zachary Huff <zach.huff.386@gmail.com>
2017-05-02 18:02:16 -0400

    Add link handler

---
+++ pritunl/handlers/link.py
@@ -0,0 +1,89 @@
+from pritunl.constants import *
+from pritunl import app
+from pritunl import auth
+from pritunl import settings
+from pritunl import utils
+from pritunl import mongo
+from pritunl import link
+
+import pymongo
+import flask
+import base64
+import hmac
+import hashlib
+import json
+import Crypto
+import Crypto.Random
+import Crypto.Cipher.AES
+
+@app.app.route('/link/state', methods=['PUT'])
+@auth.open_auth
+def link_state_put():
+    auth_token = flask.request.headers.get('Auth-Token', None)
+    auth_timestamp = flask.request.headers.get('Auth-Timestamp', None)
+    auth_nonce = flask.request.headers.get('Auth-Nonce', None)
+    auth_signature = flask.request.headers.get('Auth-Signature', None)
+    if not auth_token or not auth_timestamp or not auth_nonce or \
+            not auth_signature:
+        return flask.abort(401)
+    auth_nonce = auth_nonce[:32]
+
+    try:
+        if abs(int(auth_timestamp) - int(utils.time_now())) > \
+                settings.app.auth_time_window:
+            return flask.abort(401)
+    except ValueError:
+        return flask.abort(401)
+
+    host = link.get_location_host(auth_token)
+    if not host:
+        return flask.abort(401)
+
+    auth_string = '&'.join([
+        host.id,
+        auth_timestamp,
+        auth_nonce,
+        flask.request.method,
+        flask.request.path,
+    ])
+
+    if len(auth_string) > AUTH_SIG_STRING_MAX_LEN:
+        return flask.abort(401)
+
+    auth_test_signature = base64.b64encode(hmac.new(
+        host.secret.encode(), auth_string,
+        hashlib.sha512).digest())
+    if auth_signature != auth_test_signature:
+        return flask.abort(401)
+
+    nonces_collection = mongo.get_collection('auth_nonces')
+    try:
+        nonces_collection.insert({
+            'token': auth_token,
+            'nonce': auth_nonce,
+            'timestamp': utils.now(),
+        })
+    except pymongo.errors.DuplicateKeyError:
+        return flask.abort(401)
+
+    data = json.dumps(host.get_state(), default=lambda x: str(x))
+    data += (16 - len(data) % 16) * '\x00'
+
+    iv = Crypto.Random.new().read(16)
+    key = hashlib.sha256(host.secret).digest()
+    cipher = Crypto.Cipher.AES.new(
+        key,
+        Crypto.Cipher.AES.MODE_CBC,
+        iv,
+    )
+
+    enc_data = base64.b64encode(cipher.encrypt(data))
+
+    resp = flask.Response(response=enc_data, mimetype='text/plain')
+    resp.headers.add('Cache-Control',
+        'no-cache, no-store, must-revalidate')
+    resp.headers.add('Pragma', 'no-cache')
+    resp.headers.add('Expires', 0)
+    resp.headers.add('Cipher-IV', base64.b64encode(iv))
+
+    return resp
