817eb29f94ad2542ba576706ba64f17c09f7d9c9
Zachary Huff <zach.huff.386@gmail.com>
2017-05-05 19:45:55 -0400

    Add link utils

---
+++ pritunl/link/utils.py
@@ -0,0 +1,37 @@
+from pritunl.link.link import Link, Location, Host
+
+from pritunl import settings
+
+def get_host(host_id):
+    host = Host(id=host_id)
+    if not host:
+        return
+
+    host.link = Link(id=host.link_id)
+    if not host.link:
+        return
+
+    host.location = Location(link=host.link, id=host.location_id)
+    if not host.location:
+        return
+
+    return host
+
+def iter_links(page=None):
+    limit = None
+    skip = None
+    page_count = settings.app.link_page_count
+
+    if page is not None:
+        limit = page_count
+        skip = page * page_count if page else 0
+
+    cursor = Link.collection.find({}).sort('name')
+
+    if skip is not None:
+        cursor = cursor.skip(page * page_count if page else 0)
+    if limit is not None:
+        cursor = cursor.limit(limit)
+
+    for doc in cursor:
+        yield Link(doc=doc)
