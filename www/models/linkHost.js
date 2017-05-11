0e59990f1fee26ede149091536c7bfb2197fbb1d
Zachary Huff <zach.huff.386@gmail.com>
2017-05-11 04:31:17 -0400

    Add link host model

---
+++ www/models/linkHost.js
@@ -0,0 +1,25 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var LinkHostModel = Backbone.Model.extend({
+    defaults: {
+      'id': null,
+      'network': null
+    },
+    url: function() {
+      var url = '/link/' + this.get('link_id') + '/' +
+        this.get('location_id') + '/host';
+
+      if (this.get('id')) {
+        url += '/' + this.get('id');
+      }
+
+      return url;
+    }
+  });
+
+  return LinkHostModel;
+});
