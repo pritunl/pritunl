3e44468ca09dc0eab2c425a9d979fdfb3ae019e8
Zachary Huff <zach.huff.386@gmail.com>
2017-05-16 19:22:54 -0400

    Add modify loc host modal

---
+++ www/views/modalModifyLocHost.js
@@ -0,0 +1,47 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/modal',
+  'text!templates/modalModifyLocHost.html'
+], function($, _, Backbone, ModalView,
+    modalModifyLocHostTemplate) {
+  'use strict';
+  var ModalModifyLocationView = ModalView.extend({
+    className: 'modify-location-host-modal',
+    template: _.template(modalModifyLocHostTemplate),
+    title: 'Modify Location Host',
+    okText: 'Save',
+    body: function() {
+      return this.template(this.model.toJSON());
+    },
+    onOk: function() {
+      var name = this.$('.name input').val();
+
+      if (!name) {
+        this.setAlert('danger', 'Name can not be empty.', '.name');
+        return;
+      }
+
+      this.setLoading('Saving location host...');
+      this.model.save({
+        name: name
+      }, {
+        success: function() {
+          this.close(true);
+        }.bind(this),
+        error: function(model, response) {
+          this.clearLoading();
+          if (response.responseJSON) {
+            this.setAlert('danger', response.responseJSON.error_msg);
+          }
+          else {
+            this.setAlert('danger', this.errorMsg);
+          }
+        }.bind(this)
+      });
+    }
+  });
+
+  return ModalModifyLocationView;
+});
