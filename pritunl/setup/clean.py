33e6a734bc7cd7c83725c4e09c6da20ee5b245e7
Zachary Huff <zach.huff.386@gmail.com>
2017-06-20 13:45:56 -0400

    Fix clean setup

--- pritunl/setup/clean.py
+++ pritunl/setup/clean.py
@@ -1,138 +1,143 @@
 from pritunl import utils
+from pritunl import logger
 
 import subprocess
 import time
 
 def setup_clean():
     try:
-        utils.check_call_silent([
-            'killall',
-            'openvpn',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            'openssl',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            'pritunl-dns',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            'pritunl-web',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    time.sleep(2)
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            '-s9',
-            'openvpn',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            '-s9',
-            'openssl',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            '-s9',
-            'pritunl-dns',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-    try:
-        utils.check_call_silent([
-            'killall',
-            '-s9',
-            'pritunl-web',
-        ])
-    except subprocess.CalledProcessError:
-        pass
-
-
-
-    output = subprocess.check_output([
-        'ip',
-        '-o',
-        'link',
-        'show',
-    ])
-
-    for line in output.splitlines():
-        iface_name = line.split(':')
-        if len(iface_name) < 2:
-            continue
-        iface_name = iface_name[1].strip()
-
-        if not iface_name.startswith('pxlan'):
-            continue
-
         try:
             utils.check_call_silent([
-                'ip',
-                'link',
-                'set',
-                'down',
-                iface_name,
+                'killall',
+                'openvpn',
             ])
         except subprocess.CalledProcessError:
             pass
 
         try:
             utils.check_call_silent([
-                'ip',
-                'link',
-                'del',
-                iface_name,
+                'killall',
+                'openssl',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                'pritunl-dns',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                'pritunl-web',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        time.sleep(2)
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                '-s9',
+                'openvpn',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                '-s9',
+                'openssl',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                '-s9',
+                'pritunl-dns',
+            ])
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            utils.check_call_silent([
+                'killall',
+                '-s9',
+                'pritunl-web',
             ])
         except subprocess.CalledProcessError:
             pass
 
 
 
-    output = subprocess.check_output([
-        'iptables-save',
-    ])
+        output = subprocess.check_output([
+            'ip',
+            '-o',
+            'link',
+            'show',
+        ])
 
-    table = None
-    for line in output.splitlines():
-        line = line.strip()
+        for line in output.splitlines():
+            iface_name = line.split(':')
+            if len(iface_name) < 2:
+                continue
+            iface_name = iface_name[1].strip()
 
-        if line in ('*nat', '*filter'):
-            table = line[1:]
-            continue
+            if not iface_name.startswith('pxlan'):
+                continue
 
-        if '--comment pritunl_' not in line:
-            continue
+            try:
+                utils.check_call_silent([
+                    'ip',
+                    'link',
+                    'set',
+                    'down',
+                    iface_name,
+                ])
+            except subprocess.CalledProcessError:
+                pass
 
-        try:
-            utils.check_call_silent([
-                'iptables -t %s -D %s' % (table, line[3:]),
-            ], shell=True)
-        except subprocess.CalledProcessError:
-            pass
+            try:
+                utils.check_call_silent([
+                    'ip',
+                    'link',
+                    'del',
+                    iface_name,
+                ])
+            except subprocess.CalledProcessError:
+                pass
+
+
+
+        output = subprocess.check_output([
+            'iptables-save',
+        ])
+
+        table = None
+        for line in output.splitlines():
+            line = line.strip()
+
+            if line in ('*nat', '*filter'):
+                table = line[1:]
+                continue
+
+            if '--comment pritunl_' not in line:
+                continue
+
+            try:
+                utils.check_call_silent([
+                    'iptables -t %s -D %s' % (table, line[3:]),
+                ], shell=True)
+            except subprocess.CalledProcessError:
+                pass
+    except:
+        logger.exception('Server clean failed', 'setup')
+
