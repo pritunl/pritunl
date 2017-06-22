051e067fb6e7a65c593eb46fa3f93da9aff676ae
Zachary Huff <zach.huff.386@gmail.com>
2017-06-22 15:24:49 -0400

    Add database to mongo

--- pritunl/mongo/__init__.py
+++ pritunl/mongo/__init__.py
@@ -2,6 +2,8 @@ from pritunl.mongo.dict import MongoDict
 from pritunl.mongo.list import MongoList
 from pritunl.mongo.object import MongoObject
 
+database = None
+secondary_database = None
 collections = {}
 
 def get_collection(name):
