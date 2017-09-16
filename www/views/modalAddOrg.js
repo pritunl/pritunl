15c0300b08e32e6b0f4b2b667eed8c89622a0712
Zachary Huff <zach.huff.386@gmail.com>
2017-09-16 03:42:14 -0400

    Remove unused org options

--- www/views/modalAddOrg.js
+++ www/views/modalAddOrg.js
@@ -12,34 +12,11 @@ define([
     template: _.template(modalAddOrgTemplate),
     title: 'Add Organization',
     okText: 'Add',
-    events: function() {
-      return _.extend({
-        'click .auth-toggle': 'onAuthSelect'
-      }, ModalAddOrgView.__super__.events);
-    },
     body: function() {
       return this.template();
     },
-    getAuthSelect: function() {
-      return this.$('.auth-toggle .selector').hasClass('selected');
-    },
-    setAuthSelect: function(state) {
-      if (state) {
-        this.$('.auth-toggle .selector').addClass('selected');
-        this.$('.auth-toggle .selector-inner').show();
-        this.$('.auth-token-form').slideDown(window.slideTime);
-      } else {
-        this.$('.auth-toggle .selector').removeClass('selected');
-        this.$('.auth-toggle .selector-inner').hide();
-        this.$('.auth-token-form').slideUp(window.slideTime);
-      }
-    },
-    onAuthSelect: function() {
-      this.setAuthSelect(!this.getAuthSelect());
-    },
     onOk: function() {
       var name = this.$('.name input').val();
-      var authApi = this.getAuthSelect();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -49,8 +26,7 @@ define([
       this.setLoading('Adding organization...');
       var orgModel = new OrgModel();
       orgModel.save({
-        name: name,
-        auth_api: authApi
+        name: name
       }, {
         success: function() {
           this.close(true);
--- www/views/modalRenameOrg.js
+++ www/views/modalRenameOrg.js
@@ -11,66 +11,11 @@ define([
     template: _.template(modalRenameOrgTemplate),
     title: 'Modify Organization',
     okText: 'Modify',
-    events: function() {
-      return _.extend({
-        'click .auth-toggle': 'onAuthSelect',
-        'click .auth-token input, .auth-secret input': 'onClickInput',
-        'click .generate-new-auth-key': 'onGenerateNewKey'
-      }, ModalRenameOrgView.__super__.events);
-    },
     body: function() {
       return this.template(this.model.toJSON());
     },
-    update: function() {
-      this.$('.auth-token input').val(this.model.get('auth_token'));
-      this.$('.auth-secret input').val(this.model.get('auth_secret'));
-    },
-    getAuthSelect: function() {
-      return this.$('.auth-toggle .selector').hasClass('selected');
-    },
-    setAuthSelect: function(state) {
-      if (state) {
-        this.$('.auth-toggle .selector').addClass('selected');
-        this.$('.auth-toggle .selector-inner').show();
-        this.$('.auth-token-form').slideDown(window.slideTime);
-      } else {
-        this.$('.auth-toggle .selector').removeClass('selected');
-        this.$('.auth-toggle .selector-inner').hide();
-        this.$('.auth-token-form').slideUp(window.slideTime);
-      }
-    },
-    onAuthSelect: function() {
-      this.setAuthSelect(!this.getAuthSelect());
-    },
-    onClickInput: function(evt) {
-      this.$(evt.target).select();
-    },
-    onGenerateNewKey: function() {
-      this.setLoading('Generating new authorization key...');
-      this.model.save({
-        auth_token: true,
-        auth_secret: true
-      }, {
-        success: function() {
-          this.clearLoading();
-          this.setAlert(
-            'success', 'Successfully generated a new authorization key.');
-          this.update();
-        }.bind(this),
-        error: function(model, response) {
-          this.clearLoading();
-          if (response.responseJSON) {
-            this.setAlert('danger', response.responseJSON.error_msg);
-          }
-          else {
-            this.setAlert('danger', this.errorMsg);
-          }
-        }.bind(this)
-      });
-    },
     onOk: function() {
       var name = this.$('.name input').val();
-      var authApi = this.getAuthSelect();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -79,10 +24,7 @@ define([
 
       this.setLoading('Modifying organization...');
       this.model.save({
-        name: name,
-        auth_api: authApi,
-        auth_token: null,
-        auth_secret: null
+        name: name
       }, {
         success: function() {
           this.close(true);
