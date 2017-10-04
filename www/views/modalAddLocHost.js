6fa1e9a68c495926117121eb37798645e9a7a6b0
Zachary Huff <zach.huff.386@gmail.com>
2017-10-04 18:47:23 -0400

    Add static host to add loc host modal

--- www/views/modalAddLocHost.js
+++ www/views/modalAddLocHost.js
@@ -13,6 +13,12 @@ define([
     template: _.template(modalAddLocHostTemplate),
     title: 'Add Location Host',
     okText: 'Add',
+    hasAdvanced: true,
+    events: function() {
+      return _.extend({
+        'click .static-host-toggle': 'onStaticHostSelect'
+      }, ModalAddLocHostView.__super__.events);
+    },
     initialize: function(options) {
       this.link = options.link;
       this.location = options.location;
@@ -21,10 +27,27 @@ define([
     body: function() {
       return this.template();
     },
+    getStaticHostSelect: function() {
+      return this.$('.static-host-toggle .selector').hasClass('selected');
+    },
+    setStaticHostSelect: function(state) {
+      if (state) {
+        this.$('.static-host-toggle .selector').addClass('selected');
+        this.$('.static-host-toggle .selector-inner').show();
+      } else {
+        this.$('.static-host-toggle .selector').removeClass('selected');
+        this.$('.static-host-toggle .selector-inner').hide();
+      }
+    },
+    onStaticHostSelect: function() {
+      this.setStaticHostSelect(!this.getStaticHostSelect());
+    },
     onOk: function() {
       var name = this.$('.name input').val();
       var timeout = parseInt(this.$('.timeout input').val(), 10) || null;
       var priority = parseInt(this.$('.priority input').val(), 10) || 1;
+      var staticHost = this.getStaticHostSelect();
+      var publicAddress = this.$('.public-address input').val();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -38,7 +61,9 @@ define([
         location_id: this.location,
         name: name,
         timeout: timeout,
-        priority: priority
+        priority: priority,
+        static_host: staticHost,
+        public_address: publicAddress
       }, {
         success: function() {
           this.close(true);
