53fae752073784388edec9c58fb1b7306d333199
Zachary Huff <zach.huff.386@gmail.com>
2017-10-04 18:48:33 -0400

    Add link host conf model

---
+++ www/models/linkHostConf.js
@@ -0,0 +1,21 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var LinkHostConfModel = Backbone.Model.extend({
+    defaults: {
+      'id': null,
+      'name': null,
+      'hostname': null,
+      'conf': null
+    },
+    url: function() {
+      return '/link/' + this.get('link_id') + '/location/' +
+        this.get('location_id') + '/host/' + this.get('id') + '/conf';
+    }
+  });
+
+  return LinkHostConfModel;
+});
