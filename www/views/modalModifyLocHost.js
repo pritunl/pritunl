61039ecd98baa9eca3a06a1d946af944502367b9
Zachary Huff <zach.huff.386@gmail.com>
2017-10-04 18:54:38 -0400

    Add static host to modify loc host modal

--- www/views/modalModifyLocHost.js
+++ www/views/modalModifyLocHost.js
@@ -12,13 +12,36 @@ define([
     template: _.template(modalModifyLocHostTemplate),
     title: 'Modify Location Host',
     okText: 'Save',
+    hasAdvanced: true,
+    events: function() {
+      return _.extend({
+        'click .static-host-toggle': 'onStaticHostSelect'
+      }, ModalModifyLocationView.__super__.events);
+    },
     body: function() {
       return this.template(this.model.toJSON());
+    },
+    getStaticHostSelect: function() {
+      return this.$('.static-host-toggle .selector').hasClass('selected');
+    },
+    setStaticHostSelect: function(state) {
+      if (state) {
+        this.$('.static-host-toggle .selector').addClass('selected');
+        this.$('.static-host-toggle .selector-inner').show();
+      } else {
+        this.$('.static-host-toggle .selector').removeClass('selected');
+        this.$('.static-host-toggle .selector-inner').hide();
+      }
+    },
+    onStaticHostSelect: function() {
+      this.setStaticHostSelect(!this.getStaticHostSelect());
     },
     onOk: function() {
       var name = this.$('.name input').val();
       var timeout = parseInt(this.$('.timeout input').val(), 10) || null;
       var priority = parseInt(this.$('.priority input').val(), 10) || 1;
+      var staticHost = this.getStaticHostSelect();
+      var publicAddress = this.$('.public-address input').val();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -29,7 +52,9 @@ define([
       this.model.save({
         name: name,
         timeout: timeout,
-        priority: priority
+        priority: priority,
+        static_host: staticHost,
+        public_address: publicAddress
       }, {
         success: function() {
           this.close(true);
