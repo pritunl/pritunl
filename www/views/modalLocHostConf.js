8573689eebfb1f353e8f579d12dc9f594f22bfbc
Zachary Huff <zach.huff.386@gmail.com>
2017-10-04 18:48:01 -0400

    Add loc host conf modal

---
+++ www/views/modalLocHostConf.js
@@ -0,0 +1,46 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/modal',
+  'text!templates/modalLocHostConf.html'
+], function($, _, Backbone, ModalView, modalLocHostConfTemplate) {
+  'use strict';
+  var ModalLocHostConfView = ModalView.extend({
+    className: 'location-host-conf-modal',
+    template: _.template(modalLocHostConfTemplate),
+    title: 'Location Host Static Configuration',
+    cancelText: null,
+    okText: 'Close',
+    initialize: function() {
+      ModalLocHostConfView.__super__.initialize.call(this);
+      this.setAlert('danger', 'TODO');
+    },
+    body: function() {
+      return this.template();
+    },
+    postRender: function() {
+      this.setLoading('Getting location host static configuration...', true);
+      this.model.set({
+        'hostname': window.location.host
+      });
+      this.model.fetch({
+        success: function() {
+          this.clearLoading();
+          this.$('.conf-link textarea').val(this.model.get('conf'));
+        }.bind(this),
+        error: function(model, response) {
+          this.clearLoading();
+          if (response.responseJSON) {
+            this.setAlert('danger', response.responseJSON.error_msg);
+          }
+          else {
+            this.setAlert('danger', this.errorMsg);
+          }
+        }.bind(this)
+      });
+    }
+  });
+
+  return ModalLocHostConfView;
+});
