d9c4b6c4db2f9a98917318dc3a86cad48d8ba0ed
Zachary Huff <zach.huff.386@gmail.com>
2018-02-02 23:29:01 -0500

    Add dedicated to updates

--- pritunl/runners/updates.py
+++ pritunl/runners/updates.py
@@ -16,9 +16,12 @@ def _check_updates():
             continue
 
         try:
+            url = settings.app.notification_server
+            if settings.app.dedicated:
+                url = settings.app.dedicated + '/notification'
+
             request = urllib2.Request(
-                settings.app.notification_server +
-                '/%s' % settings.local.version_int)
+                url + '/%s' % settings.local.version_int)
             response = urllib2.urlopen(request, timeout=60)
             data = json.load(response)
 
