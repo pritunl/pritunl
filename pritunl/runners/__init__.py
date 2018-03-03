7bdc2b3ed7fac81982f2ce4eb3625a08ea716357
Zachary Huff <zach.huff.386@gmail.com>
2018-03-02 21:46:10 -0500

    Add auto restart to runners

--- pritunl/runners/__init__.py
+++ pritunl/runners/__init__.py
@@ -13,6 +13,7 @@ from pritunl.runners.instance import start_instance
 from pritunl.runners.time_sync import start_time_sync
 from pritunl.runners.limiter import start_limiter
 from pritunl.runners.listener import start_listener
+from pritunl.runners.auto_restart import start_auto_restart
 
 def start_all():
     start_settings()
@@ -29,5 +30,6 @@ def start_all():
     start_time_sync()
     start_limiter()
     start_update_server()
+    start_auto_restart()
 
     start_listener()
