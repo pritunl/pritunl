b7e4ac0c1c0ba1bb1007eac54dbd519173fa16ac
Zachary Huff <zach.huff.386@gmail.com>
2018-03-03 00:41:30 -0500

    Create new release

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -2,7 +2,7 @@ from pritunl import patches
 from pritunl.constants import *
 
 __title__ = 'pritunl'
-__version__ = '1.29.1631.29'
+__version__ = '1.29.1633.23'
 __author__ = 'Pritunl'
 __email__ = 'contact@pritunl.com'
 __license__ = 'Custom'
