ce5c337f8133cd8b1e7049ab28958102b6160bbb
Zachary Huff <zach.huff.386@gmail.com>
2018-06-08 23:25:14 -0400

    Add onc net conf const

--- pritunl/constants.py
+++ pritunl/constants.py
@@ -1110,6 +1110,36 @@ OVPN_ONC_CLIENT_CONF = """\
 }
 """
 
+OVPN_ONC_NET_CONF = """\
+    {
+      "GUID": "%s",
+      "Name": "%s",
+      "Type": "VPN",
+      "VPN": {
+        "Host": "%s",
+        "Type": "OpenVPN",
+        "OpenVPN": {
+          "AuthRetry": "interact",
+          "Auth": "%s",
+          "Cipher": "%s",
+          "IgnoreDefaultRoute": true,
+          "ClientCertType": "Ref",
+          "ClientCertRef": "%s",
+          "CompLZO": "%s",%s
+          "Port": %s,
+          "Proto": "%s",
+          "PushPeerInfo": true,
+          "RenegSec": 2592000,
+          "ServerCARefs": [
+%s
+          ],
+          "ServerPollTimeout": 4,%s
+          "RemoteCertTLS": "server",%s
+          "Verb": "2"
+        }
+      }
+    }"""
+
 OVPN_ONC_AUTH_NONE = """
         "SaveCredentials": false,
         "UserAuthenticationType": "Password",
