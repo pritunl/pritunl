630c8247d9c5f3020f00c02594a8c4625fc0035d
Zachary Huff <zach.huff.386@gmail.com>
2018-06-27 17:15:53 -0400

    Fix onc url in org

--- pritunl/organization/organization.py
+++ pritunl/organization/organization.py
@@ -311,7 +311,7 @@ class Organization(mongo.MongoObject):
             'id': key_id,
             'key_url': '/key/%s.tar' % key_id,
             'key_zip_url': '/key/%s.zip' % key_id,
-            'key_onc_url': '/key_onc/%s.zip' % key_id,
+            'key_onc_url': '/key_onc/%s.onc' % key_id,
             'view_url': '/k/%s' % short_id,
             'uri_url': '/ku/%s' % short_id,
         }
