74a55a35ff9e9c4029c76d24aad2057103cef0cb
Zachary Huff <zach.huff.386@gmail.com>
2018-07-09 22:41:43 -0400

    Add link route exclude model

---
+++ www/models/linkRouteExclude.js
@@ -0,0 +1,26 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+  'use strict';
+  var LinkRouteModel = Backbone.Model.extend({
+    defaults: {
+      'id': null,
+      'link_id': null,
+      'location_id': null
+    },
+    url: function() {
+      var url = '/link/' + this.get('link_id') + '/location/' +
+        this.get('location_id') + '/route_exclude';
+
+      if (this.get('id')) {
+        url += '/' + this.get('id');
+      }
+
+      return url;
+    }
+  });
+
+  return LinkRouteModel;
+});
