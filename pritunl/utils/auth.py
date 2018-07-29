1e9ad5970824ccc0da940f14c2c6ff31fbef2248
Zachary Huff <zach.huff.386@gmail.com>
2018-07-29 19:39:50 -0400

    Remove print from auth utils

--- pritunl/utils/auth.py
+++ pritunl/utils/auth.py
@@ -2,26 +2,18 @@ from pritunl import settings
 
 def get_onelogin_mode():
     if settings.app.sso_onelogin_mode is not None:
-        print 'onelogin_%s' % settings.app.sso_onelogin_mode
         return settings.app.sso_onelogin_mode
     if settings.app.sso_onelogin_push:
         if settings.app.sso_onelogin_skip_unavailable:
-            print 'onelogin_push_none'
             return 'push_none'
-        print 'onelogin_push'
         return 'push'
-    print 'onelogin_none'
     return ''
 
 def get_okta_mode():
     if settings.app.sso_okta_mode is not None:
-        print 'okta_%s' % settings.app.sso_onelogin_mode
         return settings.app.sso_okta_mode
     if settings.app.sso_okta_push:
         if settings.app.sso_okta_skip_unavailable:
-            print 'okta_push_none'
             return 'push_none'
-        print 'okta_push'
         return 'push'
-    print 'okta_none'
     return ''
