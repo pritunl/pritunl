f05627d56d5517fec7ce645653f8db5b20af8019
Zachary Huff <zach.huff.386@gmail.com>
2018-07-29 19:35:42 -0400

    Update sso

--- pritunl/sso/__init__.py
+++ pritunl/sso/__init__.py
@@ -2,6 +2,6 @@ from pritunl.sso.duo import Duo
 from pritunl.sso.yubico import auth_yubico
 from pritunl.sso.google import verify_google
 from pritunl.sso.radius import verify_radius
-from pritunl.sso.okta import auth_okta, auth_okta_push
-from pritunl.sso.onelogin import auth_onelogin, auth_onelogin_push
+from pritunl.sso.okta import auth_okta, auth_okta_secondary
+from pritunl.sso.onelogin import auth_onelogin, auth_onelogin_secondary
 from pritunl.sso.utils import *
