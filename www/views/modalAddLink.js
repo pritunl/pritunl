028371f2f7b5758d9c7c6920b5824dcdbeff21fc
Zachary Huff <zach.huff.386@gmail.com>
2018-09-20 15:07:46 -0400

    Add ipv6 select to add link modal view

--- www/views/modalAddLink.js
+++ www/views/modalAddLink.js
@@ -12,12 +12,34 @@ define([
     template: _.template(modalAddLinkTemplate),
     title: 'Add Link',
     okText: 'Add',
+    events: function() {
+      return _.extend({
+        'click .ipv6-toggle': 'onIpv6Select'
+      }, ModalAddLinkView.__super__.events);
+    },
     body: function() {
       return this.template();
+    },
+    getIpv6Select: function() {
+      return this.$('.ipv6-toggle .selector').hasClass('selected');
+    },
+    setIpv6Select: function(state) {
+      if (state) {
+        this.$('.ipv6-toggle .selector').addClass('selected');
+        this.$('.ipv6-toggle .selector-inner').show();
+      }
+      else {
+        this.$('.ipv6-toggle .selector').removeClass('selected');
+        this.$('.ipv6-toggle .selector-inner').hide();
+      }
+    },
+    onIpv6Select: function() {
+      this.setIpv6Select(!this.getIpv6Select());
     },
     onOk: function() {
       var name = this.$('.name input').val();
       var linkType = this.$('.link-type select').val();
+      var ipv6 = this.getIpv6Select();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -28,7 +50,8 @@ define([
       var model = new LinkModel();
       model.save({
         name: name,
-        type: linkType
+        type: linkType,
+        ipv6: ipv6
       }, {
         success: function() {
           this.close(true);
