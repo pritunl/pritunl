75b401742210dacaa1b88543f0e3df4d32a614ef
Zachary Huff <zach.huff.386@gmail.com>
2018-09-22 19:00:16 -0400

    Add ipv6 to modify link modal view

--- www/views/modalModifyLink.js
+++ www/views/modalModifyLink.js
@@ -12,11 +12,33 @@ define([
     template: _.template(modalModifyLinkTemplate),
     title: 'Modify Link',
     okText: 'Save',
+    events: function() {
+      return _.extend({
+        'click .ipv6-toggle': 'onIpv6Select'
+      }, ModalModifyLinkView.__super__.events);
+    },
     body: function() {
       return this.template(this.model.toJSON());
     },
+    getIpv6Select: function() {
+      return this.$('.ipv6-toggle .selector').hasClass('selected');
+    },
+    setIpv6Select: function(state) {
+      if (state) {
+        this.$('.ipv6-toggle .selector').addClass('selected');
+        this.$('.ipv6-toggle .selector-inner').show();
+      }
+      else {
+        this.$('.ipv6-toggle .selector').removeClass('selected');
+        this.$('.ipv6-toggle .selector-inner').hide();
+      }
+    },
+    onIpv6Select: function() {
+      this.setIpv6Select(!this.getIpv6Select());
+    },
     onOk: function() {
       var name = this.$('.name input').val();
+      var ipv6 = this.getIpv6Select();
 
       if (!name) {
         this.setAlert('danger', 'Name can not be empty.', '.name');
@@ -25,7 +47,8 @@ define([
 
       this.setLoading('Saving link...');
       this.model.save({
-        name: name
+        name: name,
+        ipv6: ipv6
       }, {
         success: function() {
           this.close(true);
