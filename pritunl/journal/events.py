396c706d18e6cd6ea38a8924062d2d4bb9de18a4
Zachary Huff <zach.huff.386@gmail.com>
2018-12-26 19:30:23 -0500

    Add journal admin events

--- pritunl/journal/events.py
+++ pritunl/journal/events.py
@@ -1,1 +1,11 @@
 WEB_SERVER_START = 'web_server_start'
+
+ADMIN_AUTH_FAILURE = 'admin_auth_failure'
+ADMIN_AUTH_SUCCESS = 'admin_auth_success'
+ADMIN_AUTH_REASON_RATE_LIMIT = 'rate_limit'
+ADMIN_AUTH_REASON_DISABLED = 'disabled'
+ADMIN_AUTH_REASON_INVALID_PASSWORD = 'invalid_password'
+ADMIN_AUTH_REASON_INVALID_OTP = 'invalid_otp'
+ADMIN_AUTH_REASON_INVALID_YUBIKEY = 'invalid_yubikey'
+ADMIN_SESSION_START = 'admin_session_start'
+ADMIN_SESSION_END = 'admin_session_end'
