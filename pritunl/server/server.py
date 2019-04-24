edf7b0e9f8bf88f959680b951cca00b0be940d98
Zachary Huff <zach.huff.386@gmail.com>
2019-04-24 16:30:10 -0400

    Remove unused imports in server

--- pritunl/server/server.py
+++ pritunl/server/server.py
@@ -27,9 +27,7 @@ import collections
 import datetime
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.asymmetric import padding
 
 dict_fields = [
     'id',
