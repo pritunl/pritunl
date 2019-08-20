aeb457357c3cc8d9011769357fc66a5e860750d3
Zachary Huff <zach.huff.386@gmail.com>
2019-08-20 18:27:03 -0400

    Add vault item

---
+++ pritunl/vault/item.py
@@ -0,0 +1,70 @@
+from pritunl.helpers import *
+from pritunl.constants import *
+from pritunl.exceptions import *
+from pritunl import settings
+from pritunl import utils
+
+import json
+
+class Item(object):
+    def __init__(self, collection, id, key, value, signature=None):
+        self._data = {
+            'c': collection,
+            'i': json.dumps(id, default=utils.json_default),
+            'k': key,
+            'v': json.dumps(value, default=utils.json_default),
+        }
+
+        if signature:
+            self._data['s'] = signature
+
+    @property
+    def _id(self):
+        return self._data['c'] + '-' + self._data['i'] + '-' + self._data['k']
+
+    @property
+    def collection(self):
+        return self._data['c']
+
+    @property
+    def id(self):
+        return json.loads(
+            self._data['s'],
+            object_hook=utils.json_object_hook_handler,
+        )
+
+    @property
+    def key(self):
+        return self._data['k']
+
+    @property
+    def value(self):
+        val = self._data['v']
+
+        if val[:8] == '$SEAV1$&':
+            return val
+
+        return json.loads(
+            val,
+            object_hook=utils.json_object_hook_handler,
+        )
+
+    @property
+    def signature(self):
+        return self._data['s']
+
+    def encrypt(self):
+        self._data['o'] = 'ea1'
+        return self
+
+    def decrypt(self):
+        self._data['o'] = 'da1'
+        return self
+
+    def sign(self):
+        self._data['o'] = 'sh1'
+        return self
+
+    def verify(self):
+        self._data['o'] = 'vh1'
+        return self
