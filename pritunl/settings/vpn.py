314858eb4152998023860cc03e3d1ea7114a73dc
Zachary Huff <zach.huff.386@gmail.com>
2021-09-20 13:46:32 -0400

    Add ncp disable to vpn settings

--- pritunl/settings/vpn.py
+++ pritunl/settings/vpn.py
@@ -30,6 +30,7 @@ class SettingsVpn(SettingsGroupMongo):
         'nat_routes': True,
         'ipv6_prefix': 'fd00',
         'ipv6_prefix_wg': 'fd01',
+        'ncp_disable': False,
         'stress_test': False,
         'vxlan_id_start': 9700,
         'vxlan_net_prefix': '100.97.',
