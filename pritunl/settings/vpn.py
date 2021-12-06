9bb75e428b86b8a9d73bc16df48fd05ffc9150c6
Zachary Huff <zach.huff.386@gmail.com>
2021-12-06 12:50:46 -0500

    Update permissions default

--- pritunl/settings/vpn.py
+++ pritunl/settings/vpn.py
@@ -24,7 +24,7 @@ class SettingsVpn(SettingsGroupMongo):
         'op_timeout': 25,
         'startup_timeout': 300,
         'link_timeout': 10,
-        'drop_permissions': True,
+        'drop_permissions': False,
         'iptables_update': False,
         'iptables_update_rate': 900,
         'bandwidth_update_rate': 15,
