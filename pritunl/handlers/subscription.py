97f08a9ad634086cc19f38cfdd67cc93bc5eecb0
Zachary Huff <zach.huff.386@gmail.com>
2022-03-20 13:22:53 -0400

    Update format

--- pritunl/handlers/subscription.py
+++ pritunl/handlers/subscription.py
@@ -10,7 +10,12 @@ import flask
 import requests
 import re
 import http.client
+import hashlib
 import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
 
 @app.app.route('/subscription', methods=['GET'])
 @auth.session_auth
@@ -38,10 +43,26 @@ def subscription_styles_get(plan, ver):
         except KeyError:
                 styles = {'etag' : 0, 'last_modified' : 0, 'data' : ''}
 
+    if styles['etag'] and styles['data']:
+        iv = hashlib.md5(styles['etag'].encode()).digest()
+        key = hashlib.sha256(settings.local.sub_url_key.encode()).digest()
+        cipher = Cipher(
+            algorithms.AES(key),
+            modes.CBC(iv),
+            backend=default_backend()
+        ).decryptor()
+        dec_data = (cipher.update(base64.b64decode(styles['data'])) +
+            cipher.finalize()).rstrip(b'\x00')
+        return utils.styles_response(
+            styles['etag'],
+            styles['last_modified'],
+            dec_data,
+        )
+
     return utils.styles_response(
         styles['etag'],
         styles['last_modified'],
-        base64.b64decode(styles['data']).decode(),
+        styles['data'],
     )
 
 @app.app.route('/subscription', methods=['POST'])
