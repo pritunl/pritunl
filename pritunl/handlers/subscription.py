cc02ed7ab0d16af7a83c70d27227c0bafdf96073
Zachary Huff <zach.huff.386@gmail.com>
2022-03-20 19:57:36 -0400

    Update format

--- pritunl/handlers/subscription.py
+++ pritunl/handlers/subscription.py
@@ -10,12 +10,6 @@ import flask
 import requests
 import re
 import http.client
-import hashlib
-import base64
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import (
-    Cipher, algorithms, modes
-)
 
 @app.app.route('/subscription', methods=['GET'])
 @auth.session_auth
@@ -42,22 +36,6 @@ def subscription_styles_get(plan, ver):
                 styles = settings.local.sub_styles[plan]
         except KeyError:
                 styles = {'etag' : 0, 'last_modified' : 0, 'data' : ''}
-
-    if styles['etag'] and styles['data']:
-        iv = hashlib.md5(styles['etag'].encode()).digest()
-        key = hashlib.sha256(settings.local.sub_url_key.encode()).digest()
-        cipher = Cipher(
-            algorithms.AES(key),
-            modes.CBC(iv),
-            backend=default_backend()
-        ).decryptor()
-        dec_data = (cipher.update(base64.b64decode(styles['data'])) +
-            cipher.finalize()).rstrip(b'\x00')
-        return utils.styles_response(
-            styles['etag'],
-            styles['last_modified'],
-            dec_data,
-        )
 
     return utils.styles_response(
         styles['etag'],
