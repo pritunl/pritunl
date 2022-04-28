ff997515782e56480c7b3c29eea9552d486fecba
Zachary Huff <zach.huff.386@gmail.com>
2022-04-28 15:11:37 -0400

    Add backoff to modal add loc host view

--- www/views/modalAddLocHost.js
+++ www/views/modalAddLocHost.js
@@ -46,6 +46,7 @@ define([
       var name = this.$('.name input').val();
       var timeout = parseInt(this.$('.timeout input').val(), 10) || null;
       var priority = parseInt(this.$('.priority input').val(), 10) || 1;
+      var backoff = parseInt(this.$('.backoff input').val(), 10) || null;
       var staticHost = this.getStaticSelect();
       var publicAddress = this.$('.public-address input').val();
       var localAddress = this.$('.local-address input').val();
@@ -64,6 +65,7 @@ define([
         name: name,
         timeout: timeout,
         priority: priority,
+        backoff: backoff,
         static: staticHost,
         public_address: publicAddress,
         local_address: localAddress,
