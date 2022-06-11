a95eeafe224dbd64a624481b7963954b65b42389
Zachary Huff <zach.huff.386@gmail.com>
2022-06-10 21:17:58 -0400

    Add callbacks

---
+++ pritunl/callbacks/__init__.py
@@ -0,0 +1,1 @@
+from pritunl.callbacks.callbacks import *
---
+++ pritunl/callbacks/callbacks.py
@@ -0,0 +1,48 @@
+
+_port_listeners = {}
+_client_listeners = {}
+_firewall_listeners = {}
+
+def add_port_listener(instance_id, callback):
+    _port_listeners[instance_id] = callback
+
+def remove_port_listener(instance_id):
+    _port_listeners.pop(instance_id, None)
+
+def on_port_forwarding(msg):
+    for listener in list(_port_listeners.values()):
+        listener(
+            msg['message']['org_id'],
+            msg['message']['user_id'],
+        )
+
+def add_client_listener(instance_id, callback):
+    _client_listeners[instance_id] = callback
+
+def remove_client_listener(instance_id):
+    _client_listeners.pop(instance_id, None)
+
+def on_client(msg):
+    for listener in list(_client_listeners.values()):
+        listener(
+            msg['message']['state'],
+            msg['message'].get('server_id'),
+            msg['message']['virt_address'],
+            msg['message']['virt_address6'],
+            msg['message']['host_address'],
+            msg['message']['host_address6'],
+        )
+
+# Call in clients init
+def add_firewall_listener(instance_id, callback):
+    _firewall_listeners[instance_id] = callback
+
+# Call on clients exit
+def remove_firewall_listener(instance_id):
+    _firewall_listeners.pop(instance_id, None)
+
+def on_firewall_check(instance_id, client_id):
+    listener = _firewall_listeners.get(instance_id)
+    if listener:
+        return listener(client_id)
+    return False
