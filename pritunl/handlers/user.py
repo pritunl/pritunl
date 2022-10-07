d40a1df9b78c5dd045cfec03aa4fd4789d05793a
Zachary Huff <zach.huff.386@gmail.com>
2022-10-07 15:13:54 -0400

    Update dns name in user handler

--- pritunl/handlers/user.py
+++ pritunl/handlers/user.py
@@ -113,7 +113,9 @@ def user_get(org_id, user_id=None, page=None):
 
         if dns_mapping:
             user_dict['dns_mapping'] = ('%s.%s.vpn' % (
-                str(usr.name).split('@')[0], org.name)).lower()
+                str(usr.name).split('@')[0].replace('.', '-'),
+                org.name.replace('.', '-'),
+            )).lower()
         else:
             user_dict['dns_mapping'] = None
         user_dict['network_links'] = []
