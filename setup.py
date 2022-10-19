9e8218720d4ec71fd9d40d4ff551d096cdce2164
Zachary Huff <zach.huff.386@gmail.com>
2022-10-19 16:24:38 -0400

    Update web files in setup

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ data_files = [
         'www/key_view_dark.html',
         'www/duo.html',
         'www/yubico.html',
+        'www/success.html',
         'www/login.html',
         'www/upgrade.html',
         'www/vendor/dist/index.html',
