750a3c5057745f46af6377886a917c522cb517f1
Zachary Huff <zach.huff.386@gmail.com>
2023-01-29 13:45:10 -0500

    Add on client link to listerners setup

--- pritunl/setup/server_listeners.py
+++ pritunl/setup/server_listeners.py
@@ -5,4 +5,5 @@ def setup_server_listeners():
     from pritunl import vxlan
     listener.add_listener('port_forwarding', callbacks.on_port_forwarding)
     listener.add_listener('client', callbacks.on_client)
+    listener.add_listener('client_links', callbacks.on_client_link)
     listener.add_listener('vxlan', vxlan.on_vxlan)
