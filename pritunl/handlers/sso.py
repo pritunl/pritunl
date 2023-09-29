debaada92ba64aa2d0bbf1b879861a407f19cf16
Zachary Huff <zach.huff.386@gmail.com>
2023-09-29 11:14:55 -0400

    Update journal entry in sso handler

--- pritunl/handlers/sso.py
+++ pritunl/handlers/sso.py
@@ -138,8 +138,8 @@ def _validate_user(username, email, sso_mode, org_id, groups, remote_addr,
                 journal.SSO_AUTH_UPDATE,
                 user_name=username,
                 remote_address=remote_addr,
-                reason=journal.SSO_AUTH_REASON_CHANGED,
-                reason_long='User sso attributes changed',
+                reason=journal.SSO_AUTH_REASON_MODIFIED,
+                reason_long='User sso attributes modified',
             )
 
             usr.clear_auth_cache()
