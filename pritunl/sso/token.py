8f57c62b3668e6fa88cc6f1bdafa72a8526dbc6f
Zachary Huff <zach.huff.386@gmail.com>
2023-11-28 17:23:45 -0500

    Add clean tokens to sso token

--- pritunl/sso/token.py
+++ pritunl/sso/token.py
@@ -4,7 +4,10 @@ from pritunl import listener
 from pritunl import utils
 from pritunl import settings
 
+import threading
+
 _tokens = {}
+_tokens_lock = threading.Lock()
 
 def _on_msg(msg):
     if msg['message'] != 'authorized':
@@ -16,11 +19,12 @@ def _on_msg(msg):
     if not token or not user_id or not server_id:
         return
 
-    _tokens[token] = {
-        'user_id': user_id,
-        'server_id': server_id,
-        'timestamp': utils.time_now()
-    }
+    with _tokens_lock:
+        _tokens[token] = {
+            'user_id': user_id,
+            'server_id': server_id,
+            'timestamp': utils.time_now()
+        }
 
 def check_token(token, user_id, server_id):
     data = _tokens.get(token)
@@ -31,6 +35,15 @@ def check_token(token, user_id, server_id):
             'timestamp'] > settings.vpn.sso_token_ttl:
         return False
     return True
+
+def clean_tokens():
+    with _tokens_lock:
+        cur_time = utils.time_now()
+        ttl = settings.vpn.sso_token_ttl * 2
+        for token in list(_tokens.keys()):
+            data = _tokens.get(token)
+            if data and cur_time - data['timestamp'] > ttl:
+                del _tokens[token]
 
 def init_token():
     listener.add_listener('tokens', _on_msg)
