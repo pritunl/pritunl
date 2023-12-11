d8be3caca458dcf8be74075763be19ee1742a909
Zachary Huff <zach.huff.386@gmail.com>
2023-12-11 15:28:08 -0800

    Clean tokens on interval in sso

--- pritunl/sso/token.py
+++ pritunl/sso/token.py
@@ -8,6 +8,7 @@ import threading
 
 _tokens = {}
 _tokens_lock = threading.Lock()
+_last_clean = utils.time_now()
 
 def _on_msg(msg):
     if msg['message'] != 'authorized':
@@ -20,6 +21,9 @@ def _on_msg(msg):
         return
 
     with _tokens_lock:
+        if utils.time_now() - _last_clean > settings.vpn.sso_token_ttl:
+            clean_tokens()
+
         _tokens[token] = {
             'user_id': user_id,
             'server_id': server_id,
@@ -37,13 +41,13 @@ def check_token(token, user_id, server_id):
     return True
 
 def clean_tokens():
-    with _tokens_lock:
-        cur_time = utils.time_now()
-        ttl = settings.vpn.sso_token_ttl * 2
-        for token in list(_tokens.keys()):
-            data = _tokens.get(token)
-            if data and cur_time - data['timestamp'] > ttl:
-                del _tokens[token]
+    cur_time = utils.time_now()
+    _last_clean = cur_time
+    ttl = settings.vpn.sso_token_ttl * 2
+    for token in list(_tokens.keys()):
+        data = _tokens.get(token)
+        if data and cur_time - data['timestamp'] > ttl:
+            del _tokens[token]
 
 def init_token():
     listener.add_listener('tokens', _on_msg)
