1ceba345faa8360297cc4811c7a4440477f3fdfc
Zachary Huff <zach.huff.386@gmail.com>
2024-12-18 16:54:34 -0500

    Remove checkout from modal subscribe view

--- www/views/modalSubscribe.js
+++ www/views/modalSubscribe.js
@@ -23,9 +23,6 @@ define([
     },
     events: function() {
       return _.extend({
-        'click .subscribe-premium': 'onPremium',
-        'click .subscribe-enterprise': 'onEnterprise',
-        'click .subscribe-enterprise-plus': 'onEnterprisePlus',
         'click .subscribe-activate': 'onActivate',
         'click .subscribe-submit': 'onSubmit',
         'click .subscribe-cancel': 'onCancel'
@@ -35,8 +32,6 @@ define([
       return this.template();
     },
     postRender: function() {
-      // Precache checkout delay to prevent lag
-      setTimeout((this.setupCheckout).bind(this), 200);
       this.$('li').tooltip();
     },
     lock: function() {
@@ -56,118 +51,6 @@ define([
         this.$('.subscribe-enterprise-plus').removeAttr('disabled');
       }
       this.$('.subscribe-activate').removeAttr('disabled');
-    },
-    setupCheckout: function() {
-      this.checkout = undefined;
-      $.ajax({
-          type: 'GET',
-          url: 'https://app.pritunl.com/checkout',
-          success: function(options) {
-            this.plans = options.plans;
-            delete options.plans;
-
-            if (this.plans.premium && this.plans.premium.amount) {
-              this.$('.subscribe-premium').text('Get Premium $' +
-                (this.plans.premium.amount / 100) + '/month');
-            }
-            if (this.plans.enterprise && this.plans.enterprise.amount) {
-              this.$('.subscribe-enterprise').text('Get Enterprise $' +
-                (this.plans.enterprise.amount / 100) + '/month');
-            }
-            if (this.plans.enterprise_plus &&
-                this.plans.enterprise_plus.amount) {
-              this.$('.subscribe-enterprise-plus').text('Get Enterprise+ $' +
-                (this.plans.enterprise_plus.amount / 100) + '/month');
-            }
-            this.configCheckout(options);
-          }.bind(this),
-          error: function() {
-            this.setAlert('danger', 'Failed to load checkout data, ' +
-              'please try again later.');
-            this.checkout = false;
-          }.bind(this)
-      });
-    },
-    configCheckout: function(options) {
-      $.getCachedScript('https://checkout.stripe.com/checkout.js', {
-        success: function() {
-          var ordered = false;
-          this.checkout = window.StripeCheckout.configure(_.extend({
-            closed: function() {
-              setTimeout(function() {
-                if (!ordered) {
-                  this.unlock();
-                }
-              }.bind(this), 250);
-            }.bind(this),
-            token: function(token) {
-              if (window.demo) {
-                this.unlock(true);
-                this.onSubmit();
-                return;
-              }
-              ordered = true;
-              this.lock();
-              this.setLoading('Order processing, please wait...', true, 0);
-
-              $.ajax({
-                type: 'POST',
-                url: 'https://app.pritunl.com/subscription',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify({
-                  'plan': this.plan,
-                  'card': token.id,
-                  'email': token.email
-                }),
-                success: function(response) {
-                  this.setAlert('success', response.msg);
-                  this.clearLoading();
-                  this.unlock(true);
-                }.bind(this),
-                error: function(response) {
-                  if (response.responseJSON) {
-                    this.setAlert('danger', response.responseJSON.error_msg);
-                  }
-                  else {
-                    this.setAlert('danger', 'Server error occurred, ' +
-                      'please try again later.');
-                  }
-                  this.clearLoading();
-                  this.unlock();
-                }.bind(this)
-              });
-            }.bind(this)
-          }, options));
-        }.bind(this),
-        error: function() {
-          this.setAlert('danger', 'Failed to load upgrade checkout, ' +
-            'please try again later.');
-          this.checkout = false;
-        }.bind(this)
-      });
-    },
-    openCheckout: function(plan) {
-      if (this.checkout === undefined) {
-        setTimeout(function() {
-          this.openCheckout(plan);
-        }.bind(this), 10);
-      }
-      else if (this.checkout === false) {
-        this.unlock();
-      }
-      else {
-        this.checkout.open(this.plans[plan]);
-      }
-    },
-    _onCheckout: function(plan) {
-      this.lock();
-      this.clearAlert();
-      this.plan = plan;
-      if (this.checkout === false) {
-        this.setupCheckout();
-      }
-      this.openCheckout(plan);
     },
     onPremium: function() {
       this._onCheckout('premium');
