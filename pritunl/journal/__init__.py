385feb43de332beec8d94f998fa3f57fb001c089
Eduardo Borges <eduardo.borges@avitaseg.com.br>
2025-03-18 00:10:21 -0300

    Change journal timestamp type from float to int

--- pritunl/journal/__init__.py
+++ pritunl/journal/__init__.py
@@ -13,7 +13,7 @@ def get_base_entry(event):
     data = {
         'id': bson.ObjectId(),
         'event': event,
-        'timestamp': utils.time_now(),
+        'timestamp': int(utils.time_now() * 1000),
     }
 
     data.update(settings.local.host.journal_data)
