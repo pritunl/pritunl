d81bb1836aa991d944a9e0b8e2874172d17d77ff
Zachary Huff <zach.huff.386@gmail.com>
2025-10-08 13:25:58 -0400

    Update route task for multiple networks in route advertisement

--- pritunl/tasks/route.py
+++ pritunl/tasks/route.py
@@ -32,7 +32,12 @@ class TaskRoute(task.Task):
                 server_id = doc['server_id']
                 vpc_region = doc['vpc_region']
                 vpc_id = doc['vpc_id']
-                network = doc['network']
+
+                if doc.get('network'):
+                    self.routes_collection.delete_one({
+                        '_id': doc['_id'],
+                    })
+                    continue
 
                 svr = server.get_by_id(server_id)
                 if not svr:
@@ -41,7 +46,7 @@ class TaskRoute(task.Task):
                     })
                     continue
 
-                match = False
+                networks = []
                 for route in svr.get_routes(include_server_links=True):
                     route_advertise = route['advertise'] or \
                         (route['vpc_region'] and route['vpc_id'])
@@ -51,17 +56,11 @@ class TaskRoute(task.Task):
                     if netmap:
                         route_network = netmap
 
-                    if route_advertise and network == route_network:
-                        match = True
+                    if route_advertise:
+                        networks.append(route_network)
 
-                if not match:
-                    self.routes_collection.delete_one({
-                        '_id': doc['_id'],
-                    })
-                    continue
-
-                messenger.publish('instance', ['route_advertisement',
-                    server_id, vpc_region, vpc_id, network])
+                messenger.publish('instance', ['route_advertisement2',
+                    server_id, vpc_region, vpc_id, networks])
         except GeneratorExit:
             raise
         except:
