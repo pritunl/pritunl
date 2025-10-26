69c6e7d43f55012ba07cf7688f0a2d54dc963ef9
Zachary Huff <zach.huff.386@gmail.com>
2025-10-26 09:09:00 -0400

    Update sync in subscription runner

--- pritunl/runners/subscription.py
+++ pritunl/runners/subscription.py
@@ -8,10 +8,14 @@ import threading
 
 @interrupter
 def _subscription_thread():
+    ok = True
     while True:
         try:
-            yield interrupter_sleep(SUBSCRIPTION_UPDATE_RATE)
-            subscription.update()
+            if ok:
+                yield interrupter_sleep(SUBSCRIPTION_UPDATE_RATE)
+            else:
+                yield interrupter_sleep(60)
+            ok = subscription.update()
         except GeneratorExit:
             raise
         except:
