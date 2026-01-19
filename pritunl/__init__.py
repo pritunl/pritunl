c3a12d9df94c437014b32b11c45ef6df5439c3da
Zachary Huff <zach.huff.386@gmail.com>
2026-01-19 18:37:15 -0500

    Create new release

--- pritunl/__init__.py
+++ pritunl/__init__.py
@@ -2,7 +2,7 @@ from pritunl import patches
 from pritunl.constants import *
 
 __title__ = 'pritunl'
-__version__ = '1.32.4469.94'
+__version__ = '1.32.4512.98'
 __author__ = 'Pritunl'
 __email__ = 'contact@pritunl.com'
 __license__ = 'Custom'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import shlex
 import shutil
 import fileinput
 
-VERSION = '1.32.4469.94'
+VERSION = '1.32.4512.98'
 PATCH_DIR = 'build'
 install_systemd = True
 install_upstart = False
