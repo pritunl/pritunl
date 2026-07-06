3887eabd5e6b77d51d51913154f11677b16cf622
Zachary Huff <zach.huff.386@gmail.com>
2026-07-06 19:38:21 -0400

    Add clean domain to filter utils

--- pritunl/utils/filter.py
+++ pritunl/utils/filter.py
@@ -2,6 +2,54 @@ from pritunl.utils.misc import filter_str
 from pritunl import database
 
 import flask
+import re
+import ipaddress
+import urllib.parse
+
+_domain_re = re.compile(
+    r'^(?=.{1,253}$)'
+    r'([a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)*'
+    r'[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$'
+)
+
+def clean_domain(in_str):
+    if in_str is None:
+        return None
+    in_str = str(in_str).strip()
+    if not in_str:
+        return None
+
+    if '://' not in in_str:
+        try:
+            if ipaddress.ip_address(in_str).version == 6:
+                in_str = '[%s]' % in_str
+        except ValueError:
+            pass
+        in_str = 'http://' + in_str
+
+    try:
+        parsed = urllib.parse.urlsplit(in_str)
+        hostname = parsed.hostname
+        port = parsed.port
+    except ValueError:
+        return None
+
+    if not hostname:
+        return None
+    hostname = hostname.lower()
+
+    is_ipv6 = False
+    try:
+        is_ipv6 = ipaddress.ip_address(hostname).version == 6
+    except ValueError:
+        if not _domain_re.match(hostname):
+            return None
+
+    host = '[%s]' % hostname if is_ipv6 else hostname
+
+    if port is not None:
+        return '%s:%d' % (host, port)
+    return host
 
 def json_str(key):
     return str(flask.request.json[key])
